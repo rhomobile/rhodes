b3b97f16d9db8502448d112cb5707fbcad028a5f
adamblum <a@adamblum.com>
2008-11-04 12:13:49 -0800

    delete old junk apps

--- apps/Rhosugaroldui/Account/config.rb
+++
@@ -1,3 +0,0 @@
-require 'rho'
-
-Rho::RhoConfig::add_source("Account", {"url"=>"http://rhosync.rhomobile.com/sources/1", "source_id"=>1})
--- apps/Rhosugaroldui/Employee/config.rb
+++
@@ -1,3 +0,0 @@
-require 'rho'
-
-Rho::RhoConfig::add_source("Employee", {"url"=>"http://rhosync.rhomobile.com/sources/3", "source_id"=>3})
