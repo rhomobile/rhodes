6d2742e6227c57203611bc29edd55216aec75333
genywind <evgeny@rhomobile.com>
2008-11-11 22:04:27 +0300

    Add ERB files compilation

--- bb/Xruby/preverifyBB.bat
+++ bb/Xruby/preverifyBB.bat
@@ -1,3 +1,4 @@
+ruby compileERB.rb
 java -jar xruby-0.3.3.jar -c RhoSrc
 move RhoSrc.jar RhoBundle.jar
 "%BB_JDE%\bin\preverify.exe" -classpath "%BB_JDE%\lib\net_rim_api.jar";"..\RubyVM\RubyVM.jar" -d ..\RhoBundle RhoBundle.jar
