19711b2d2ffaadbb8fc3399585a24d7bf2430215
genywind <evgeny@rhomobile.com>
2008-11-12 00:51:40 +0300

    Fix BB build

--- bb/Xruby/preverifyBB.bat
+++ bb/Xruby/preverifyBB.bat
@@ -1,5 +1,7 @@
 rmdir RhoBundle /S /Q
 xcopy "../../shared/framework/lib" RhoBundle /S /I /EXCLUDE:excludeFRMWK.txt
+del "RhoBundle\date.rb"
+del "RhoBundle\rho\rhofsconnector.rb"
 xcopy "../../apps/Rhosugar" "RhoBundle/apps/Rhosugar" /S /I /EXCLUDE:excludeAPPS.txt
 ruby compileERB.rb
 java -jar xruby-0.3.3.jar -c RhoBundle
