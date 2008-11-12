f859a6bf516b1d71e8aa710d98214deaa5bf0b5b
larsburgess <lars@.(none)>
2008-11-11 17:47:12 -0800

    Finished device->server sync.  TODO: Write tests for update and delete operations.

--- bb/RubyVM/src2/com/rho/sync/SyncObject.java
+++ bb/RubyVM/src2/com/rho/sync/SyncObject.java
@@ -1,18 +1,77 @@
+/*
+ *  rhodes
+ *
+ *  Copyright (C) 2008 Lars Burgess. All rights reserved.
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package com.rho.sync;
 
 import com.xruby.runtime.builtin.RubyHash;
 import com.xruby.runtime.lang.RubyValue;
 
+/**
+ * The Class SyncObject.
+ */
 public class SyncObject {
+	
+	/**
+	 * Delete from database by source.
+	 * 
+	 * @param id the id
+	 */
+	public static void deleteFromDatabaseBySource(int id) {
+		RubyHash hash = SyncUtil.createHash();
+		hash.add(SyncUtil.createString("source_id"), SyncUtil
+				.createInteger((long) id));
+		SyncUtil.adapter.deleteFromTable(SyncUtil
+				.createString(SyncConstants.OBJECTS_TABLE), (RubyValue) hash);
+	}
+	
+	/** The _primary key. */
 	private int _primaryKey = -1;
+	
+	/** The _attrib. */
 	private String _attrib;
+	
+	/** The _source id. */
 	private int _sourceId = -1;
+	
+	/** The _object. */
 	private String _object;
+	
+	/** The _value. */
 	private String _value;
+	
+	/** The _created at. */
 	private String _createdAt;
+	
+	/** The _updated at. */
 	private String _updatedAt;
+
+	/** The _update type. */
 	private String _updateType;
 
+	/**
+	 * Instantiates a new sync object.
+	 * 
+	 * @param attrib the attrib
+	 * @param sourceId the source id
+	 * @param object the object
+	 * @param value the value
+	 * @param updateType the update type
+	 */
 	public SyncObject(String attrib, int sourceId, String object, String value,
 			String updateType) {
 		this._attrib = attrib;
@@ -22,78 +81,11 @@ public class SyncObject {
 		this._updateType = updateType;
 	}
 
-	public int getPrimaryKey() {
-		return _primaryKey;
-	}
-
-	public void setPrimaryKey(int primaryKey) {
-		this._primaryKey = primaryKey;
-	}
-
-	public String getAttrib() {
-		return _attrib;
-	}
-
-	public void setAttrib(String attrib) {
-		this._attrib = attrib;
-	}
-
-	public int getSourceId() {
-		return _sourceId;
-	}
-
-	public void setSourceId(int sourceId) {
-		this._sourceId = sourceId;
-	}
-
-	public String getObject() {
-		return _object;
-	}
-
-	public void setObject(String object) {
-		this._object = object;
-	}
-
-	public String getValue() {
-		return _value == null ? "" : _value;
-	}
-
-	public void setValue(String value) {
-		this._value = value;
-	}
-
-	public String getCreatedAt() {
-		return _createdAt;
-	}
-
-	public void setCreatedAt(String createdAt) {
-		this._createdAt = createdAt;
-	}
-
-	public String getUpdatedAt() {
-		return _updatedAt;
-	}
-
-	public void setUpdatedAt(String updatedAt) {
-		this._updatedAt = updatedAt;
-	}
-
-	public String getUpdateType() {
-		return _updateType;
-	}
-
-	public void setUpdateType(String updateType) {
-		this._updateType = updateType;
-	}
-
-	public static void deleteFromDatabaseBySource(int id) {
-		RubyHash hash = SyncUtil.createHash();
-		hash.add(SyncUtil.createString("source_id"), SyncUtil
-				.createInteger((long) id));
-		SyncUtil.adapter.deleteFromTable(SyncUtil
-				.createString(SyncConstants.OBJECTS_TABLE), (RubyValue) hash);
-	}
-
+	/**
+	 * Dehydrate.
+	 * 
+	 * @return the int
+	 */
 	public int dehydrate() {
 		try {
 			SyncUtil.adapter.insertIntoTable(SyncUtil
@@ -107,6 +99,29 @@ public class SyncObject {
 		return SyncConstants.SYNC_OBJECT_SUCCESS;
 	}
 
+	/**
+	 * Gets the attrib.
+	 * 
+	 * @return the attrib
+	 */
+	public String getAttrib() {
+		return _attrib;
+	}
+
+	/**
+	 * Gets the created at.
+	 * 
+	 * @return the created at
+	 */
+	public String getCreatedAt() {
+		return _createdAt;
+	}
+
+	/**
+	 * Gets the hash from values.
+	 * 
+	 * @return the hash from values
+	 */
 	private RubyHash getHashFromValues() {
 		RubyHash hash = SyncUtil.createHash();
 		hash.add(SyncUtil.createString("source_id"), SyncUtil
@@ -120,5 +135,131 @@ public class SyncObject {
 		hash.add(SyncUtil.createString("update_type"), SyncUtil
 				.createString(this.getUpdateType()));
 		return hash;
+	}
+
+	/**
+	 * Gets the object.
+	 * 
+	 * @return the object
+	 */
+	public String getObject() {
+		return _object;
+	}
+
+	/**
+	 * Gets the primary key.
+	 * 
+	 * @return the primary key
+	 */
+	public int getPrimaryKey() {
+		return _primaryKey;
+	}
+
+	/**
+	 * Gets the source id.
+	 * 
+	 * @return the source id
+	 */
+	public int getSourceId() {
+		return _sourceId;
+	}
+
+	/**
+	 * Gets the updated at.
+	 * 
+	 * @return the updated at
+	 */
+	public String getUpdatedAt() {
+		return _updatedAt;
+	}
+
+	/**
+	 * Gets the update type.
+	 * 
+	 * @return the update type
+	 */
+	public String getUpdateType() {
+		return _updateType;
+	}
+
+	/**
+	 * Gets the value.
+	 * 
+	 * @return the value
+	 */
+	public String getValue() {
+		return _value == null ? "" : _value;
+	}
+
+	/**
+	 * Sets the attrib.
+	 * 
+	 * @param attrib the new attrib
+	 */
+	public void setAttrib(String attrib) {
+		this._attrib = attrib;
+	}
+
+	/**
+	 * Sets the created at.
+	 * 
+	 * @param createdAt the new created at
+	 */
+	public void setCreatedAt(String createdAt) {
+		this._createdAt = createdAt;
+	}
+
+	/**
+	 * Sets the object.
+	 * 
+	 * @param object the new object
+	 */
+	public void setObject(String object) {
+		this._object = object;
+	}
+
+	/**
+	 * Sets the primary key.
+	 * 
+	 * @param primaryKey the new primary key
+	 */
+	public void setPrimaryKey(int primaryKey) {
+		this._primaryKey = primaryKey;
+	}
+
+	/**
+	 * Sets the source id.
+	 * 
+	 * @param sourceId the new source id
+	 */
+	public void setSourceId(int sourceId) {
+		this._sourceId = sourceId;
+	}
+
+	/**
+	 * Sets the updated at.
+	 * 
+	 * @param updatedAt the new updated at
+	 */
+	public void setUpdatedAt(String updatedAt) {
+		this._updatedAt = updatedAt;
+	}
+
+	/**
+	 * Sets the update type.
+	 * 
+	 * @param updateType the new update type
+	 */
+	public void setUpdateType(String updateType) {
+		this._updateType = updateType;
+	}
+
+	/**
+	 * Sets the value.
+	 * 
+	 * @param value the new value
+	 */
+	public void setValue(String value) {
+		this._value = value;
 	}
 }
--- bb/RubyVM/src2/com/rho/sync/SyncUtil.java
+++ bb/RubyVM/src2/com/rho/sync/SyncUtil.java
@@ -1,3 +1,21 @@
+/*
+ *  rhodes
+ *
+ *  Copyright (C) 2008 Lars Burgess. All rights reserved.
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package com.rho.sync;
 
 import java.io.IOException;
@@ -9,16 +27,72 @@ import com.xruby.runtime.builtin.RubyArray;
 import com.xruby.runtime.builtin.RubyHash;
 import com.xruby.runtime.builtin.RubyInteger;
 import com.xruby.runtime.builtin.RubyString;
+import com.xruby.runtime.lang.RubyValue;
 
+/**
+ * The Class SyncUtil.
+ */
 public class SyncUtil {
 
+	/** The adapter. */
 	public static PerstLiteAdapter adapter = null;
 
-	public static int fetchRemoteChanges(String url, int id) {
+	/**
+	 * Creates the array.
+	 * 
+	 * @return the ruby array
+	 */
+	public static RubyArray createArray() {
+		return new RubyArray();
+	}
+
+	/**
+	 * Creates the hash.
+	 * 
+	 * @return the ruby hash
+	 */
+	public static RubyHash createHash() {
+		return ObjectFactory.createHash();
+	}
+
+	/**
+	 * Creates the integer.
+	 * 
+	 * @param val
+	 *            the val
+	 * 
+	 * @return the ruby integer
+	 */
+	public static RubyInteger createInteger(long val) {
+		return ObjectFactory.createInteger(val);
+	}
+
+	/**
+	 * Creates the string.
+	 * 
+	 * @param val
+	 *            the val
+	 * 
+	 * @return the ruby string
+	 */
+	public static RubyString createString(String val) {
+		return ObjectFactory.createString(val);
+	}
+
+	/**
+	 * Fetch remote changes.
+	 * 
+	 * @param source
+	 *            the source
+	 * 
+	 * @return the int
+	 */
+	public static int fetchRemoteChanges(SyncSource source) {
 		int count = 0;
 		String data = null;
 		try {
-			data = SyncManager.fetchRemoteData(url + SyncConstants.SYNC_FORMAT);
+			data = SyncManager.fetchRemoteData(source.get_sourceUrl()
+					+ SyncConstants.SYNC_FORMAT);
 		} catch (IOException e) {
 			System.out
 					.println("There was an error fetching data from the sync source: "
@@ -28,7 +102,7 @@ public class SyncUtil {
 			ArrayList list = SyncJSONParser.parseJSONList(data);
 			count = list.size();
 			if (count > 0) {
-				SyncObject.deleteFromDatabaseBySource(id);
+				SyncObject.deleteFromDatabaseBySource(source.get_sourceId());
 				for (int i = 0; i < count; i++) {
 					int success = ((SyncObject) list.get(i)).dehydrate();
 					if (success != SyncConstants.SYNC_OBJECT_SUCCESS) {
@@ -41,39 +115,14 @@ public class SyncUtil {
 		return count;
 	}
 
-	public static int pushRemoteChanges(SyncOperation[] list) {
-		int success = 0;
-		StringBuffer data = new StringBuffer();
-		if (list.length == 0) {
-			return SyncConstants.SYNC_PUSH_CHANGES_OK;
-		}
-
-		for (int i = 0; i < list.length; i++) {
-			data.append(list[i].get_postBody());
-			if (i != (list.length - 1)) {
-				data.append("&");
-			}
-		}
-		try {
-			success = SyncManager.pushRemoteData(list[0].get_source()
-					.get_sourceUrl(), data.toString());
-		} catch (IOException e) {
-			System.out.println("There was an error pushing changes: "
-					+ e.getMessage());
-			success = SyncConstants.SYNC_PUSH_CHANGES_ERROR;
-		}
-
-		return success == SyncConstants.SYNC_PUSH_CHANGES_OK ? SyncConstants.SYNC_PUSH_CHANGES_OK
-				: SyncConstants.SYNC_PUSH_CHANGES_ERROR;
-	}
-
-	public static RubyArray getSourceList() {
-		RubyArray arr = createArray();
-		arr.add(createString("sources"));
-		arr.add(createString("*"));
-		return (RubyArray) adapter.selectFromTable(arr);
-	}
-
+	/**
+	 * Gets the object value list.
+	 * 
+	 * @param id
+	 *            the id
+	 * 
+	 * @return the object value list
+	 */
 	public static RubyArray getObjectValueList(int id) {
 		RubyArray arr = createArray();
 		RubyHash where = createHash();
@@ -84,22 +133,105 @@ public class SyncUtil {
 		return (RubyArray) adapter.selectFromTable(arr);
 	}
 
-	public static RubyString createString(String val) {
-		return ObjectFactory.createString(val);
+	/**
+	 * Gets the op list from database.
+	 * 
+	 * @param type
+	 *            the type
+	 * @param source
+	 *            the source
+	 * 
+	 * @return the op list from database
+	 */
+	public static ArrayList getOpListFromDatabase(String type, SyncSource source) {
+		RubyArray arr = createArray();
+		RubyHash where = createHash();
+		String operation = null;
+		arr.add(createString("object_values"));
+		arr.add(createString("*"));
+		where.add(createString("update_type"), createString(type));
+		where.add(createString("source_id"), createInteger(source
+				.get_sourceId()));
+		arr.add(where);
+		RubyArray rows = (RubyArray) adapter.selectFromTable(arr);
+		ArrayList objects = getSyncObjectList(rows);
+		ArrayList results = new ArrayList();
+
+		if (type != null) {
+			if (type.equalsIgnoreCase("create")) {
+				operation = SyncConstants.UPDATE_TYPE_CREATE;
+			} else if (type.equalsIgnoreCase("update")) {
+				operation = SyncConstants.UPDATE_TYPE_UPDATE;
+			} else if (type.equalsIgnoreCase("delete")) {
+				operation = SyncConstants.UPDATE_TYPE_DELETE;
+			}
+		}
+
+		for (int i = 0; i < objects.size(); i++) {
+			SyncObject current = (SyncObject) objects.get(i);
+			SyncOperation newOperation = new SyncOperation(operation, current);
+			results.add(newOperation);
+			System.out
+					.println("Adding sync operation (attrib, source_id, object, value, update_type, uri): "
+							+ current.getAttrib()
+							+ ", "
+							+ current.getSourceId()
+							+ ", "
+							+ current.getObject()
+							+ ", "
+							+ (current.getValue() == null ? "null" : current
+									.getValue())
+							+ ", "
+							+ operation
+							+ ", "
+							+ source.get_sourceUrl());
+		}
+		return results;
 	}
 
-	public static RubyInteger createInteger(long val) {
-		return ObjectFactory.createInteger(val);
+	/**
+	 * Gets the source list.
+	 * 
+	 * @return the source list
+	 */
+	public static RubyArray getSourceList() {
+		RubyArray arr = createArray();
+		arr.add(createString("sources"));
+		arr.add(createString("*"));
+		return (RubyArray) adapter.selectFromTable(arr);
 	}
 
-	public static RubyHash createHash() {
-		return ObjectFactory.createHash();
+	/**
+	 * Gets the sync object list.
+	 * 
+	 * @param list
+	 *            the list
+	 * 
+	 * @return the sync object list
+	 */
+	public static ArrayList getSyncObjectList(RubyArray list) {
+		ArrayList results = new ArrayList();
+		for (int i = 0; i < list.size(); i++) {
+			RubyHash element = (RubyHash) list.at(createInteger(i));
+			String attrib = element.get(createString("attrib")).asString();
+			RubyValue val = element.get(createString("value"));
+			String value = val == null ? null : val.asString();
+			String object = element.get(createString("object")).asString();
+			String updateType = element.get(createString("update_type"))
+					.asString();
+			int sourceId = element.get(createString("source_id")).toInt();
+			results.add(new SyncObject(attrib, sourceId, object, value,
+					updateType));
+		}
+		return results;
 	}
 
-	public static RubyArray createArray() {
-		return new RubyArray();
-	}
-
+	/**
+	 * Prints the results.
+	 * 
+	 * @param objects
+	 *            the objects
+	 */
 	public static void printResults(RubyArray objects) {
 		// Debug code to print results
 		for (int j = 0; j < objects.size(); j++) {
@@ -111,5 +243,103 @@ public class SyncUtil {
 					.toString();
 			System.out.println("value[" + j + "][" + attrib + "]: " + value);
 		}
+	}
+
+	/**
+	 * Process local changes.
+	 * 
+	 * @return the int
+	 */
+	public static int processLocalChanges() {
+		RubyArray sources = SyncUtil.getSourceList();
+
+		for (int i = 0; i < sources.size(); i++) {
+			RubyHash element = (RubyHash) sources.at(SyncUtil.createInteger(i));
+			String url = element.get(PerstLiteAdapter.URL).toString();
+			int id = element.get(PerstLiteAdapter.SOURCE_ID).toInt();
+			SyncSource current = new SyncSource(url, id);
+			System.out.println("URL: " + current.get_sourceUrl());
+			int success = 0;
+			success += processOpList(current, "create");
+			success += processOpList(current, "update");
+			success += processOpList(current, "delete");
+			if (success > 0) {
+				System.out
+						.println("Remote update failed, not continuing with sync...");
+			} else {
+				int available = SyncUtil.fetchRemoteChanges(current);
+				System.out.println("Successfully processed " + available
+						+ " records...");
+				if (SyncConstants.DEBUG) {
+					RubyArray objects = SyncUtil.getObjectValueList(current
+							.get_sourceId());
+					SyncUtil.printResults(objects);
+				}
+			}
+		}
+		return SyncConstants.SYNC_PROCESS_CHANGES_OK;
+	}
+
+	/**
+	 * Process op list.
+	 * 
+	 * @param source
+	 *            the source
+	 * @param type
+	 *            the type
+	 * 
+	 * @return the int
+	 */
+	private static int processOpList(SyncSource source, String type) {
+		int success = SyncConstants.SYNC_PUSH_CHANGES_OK;
+		ArrayList list = getOpListFromDatabase(type, source);
+		if (list.size() == 0) {
+			return success;
+		}
+		System.out.println("Found " + list.size()
+				+ " available records for processing...");
+		if (pushRemoteChanges(source, list) != SyncConstants.SYNC_PUSH_CHANGES_OK) {
+			success = SyncConstants.SYNC_PUSH_CHANGES_ERROR;
+		}
+		return success;
+	}
+
+	/**
+	 * Push remote changes.
+	 * 
+	 * @param source
+	 *            the source
+	 * @param list
+	 *            the list
+	 * 
+	 * @return the int
+	 */
+	public static int pushRemoteChanges(SyncSource source, ArrayList list) {
+		int success = 0;
+		StringBuffer data = new StringBuffer();
+		String url = null;
+		if (list.size() == 0) {
+			return SyncConstants.SYNC_PUSH_CHANGES_OK;
+		}
+
+		for (int i = 0; i < list.size(); i++) {
+			data.append(((SyncOperation) list.get(i)).get_postBody());
+			if (i != (list.size() - 1)) {
+				data.append("&");
+			}
+		}
+		try {
+			// Construct the post url
+			url = source.get_sourceUrl() + "/"
+					+ ((SyncOperation) list.get(0)).get_operation();
+			success = SyncManager.pushRemoteData(url, data.toString());
+		} catch (IOException e) {
+			System.out.println("There was an error pushing changes: "
+					+ e.getMessage());
+			success = SyncConstants.SYNC_PUSH_CHANGES_ERROR;
+		}
+
+		return success == SyncConstants.SYNC_PUSH_CHANGES_OK ? SyncConstants.SYNC_PUSH_CHANGES_OK
+				: SyncConstants.SYNC_PUSH_CHANGES_ERROR;
 	}
 }
