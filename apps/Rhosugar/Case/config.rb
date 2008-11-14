0cd142bc036f9919fff0426760ff8a65d3015ca5
lars <larsburgess@gmail.com>
2008-11-14 14:43:21 -0800

    rhosync is now hosted on rhohub.com

--- apps/Rhosugar/Case/config.rb
+++ apps/Rhosugar/Case/config.rb
@@ -1,3 +1,3 @@
 require 'rho'
 
-Rho::RhoConfig::add_source("Case", {"url"=>"http://rhosync.rhomobile.com/sources/2", "source_id"=>2})
+Rho::RhoConfig::add_source("Case", {"url"=>"http://rhosync.rhohub.com/sources/2", "source_id"=>2})
