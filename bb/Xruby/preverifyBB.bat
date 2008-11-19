dcb1245ff11ba8c0c9aaad95908d2a0cad4e507d
genywind <evgeny@rhomobile.com>
2008-11-19 17:37:22 +0300

    Create BB bundle with apps. Added TODO

--- bb/Xruby/preverifyBB.bat
+++ bb/Xruby/preverifyBB.bat
@@ -4,6 +4,16 @@ del "RhoBundle\date.rb"
 del "RhoBundle\rho\rhofsconnector.rb"
 xcopy "../../apps/Rhosugar" "RhoBundle/apps/Rhosugar" /S /I /EXCLUDE:excludeAPPS.txt
 ruby compileERB.rb
+
 java -jar xruby-0.3.3.jar -c RhoBundle
+
+rmdir apps /S /Q
+xcopy "../../apps/*.html" "./apps" /I /F
+xcopy "../../apps/Rhosugar/*.html" "./apps/Rhosugar" /I /F
+xcopy "../../apps/shared" "apps/shared" /S /I
+
+jar uf RhoBundle.jar apps/*.*
+
 "%BB_JDE%\bin\preverify.exe" -classpath "%BB_JDE%\lib\net_rim_api.jar";"..\RubyVM\RubyVM.jar" -d ..\RhoBundle RhoBundle.jar
 rmdir RhoBundle /S /Q
+rmdir apps /S /Q
