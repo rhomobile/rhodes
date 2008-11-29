e2b403dcfcee0a23b79befbf38bb9634aa5a7872
genywind <evgeny@rhomobile.com>
2008-11-29 18:00:20 +0300

    Create one cod-file

---
+++ bb/Xruby/buildAll.bat
@@ -0,0 +1,5 @@
+rmdir bin /S /Q
+mkdir bin
+
+"%BB_JDE%\bin\rapc.exe" -javacompiler=%JAVA_HOME%\bin\javac.exe -quiet import="%BB_JDE%\lib\net_rim_api.jar" codename=bin\RhoApp RhoApp.rapc ..\PerstDb\blackberry_jde\PerstImplJSR75.jar ..\PerstDb\blackberry_jde\PerstLite.jar ..\RhoBundle\RhoBundle.jar ..\RubyVM\RubyVM.jar ..\rhodes\rhodes.jar
+call signAll.bat
--- bb/Xruby/signAll.bat
+++ bb/Xruby/signAll.bat
@@ -1,30 +1,3 @@
-rmdir bin /S /Q
-mkdir bin
-xcopy "../RubyVM/RubyVM.cod" "./bin" /I /F
-xcopy "../RubyVM/RubyVM.alx" "./bin" /I /F
-xcopy "../RubyVM/RubyVM.cso" "./bin" /I /F
-xcopy "../RubyVM/RubyVM.csl" "./bin" /I /F
-
-xcopy "../rhodes/rhodes.cod" "./bin" /I /F
-xcopy "../rhodes/rhodes.alx" "./bin" /I /F
-xcopy "../rhodes/rhodes.cso" "./bin" /I /F
-xcopy "../rhodes/rhodes.csl" "./bin" /I /F
-
-xcopy "../RhoBundle/RhoBundle.cod" "./bin" /I /F
-xcopy "../RhoBundle/RhoBundle.alx" "./bin" /I /F
-xcopy "../RhoBundle/RhoBundle.cso" "./bin" /I /F
-xcopy "../RhoBundle/RhoBundle.csl" "./bin" /I /F
-
-xcopy "../PerstDb/blackberry_jde/PerstImplJSR75.cod" "./bin" /I /F
-xcopy "../PerstDb/blackberry_jde/PerstImplJSR75.alx" "./bin" /I /F
-xcopy "../PerstDb/blackberry_jde/PerstImplJSR75.cso" "./bin" /I /F
-xcopy "../PerstDb/blackberry_jde/PerstImplJSR75.csl" "./bin" /I /F
-
-xcopy "../PerstDb/blackberry_jde/PerstLite.cod" "./bin" /I /F
-xcopy "../PerstDb/blackberry_jde/PerstLite.alx" "./bin" /I /F
-xcopy "../PerstDb/blackberry_jde/PerstLite.cso" "./bin" /I /F
-xcopy "../PerstDb/blackberry_jde/PerstLite.csl" "./bin" /I /F
-
 cd bin
-java -jar "%BB_JDE%\bin\SignatureTool.jar" RubyVM.cod PerstLite.cod PerstImplJSR75.cod rhodes.cod RhoBundle.cod
+java -jar "%BB_JDE%\bin\SignatureTool.jar" RhoApp.cod
 cd ..
