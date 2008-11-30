13598472644f8275fb34a1c4d80dc4504d5d684c
genywind <evgeny@rhomobile.com>
2008-11-30 23:16:15 +0300

    Added script for building one cod-file

---
+++ bb/Xruby/preverify.bat
@@ -0,0 +1,1 @@
+"%BB_JDE%\bin\preverify.exe" -classpath "%BB_JDE%\lib\net_rim_api.jar" -d bin tmp/rhodes.jar
--- bb/Xruby/preverifyBB.bat
+++ bb/Xruby/preverifyBB.bat
@@ -1,19 +1,11 @@
-rmdir RhoBundle /S /Q
-xcopy "../../shared/framework/lib" RhoBundle /S /I /EXCLUDE:excludeFRMWK.txt
-del "RhoBundle\date.rb"
-del "RhoBundle\rho\rhofsconnector.rb"
-xcopy "../../apps/Rhosugar" "RhoBundle/apps/Rhosugar" /S /I /EXCLUDE:excludeAPPS.txt
-ruby compileERB.rb
+call compileApp.bat
 
-java -jar xruby-0.3.3.jar -c RhoBundle
+@IF "%1" == "" (
+ @set RubyVMDir=..\RubyVM
+ @set BundleDir=..\RhoBundle
+) ELSE (
+ @set RubyVMDir=%1
+ @set BundleDir=%1 
+)
 
-rmdir apps /S /Q
-xcopy "../../apps/*.html" "./apps" /I /F
-xcopy "../../apps/Rhosugar/*.html" "./apps/Rhosugar" /I /F
-REM xcopy "../../apps/shared" "apps/shared" /S /I
-
-jar uf RhoBundle.jar apps/*.*
-
-"%BB_JDE%\bin\preverify.exe" -classpath "%BB_JDE%\lib\net_rim_api.jar";"..\RubyVM\RubyVM.jar" -d ..\RhoBundle RhoBundle.jar
-rmdir RhoBundle /S /Q
-rmdir apps /S /Q
+"%BB_JDE%\bin\preverify.exe" -classpath "%BB_JDE%\lib\net_rim_api.jar";"%RubyVMDir%\RubyVM.jar" -d %BundleDir% RhoBundle.jar
