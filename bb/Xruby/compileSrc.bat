14dde5f80e64aa9b1a03387db1599201a2adc2c1
larsburgess <lars@.(none)>
2008-11-30 23:50:09 -0800

    wrapping command line calls in quotations to support spaces

--- bb/Xruby/compileSrc.bat
+++ bb/Xruby/compileSrc.bat
@@ -1,1 +1,1 @@
-%JAVA_HOME%\bin\javac.exe -g -d tmp -classpath RhoBundle.jar -bootclasspath "%BB_JDE%\lib\net_rim_api.jar" -source 1.3 -target 1.3 @RubyVM_build.files @PerstLite_build.files @rhodes_build.files
+"%JAVA_HOME%\bin\javac.exe" -g -d tmp -classpath RhoBundle.jar -bootclasspath "%BB_JDE%\lib\net_rim_api.jar" -source 1.3 -target 1.3 @RubyVM_build.files @PerstLite_build.files @rhodes_build.files
