3c4b63482dfc44d7a548430b3529c5d686c598af
genywind <evgeny@rhomobile.com>
2008-12-02 16:41:25 +0300

    Add web folder to build, support different vesrions, add properties file

---
+++ bb/build/compileApp.bat
@@ -0,0 +1,18 @@
+rmdir RhoBundle /S /Q
+xcopy "../../shared/framework/lib" RhoBundle /S /I /EXCLUDE:excludeFRMWK.txt
+del "RhoBundle\date.rb"
+del "RhoBundle\rho\rhofsconnector.rb"
+xcopy "../../apps/Rhosugar" "RhoBundle/apps/Rhosugar" /S /I /EXCLUDE:excludeAPPS.txt
+ruby compileERB.rb
+
+java -jar lib/xruby-0.3.3.jar -c RhoBundle
+
+rmdir apps /S /Q
+xcopy "../../apps/*.html" "./apps" /I /F
+xcopy "../../apps/Rhosugar/*.html" "./apps/Rhosugar" /I /F
+@REM xcopy "../../apps/shared" "apps/shared" /S /I
+
+jar uf RhoBundle.jar apps/*.*
+
+rmdir RhoBundle /S /Q
+rmdir apps /S /Q
