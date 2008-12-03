cbd04af1474d62015f29f1d8be2caf9bc552d9c9
lars <larsburgess@gmail.com>
2008-12-03 15:04:45 -0800

    fixing push by source [#44 state:resolved]

--- shared/syncengine/SyncOperation.c
+++ shared/syncengine/SyncOperation.c
@@ -21,15 +21,16 @@
 
 #include "SyncOperation.h"
 #include "Constants.h"
-#include "SyncUtil.h"
 
 static char* attr_format = "attrvals[][%s]=%s";
 
 static sqlite3_stmt *op_list_select_statement = NULL;
+static sqlite3_stmt *op_list_sources_statement = NULL;
 static sqlite3_stmt *op_list_delete_statment = NULL;
 
 void finalize_op_statements() {
     if (op_list_select_statement) sqlite3_finalize(op_list_select_statement);
+	if (op_list_sources_statement) sqlite3_finalize(op_list_sources_statement);
 	if (op_list_delete_statment) sqlite3_finalize(op_list_delete_statment);
 }
 
@@ -100,21 +101,21 @@ void set_sync_post_body(pSyncOperation op) {
 }
 
 /* Retrieves the current list of objects for remote processing */
-int get_op_list_from_database(pSyncOperation *list, sqlite3* database, int max_count, char *type) {
+int get_op_list_from_database(pSyncOperation *list, sqlite3* database, int max_count, pSource source, char *type) {
 	pSyncOperation current_op;
 	int i,count = 0;
 	if (op_list_select_statement == NULL) {
 		const char *sql;
-		printf("Checking for new sync operations...\n");
-		sql = "SELECT attrib, o.source_id, object, value, update_type, s.source_id, s.source_url \
-			   FROM object_values o, sources s WHERE update_type =? and o.source_id=s.source_id";
+		sql = "SELECT attrib, source_id, object, value, update_type \
+			   FROM object_values where source_id=? and update_type =?";
 		if (sqlite3_prepare_v2(database, sql, -1, &op_list_select_statement, NULL) != SQLITE_OK) {
 			printf("Error: failed to prepare statement with message '%s'.", sqlite3_errmsg(database));
 		}
-		sqlite3_bind_text(op_list_select_statement, 1, type, -1, SQLITE_TRANSIENT);
+		sqlite3_bind_int(op_list_select_statement, 1, source->_source_id);
+		sqlite3_bind_text(op_list_select_statement, 2, type, -1, SQLITE_TRANSIENT);
 		/* Step through each row and create the sync operation value */
 		while(sqlite3_step(op_list_select_statement) == SQLITE_ROW) {
-			char *tmp_operation,*tmp_attrib,*tmp_object,*tmp_value,*tmp_uri;
+			char *tmp_operation,*tmp_attrib,*tmp_object,*tmp_value;
 			int tmp_source_id;
 			pSyncObject tmp_sync_object;
 
@@ -130,12 +131,11 @@ int get_op_list_from_database(pSyncOperation *list, sqlite3* database, int max_c
 			tmp_source_id = (int)sqlite3_column_int(op_list_select_statement, 1);
 			tmp_object = (char *)sqlite3_column_text(op_list_select_statement, 2);
 			tmp_value = (char *)sqlite3_column_text(op_list_select_statement, 3);
-			tmp_uri = (char *)sqlite3_column_text(op_list_select_statement, 6);
 			tmp_sync_object = (pSyncObject)SyncObjectCreateWithValues(NULL, -1, tmp_attrib, 
 																	  tmp_source_id, tmp_object, 
 																	  tmp_value, type, 0, 0);
 			
-			current_op = (pSyncOperation)SyncOperationCreate(tmp_sync_object, tmp_uri, tmp_operation);
+			current_op = (pSyncOperation)SyncOperationCreate(tmp_sync_object, source->_source_url, tmp_operation);
 			list[count] = current_op;
 			count++;
 		} 
