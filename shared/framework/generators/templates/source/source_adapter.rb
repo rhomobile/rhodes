96f4f7c3a38a92d1e5a48fa60d246ec4936f9f3c
adamblum <a@adamblum.com>
2008-12-11 11:44:33 -0800

    use the base class SourceAdapter in the generic source adapter template

--- shared/framework/generators/templates/source/source_adapter.rb
+++ shared/framework/generators/templates/source/source_adapter.rb
@@ -1,38 +1,35 @@
-class <%=name%> 
-  attr_accessor :client
+class <%=name%> < SourceAdapter
   
   def initialize(source)
-    @source=source
+    super
   end
 
   def login
+    #TODO: Write some code here
   end
 
   def query
+    # TODO: write some code here
   end
 
   def sync
-    @result.entry_list.each do |x|
-      x.name_value_list.each do |y|
-        o=ObjectValue.new
-        o.source_id=@source.id
-        o.object=x['id']
-        o.attrib=y.name
-        o.value=y.value
-        o.save
-      end
-    end
+    # usually the generic base class sync does the job
+    super
   end
 
   def create(name_value_list)
+    #TODO: write some code here
   end
 
   def update(name_value_list)
+    #TODO: write some code here
   end
 
   def delete(name_value_list)
+    #TODO: write some code here if applicable
   end
 
   def logoff
+    #TODO: write some code here if applicable
   end
 end
