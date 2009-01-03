ed0ca87b55f65d1f482c7611d2289a23855da1f6
vtrv101 <vlad@.(none)>
2009-01-03 15:08:20 -0800

    Updated PIM phonebook support

--- shared/ruby/ext/phonebook/phonebook.h
+++ shared/ruby/ext/phonebook/phonebook.h
@@ -9,9 +9,15 @@ extern "C" {
  * List of supported phonebook's fields (visible in ruby)
  ********************************************/
 
-static const char* RUBY_PB_ID = "id";
-static const char* RUBY_PB_FIRST_NAME = "first_name";
-static const char* RUBY_PB_LAST_NAME = "last_name";
+#define RUBY_PB_ID "id"
+#define RUBY_PB_FIRST_NAME "first_name"
+#define RUBY_PB_LAST_NAME "last_name"
+#define RUBY_PB_MOBILE_NUMBER "mobile_number"
+#define RUBY_PB_HOME_NUMBER "home_number"
+#define RUBY_PB_BUSINESS_NUMBER "business_number"
+#define RUBY_PB_EMAIL_ADDRESS "email_address"
+#define RUBY_PB_COMPANY_NAME "company_name"
+
 
 #ifdef __cplusplus
 }
