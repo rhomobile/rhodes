85ea65048e3ddd5e7b8acabe2ca1e448d1ab4045
lars <larsburgess@gmail.com>
2009-01-06 11:11:59 -0800

    use rhosyncdev

--- apps/Rhosugar/Employee/config.rb
+++ apps/Rhosugar/Employee/config.rb
@@ -1,3 +1,3 @@
 require 'rho'
 
-Rho::RhoConfig::add_source("Employee", {"url"=>"http://rhosync.local/sources/3", "source_id"=>3})
+Rho::RhoConfig::add_source("Employee", {"url"=>"http://rhosyncdev.rhohub.com/apps/3/sources/3", "source_id"=>3})
