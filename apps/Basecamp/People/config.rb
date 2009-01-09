94a8a2a1c311d1e36406f478f4afe84a82cb1ac7
lars <larsburgess@gmail.com>
2009-01-08 16:01:00 -0800

    default source domain should be rhosyncdev.rhohub.com

--- apps/Basecamp/People/config.rb
+++ apps/Basecamp/People/config.rb
@@ -1,3 +1,3 @@
 require 'rho'
 
-Rho::RhoConfig::add_source("People", {"url"=>"http://rhosync.local/apps/1/sources/6", "source_id"=>6})
+Rho::RhoConfig::add_source("People", {"url"=>"http://rhosyncdev.rhohub.com/apps/1/sources/6", "source_id"=>6})
