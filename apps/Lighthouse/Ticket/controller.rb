78e3a973138bc35549d6069d1a8e6224135d8669
Vidal Graupera <vidal@vdggroup.com>
2009-01-09 15:23:19 -0800

    added todays tickets, whos responsible, and started assigned to me

--- apps/Lighthouse/Ticket/controller.rb
+++ apps/Lighthouse/Ticket/controller.rb
@@ -7,10 +7,30 @@ class TicketController < Rho::RhoController
   
   #GET /Ticket
   def index
+    @title = "All tickets"	
     @tickets = Ticket.find(:all)
     render :index
   end
 
+  #GET /Ticket/today
+  def today
+    @title = "Todays tickets"
+    @tickets = Ticket.find(:all)
+    today = DateTime.now
+    @tickets = @tickets.reject do |ticket|
+      ttime = DateTime.parse(ticket.created_at)
+      (ttime.day != today.day) || (ttime.month != today.month) || (ttime.year != today.year)
+    end
+    render :index
+  end
+  
+  #GET /Ticket/assgined_to_me
+  def assgined_to_me
+    @title = "Assigned to me"
+    @tickets = Ticket.find(:all)
+    render :index
+  end
+  
   # GET /Ticket/1
   def show
     @ticket = Ticket.find(@params['id'])
