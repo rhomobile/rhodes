8fd9e740185c92b3b70ec37c7b6a26a2d18d9051
lars <larsburgess@gmail.com>
2009-01-12 09:50:11 -0800

    use rhosyncdev site

--- apps/Lighthouse/Project/config.rb
+++ apps/Lighthouse/Project/config.rb
@@ -1,3 +1,3 @@
 require 'rho'
 
-Rho::RhoConfig::add_source("Project", {"url"=>"http://rhosync.local/apps/2/sources/7", "source_id"=>7})
+Rho::RhoConfig::add_source("Project", {"url"=>"http://rhosyncdev.rhohub.com/apps/3/sources/5", "source_id"=>5})
--- apps/Lighthouse/User/config.rb
+++ apps/Lighthouse/User/config.rb
@@ -1,3 +1,3 @@
 require 'rho'
 
-Rho::RhoConfig::add_source("User", {"url"=>"http://rhosync.local/apps/2/sources/8", "source_id"=>8})
+Rho::RhoConfig::add_source("User", {"url"=>"http://rhosyncdev.rhohub.com/apps/3/sources/7", "source_id"=>7})
