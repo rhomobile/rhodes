2a50148d4f038d4e7cb17a91592649baf20ef4a5
Anton Antonov <anton@rhomobile.com>
2009-01-16 20:23:01 +0300

    remove my own url to rhobumdle zip file

--- apps/config.rb
+++ apps/config.rb
@@ -2,6 +2,6 @@ require 'rho'
 
 Rho::RhoConfig.start_path = '/'
 
-Rho::RhoConfig.rhobundle_zip_url = 'http://89.113.219.245:8080/admin/rhobundle.zip'
+Rho::RhoConfig.rhobundle_zip_url = nil
 
 Rho::RhoConfig.rhobundle_zip_pwd = nil
