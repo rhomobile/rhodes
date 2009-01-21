dde9382da165ae98916474540adf25d9ea40944e
genywind <evgeny@rhomobile.com>
2009-01-21 17:59:05 +0300

    [#166] Compare version in DB and in rhodes.rb. If they different, remove DB file and create new one

--- bb/RubyVM/src2/com/rho/db/PerstLiteAdapter.java
+++ bb/RubyVM/src2/com/rho/db/PerstLiteAdapter.java
@@ -11,6 +11,7 @@ import org.garret.perst.StorageFactory;
 import org.garret.perst.Types;
 import org.garret.perst.Key;
 
+import com.rho.sync.SyncUtil;
 import com.xruby.runtime.builtin.*;
 import com.xruby.runtime.lang.*;
 
@@ -24,7 +25,7 @@ public class PerstLiteAdapter  extends RubyBasic {
 	
 	public static final RubyString ALL = ObjectFactory.createString("*");
 	
-	private static final String DB_FILENAME = "syncdb_5.dbs";
+	private static final String DB_FILENAME = "syncdb.dbs";
 
 	public static class Table_base1 extends Persistent{
 	    public Table_base1(){}
@@ -427,8 +428,10 @@ public class PerstLiteAdapter  extends RubyBasic {
 	public static class Table_client_info extends Table_base1 { 
 		
 	    String client_id = "";
+	    String version = "";
 		
 		public static final RubyString CLIENT_ID = ObjectFactory.createString("client_id");
+		public static final RubyString VERSION = ObjectFactory.createString("version");
 	    
 		public static class TableRoot extends TableRootBase { 
 		    Index client_id;
@@ -494,12 +497,14 @@ public class PerstLiteAdapter  extends RubyBasic {
 	    public void writeObject(IOutputStream out) { 
 	    	super.writeObject(out);
 	    	out.writeString(client_id);
+	    	out.writeString(version);
 	    }
 
 	    // Deserialize the object
 	    public void readObject(IInputStream in) { 
 	    	super.readObject(in);
 	    	client_id = in.readString();
+	    	version = in.readString();
 	    }
 	    
 	    RubyHash getValueByName(RubyString name){
@@ -508,6 +513,8 @@ public class PerstLiteAdapter  extends RubyBasic {
 	    	
 	    	if ( bAll || name.equals(CLIENT_ID) )
 	    		res.add( CLIENT_ID, ObjectFactory.createString(client_id) );
+	    	if ( bAll || name.equals(VERSION) )
+	    		res.add( VERSION, ObjectFactory.createString(version) );
 	    	
 	    	return res;
 	    }
@@ -518,6 +525,10 @@ public class PerstLiteAdapter  extends RubyBasic {
 	        RubyValue val = hash.getValue(CLIENT_ID);
 	        if ( val != RubyConstant.QNIL )
 	        	client_id = val.toStr();
+	        val = hash.getValue(VERSION);
+	        if ( val != RubyConstant.QNIL )
+	        	version = val.toStr();
+	        
 	    }
 	    
 	};
@@ -538,11 +549,11 @@ public class PerstLiteAdapter  extends RubyBasic {
 	
     //@RubyLevelMethod(name="initialize")
     public PerstLiteAdapter initialize(RubyValue v) {
-    	createDB();
+    	createDB(v !=null && v != RubyConstant.QNIL ? v.toString() : "");
         return this;
     }
 	
-	void createDB() {
+	void createDB(String strVer) {
 		if (isClosed() == RubyConstant.QFALSE) {
 			return;
 		}
@@ -564,6 +575,26 @@ public class PerstLiteAdapter  extends RubyBasic {
             root.put(Table_sources.name(), new Table_sources.TableRoot(m_storage) );
             root.put(Table_object_values.name(), new Table_object_values.TableRoot(m_storage) );
             root.put(Table_client_info.name(), new Table_client_info.TableRoot(m_storage) );
+
+            //Write version
+			RubyHash hash = SyncUtil.createHash();
+			hash.add(Table_client_info.VERSION, ObjectFactory.createString(strVer));
+            insertIntoTable(ObjectFactory.createString(Table_client_info.name()), hash );
+        }else if ( strVer != null && strVer.length() > 0 ){
+        	//Check version
+        	RubyArray results = (RubyArray)selectFromTable(ObjectFactory.createString(Table_client_info.name()),
+        			Table_client_info.VERSION, RubyConstant.QNIL, RubyConstant.QNIL );
+    		if ( results.size() > 0 ){
+    			RubyHash item = (RubyHash)results.get(0);
+    			RubyValue value = item.getValue(Table_client_info.VERSION); 
+    			if ( value == null || value == RubyConstant.QNIL ||
+    				 !value.toString().equalsIgnoreCase(strVer) ){
+    				m_storage.close();
+    				m_storage = null;
+    				org.garret.perst.impl.Jsr75File.delete(DB_FILENAME);
+    				createDB(strVer);
+    			}
+    		}
         }
 	}
 	
