6bb98d2bd7c1ef35456e51b89990706728021828
Vidal Graupera <vidal@vdggroup.com>
2009-01-23 18:23:52 -0800

    new sugarcrm example 2.0

---
+++ apps/RhoSugarCRM/SugarAccount/controller.rb
@@ -0,0 +1,48 @@
+require 'rho/rhocontroller'
+
+class SugarAccountController < Rho::RhoController
+
+  #GET /SugarAccount
+  def index
+    @SugarAccounts = SugarAccount.find(:all)
+    render
+  end
+
+  # GET /SugarAccount/1
+  def show
+    @SugarAccounts = SugarAccount.find(@params['object'])
+  end
+
+  # GET /SugarAccount/new
+  def new
+    @SugarAccount = SugarAccount.new
+    render :action => :new
+  end
+
+  # GET /SugarAccount/1/edit
+  def edit
+    @SugarAccount = SugarAccount.find(@params['id'])
+    render :action => :edit
+  end
+
+  # POST /SugarAccount/create
+  def create
+    @SugarAccount = SugarAccount.new(@params['SugarAccount'])
+    @SugarAccount.save
+    redirect :index
+  end
+
+  # POST /SugarAccount/1/update
+  def update
+    @SugarAccount = SugarAccount.find(@params['id'])
+    @SugarAccount.update_attributes(@params['SugarAccount'])
+    redirect :index
+  end
+
+  # POST /SugarAccount/1/delete
+  def delete
+    @SugarAccount = SugarAccount.find(@params['id'])
+    @SugarAccount.destroy
+    redirect :index
+  end
+end
