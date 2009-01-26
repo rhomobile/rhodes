688599a33141c2afac6e8bf48876293ac2e241be
ldm314 <Brian@.(none)>
2009-01-26 09:58:09 -0800

    Get RAPC working, factor out helpers into their own class 'Jake'

---
+++ bb/build/Jake.rb
@@ -0,0 +1,229 @@
+require 'pathname'
+  
+class Hash
+  def fetch_r(key)
+    if self.has_key?(key) and not self[key].is_a?(Hash)
+      return self[key]
+    else
+      self.each do |val|
+	value = false 
+	if val.is_a?(Array)
+          val.each do |x|
+            value = x.fetch_r(key) if x.is_a?(Hash)
+  	    return value if value
+  	  end
+ 	end
+        value = val.fetch_r(key) if val.is_a?(Hash)
+  	return value if value
+      end
+    end
+    return false  
+  end
+end
+  
+class Jake
+
+
+  def self.config(configfile)
+    @@config = YAML::load(configfile)
+    self.config_parse(@@config)
+    @@config
+  end
+
+  def self.get_absolute(path)
+    currentdir = pwd
+    
+    path = currentdir + "/" + path
+  
+    patharray = path.split(/\//)
+  
+    while idx = patharray.index("..") do
+      if idx == 0
+        raise "error getting absolute"
+      end
+     
+      if patharray[idx-1] != ".."
+        patharray.delete_at(idx)
+        patharray.delete_at(idx-1)
+      end
+    end
+    return patharray.join("/")  
+  end	
+  
+  def self.config_parse(conf)
+    if conf.is_a?(Array)
+      conf.collect! do |x|
+        if x.is_a?(Hash) or x.is_a?(Array)
+          config_parse(x)
+  	x
+        else
+  	if x =~ /%(.*?)%/
+  	  x.gsub!(/%.*?%/, @@config.fetch_r($1).to_s)
+          end
+  	x
+        end
+      end
+    elsif conf.is_a?(Hash)
+      conf = conf.collect do |k,x|
+  	    
+        if x.is_a?(Hash) or x.is_a?(Array)
+          config_parse(x)
+  	x
+        else 
+  	if x.to_s =~ /%(.*?)%/
+  	  x.gsub!(/%.*?%/, @@config.fetch_r($1).to_s)
+          end
+  	x
+        end
+      end
+    end
+  end
+  
+  def self.run(command, args, wd=nil)
+    argstr = " "
+    currentdir = ""
+  
+    args.each do |x|
+      x = x.to_s
+  #    x.gsub!(/"/,"\\\"")
+      argstr +=  x + " "
+      #argstr += "\"" + x + "\" "
+    end
+  
+    if not wd.nil?
+      currentdir = pwd()
+      chdir wd  
+    end
+  puts "PWD:" + pwd
+  puts "CMD:" + command
+  puts "ARGS:" + argstr
+    
+    command = command + " " + argstr
+    #retval =  `#{command} #{argstr}`
+    retval = %x[#{command}]
+  
+   # bb = IO.popen(command);
+   # b = bb.readlines
+   # retval = b.join
+  
+    if not wd.nil?
+      chdir currentdir
+    end
+  
+    return retval
+  
+  end
+  
+  def self.unjar(src,targetdir)
+    cmd =  @@config["env"]["paths"][@@config["env"]["bbver"]]["java"] + "/jar.exe"
+    p = Pathname.new(src)
+    src = p.realpath
+    currentdir = pwd()
+  
+    args = Array.new
+  
+    args << "xvf"
+    args << src.to_s
+  
+    chdir targetdir
+    puts run(cmd,args)
+    chdir currentdir
+  end
+  
+  def self.jar(target,manifest,files,isfolder=false)
+    cmd =  @@config["env"]["paths"][@@config["env"]["bbver"]]["java"] + "/jar.exe"
+    
+    args = []
+    args << "cfM"
+    args << target
+    #args << manifest
+    if isfolder
+      mkdir files + "/META-INF" if not FileTest.exists? files + "/META-INF"
+      cp manifest, files + "/META-INF/MANIFEST.MF"
+      args << "-C"
+      args << files
+      args << "."
+    else
+      args << files
+    end
+  
+    puts run(cmd,args)
+  
+  
+  end
+  
+  def self.rapc(output,destdir,imports,files,title=nil,vendor=nil,version=nil,library=true,quiet=true, nowarn=true)
+    cmd = @@config["env"]["paths"][@@config["env"]["bbver"]]["java"] + "/java.exe"
+   cmd = "java.exe"
+    
+    jdehome = @@config["env"]["paths"][@@config["env"]["bbver"]]["jde"]
+    currentdir = pwd()
+  
+  
+    chdir destdir
+  
+    if output and title and version and vendor
+      f = File.new(output + ".rapc", "w")
+      f.write "MicroEdition-Profile: MIDP-2.0\r\n"
+      f.write "MicroEdition-Configuration: CLDC-1.1\r\n"
+      f.write "MIDlet-Name: " + output + "\r\n"
+      f.write "MIDlet-Version: " + version + "\r\n"
+      f.write "MIDlet-Vendor: " + vendor + "\r\n"
+      f.write "MIDlet-Jar-URL: " + output + ".jar\r\n"
+      f.write "MIDlet-Jar-Size: 0\r\n"
+      f.write "RIM-Library-Flags: 2\r\n" if library
+      f.close
+    end
+  
+  
+    args = []
+    args << "-classpath"
+  #  args << "-jar"
+    args << '"' + jdehome + "/bin/rapc.jar\""
+    args << "net.rim.tools.compiler.Compiler"
+  #  args << "-quiet" if quiet
+    args << "-noWarn" if nowarn
+    args << '"import=' + imports + '"'
+    args << 'codename=' + output
+    args << 'library=' + output if library
+    args << output + '.rapc'
+    args << files
+  
+    puts run("cmd /C " + cmd,args)
+    chdir currentdir
+  
+  end
+  
+  def self.ant(dir,target)
+  
+    bindir = @@config["build"]["bindir"]
+    srcdir = @@config["build"]["srcdir"]
+    rubypath = @@config["build"]["rubypath"]
+    shareddir = @@config["build"]["shareddir"]
+    targetdir = @@config["build"]["targetdir"]
+    rubyVMdir = @@config["build"]["rubyVMdir"]
+    excludelib = @@config["build"]["excludelib"]
+    compileERB = @@config["build"]["compileERB"]
+    
+  
+    args = []
+    args << "-buildfile"
+    args << dir + "/build.xml"
+    args << "-d" 
+    args << '"-Dbin.dir=' + get_absolute(bindir) + '"'
+    args << '"-Dsrc.dir=' + get_absolute(srcdir) + '"'
+    args << '"-Druby.path=' + get_absolute(rubypath) + '"'
+    args << '"-DsharedAnt.dir=' + get_absolute(shareddir) + '"'
+    args << '"-Dtarget.dir=' + get_absolute(targetdir) + '"'
+    args << '"-DRubyVM.dir=' + get_absolute(rubyVMdir) + '"'
+    args << '"-Dexclude.lib=' + excludelib + '"'
+    args << '"-DcompileERB.path=' + get_absolute(compileERB) + '"'
+    args << '"-Dsrclib.dir=' + get_absolute(srcdir) + '"'
+  
+  
+    args << target
+    #puts args.to_s
+    puts run("ant.bat",args,dir)
+  end
+end
+  
