970a6a232df344e851a4ef3b42699416b97ed1ca
Vidal Graupera <vidal@vdggroup.com>
2009-01-27 17:07:12 -0800

    added teams and users to sugarcrm 2 example

---
+++ apps/RhoSugarCRM/SugarTeam/config.rb
@@ -0,0 +1,3 @@
+require 'rho'
+
+Rho::RhoConfig::add_source("SugarLead", {"url"=>"http://rhosyncdev.rhohub.com/apps/1/sources/18", "source_id"=>18})
---
+++ apps/RhoSugarCRM/SugarUser/config.rb
@@ -0,0 +1,3 @@
+require 'rho'
+
+Rho::RhoConfig::add_source("SugarLead", {"url"=>"http://rhosyncdev.rhohub.com/apps/1/sources/19", "source_id"=>19})
