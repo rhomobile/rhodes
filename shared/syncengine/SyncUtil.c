e5382f482af0470ef8f3f62bdedcd65023ad81ef
genywind <evgeny@rhomobile.com>
2009-02-02 21:21:31 +0300

    No stricmp on iPhone !!!

--- shared/syncengine/SyncUtil.c
+++ shared/syncengine/SyncUtil.c
@@ -334,7 +334,7 @@ char *get_db_session_by_server(char* source_url) {
 
       char* szServer2 = parseServerFromUrl(url);
       if ( sess && strlen(sess) > 0 &&
-           szServer && szServer2 && stricmp(szServer,szServer2) == 0 ){
+           szServer && szServer2 && strcmp(szServer,szServer2) == 0 ){
         session = str_assign(sess);
         break;
       }
