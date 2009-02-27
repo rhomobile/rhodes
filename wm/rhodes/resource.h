b50fe9a4c85dbad5d9811741deb3c796feb126f7
vtrv101 <vlad@.(none)>
2009-02-26 17:24:52 -0800

    Added Options menu to WM

--- wm/rhodes/resource.h
+++ wm/rhodes/resource.h
@@ -25,13 +25,15 @@
 #define IDM_SYNC                        40011
 #define ID__RELOADRHOBUNDLE             40012
 #define IDM_RELOADRHOBUNDLE             40013
+#define ID__OPTIONS                     40014
+#define IDM_OPTIONS                     40015
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        101
-#define _APS_NEXT_COMMAND_VALUE         40014
+#define _APS_NEXT_COMMAND_VALUE         40016
 #define _APS_NEXT_CONTROL_VALUE         1003
 #define _APS_NEXT_SYMED_VALUE           108
 #endif
