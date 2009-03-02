bfb7ad9dea3bc1c2129610da804b9b5a3d4f6ab8
Vidal Graupera <vidal@vdggroup.com>
2009-03-02 09:48:49 -0800

    remove lighthouse sample, new location: http://github.com/VGraupera/rhodes-issues2go/tree/master

--- apps/Lighthouse/TicketVersions/config.rb
+++
@@ -1,3 +0,0 @@
-require 'rho'
-
-Rho::RhoConfig::add_source("TicketVersions", {"url"=>"http://rhosyncdev.rhohub.com/apps/4/sources/9", "source_id"=>9})
--- apps/Lighthouse/User/config.rb
+++
@@ -1,3 +0,0 @@
-require 'rho'
-
-Rho::RhoConfig::add_source("User", {"url"=>"http://rhosyncdev.rhohub.com/apps/4/sources/8", "source_id"=>8})
