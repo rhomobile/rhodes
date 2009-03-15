20fadf33aa368eabdd55b912df567de0cd434c90
genywind <evgeny@rhomobile.com>
2009-03-16 01:57:10 +0300

    Undo Anton changes, cause it kill WM and iPhone

--- platform/shared/syncengine/Notifications.cpp
+++ platform/shared/syncengine/Notifications.cpp
@@ -7,6 +7,8 @@
  *
  */
 
+#ifndef __SYMBIAN32__
+
 #if defined(_WIN32_WCE)
 // Fixing compiler error C2039: 'wcsftime' : is not a member of '`global namespace''
 size_t __cdecl wcsftime(wchar_t *, size_t, const wchar_t *, const struct tm*);
@@ -16,17 +18,9 @@ extern "C" int _shttpd_snprintf(char *buf, size_t buflen, const char *fmt, ...);
 #define snprintf _shttpd_snprintf
 #endif
 
-#ifdef __SYMBIAN32__
-#include <pthread.h>
-#include <string.h>
-#include <stdlib.h>
-#include <stdapis/stdio.h>
-#endif
+#include <string>
+#include <map>
 
-//#include <string>
-//#include <map>
-
-#include "Constants.h"
 #include "Notifications.h"
 #include "UniversalLock.h"
 // TODO: temporary fix for compile error
@@ -34,21 +28,7 @@ extern "C" int _shttpd_snprintf(char *buf, size_t buflen, const char *fmt, ...);
 #include "tcmalloc/rhomem.h"
 #endif
 
-//static std::map<int,notification_t*> _notifications;
-static notification_t** _notifications;
-
-static notification_t** get_notifications() 
-{
-	static int isInitialized = 0;
-	if ( !isInitialized )
-	{
-		isInitialized = 1;
-		_notifications = (notification_t**)calloc(MAX_SOURCES,sizeof(notification_t*));
-	}
-	
-	return _notifications;
-}
-
+static std::map<int,notification_t*> _notifications;
 INIT_LOCK(notify);
 
 extern "C" void perform_notification(char* callback, char* params);
@@ -58,11 +38,11 @@ extern "C" char* HTTPResolveUrl(char* url);
 static void _clear_notification(int source_id);
 	
 static notification_t* get_notification(int source_id) {
-	
-	if ( source_id < MAX_SOURCES && source_id >= 0 )
-		return get_notifications()[source_id];
-	else
-		return NULL;
+	std::map<int,notification_t*>::iterator it = _notifications.find(source_id);
+	if (it!=_notifications.end()) {
+		return it->second;
+	}
+	return NULL;
 }
 
 void fire_notification(int source_id,int sucess) {
@@ -94,11 +74,11 @@ void free_notifications() {
     LOCK(notify);
 
 	try {
-		int i = 0;
-		for ( ; i < MAX_SOURCES; i++ ) {
-			_clear_notification(i);
+		std::map<int,notification_t*>::iterator it;
+		for ( it=_notifications.begin() ; it != _notifications.end(); it++ ) {
+			_clear_notification(it->first);
 		}
-		memset(get_notifications(), 0, MAX_SOURCES * sizeof(notification_t*));
+		_notifications.clear();
 	} catch(...) {
 	}
 	
@@ -113,10 +93,10 @@ void set_notification(int source_id, const char *url, char* params) {
 			notification_t* notification = (notification_t*) malloc(sizeof(notification_t));
 			_clear_notification(source_id);
 			notification->url = HTTPResolveUrl(strdup(url));
-			notification->params = params ? strdup(params) : NULL;
+			notification->params = strdup(params);
 			printf("Resolved [%s] in [%s]\n", url, notification->url);								 
 			printf("Setting notification (%s) for source %d\n", notification->url, source_id);			
-			get_notifications()[source_id] = notification;
+			_notifications[source_id] = notification;
 		}
 	} catch(...) {
 	}
@@ -138,10 +118,8 @@ void free_notification_t(notification_t* pn) {
 
 static void _clear_notification(int source_id) {
 	try {
-		notification_t * pnt = get_notification(source_id);
-		if ( pnt )
-			free_notification_t(pnt);		
-		get_notifications()[source_id]=0;
+		free_notification_t(get_notification(source_id));		
+		_notifications.erase(source_id);
 	} catch(...) {
 	}
 }
@@ -152,3 +130,21 @@ void clear_notification(int source_id) {
 	_clear_notification(source_id); 
 	UNLOCK(notify);
 }
+
+#else //__SYMBIAN32__
+
+extern "C"{
+void fire_notification(int source_id) {
+}
+
+void clear_notification(int source_id) {
+}
+
+void set_notification(int source_id, const char *url) {
+}
+
+void free_notifications() {
+}
+}
+
+#endif
