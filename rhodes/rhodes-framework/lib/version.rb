25fc3c5a0e10fe70f19c15038b9c4a840209d726
lars <larsburgess@gmail.com>
2009-03-31 13:26:01 -0700

    version bump for 1.0.2

--- rhodes/rhodes-framework/lib/version.rb
+++ rhodes/rhodes-framework/lib/version.rb
@@ -1,8 +1,8 @@
 module RhodesFramework
   unless defined? RhodesFramework::VERSION
-    VERSION = '1.0.1'
+    VERSION = '1.0.2'
   end
   unless defined? RhodesFramework::DBVERSION
-    DBVERSION = '1.0.1'
+    DBVERSION = '1.0.2'
   end
 end
