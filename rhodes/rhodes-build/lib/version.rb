f1c23b33385a5e25f584be2dc4aa058f5b2fbbf2
lars <larsburgess@gmail.com>
2009-04-07 11:23:01 -0700

    updating mac prebuild for release

--- rhodes/rhodes-build/lib/version.rb
+++ rhodes/rhodes-build/lib/version.rb
@@ -1,9 +1,9 @@
 module RhodesBuild
   unless defined? RhodesBuild::VERSION
-    VERSION = '1.0.2'
+    VERSION = '1.0.3'
   end
   unless defined? RhodesBuild::DBVERSION
-    DBVERSION = '1.0.2'
+    DBVERSION = '1.0.3'
   end
   
 end
