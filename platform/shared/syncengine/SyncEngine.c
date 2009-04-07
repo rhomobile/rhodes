5b36e326bb2f0b3dc7c9da5b01e1f6ab3aa1ff33
genywind <evgeny@rhomobile.com>
2009-04-07 23:40:49 +0400

    [#518] No Sync Notify sent when client gives up after 3 errors

--- platform/shared/syncengine/SyncEngine.c
+++ platform/shared/syncengine/SyncEngine.c
@@ -163,18 +163,23 @@ int process_local_changes() {
 #else
           int bStopSync = 0;
 
-		  for(i = nStartSrc; i < source_length && isContinueSync() && !bStopSync; i++)
+		  for(i = nStartSrc; i < source_length && isContinueSync(); i++)
 		  {
-			  ask_params = get_params_for_source(source_list[i], database);
-			  available_remote = fetch_remote_changes(database, client_id, source_list[i], ask_params, &bStopSync );
-			  if(available_remote > 0) {
-				  RAWLOG_INFO1("Successfully processed %i records...", available_remote);
-			  }
+              if ( !bStopSync ){
+			      ask_params = get_params_for_source(source_list[i], database);
+			      available_remote = fetch_remote_changes(database, client_id, source_list[i], ask_params, &bStopSync );
+			      if(available_remote > 0) {
+				      RAWLOG_INFO1("Successfully processed %i records...", available_remote);
+			      }
+
+			      if (ask_params) 
+                      free(ask_params);
+              }else
+                  available_remote = 0;
+
 			  if( isContinueSync() ) {
 				  fire_notification(source_list[i]->_source_id,available_remote);
 			  }
-			  if (ask_params) 
-                  free(ask_params);
 		  }
 #endif	  
 	  }
