c5ad020c58020b928291f22f6bb2df7bb4e1cf28
genywind <evgeny@rhomobile.com>
2009-04-09 22:26:33 +0400

    Add test solution for WM, add stlport sources for Symbian

---
+++ platform/wm/Tests/Tests.cpp
@@ -0,0 +1,21 @@
+void runAllLogTests();
+extern "C" void runAllProfilingTests();
+extern "C" void InitRhoLog(const char* path);
+
+int wmain(int argc, wchar_t* argv[])
+{
+
+//    runAllLogTests();
+
+    InitRhoLog("");
+
+	runAllProfilingTests();
+    return 0;
+}
+
+#if defined( __SYMBIAN32__ ) || defined(_WIN32_WCE)
+extern "C" char *getenv(const char *env)
+{
+	return 0;
+}
+#endif
