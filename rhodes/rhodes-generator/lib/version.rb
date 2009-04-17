a21869f6db3b6722958f8ba88df88e4762787953
lars <larsburgess@gmail.com>
2009-04-17 13:53:15 -0700

    updating mac prebuild for 1.0.5

--- rhodes/rhodes-generator/lib/version.rb
+++ rhodes/rhodes-generator/lib/version.rb
@@ -1,9 +1,9 @@
 module RhodesGenerator
   unless defined? RhodesGenerator::VERSION
-    VERSION = '1.0.4'
+    VERSION = '1.0.5'
   end
   unless defined? RhodesGenerator::DBVERSION
-    DBVERSION = '1.0.4'
+    DBVERSION = '1.0.5'
   end
   
 end
