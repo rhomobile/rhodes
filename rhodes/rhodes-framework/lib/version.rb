2bbe2401b8d6fb6ed6c48aff1aa5fe08996e425a
lars <larsburgess@gmail.com>
2009-04-17 08:46:04 -0700

    updating mac prebuilt

--- rhodes/rhodes-framework/lib/version.rb
+++ rhodes/rhodes-framework/lib/version.rb
@@ -1,8 +1,8 @@
 module RhodesFramework
   unless defined? RhodesFramework::VERSION
-    VERSION = '1.0.3'
+    VERSION = '1.0.4'
   end
   unless defined? RhodesFramework::DBVERSION
-    DBVERSION = '1.0.3'
+    DBVERSION = '1.0.4'
   end
 end
