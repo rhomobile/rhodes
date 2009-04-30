b5960044af506bf9647b868d3a14baa45ab2f002
larsburgess <larsburgess@gmail.com>
2009-04-29 22:41:49 -0700

    updating version for 1.0.9

--- rhodes/rhodes-framework/lib/rhodes.rb
+++ rhodes/rhodes-framework/lib/rhodes.rb
@@ -1,9 +1,9 @@
 module Rhodes
   unless defined? Rhodes::VERSION
-    VERSION = '1.0.8'
+    VERSION = '1.0.9'
   end
   unless defined? Rhodes::DBVERSION
-    DBVERSION = '1.0.8'
+    DBVERSION = '1.0.9'
   end
   
 end
