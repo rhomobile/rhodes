bda4950992263e37269cb32e395ebe441755ffd9
lars <larsburgess@gmail.com>
2009-05-08 16:39:15 -0700

    setting version to 1.1.0

--- rhodes/rhodes-framework/lib/version.rb
+++ rhodes/rhodes-framework/lib/version.rb
@@ -1,8 +1,8 @@
 module RhodesFramework
   unless defined? RhodesFramework::VERSION
-    VERSION = '1.0.10'
+    VERSION = '1.1.0'
   end
   unless defined? RhodesFramework::DBVERSION
-    DBVERSION = '1.0.10'
+    DBVERSION = '1.1.0'
   end
 end
--- rhodes/rhodes-generator/lib/version.rb
+++ rhodes/rhodes-generator/lib/version.rb
@@ -1,9 +1,9 @@
 module RhodesGenerator
   unless defined? RhodesGenerator::VERSION
-    VERSION = '1.0.10'
+    VERSION = '1.1.0'
   end
   unless defined? RhodesGenerator::DBVERSION
-    DBVERSION = '1.0.10'
+    DBVERSION = '1.1.0'
   end
   
 end
