7217d385621bba6aaad9ba3e2dbf763d78684070
lars <larsburgess@gmail.com>
2009-05-08 13:48:47 -0700

    fixing bug in 1.0.9 release, pushing 1.0.10 mac prebuilt

--- rhodes/rhodes/lib/rhodes.rb
+++ rhodes/rhodes/lib/rhodes.rb
@@ -1,9 +1,9 @@
 module Rhodes
   unless defined? Rhodes::VERSION
-    VERSION = '1.0.9'
+    VERSION = '1.0.10'
   end
   unless defined? Rhodes::DBVERSION
-    DBVERSION = '1.0.9'
+    DBVERSION = '1.0.10'
   end
   
 end
