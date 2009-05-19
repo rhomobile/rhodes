a6c0ad4952c1e584245328e49c3a8c3cddc690c8
lars <larsburgess@gmail.com>
2009-05-19 11:57:10 -0700

    removed source_id from rhogen argument list, it is generated automatically [#368 state:resolved]

--- rhodes/rhodes-generator/generators/templates/model/config.rb
+++ rhodes/rhodes-generator/generators/templates/model/config.rb
@@ -1,3 +1,3 @@
 require 'rho'
 
-Rho::RhoConfig::add_source("<%= class_name %>", {"url"=>"<%= source_url %>", "source_id"=><%= source_id %><% if type and type == "ask" %>, "type"=>"ask"<%end%>})
+Rho::RhoConfig::add_source("<%= class_name %>", {"url"=>"<%= source_url %>", "source_id"=><%= name.camel_case.hash.abs % 10000 %><% if type and type == "ask" %>, "type"=>"ask"<%end%>})
