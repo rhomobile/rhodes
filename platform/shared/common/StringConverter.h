5507911f6dd4be720d3e3ac622400571517b94c6
genywind <evgeny@rhomobile.com>
2009-05-20 16:45:31 +0400

    Remove odd ocde

--- platform/shared/common/StringConverter.h
+++ platform/shared/common/StringConverter.h
@@ -38,9 +38,9 @@ inline String convertToStringA( wchar_t* szValue ){
     return convertToStringA( (const wchar_t*)szValue );
 }
 
-inline String convertToStringA( const char* szValue ){ 
+/*inline String convertToStringA( const char* szValue ){ 
 	return String(szValue);
-}
+}*/
 	
 template<>  inline String convertToStringA<StringW>( const StringW& strValue ){
     return convertToStringA(strValue.c_str());
