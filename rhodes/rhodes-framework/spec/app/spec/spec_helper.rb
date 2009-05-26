5c8718460ec37091674ef6aba0f3416346080dcf
genywind <evgeny@rhomobile.com>
2009-05-27 00:53:35 +0400

    Remove 'delete source table'

--- rhodes/rhodes-framework/spec/app/spec/spec_helper.rb
+++ rhodes/rhodes-framework/spec/app/spec/spec_helper.rb
@@ -10,6 +10,6 @@ describe "rhom initializer", :shared => true do
   after(:each) do
     Rhom::RhomDbAdapter.delete_all_from_table('client_info')
     Rhom::RhomDbAdapter.delete_all_from_table('object_values')
-    Rhom::RhomDbAdapter.delete_all_from_table('sources')
+    #Rhom::RhomDbAdapter.delete_all_from_table('sources')
   end
 end
