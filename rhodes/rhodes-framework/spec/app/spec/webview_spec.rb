363507cc8adde961de39bbb383d36531b1d01869
genywind <evgeny@rhomobile.com>
2009-06-03 19:56:04 +0400

    Return back webview test

--- rhodes/rhodes-framework/spec/app/spec/webview_spec.rb
+++ rhodes/rhodes-framework/spec/app/spec/webview_spec.rb
@@ -21,7 +21,7 @@ require 'spec/spec_helper'
 
 describe "WebView" do
   
-  #it "should evaulate simple javascript" do
-   # WebView.execute_js("1+1").should == "2"
-  #end
+  it "should evaulate simple javascript" do
+    WebView.execute_js("1+1").should == "2"
+  end
 end
