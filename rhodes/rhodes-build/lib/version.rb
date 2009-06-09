f418d87c1930e4623f2c5990d231ac27f6dd2aec
lars <larsburgess@gmail.com>
2009-06-09 13:28:53 -0700

    * updating version for 1.2 branch

--- rhodes/rhodes-build/lib/version.rb
+++ rhodes/rhodes-build/lib/version.rb
@@ -1,9 +1,9 @@
 module RhodesBuild
   unless defined? RhodesBuild::VERSION
-    VERSION = '1.1.1'
+    VERSION = '1.2.0'
   end
   unless defined? RhodesBuild::DBVERSION
-    DBVERSION = '1.1.1'
+    DBVERSION = '1.2.0'
   end
   
 end
--- rhodes/rhodes-framework/spec/app/spec_runner.rb
+++ rhodes/rhodes-framework/spec/app/spec_runner.rb
@@ -10,21 +10,12 @@ class SpecRunner < MSpecScript
     # turn on exception backtrace
     MSpec.backtrace = true
     
-#    Dir.entries(File.join(File.dirname(__FILE__),'spec')).each do |x|
-#      x = nil if not x =~ /.iseq/
-#      puts "Adding spec file: #{x}" if x
-#      puts "Adding spec file: " + File.join(File.dirname(__FILE__),'spec',File.basename(x,'.iseq')) if x
-
-#      config[:files] << File.join(File.dirname(__FILE__),'spec',File.basename(x,'.iseq')) if x
-      
-#    end
-#   config[:files] << 'spec/rhom_object_factory_spec'
-   config[:files] << 'spec/rhom_spec'
-   config[:files] << 'spec/rhoruby_spec'
-   config[:files] << 'spec/rho_controller_spec'
-   config[:files] << 'spec/rho_spec'
-   config[:files] << 'spec/spec_helper'
-   config[:files] << 'spec/webview_spec'
+    config[:files] << 'spec/rhom_spec'
+    config[:files] << 'spec/rhoruby_spec'
+    config[:files] << 'spec/rho_controller_spec'
+    config[:files] << 'spec/rho_spec'
+    config[:files] << 'spec/spec_helper'
+    config[:files] << 'spec/webview_spec'
     
   end
 
