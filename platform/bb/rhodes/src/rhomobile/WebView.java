955ad03cf1588cee6f9adbfa17e215bc5a96762d
larsburgess <larsburgess@gmail.com>
2009-06-10 17:19:22 -0700

    Revert "adding plumbing for dynamically changing menu based on ruby array [#749]"
    This reverts commit 2bacb8cd96083ae93c84c400f6e28590e25e5077.

--- platform/bb/rhodes/src/rhomobile/WebView.java
+++ platform/bb/rhodes/src/rhomobile/WebView.java
@@ -1,23 +1,13 @@
 package rhomobile;
 
-import j2me.util.ArrayList;
-
-import java.util.Hashtable;
-
-import javolution.util.FastTable;
-
 import com.xruby.runtime.lang.RubyBasic;
 import com.xruby.runtime.lang.RubyBlock;
 import com.xruby.runtime.lang.RubyClass;
 import com.xruby.runtime.lang.RubyConstant;
 import com.xruby.runtime.lang.RubyOneArgMethod;
 import com.xruby.runtime.lang.RubyNoArgMethod;
-import com.xruby.runtime.lang.RubyTwoArgMethod;
 import com.xruby.runtime.lang.RubyValue;
 import com.xruby.runtime.builtin.ObjectFactory;
-import com.xruby.runtime.builtin.RubyArray;
-import com.xruby.runtime.builtin.RubyHash;
-import com.xruby.runtime.builtin.RubyString;
 
 public class WebView extends RubyBasic {
 
@@ -42,17 +32,6 @@ public class WebView extends RubyBasic {
 		return ObjectFactory.createString(url);
 	}
 	
-	public static RubyValue add_menu_items(RubyValue arg0, RubyValue arg1) {
-		RubyArray items = (RubyArray) (new RubyArray(arg0)).get(0);
-		for(int i = 0; i < items.size(); i++) {
-			RubyHash currentItem = (RubyHash)items.get(i);
-			RubyString title = (RubyString)currentItem.keys().get(0);
-			RubyString path = (RubyString)currentItem.get(title);
-			RhodesApplication.getInstance().addMenuItem(title.toString(), path.toString());
-		}
-		return RubyConstant.QTRUE;
-	}
-	
 	public static void initMethods(RubyClass klass) {
 		klass.getSingletonClass().defineMethod("refresh", new RubyNoArgMethod() {
 			protected RubyValue run(RubyValue receiver, RubyBlock block) {
@@ -69,11 +48,7 @@ public class WebView extends RubyBasic {
 				return WebView.current_location();
 			}
 		});		
-		klass.getSingletonClass().defineMethod("add_menu_items", new RubyTwoArgMethod() {
-			protected RubyValue run(RubyValue receiver, RubyValue arg0, RubyValue arg1, RubyBlock block) {
-				return WebView.add_menu_items(arg0, arg1);
-			}
-		});
+		
 	}
 	
 }
