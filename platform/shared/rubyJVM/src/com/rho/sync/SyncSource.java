704b7ec12accb9d6bfef1afd43c06178e3e711a1
genywind <evgeny@rhomobile.com>
2009-07-07 18:43:14 +0400

    [#831] - java sync engine - 'delete' db_operation has no effect on client

--- platform/shared/rubyJVM/src/com/rho/sync/SyncSource.java
+++ platform/shared/rubyJVM/src/com/rho/sync/SyncSource.java
@@ -156,8 +156,7 @@ class SyncSource
 	        strUrl += "objects";
 	        String strQuery = SyncEngine.SYNC_SOURCE_FORMAT() + "&client_id=" + getSync().getClientID();
 	
-	        String strBody = "";
-	        makePushBody(strBody, arUpdateTypes[i]);
+	        String strBody = makePushBody(arUpdateTypes[i]);
 	        if ( strBody.length() > 0 )
 	        {
 			    LOG.INFO("Push client changes to server. Source id: " + getID() + "Size :" + strBody.length());
@@ -189,8 +188,9 @@ class SyncSource
  * update: attrvals[][attrib]=<name|industry>&attrvals[][object]=<remoteid>&attrvals[][value]=<some new value>
  * delete: attrvals[][attrib]=<name|industry>&attrvals[][object]=<remoteid>
  */
-	void makePushBody(String strBody, String szUpdateType)throws DBException
+	String makePushBody( String szUpdateType)throws DBException
 	{
+		String strBody = "";
 	    //boolean bFirst = true;
 	    IDBResult res = getDB().executeSQL("SELECT attrib, object, value, attrib_type "+
 						 "FROM object_values where source_id=? and update_type =?", getID(), szUpdateType );
@@ -234,6 +234,8 @@ class SyncSource
 	
 	        strBody += strSrcBody;
 	    }
+	    
+	    return strBody;
 	}
 
 	void getAndremoveAsk()throws DBException
@@ -354,7 +356,7 @@ class SyncSource
 	        m_nInserted++;
 	    }else if ( szDbOp != null && szDbOp.equals("delete") )
 	    {
-	        getDB().executeSQL("DELETE FROM object_values where id=?", oJsonEntry.getUInt64("id") );
+	        getDB().executeSQL("DELETE FROM object_values where id=?", oJsonEntry.getLong("id") );
 	
 	        m_nDeleted++;
 	    }else{
