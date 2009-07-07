1732d270cb67764f5081e88156b887a3aa71b3d7
vtrv101 <vlad@.(none)>
2009-07-07 13:53:25 -0700

    fixed linking errors with new alert api on WM

---
+++ platform/wm/rhodes/alert.cpp
@@ -0,0 +1,10 @@
+#include "stdafx.h"
+
+extern "C" void alert_show_popup(char* message) {
+}
+
+extern "C" void alert_vibrate(void*) {
+}
+
+extern "C" void alert_play_file(char* file_name, ...) {
+}
