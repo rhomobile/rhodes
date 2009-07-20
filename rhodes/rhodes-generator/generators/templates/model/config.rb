aa9e2ad0c98c415fba95ba137212e116efbb5477
lars <larsburgess@gmail.com>
2009-07-20 10:45:43 -0700

    * provide syncserver argument on 'rhogen app' instead of 'rhogen model' [#866 state:resolved]

--- rhodes/rhodes-generator/generators/templates/model/config.rb
+++ rhodes/rhodes-generator/generators/templates/model/config.rb
@@ -1,3 +1,3 @@
 require 'rho'
 
-Rho::RhoConfig::add_source("<%= class_name %>", {"url"=>"<%= source_url %>", "source_id"=><%= name.camel_case.hash.abs % 10000 %><% if type and type == "ask" %>, "type"=>"ask"<%end%>})
+Rho::RhoConfig::add_source("<%=class_name%>", {"url"=>"#{Rho::RhoConfig.syncserver}<%=class_name%>", "source_id"=><%= name.camel_case.hash.abs % 10000 %><% if type and type == "ask" %>, "type"=>"ask"<%end%>})
