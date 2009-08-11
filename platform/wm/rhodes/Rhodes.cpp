c2031154ba5928fae7420869b613022d42afedfa
larsburgess <larsburgess@gmail.com>
2009-08-10 19:07:06 -0700

    * fix WM link error by stubbing rho_syscall * fix sync notification params to show all three (total_count, processed_count & status)

--- platform/wm/rhodes/Rhodes.cpp
+++ platform/wm/rhodes/Rhodes.cpp
@@ -234,6 +234,10 @@ extern "C" char* webview_current_location() {
 	return get_current_location();
 }
 
+extern "C" VALUE rho_syscall(const char* callname, int nparams, char** param_names, char** param_values) {
+	return 0;
+}
+
 /*BOOL EnumRhodesWindowsProc(HWND hwnd,LPARAM lParam)
 {
 	char buf[255] = {0};
