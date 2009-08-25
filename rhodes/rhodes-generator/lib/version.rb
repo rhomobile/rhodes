bbaf38fb3a4c67c8cd003ed48d0d992787d1b3a4
lars <larsburgess@gmail.com>
2009-08-25 16:12:07 -0700

    * prepping gems for 1.2.2

--- rhodes/rhodes-generator/lib/version.rb
+++ rhodes/rhodes-generator/lib/version.rb
@@ -1,9 +1,9 @@
 module RhodesGenerator
   unless defined? RhodesGenerator::VERSION
-    VERSION = '1.2.1'
+    VERSION = '1.2.2'
   end
   unless defined? RhodesGenerator::DBVERSION
-    DBVERSION = '1.2.1'
+    DBVERSION = '1.2.2'
   end
   
 end
--- rhodes/rhodes/lib/rhodes.rb
+++ rhodes/rhodes/lib/rhodes.rb
@@ -1,9 +1,9 @@
 module Rhodes
   unless defined? Rhodes::VERSION
-    VERSION = '1.2.1'
+    VERSION = '1.2.2'
   end
   unless defined? Rhodes::DBVERSION
-    DBVERSION = '1.2.1'
+    DBVERSION = '1.2.2'
   end
   
 end
