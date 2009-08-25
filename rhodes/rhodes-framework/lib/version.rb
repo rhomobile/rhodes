0afe6ee0a9df3225317f6cac0b63f06e0be797e7
lars <larsburgess@gmail.com>
2009-08-25 16:12:07 -0700

    * prepping gems for 1.2.2

--- rhodes/rhodes-framework/lib/version.rb
+++ rhodes/rhodes-framework/lib/version.rb
@@ -1,8 +1,8 @@
 module RhodesFramework
   unless defined? RhodesFramework::VERSION
-    VERSION = '1.2.1'
+    VERSION = '1.2.2'
   end
   unless defined? RhodesFramework::DBVERSION
-    DBVERSION = '1.2.1'
+    DBVERSION = '1.2.2'
   end
 end
