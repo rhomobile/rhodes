7906334a4188fafc44d55c03812c40b6c121edb7
rhomobile <rhomobile@rhomobiles-mac-mini.local>
2009-09-11 14:29:03 -0700

    Add osx launcher app

---
+++ platform/osx/Rhodes Launcher/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  Rhodes Launcher
+//
+//  Created by rhomobile on 9/11/09.
+//  Copyright __MyCompanyName__ 2009. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc,  (const char **) argv);
+}
