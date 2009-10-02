06b18e69d13eb93845810f977f4b7f76b5776ce9
genywind <evgeny@rhomobile.com>
2009-10-02 16:35:03 +0400

    Add can_modify before edit

--- rhodes/rhodes-generator/generators/templates/model/controller.rb
+++ rhodes/rhodes-generator/generators/templates/model/controller.rb
@@ -23,7 +23,12 @@ class <%= class_name %>Controller < Rho::RhoController
   # GET /<%= class_name %>/{1}/edit
   def edit
     @<%= name.downcase %> = <%= class_name %>.find(@params['id'])
-    render :action => :edit
+    
+    if @<%= name.downcase %> && !@<%= name.downcase %>.can_modify
+        render :action => :cannot_edit
+    else    
+        render :action => :edit
+    end    
   end
 
   # POST /<%= class_name %>/create
