e61991c130f37a618f19fc37ce43cff896b85c15
genywind <evgeny@rhomobile.com>
2009-10-15 15:20:49 +0400

    Fix iPhone build

--- platform/shared/net/URI.cpp
+++ platform/shared/net/URI.cpp
@@ -3,12 +3,35 @@
 namespace rho {
 namespace net {
 
+static void toHexString(int i, String& strRes, int radix)
+{
+    char buf[33];
+	bool neg= false;
+	int f, n;
+	if(i<0) { neg= true; i= -i; };
+	f= 32;
+	buf[f--]= 0;
+	do
+	{
+		n= i%radix;
+		if(n<10) buf[f]= '0'+n;
+		else buf[f]= 'a'+n-10;
+		i= i/radix;
+		f--;
+	}
+	while(i>0);
+
+	if(neg) 
+        buf[f--]= '-';
+
+    strRes += (buf+f+1);
+}
+
 /*static*/ void URI::urlEncode(const String& fullPath, String& strRes)
 {
     int len = fullPath.length();
 
     char c;
-    char buf[4];
     boolean bFound = false;
     for  (int index=0; index < len ; index++){
       c = fullPath.at(index);
@@ -30,13 +53,10 @@ namespace net {
 		    strRes += '%';
 		    if (c > 15) { // is it a non-control char, ie. >x0F so 2 chars
 			    //sb.append(Integer.toHexString((int)c)); // just add % and the string
-                _itoa(c, buf, 16);
-                strRes += buf;
+                toHexString(c, strRes, 16);
 		    } else {
-                buf[0] = '0';
-                _itoa(c, buf+1, 16);
-                strRes += buf;
-
+                strRes += '0';
+                toHexString(c, strRes, 16);
 			    //sb.append("0" + Integer.toHexString((int)c));
 			    // otherwise need to add a leading 0
 		    }
