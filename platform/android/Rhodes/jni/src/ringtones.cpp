f07c5524cf53fbee7a9385a43439ac9bff7fc576
Dmitry Moskalchuk <dmitry@rhomobile.com>
2009-11-15 23:27:31 +0300

    Slight refactoring or jni code (android)

--- platform/android/Rhodes/jni/src/ringtones.cpp
+++ platform/android/Rhodes/jni/src/ringtones.cpp
@@ -6,54 +6,16 @@
 RHO_GLOBAL VALUE ringtone_manager_get_all()
 {
     RHO_LOG_CALLBACK;
-    jclass clsRingtoneManager = getJNIClass(RHODES_JAVA_CLASS_RINGTONE_MANAGER);
-    if (!clsRingtoneManager) return Qnil;
-    jclass clsMap = getJNIClass(RHODES_JAVA_CLASS_MAP);
-    if (!clsMap) return Qnil;
-    jclass clsSet = getJNIClass(RHODES_JAVA_CLASS_SET);
-    if (!clsSet) return Qnil;
-    jclass clsIterator = getJNIClass(RHODES_JAVA_CLASS_ITERATOR);
-    if (!clsIterator) return Qnil;
+    jclass cls = getJNIClass(RHODES_JAVA_CLASS_RINGTONE_MANAGER);
+    if (!cls) return Qnil;
 
-    jmethodID midGetAllRingtones = getJNIClassStaticMethod(clsRingtoneManager, "getAllRingtones", "()Ljava/util/Map;");
-    if (!midGetAllRingtones) return Qnil;
-    jmethodID midGet = getJNIClassMethod(clsMap, "get", "(Ljava/lang/Object;)Ljava/lang/Object;");
-    if (!midGet) return Qnil;
-    jmethodID midKeySet = getJNIClassMethod(clsMap, "keySet", "()Ljava/util/Set;");
-    if (!midKeySet) return Qnil;
-    jmethodID midIterator = getJNIClassMethod(clsSet, "iterator", "()Ljava/util/Iterator;");
-    if (!midIterator) return Qnil;
-    jmethodID midHasNext = getJNIClassMethod(clsIterator, "hasNext", "()Z");
-    if (!midHasNext) return Qnil;
-    jmethodID midNext = getJNIClassMethod(clsIterator, "next", "()Ljava/lang/Object;");
-    if (!midNext) return Qnil;
+    jmethodID mid = getJNIClassStaticMethod(cls, "getAllRingtones", "()Ljava/util/Map;");
+    if (!mid) return Qnil;
 
     JNIEnv *env = jnienv();
-    jobject objMap = env->CallStaticObjectMethod(clsRingtoneManager, midGetAllRingtones);
-    if (!objMap) return Qnil;
-    jobject objSet = env->CallObjectMethod(objMap, midKeySet);
-    if (!objSet) return Qnil;
-    jobject objIterator = env->CallObjectMethod(objSet, midIterator);
-    if (!objIterator) return Qnil;
-
-    VALUE retval = createHash();
-    while(env->CallObjectMethod(objIterator, midHasNext))
-    {
-        jstring objKey = (jstring)env->CallObjectMethod(objIterator, midNext);
-        if (!objKey) return Qnil;
-        jstring objValue = (jstring)env->CallObjectMethod(objMap, midGet, objKey);
-        if (!objValue) return Qnil;
-
-        const char *strKey = env->GetStringUTFChars(objKey, JNI_FALSE);
-        const char *strValue = env->GetStringUTFChars(objValue, JNI_FALSE);
-        addStrToHash(retval, strKey, strValue, strlen(strValue));
-        env->ReleaseStringUTFChars(objKey, strKey);
-        env->ReleaseStringUTFChars(objValue, strValue);
-
-        env->DeleteLocalRef(objKey);
-        env->DeleteLocalRef(objValue);
-    }
-    return retval;
+    jobject obj = env->CallStaticObjectMethod(cls, mid);
+    if (!obj) return Qnil;
+    return convertJavaMapToRubyHash(obj);
 }
 
 RHO_GLOBAL void ringtone_manager_stop()
