a3be11a63c0c02de85a04675385211094ded1f4d
Dmitry Moskalchuk <dm@crystax.net>
2009-11-17 15:48:56 +0300

    Fix issue #1185 - server error msg with undefined method 'show_popup' when import a contact from address book that already exist

--- platform/android/Rhodes/src/com/rhomobile/rhodes/alert/Alert.java
+++ platform/android/Rhodes/src/com/rhomobile/rhodes/alert/Alert.java
@@ -1,24 +1,72 @@
 package com.rhomobile.rhodes.alert;
 
+import java.io.IOException;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.media.MediaPlayer;
+import android.os.Vibrator;
+
 import com.rhomobile.rhodes.Logger;
+import com.rhomobile.rhodes.Rhodes;
+import com.rhomobile.rhodes.RhodesInstance;
 
 public class Alert {
 	
 	private static final String TAG = "Alert";
+	
+	private static class ShowDialog implements Runnable {
+		private AlertDialog.Builder builder;
+		
+		public ShowDialog(AlertDialog.Builder b) {
+			builder = b;
+		}
+
+		public void run() {
+			builder.show();
+		}
+	};
 
 	public static void showPopup(String message) {
-		// TODO: implement
-		Logger.E(TAG, "showPopup: not implemented");
+		try {
+			AlertDialog.Builder builder = new AlertDialog.Builder(RhodesInstance.getInstance());
+			builder.setTitle("Alert");
+			builder.setMessage(message);
+			builder.setCancelable(false);
+			builder.setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+				public void onClick(DialogInterface dialog, int which) {
+					// Do nothing here
+				}					
+			});
+			RhodesInstance.getInstance().uiHandler.post(new ShowDialog(builder));
+		}
+		catch (Exception e) {
+			Logger.E(TAG, e.getMessage());
+		}
 	}
 	
 	public static void vibrate(int duration) {
-		// TODO: implement
-		Logger.E(TAG, "vibrate: not implemented");
+		try {
+			Rhodes instance = RhodesInstance.getInstance();
+			Vibrator vibrator = (Vibrator)instance.getSystemService(Context.VIBRATOR_SERVICE);
+			vibrator.vibrate(duration);
+		}
+		catch (Exception e) {
+			Logger.E(TAG, e.getMessage());
+		}
 	}
 	
 	public static void playFile(String fileName, String mediaType) {
-		// TODO: implement
-		Logger.E(TAG, "playFile: not implemented");
+		try {
+			MediaPlayer mp = new MediaPlayer();
+			String source = RhodesInstance.getInstance().getRootPath() + "/apps/" + fileName;
+			mp.setDataSource(source);
+			mp.prepare();
+			mp.start();
+		} catch (Exception e) {
+			Logger.E(TAG, e.getMessage());
+		}
 	}
 	
 }
