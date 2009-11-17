4a2abf889a114c0cb4782f62493380abbaabaef2
Dmitry Moskalchuk <dm@crystax.net>
2009-11-17 13:56:24 +0300

    Add some logging to alerts

--- platform/android/Rhodes/src/com/rhomobile/rhodes/alert/Alert.java
+++ platform/android/Rhodes/src/com/rhomobile/rhodes/alert/Alert.java
@@ -1,17 +1,24 @@
 package com.rhomobile.rhodes.alert;
 
+import com.rhomobile.rhodes.Logger;
+
 public class Alert {
+	
+	private static final String TAG = "Alert";
 
 	public static void showPopup(String message) {
 		// TODO: implement
+		Logger.E(TAG, "showPopup: not implemented");
 	}
 	
 	public static void vibrate(int duration) {
 		// TODO: implement
+		Logger.E(TAG, "vibrate: not implemented");
 	}
 	
 	public static void playFile(String fileName, String mediaType) {
 		// TODO: implement
+		Logger.E(TAG, "playFile: not implemented");
 	}
 	
 }
