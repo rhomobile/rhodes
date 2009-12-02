5df01ab964a8f2e18c6ef634d17958efa2aabaf2
genywind <evgeny@rhomobile.com>
2009-12-02 11:14:46 +0300

    report erb or rb error file

--- lib/build/compileERB/default.rb
+++ lib/build/compileERB/default.rb
@@ -6,22 +6,27 @@ ext = ".erb"
 
 Find.find(dir) do |path| 
   if File.extname(path) == ext
-    strFile = IO.read(path)
-    strFile.force_encoding('utf-8')
-	rbText = ERB.new( strFile ).src
-	
-	#create iseq
-	
-	seq = __rho_compile(rbText)
-	arr = seq.to_a
-	
-	newName = File.basename(path).sub('.erb','_erb.iseq')
-	fName = File.join(File.dirname(path), newName)
-	fseq = File.new(fName, "w")
-	
-	Marshal.dump(arr,fseq)
-	
-	fseq.close()
+    begin
+        strFile = IO.read(path)
+        strFile.force_encoding('utf-8')
+	    rbText = ERB.new( strFile ).src
+     	
+	    #create iseq
+    	
+	    seq = __rho_compile(rbText)
+	    arr = seq.to_a
+    	
+	    newName = File.basename(path).sub('.erb','_erb.iseq')
+	    fName = File.join(File.dirname(path), newName)
+	    fseq = File.new(fName, "w")
+    	
+	    Marshal.dump(arr,fseq)
+    	
+	    fseq.close()
+	rescue Exception => e
+	    puts 'erb compilation failed: ' + path
+	    raise
+	end 
 	
   end
 end
