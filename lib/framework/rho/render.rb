928a1fc43793903871b01cb2f9eb9294940b4d79
rhomobile <rhomobile@rhomobiles-mac-mini.local>
2009-12-07 16:51:41 -0800

    implement #1261 call render by default. also clear rholog.txt when rake run:iphone is called

--- lib/framework/rho/render.rb
+++ lib/framework/rho/render.rb
@@ -41,9 +41,17 @@ module Rho
 
     def render(options = nil)
       options = {} if options.nil? or !options.is_a?(Hash)
-      options[:action] = :index if options[:action].nil?
-
-      @content = eval_compiled_file(@request[:modelpath]+options[:action].to_s+'_erb.iseq', binding )
+      if options[:action].nil?
+        called_action = @request['action'].nil? ? default_action : @request['action']
+        if File.exist?(@request[:modelpath]+called_action.to_s+'_erb.iseq')
+          @content = eval_compiled_file(@request[:modelpath]+called_action.to_s+'_erb.iseq', binding )
+        else          
+          @content = ""
+        end
+      else
+        @content = eval_compiled_file(@request[:modelpath]+options[:action].to_s+'_erb.iseq', binding )
+      end
+      
       puts 'render content: ' + @content.length.to_s
       if xhr? and options[:use_layout_on_ajax] != true
         options[:layout] = false
@@ -59,7 +67,7 @@ module Rho
 
       RhoController.start_objectnotify()
       @back_action = options[:back] if options[:back]
-
+      @rendered = true
       @content
     end
 
