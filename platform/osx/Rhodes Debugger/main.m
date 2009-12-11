3b05dfe2461610001d5a1ff674fc7bec4518c7a1
rhomobile <rhomobile@rhomobiles-mac-mini.local>
2009-12-10 17:24:24 -0800

    Add rhodes debugger project

---
+++ platform/osx/Rhodes Debugger/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  Rhodes Debugger
+//
+//  Created by rhomobile on 12/9/09.
+//  Copyright __MyCompanyName__ 2009. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc,  (const char **) argv);
+}
