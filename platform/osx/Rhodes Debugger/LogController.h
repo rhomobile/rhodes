08030fa83ab4271c9c9fe252f69d52b1f03db192
rhomobile <rhomobile@rhomobiles-mac-mini.local>
2009-12-10 17:24:24 -0800

    Add rhodes debugger project

---
+++ platform/osx/Rhodes Debugger/LogController.h
@@ -0,0 +1,58 @@
+//
+//  LogController.h
+//
+
+//	Purpose: A LogController is used to execute a task and have the output displayed in an
+//	NSTextView. The best way to do this is to instantiate a LogController object in Interface 
+//	Builder and create the following connections:
+//		LogController* IBOutlet in your app's controller -> Instantiated object
+//		Instantiated object's outputTextView -> an NSTextView in your app's window
+//		Instantiated object's delegate -> your app's controller
+//
+
+#import <Cocoa/Cocoa.h>
+
+// Tell the compiler about what our delegate could possibly implement.
+// If you don't do this, you get compiler warnings when you try to call the delegate methods.
+@protocol LogControllerDelegate 
+@optional
+- (void) logControllerTaskDidFinish:(id)sender;
+- (NSAttributedString *) logController:(id)sender willAppendNewString:(NSString *)plainString;
+@end 
+
+@interface LogController : NSObject {
+	
+	IBOutlet NSTextView *outputTextView;
+	// Delegate implements the LogControllerDelegate protocol
+	IBOutlet NSObject <LogControllerDelegate> *delegate;
+	
+	NSTask *task;
+	NSPipe *pipe;
+	NSPipe *inpipe;
+	bool isWaiting, finished;
+	NSFileHandle *stdoutFileHandle;
+	NSFileHandle *stdinFileHandle;
+	NSDictionary *defaultStringAttributes;
+	NSString *waitString;
+}
+@property(retain) NSTextView *outputTextView;
+@property(assign) NSObject *delegate;
+@property(retain) NSTask *task;
+@property(retain) NSPipe *pipe;
+@property(retain) NSPipe *inpipe;
+@property(assign) bool isWaiting;
+@property(assign) bool finished;
+@property(retain) NSFileHandle *stdoutFileHandle;
+@property(retain) NSFileHandle *stdinFileHandle;
+@property(retain) NSDictionary *defaultStringAttributes;
+@property(retain) NSString *waitString;
+
+-(void) launchTask;
+-(void) taskFinished:(NSNotification *)notification;
+-(void) gotData:(NSNotification *)notification;
+-(void) appendString:(NSString *)stringData;
+-(void) appendAttributedString:(NSAttributedString *)stringData;
+-(void) clearTextView;
+-(void)sendCmd:(NSString *)cmd;
+
+@end
---
+++ platform/osx/Rhodes Debugger/LogController.m
@@ -0,0 +1,175 @@
+//
+//  LogController.m
+//
+//
+
+#import "LogController.h"
+
+@implementation LogController
+@synthesize delegate;
+@synthesize outputTextView;
+@synthesize task;
+@synthesize pipe;
+@synthesize inpipe;
+@synthesize stdoutFileHandle;
+@synthesize stdinFileHandle;
+@synthesize defaultStringAttributes;
+@synthesize isWaiting;
+@synthesize finished;
+@synthesize waitString;
+
+- (id) init
+{
+	self = [super init];
+	if (self != nil) {
+		[self setTask:nil];
+		[self setPipe:nil];
+		[self setStdoutFileHandle:nil];
+		[self setDefaultStringAttributes:nil];
+		[self setWaitString:@""];
+	}
+	return self;
+}
+
+- (void) dealloc
+{
+	[self setTask:nil];
+	[self setPipe:nil];
+	[self setStdoutFileHandle:nil];
+	[self setDefaultStringAttributes:nil];
+	[super dealloc];
+}
+
+-(void) launchTask
+{
+	[self setPipe:[NSPipe pipe]];
+	[self setInpipe:[NSPipe pipe]];
+	
+	// Catch the stdout in our pipe
+	[self setStdoutFileHandle:[pipe fileHandleForReading]];
+	[self setStdinFileHandle:[inpipe fileHandleForWriting]];
+	[task setStandardOutput:pipe];
+	[task setStandardError:pipe];
+	[task setStandardInput:inpipe];
+	
+	[stdoutFileHandle readInBackgroundAndNotify];
+	
+	// Get a notification when the task ends
+	[[NSNotificationCenter defaultCenter]
+	 addObserver: self
+	 selector: @selector(taskFinished:)
+	 name: NSTaskDidTerminateNotification
+	 object: task];
+	
+	// Listen for notifications from readInBackgroundAndNotify
+	[[NSNotificationCenter defaultCenter]
+	 addObserver: self
+	 selector: @selector(gotData:)
+	 name: NSFileHandleReadCompletionNotification
+	 object: stdoutFileHandle];
+	
+	// Clear the output text view
+	[[[outputTextView textStorage] mutableString] setString:@""];
+	
+	[outputTextView setEditable:false];
+	[self appendString:@"Execution Started\n\n"];
+	
+	finished = false;
+	// Run!
+    [task launch];
+}
+
+- (void)taskFinished:(NSNotification *)notification
+{
+	// We're done - stop listening for notifications
+	
+	[[NSNotificationCenter defaultCenter] removeObserver: self
+													name: NSFileHandleReadCompletionNotification
+												  object: stdoutFileHandle];
+    [[NSNotificationCenter defaultCenter] removeObserver: self
+													name: NSTaskDidTerminateNotification
+												  object: task];
+	if ( delegate && [delegate respondsToSelector:@selector(logControllerTaskDidFinish:)] ) {
+		[delegate logControllerTaskDidFinish:self];
+	}
+	[self appendString:@"\nExecution has terminated\n"];
+	[self setFinished:true];
+	[self setIsWaiting:false];
+
+}
+
+- (void)gotData: (NSNotification *)inNotification
+{
+	NSDictionary	*userInfoDictionary=NULL;
+	NSData			*data=NULL;
+	NSString		*stringData=NULL;
+	
+	userInfoDictionary = [inNotification userInfo];
+	require(userInfoDictionary != NULL, exit);
+	
+	data = [userInfoDictionary objectForKey: NSFileHandleNotificationDataItem];
+	require(data != NULL, exit);
+	
+	stringData = [[NSString alloc] initWithData: data encoding: NSUTF8StringEncoding];
+	require(stringData != NULL, exit);
+	
+	// We got some data - append it to the end of the master script output
+	//[self appendString:@"###"];
+	[self appendString:stringData];
+	
+	if ([[self waitString] length] > 0) {
+		if ([stringData rangeOfString:[self waitString]].location != NSNotFound) {
+			isWaiting = true;
+		}
+	}	
+exit:
+	
+	// Continue to listen for more data
+	[stdoutFileHandle readInBackgroundAndNotify];
+	
+	if (stringData != NULL)
+	{
+		[stringData release];
+	}
+}
+
+-(void)appendString:(NSString *)stringData
+{
+	NSAttributedString *attribString;
+	if ( delegate && [delegate respondsToSelector:@selector(logController:willAppendNewString:)] ) {
+		attribString = [delegate logController:self willAppendNewString:stringData];
+	} else if (defaultStringAttributes) {
+		attribString=[[[NSAttributedString alloc] initWithString:stringData attributes:defaultStringAttributes] autorelease];
+	} else {
+		attribString=[[[NSAttributedString alloc] initWithString:stringData] autorelease];
+	}
+	
+	[self appendAttributedString:attribString];
+}
+
+-(void)appendAttributedString:(NSAttributedString *)stringData
+{
+	if( !stringData || [[stringData string] isEqualToString:@""])
+		return;
+	
+	[[outputTextView textStorage] beginEditing];
+	[[outputTextView textStorage] appendAttributedString:stringData];
+	[[outputTextView textStorage] endEditing];
+	[outputTextView scrollRangeToVisible:NSMakeRange([[outputTextView textStorage] length],0)];
+}
+
+-(void)clearTextView
+{
+	NSAttributedString *attribString=[[NSAttributedString alloc] initWithString:@""];
+	[[outputTextView textStorage] setAttributedString:attribString];
+	[attribString release];
+}
+
+-(void)sendCmd:(NSString *)cmd {
+	cmd = [cmd stringByAppendingString:@"\n"];
+	while (! [self isWaiting] ) { [[NSRunLoop currentRunLoop] runUntilDate:[NSDate dateWithTimeIntervalSinceNow:0.5]]; }
+	[self setIsWaiting:false];
+	[self appendString:cmd];
+	[[self stdinFileHandle] writeData:[cmd dataUsingEncoding:NSASCIIStringEncoding]];
+}
+@end
