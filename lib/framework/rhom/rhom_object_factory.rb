94d273ec288674a84d28f7378fd0dbff38796651
genywind <evgeny@rhomobile.com>
2009-12-12 00:19:19 +0300

    add error check to backend_refresh_time

--- lib/framework/rhom/rhom_object_factory.rb
+++ lib/framework/rhom/rhom_object_factory.rb
@@ -85,7 +85,12 @@ module Rhom
 
                 def backend_refresh_time
                   result = ::Rhom::RhomDbAdapter::select_from_table('sources', 'backend_refresh_time', {"source_id"=>get_source_id} )
-                  Time.at(result[0]['backend_refresh_time'].to_i)
+                  nTime = 0
+                  if result && result.length > 0 && result[0]['backend_refresh_time']
+                    nTime = result[0]['backend_refresh_time'].to_i
+                  end
+                    
+                  Time.at(nTime)
                 end
               
                 def get_source_id
