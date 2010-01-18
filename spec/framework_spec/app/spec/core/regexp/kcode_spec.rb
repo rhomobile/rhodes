e4c80f8c1dca98525fba12ae8a83b39ee459052b
Brian Moore <brian@Brian-Moores-MacBook-Pro.local>
2010-01-18 14:25:00 -0800

    Fixing more tests

--- spec/framework_spec/app/spec/core/regexp/kcode_spec.rb
+++ spec/framework_spec/app/spec/core/regexp/kcode_spec.rb
@@ -1,15 +1,16 @@
-require File.dirname(File.join(__rhoGetCurrentDir(), __FILE__)) + '/../../spec_helper'
-
-describe "Regexp#kcode" do
-  it "returns the character set code" do
-    default = /f.(o)/.kcode
-    default.should_not == 'sjis'
-    default.should_not == 'euc'
-    default.should_not == 'utf8'
-
-    /ab+c/s.kcode.should == "sjis"
-    /a(.)+s/n.kcode.should == "none"
-    /xyz/e.kcode.should == "euc"
-    /cars/u.kcode.should == "utf8"
-  end
-end
+# XXX no kcode in ruby 1.9
+#require File.dirname(File.join(__rhoGetCurrentDir(), __FILE__)) + '/../../spec_helper'
+#
+#describe "Regexp#kcode" do
+#  it "returns the character set code" do
+#    default = /f.(o)/.kcode
+#    default.should_not == 'sjis'
+#    default.should_not == 'euc'
+#    default.should_not == 'utf8'
+#
+#    /ab+c/s.kcode.should == "sjis"
+#    /a(.)+s/n.kcode.should == "none"
+#    /xyz/e.kcode.should == "euc"
+#    /cars/u.kcode.should == "utf8"
+#  end
+#end
