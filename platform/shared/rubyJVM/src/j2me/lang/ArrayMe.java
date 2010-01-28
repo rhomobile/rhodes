ad30914779965f61fc576452ded009f4d6d0ed04
genywind <evgeny@rhomobile.com>
2010-01-29 00:29:37 +0300

    add more info to not impl

--- platform/shared/rubyJVM/src/j2me/lang/ArrayMe.java
+++ platform/shared/rubyJVM/src/j2me/lang/ArrayMe.java
@@ -5,19 +5,19 @@ public class ArrayMe {
     public static int[] clone(int[] arThis)
     {
     	//TODO: clone
-    	throw new RuntimeException("Not Implemented");
+    	throw new RuntimeException("Not Implemented - ArrayMe::int[] clone(int[] arThis)");
     	//return arThis;
     }
     public static byte[] clone(byte[] arThis)
     {
     	//TODO: clone
-    	throw new RuntimeException("Not Implemented");
+    	throw new RuntimeException("Not Implemented - ArrayMe::byte[] clone(byte[] arThis)");
     	//return arThis;
     }
     public static char[] clone(char[] arThis)
     {
     	//TODO: clone
-    	throw new RuntimeException("Not Implemented");
+    	throw new RuntimeException("Not Implemented - ArrayMe::char[] clone(char[] arThis)");
     	//return arThis;
     }
     public static Object[] clone(Object[] arThis)
