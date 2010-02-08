9fe6d6a67ec410051d0960be8ac6bfbc17b9c6be
Brian Moore <brian@Brian-Moores-MacBook-Pro.local>
2010-02-08 09:57:35 -0800

    fix rubylinux

--- platform/shared/ruby/load.c
+++ platform/shared/ruby/load.c
@@ -431,8 +431,11 @@ VALUE
 rb_f_require(VALUE obj, VALUE fname)
 {
     //RHO
+    #ifndef RUBYLINUX
     return rb_require_compiled(obj, fname);
-    //return rb_require_safe(fname, rb_safe_level());
+    #else
+    return rb_require_safe(fname, rb_safe_level());
+    #endif
     //RHO END
 }
 
@@ -610,8 +613,11 @@ rb_require(const char *fname)
     VALUE fn = rb_str_new2(fname);
     OBJ_FREEZE(fn);
     // RHO
+    #ifndef RUBYLINUX
     return rb_require_compiled(Qnil, fn);
-    //return rb_require_safe(fn, rb_safe_level());
+    #else
+    return rb_require_safe(fn, rb_safe_level());
+    #endif
     // RHO END
 }
 
