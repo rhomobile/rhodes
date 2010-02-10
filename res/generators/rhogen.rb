4bd3fc5e79311bac98ac5f0bae2d8692ad2a8146
lars <larsburgess@gmail.com>
2010-02-10 11:46:58 -0800

    adding sync priority option to model generator

--- res/generators/rhogen.rb
+++ res/generators/rhogen.rb
@@ -117,6 +117,7 @@ module Rhogen
         attributes  - list of one or more string attributes (i.e. name,industry,progress), NO spaces between attributes
         
       Optional:
+        priority    - optional sync priority (i.e. 100)
         type        - optional type (i.e. "ask" for an ask model) 
     DESC
 
@@ -124,7 +125,8 @@ module Rhogen
 
     first_argument :name, :required => true, :desc => "model name"
     second_argument :attributes, :as => :array, :required => true, :desc => "list of one or more string attributes (i.e. name,industry,progress), NO spaces between attributes"
-    third_argument :type, :required => false, :desc => "optional type (i.e. \"ask\" for an ask model)"
+    third_argument :priority, :required => false, :desc => "optional sync priority (i.e. \"100\")"
+    fourth_argument :type, :required => false, :desc => "optional type (i.e. \"ask\" for an ask model)"
 
     template :config do |template|
       @model_sync_server = syncserver_exists? ? class_name : ''
