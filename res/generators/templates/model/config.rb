fe552323bc455e9b750dcd2ce8a8b60350699b39
lars <larsburgess@gmail.com>
2010-02-10 11:46:58 -0800

    adding sync priority option to model generator

--- res/generators/templates/model/config.rb
+++ res/generators/templates/model/config.rb
@@ -1,3 +1,3 @@
 require 'rho'
 
-Rho::RhoConfig::add_source("<%=class_name%>", {"url"=>"<%=@model_sync_server%>", <% if type and type == "ask" %>, "type"=>"ask"<%end%>})
+Rho::RhoConfig::add_source("<%=class_name%>", {"url"=>"<%=@model_sync_server%>"<% if type and type == 'ask' %>, ", "type"=>"ask"<%end%><% if priority %>, "priority"=><%=priority%><%end%>})
