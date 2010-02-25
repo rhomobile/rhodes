fd0a96b11831ae71f89de78de650f827f416abac
Dmitry Moskalchuk <dmitry@rhomobile.com>
2010-02-25 22:08:01 +0300

    Fix Java warnings

--- platform/android/Rhodes/src/com/rhomobile/rhodes/mapview/AnnotationsOverlay.java
+++ platform/android/Rhodes/src/com/rhomobile/rhodes/mapview/AnnotationsOverlay.java
@@ -7,7 +7,6 @@ import android.graphics.drawable.Drawable;
 import com.google.android.maps.GeoPoint;
 import com.google.android.maps.ItemizedOverlay;
 import com.google.android.maps.OverlayItem;
-import com.google.android.maps.Projection;
 import com.rhomobile.rhodes.WebView;
 
 public class AnnotationsOverlay extends ItemizedOverlay<OverlayItem> {
