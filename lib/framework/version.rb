87c62ea03f675c66b56eb4099ba5b4833305f011
lars <larsburgess@gmail.com>
2010-02-24 16:27:14 -0800

    prepping for 1.4.2 tag

--- lib/framework/version.rb
+++ lib/framework/version.rb
@@ -1,8 +1,8 @@
 module RhodesFramework
   unless defined? RhodesFramework::VERSION
-    VERSION = '1.4.1'
+    VERSION = '1.4.2'
   end
   unless defined? RhodesFramework::DBVERSION
-    DBVERSION = '1.4.1'
+    DBVERSION = '1.4.2'
   end
 end
--- lib/rhodes.rb
+++ lib/rhodes.rb
@@ -1,9 +1,9 @@
 module Rhodes
   unless defined? Rhodes::VERSION
-    VERSION = '1.4.1'
+    VERSION = '1.4.2'
   end
   unless defined? Rhodes::DBVERSION
-    DBVERSION = '1.4.1'
+    DBVERSION = '1.4.2'
   end
   
 end
