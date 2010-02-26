aabe3cf58629ebde6667c9dfb56479cf539e0b4f
genywind <evgeny@rhomobile.com>
2010-02-26 10:38:12 +0300

    check db version in db_migrate

--- platform/shared/db/DBAdapter.cpp
+++ platform/shared/db/DBAdapter.cpp
@@ -108,8 +108,8 @@ void CDBAdapter::open (String strDbPath, String strVer, boolean bTemp)
 boolean CDBAdapter::migrateDB(const CDBVersion& dbVer, const String& strRhoDBVer, const String& strAppDBVer )
 {
     LOG(INFO) + "Try migrate database from " + dbVer.m_strRhoVer + " to " + strRhoDBVer;
-    //1.2.2 -> 1.5.0,1.4.1
-    if ( (dbVer.m_strRhoVer.compare("1.2.2") == 0 || dbVer.m_strRhoVer.compare("1.2.4") == 0)&& (strRhoDBVer.compare("1.5.0")==0||strRhoDBVer.compare("1.4.1")==0) )
+    //1.2.x -> 1.5.x,1.4.x
+    if ( (dbVer.m_strRhoVer.find("1.2") == 0)&& (strRhoDBVer.find("1.5")==0||strRhoDBVer.find("1.4")==0) )
     {
     //sources
     //priority INTEGER, ADD
