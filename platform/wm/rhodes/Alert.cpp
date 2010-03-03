57beb43c58ad58b55b158b9e14104d1604a0c0cc
Igor Tarasov <igor@rhomobile.com>
2010-03-03 17:08:38 +0300

    WM: Alerts refactoring;

---
+++ platform/wm/rhodes/Alert.cpp
@@ -0,0 +1,72 @@
+#include "stdafx.h"
+
+#if defined(_WIN32_WCE)
+#include <soundfile.h>
+#endif
+
+#include <common/RhodesApp.h>
+
+#include "Alert.h"
+#include "MainWindow.h"
+#include "Vibrate.h"
+
+extern "C" HWND getMainWnd();
+
+IMPLEMENT_LOGCLASS(CAlert, "Alert");
+
+void CAlert::showPopup(String message)
+{
+    HWND main_wnd = getMainWnd();
+	::PostMessage(main_wnd, WM_ALERT_SHOWPOPUP, 0,
+                  (LPARAM )strdup(message.c_str()));
+}
+
+#if defined(_WIN32_WCE)
+void CAlert::vibrate()
+{
+    CVibrate::getCVibrate().toggle();
+}
+
+void CAlert::playFile(String fileName)
+{
+    rho::String path = RHODESAPP().getRhoRootPath() + "apps" + fileName;
+    HSOUND hSound;
+    
+    rho::String::size_type pos = 0;
+    while ( (pos = path.find('/', pos)) != rho::String::npos ) {
+        path.replace( pos, 1, "\\");
+        pos++;
+    }
+
+    USES_CONVERSION;
+    //SndPlaySync(A2T(path.c_str()),  SND_PLAY_IGNOREUSERSETTINGS);
+    HRESULT hr = SndOpen(A2T(path.c_str()), &hSound);
+    hr = SndPlayAsync (hSound, 0);
+      
+    if (hr != S_OK) {
+        LOG(WARNING) + "OnAlertPlayFile: failed to play file"; 
+    }
+    
+    WaitForSingleObject(hSound, INFINITE);
+                        
+    hr = SndClose(hSound);
+    SndStop(SND_SCOPE_PROCESS, NULL);
+}
+
+#endif //_WIN32_WCE
+
+extern "C" void alert_show_popup(char* message) {
+    CAlert::showPopup(message);
+}
+
+extern "C" void alert_vibrate(void*) {
+#if defined(_WIN32_WCE)
+    CAlert::vibrate();
+#endif
+}
+
+extern "C" void alert_play_file(char* file_name, ...) {
+#if defined(_WIN32_WCE)
+    CAlert::playFile(file_name);
+#endif
+}
---
+++ platform/wm/rhodes/Vibrate.h
@@ -0,0 +1,35 @@
+#ifndef __VIBRATE__H__
+#define __VIBRATE__H__
+
+#include <logging/RhoLog.h>
+#include <common/RhoThread.h>
+
+using namespace rho;
+using namespace rho::common;
+
+class CVibrate :  public CRhoThread
+{
+    DEFINE_LOGCLASS;
+    
+  public:
+    static CVibrate& getCVibrate();
+    
+    void toggle ();
+    void toggle (int seconds);
+    void untoggle ();
+    
+  private:
+    CVibrate (IRhoClassFactory* factory);
+    ~CVibrate ();
+    
+    virtual void run();
+    
+    int m_nDuration;
+    bool m_bToggled;
+    
+    static void createCVibrate();
+    static CVibrate *m_pInstance;    
+    static CMutex m_mxLocker;
+};
+
+#endif // __VIBRATE__H__
