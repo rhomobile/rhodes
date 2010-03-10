c732ca28835347161f5829cb484765e30f51af3d
Dmitry Moskalchuk <dmitry@rhomobile.com>
2010-03-10 14:49:52 +0300

    Support cookies for AsyncHttp for iPhone and Android

--- platform/shared/net/CURLNetRequest.cpp
+++ platform/shared/net/CURLNetRequest.cpp
@@ -23,6 +23,7 @@ class CURLNetResponseImpl : public INetResponse
 {
     String m_data;
     int   m_nRespCode;
+    String m_cookies;
 
 public:
     CURLNetResponseImpl(String const &data, int nRespCode) : m_data(data), m_nRespCode(nRespCode){}
@@ -63,6 +64,16 @@ public:
     {
         m_data = data;
     }
+    
+    void setCookies(String s)
+    {
+        m_cookies = s;
+    }
+    
+    String getCookies()
+    {
+        return m_cookies;
+    }
 };
 
 CURLNetRequest::CURLNetRequest()
@@ -79,6 +90,31 @@ CURLNetRequest::~CURLNetRequest()
 	curl_multi_cleanup(curlm);
 }
 
+INetResponse *CURLNetRequest::makeResponse(String strBody, int nErrorCode)
+{
+    std::auto_ptr<CURLNetResponseImpl> resp(new CURLNetResponseImpl(strBody, nErrorCode));
+    if (resp->isOK())
+        resp->setCookies(makeCookies());
+    return resp.release();
+}
+
+static size_t curlBodyFileCallback(void *ptr, size_t size, size_t nmemb, void *opaque)
+{
+    common::CRhoFile *pFile = (common::CRhoFile*)opaque;
+    size_t nBytes = size*nmemb;
+    RAWTRACE1("Received %d bytes (blob)", nBytes);
+    return pFile->write(ptr, nBytes);
+}
+
+static size_t curlBodyStringCallback(void *ptr, size_t size, size_t nmemb, void *opaque)
+{
+    String *pStr = (String *)opaque;
+    size_t nBytes = size*nmemb;
+    RAWTRACE1("Received %d bytes (string)", nBytes);
+    pStr->append((const char *)ptr, nBytes);
+    return nBytes;
+}    
+
 INetResponse* CURLNetRequest::pullData(const String& strUrl, IRhoSession* oSession )
 {
     return doRequest("GET", strUrl, String(), oSession, null );
@@ -93,21 +129,9 @@ INetResponse* CURLNetRequest::pullCookies(const String& strUrl, const String& st
 {
 	INetResponse* pResp = doRequest("POST", strUrl, strBody, oSession, null );
 	if ( pResp->isOK() )
-	{
-		String strCookie = makeCookies();
-		((CURLNetResponseImpl*)pResp)->setCharData(strCookie);
-	}
+		((CURLNetResponseImpl*)pResp)->setCharData(pResp->getCookies());
 		
 	return pResp;
-}
-
-static size_t curlBodyStringCallback(void *ptr, size_t size, size_t nmemb, void *opaque)
-{
-	String *pStr = (String *)opaque;
-	size_t nBytes = size*nmemb;
-	RAWTRACE1("Received %d bytes (string)", nBytes);
-	pStr->append((const char *)ptr, nBytes);
-	return nBytes;
 }
 	
 INetResponse* CURLNetRequest::doRequest( const char* method, const String& strUrl, const String& strBody, IRhoSession* oSession, Hashtable<String,String>* pHeaders )
@@ -142,7 +166,7 @@ INetResponse* CURLNetRequest::doRequest( const char* method, const String& strUr
         RAWTRACE("END RESPONSE-----");
 	}
 	
-    return new CURLNetResponseImpl(strRespBody,nRespCode);
+    return makeResponse(strRespBody, nRespCode);
 }
 
 INetResponse* CURLNetRequest::pushFile(const String& strUrl, const String& strFilePath, IRhoSession* oSession, Hashtable<String,String>* pHeaders)
@@ -183,15 +207,7 @@ INetResponse* CURLNetRequest::pushFile(const String& strUrl, const String& strFi
     rho_net_impl_network_indicator(0);
     
 	nRespCode = getResponseCode(err, strRespBody, oSession);
-    return new CURLNetResponseImpl(strRespBody, nRespCode);
-}
-
-static size_t curlBodyFileCallback(void *ptr, size_t size, size_t nmemb, void *opaque)
-{
-	common::CRhoFile *pFile = (common::CRhoFile*)opaque;
-	size_t nBytes = size*nmemb;
-	RAWTRACE1("Received %d bytes (blob)", nBytes);
-	return pFile->write(ptr, nBytes);
+    return makeResponse(strRespBody, nRespCode);
 }
 	
 INetResponse* CURLNetRequest::pullFile(const String& strUrl, const String& strFilePath, IRhoSession* oSession, Hashtable<String,String>* pHeaders)
@@ -221,7 +237,7 @@ INetResponse* CURLNetRequest::pullFile(const String& strUrl, const String& strFi
     rho_net_impl_network_indicator(0);
 	
 	nRespCode = getResponseCode(err, strRespBody, oSession );
-    return new CURLNetResponseImpl(strRespBody, nRespCode);
+    return makeResponse(strRespBody, nRespCode);
 }
 	
 int CURLNetRequest::getResponseCode(CURLMcode err, const String& strRespBody, IRhoSession* oSession )	
@@ -262,193 +278,194 @@ String CURLNetRequest::makeCookies()
 	String cookies;
 	curl_slist *rcookies = NULL;
 	if (curl_easy_getinfo(curl, CURLINFO_COOKIELIST, &rcookies) != 0) {
-		RAWLOG_ERROR1("Error when retrieving cookies: %s", errbuf);
-	}
-	else {
-		for(curl_slist *cookie = rcookies; cookie; cookie = cookie->next) {
-			char *data = cookie->data;
-			
-			// Parse cookie which is in Netscape format:
-			// domain <Tab> tailmatch <Tab> path <Tab> secure <Tab> expires <Tab> name <Tab> value
-			char *s = data;
-            
-			// Skip 'domain'
-			for (; *s == '\t'; ++s);
-			for (; *s != '\t' && *s != '\0'; ++s);
-			if (*s == '\0') continue;
-            
-			// Skip 'tailmatch'
-			for (; *s == '\t'; ++s);
-			for (; *s != '\t' && *s != '\0'; ++s);
-			if (*s == '\0') continue;
-            
-			// Skip 'path'
-			for (; *s == '\t'; ++s);
-			for (; *s != '\t' && *s != '\0'; ++s);
-			if (*s == '\0') continue;
-            
-			// Skip 'secure'
-			for (; *s == '\t'; ++s);
-			for (; *s != '\t' && *s != '\0'; ++s);
-			if (*s == '\0') continue;
-			
-			// Skip 'expires'
-			for (; *s == '\t'; ++s);
-			for (; *s != '\t' && *s != '\0'; ++s);
-			if (*s == '\0') continue;
-			
-			// Parse 'name'
-			for (; *s == '\t'; ++s);
-			char *name_start = s;
-			for (; *s != '\t' && *s != '\0'; ++s);
-			if (*s == '\0') continue;
-			char *name_finish = s;
-			String name(name_start, name_finish);
-			
-			// Parse 'value'
-			for (; *s == '\t'; ++s);
-			char *value = s;
-			
-			cookies += name;
-			cookies += "=";
-			cookies += value;
-			cookies += ";";
-		}
-		curl_slist_free_all(rcookies);
-	}
+        // No cookies
+        RAWTRACE("No cookies");
+        return cookies;
+    }
+	
+    for(curl_slist *cookie = rcookies; cookie; cookie = cookie->next) {
+        char *data = cookie->data;
+        
+        // Parse cookie which is in Netscape format:
+        // domain <Tab> tailmatch <Tab> path <Tab> secure <Tab> expires <Tab> name <Tab> value
+        char *s = data;
+        
+        // Skip 'domain'
+        for (; *s == '\t'; ++s);
+        for (; *s != '\t' && *s != '\0'; ++s);
+        if (*s == '\0') continue;
+        
+        // Skip 'tailmatch'
+        for (; *s == '\t'; ++s);
+        for (; *s != '\t' && *s != '\0'; ++s);
+        if (*s == '\0') continue;
+        
+        // Skip 'path'
+        for (; *s == '\t'; ++s);
+        for (; *s != '\t' && *s != '\0'; ++s);
+        if (*s == '\0') continue;
+        
+        // Skip 'secure'
+        for (; *s == '\t'; ++s);
+        for (; *s != '\t' && *s != '\0'; ++s);
+        if (*s == '\0') continue;
+        
+        // Skip 'expires'
+        for (; *s == '\t'; ++s);
+        for (; *s != '\t' && *s != '\0'; ++s);
+        if (*s == '\0') continue;
+        
+        // Parse 'name'
+        for (; *s == '\t'; ++s);
+        char *name_start = s;
+        for (; *s != '\t' && *s != '\0'; ++s);
+        if (*s == '\0') continue;
+        char *name_finish = s;
+        String name(name_start, name_finish);
+        
+        // Parse 'value'
+        for (; *s == '\t'; ++s);
+        char *value = s;
+        
+        cookies += name;
+        cookies += "=";
+        cookies += value;
+        cookies += ";";
+    }
+    curl_slist_free_all(rcookies);
 	
 	return cookies;
 }
-	
-	static int curl_trace(CURL *curl, curl_infotype type, char *data, size_t size, void *opaque)
-	{
-		const char *text = "";
-		switch (type) {
-			case CURLINFO_TEXT:         text = "== Info"; break;
-			case CURLINFO_HEADER_IN:    text = "<= Recv headers"; break;
-			case CURLINFO_HEADER_OUT:   text = "=> Send headers"; break;
-			case CURLINFO_DATA_IN:      text = "<= Recv data"; break;
-			case CURLINFO_DATA_OUT:     text = "=> Send data"; break;
-			case CURLINFO_SSL_DATA_IN:  text = "<= Recv SSL data"; break;
-			case CURLINFO_SSL_DATA_OUT: text = "=> Send SSL data"; break;
-			default: return 0;
-		}
-		
-		String strData(data, size);
-		RAWLOG_INFO3("%s (%d bytes): %s", text, size, strData.c_str());
-		return 0;
-	}
 
-	static size_t curlHeaderCallback(void *ptr, size_t size, size_t nmemb, void *opaque)
-	{
-		Hashtable<String,String>* pHeaders = (Hashtable<String,String>*)opaque;
-		size_t nBytes = size*nmemb;
-		String strHeader((const char *)ptr, nBytes);
-		RAWTRACE1("Received header: %s", strHeader.c_str());
-		
-        int nSep = strHeader.find(':');
-        if (nSep > 0 )
-		{		
-			String strName = trim(strHeader.substr(0, nSep));
-			String strValue = trim(strHeader.substr(nSep+1, strHeader.length() - (nSep+3) ));
-		
-			pHeaders->put(strName, strValue);
-		}
-		
-		return nBytes;
-	}
-	
-	curl_slist *set_curl_options(bool trace, CURL *curl, const char *method, const String& strUrl, IRhoSession* pSession, Hashtable<String,String>* pHeaders)
-	{
-		curl_easy_reset(curl);
-		if (strcasecmp(method, "GET") == 0)
-			curl_easy_setopt(curl, CURLOPT_HTTPGET, 1);
-		else if (strcasecmp(method, "POST") == 0)
-			curl_easy_setopt(curl, CURLOPT_POST, 1);
-		curl_easy_setopt(curl, CURLOPT_URL, strUrl.c_str());
-		
-		// Just to enable cookie parser
-		curl_easy_setopt(curl, CURLOPT_COOKIEFILE, "");
-		// It will clear all stored cookies
-		curl_easy_setopt(curl, CURLOPT_COOKIELIST, "ALL");
-		String session;
-		if (pSession)
-			session = pSession->getSession();
-		
-		if (!session.empty()) {
-			//RAWTRACE1("Set cookie: %s", session.c_str());
-			curl_easy_setopt(curl, CURLOPT_COOKIE, session.c_str());
-		}
-		
-		curl_easy_setopt(curl, CURLOPT_TIMEOUT, 180);
-		curl_easy_setopt(curl, CURLOPT_TCP_NODELAY, 1);
-		
-		// Set very large timeout in case of local requests
-		// It is required because otherwise requests (especially requests to writing!)
-		// could repeated twice or more times
-		if (net::URI::isLocalHost(strUrl))
-			curl_easy_setopt(curl, CURLOPT_TIMEOUT, 10*24*60*60);
-		
-		curl_slist *hdrs = NULL;
-		// Disable "Expect: 100-continue"
-		hdrs = curl_slist_append(hdrs, "Expect:");
-		// Add Keep-Alive header
-		hdrs = curl_slist_append(hdrs, "Connection: Keep-Alive");
-		if (pHeaders)
-		{
-			for ( Hashtable<String,String>::iterator it = pHeaders->begin();  it != pHeaders->end(); ++it )
-			{
-				String strHeader = it->first + ":" + it->second;
-				hdrs = curl_slist_append(hdrs, strHeader.c_str());
-				curl_easy_setopt(curl, CURLOPT_HTTPHEADER, hdrs);
-			}
-			
-			//set header callback
-			curl_easy_setopt(curl, CURLOPT_HEADERDATA, pHeaders);
-			curl_easy_setopt(curl, CURLOPT_HEADERFUNCTION, &curlHeaderCallback);
-			
-		}
-		
-		curl_easy_setopt(curl, CURLOPT_HTTPHEADER, hdrs);
-		
-		if (trace) {
-			curl_easy_setopt(curl, CURLOPT_DEBUGFUNCTION, &curl_trace);
-			curl_easy_setopt(curl, CURLOPT_DEBUGDATA, NULL);
-			curl_easy_setopt(curl, CURLOPT_VERBOSE, 1L);
-		}
-		
-		return hdrs;
-	}
-	
-	CURLMcode do_curl_perform(CURLM *curlm, CURL *curl)
-	{
-		int running = 0;
-		curl_multi_add_handle(curlm, curl);
-		CURLMcode err;
-		for(;;) {
-			err = curl_multi_perform(curlm, &running);
-			if (err == CURLM_CALL_MULTI_PERFORM)
-				continue;
-			if (err == CURLM_OK && running > 0) {
-				//RAWTRACE("curl_multi_perform returns OK, but we still have active transfers");
-				fd_set rfd, wfd, efd;
-				int n = 0;
-				FD_ZERO(&rfd);
-				FD_ZERO(&wfd);
-				FD_ZERO(&efd);
-				curl_multi_fdset(curlm, &rfd, &wfd, &efd, &n);
-				if (n < 0) n = 0;
-				timeval tv;
-				tv.tv_sec = 0;
-				tv.tv_usec = 100000;
-				select(n, &rfd, &wfd, &efd, &tv);
-				continue;
-			}
-			break;
-		}
-		curl_multi_remove_handle(curlm, curl);
-		return err;
-	}
+static int curl_trace(CURL *curl, curl_infotype type, char *data, size_t size, void *opaque)
+{
+    const char *text = "";
+    switch (type) {
+        case CURLINFO_TEXT:         text = "== Info"; break;
+        case CURLINFO_HEADER_IN:    text = "<= Recv headers"; break;
+        case CURLINFO_HEADER_OUT:   text = "=> Send headers"; break;
+        case CURLINFO_DATA_IN:      text = "<= Recv data"; break;
+        case CURLINFO_DATA_OUT:     text = "=> Send data"; break;
+        case CURLINFO_SSL_DATA_IN:  text = "<= Recv SSL data"; break;
+        case CURLINFO_SSL_DATA_OUT: text = "=> Send SSL data"; break;
+        default: return 0;
+    }
+    
+    String strData(data, size);
+    RAWLOG_INFO3("%s (%d bytes): %s", text, size, strData.c_str());
+    return 0;
+}
+
+static size_t curlHeaderCallback(void *ptr, size_t size, size_t nmemb, void *opaque)
+{
+    Hashtable<String,String>* pHeaders = (Hashtable<String,String>*)opaque;
+    size_t nBytes = size*nmemb;
+    String strHeader((const char *)ptr, nBytes);
+    RAWTRACE1("Received header: %s", strHeader.c_str());
+    
+    int nSep = strHeader.find(':');
+    if (nSep > 0 )
+    {		
+        String strName = trim(strHeader.substr(0, nSep));
+        String strValue = trim(strHeader.substr(nSep+1, strHeader.length() - (nSep+3) ));
+    
+        pHeaders->put(strName, strValue);
+    }
+    
+    return nBytes;
+}
+
+curl_slist *set_curl_options(bool trace, CURL *curl, const char *method, const String& strUrl, IRhoSession* pSession, Hashtable<String,String>* pHeaders)
+{
+    curl_easy_reset(curl);
+    if (strcasecmp(method, "GET") == 0)
+        curl_easy_setopt(curl, CURLOPT_HTTPGET, 1);
+    else if (strcasecmp(method, "POST") == 0)
+        curl_easy_setopt(curl, CURLOPT_POST, 1);
+    curl_easy_setopt(curl, CURLOPT_URL, strUrl.c_str());
+    
+    // Just to enable cookie parser
+    curl_easy_setopt(curl, CURLOPT_COOKIEFILE, "");
+    // It will clear all stored cookies
+    curl_easy_setopt(curl, CURLOPT_COOKIELIST, "ALL");
+    String session;
+    if (pSession)
+        session = pSession->getSession();
+    
+    if (!session.empty()) {
+        //RAWTRACE1("Set cookie: %s", session.c_str());
+        curl_easy_setopt(curl, CURLOPT_COOKIE, session.c_str());
+    }
+    
+    curl_easy_setopt(curl, CURLOPT_TIMEOUT, 180);
+    curl_easy_setopt(curl, CURLOPT_TCP_NODELAY, 1);
+    
+    // Set very large timeout in case of local requests
+    // It is required because otherwise requests (especially requests to writing!)
+    // could repeated twice or more times
+    if (net::URI::isLocalHost(strUrl))
+        curl_easy_setopt(curl, CURLOPT_TIMEOUT, 10*24*60*60);
+    
+    curl_slist *hdrs = NULL;
+    // Disable "Expect: 100-continue"
+    hdrs = curl_slist_append(hdrs, "Expect:");
+    // Add Keep-Alive header
+    hdrs = curl_slist_append(hdrs, "Connection: Keep-Alive");
+    if (pHeaders)
+    {
+        for ( Hashtable<String,String>::iterator it = pHeaders->begin();  it != pHeaders->end(); ++it )
+        {
+            String strHeader = it->first + ":" + it->second;
+            hdrs = curl_slist_append(hdrs, strHeader.c_str());
+            curl_easy_setopt(curl, CURLOPT_HTTPHEADER, hdrs);
+        }
+        
+        //set header callback
+        curl_easy_setopt(curl, CURLOPT_HEADERDATA, pHeaders);
+        curl_easy_setopt(curl, CURLOPT_HEADERFUNCTION, &curlHeaderCallback);
+        
+    }
+    
+    curl_easy_setopt(curl, CURLOPT_HTTPHEADER, hdrs);
+    
+    if (trace) {
+        curl_easy_setopt(curl, CURLOPT_DEBUGFUNCTION, &curl_trace);
+        curl_easy_setopt(curl, CURLOPT_DEBUGDATA, NULL);
+        curl_easy_setopt(curl, CURLOPT_VERBOSE, 1L);
+    }
+    
+    return hdrs;
+}
+
+CURLMcode do_curl_perform(CURLM *curlm, CURL *curl)
+{
+    int running = 0;
+    curl_multi_add_handle(curlm, curl);
+    CURLMcode err;
+    for(;;) {
+        err = curl_multi_perform(curlm, &running);
+        if (err == CURLM_CALL_MULTI_PERFORM)
+            continue;
+        if (err == CURLM_OK && running > 0) {
+            //RAWTRACE("curl_multi_perform returns OK, but we still have active transfers");
+            fd_set rfd, wfd, efd;
+            int n = 0;
+            FD_ZERO(&rfd);
+            FD_ZERO(&wfd);
+            FD_ZERO(&efd);
+            curl_multi_fdset(curlm, &rfd, &wfd, &efd, &n);
+            if (n < 0) n = 0;
+            timeval tv;
+            tv.tv_sec = 0;
+            tv.tv_usec = 100000;
+            select(n, &rfd, &wfd, &efd, &tv);
+            continue;
+        }
+        break;
+    }
+    curl_multi_remove_handle(curlm, curl);
+    return err;
+}
 	
 } // namespace net
 } // namespace rho
