2368b09000fe892002130806297647ac22bce421
Dmitry Moskalchuk <dmitry@rhomobile.com>
2010-03-11 20:31:50 +0300

    Implement splash screen for iPhone

---
+++ platform/iphone/Classes/rho/common/SplashScreenImpl.h
@@ -0,0 +1,22 @@
+/*
+ *  SplashScreenImpl.h
+ *  rhorunner
+ *
+ *  Created by Dmitry Moskalchuk on 11.03.10.
+ *  Copyright 2010 Rhomobile. All rights reserved.
+ *
+ */
+
+#pragma once
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+void rho_splash_screen_start();
+void rho_splash_screen_hide();
+
+#ifdef __cplusplus
+}
+#endif
