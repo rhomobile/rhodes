621f45975c235aea6bd82330ee6e13ec9c363bb0
genywind <evgeny@rhomobile.com>
2010-03-12 15:04:11 +0300

    add new version of load_offline_data

--- lib/framework/rho/rhoutils.rb
+++ lib/framework/rho/rhoutils.rb
@@ -15,19 +15,15 @@ module Rho
             end
         end
                 
-        Rhom::RhomDbAdapter.destroy_table(filename)
+        Rhom::RhomDbAdapter.delete_all_from_table(filename)
         Rhom::RhomDbAdapter.start_transaction
-        commitRecCount = 10000
-        
-        row_index=0
+
+        first_row=true
         prefix = dir_prefix.nil? ? "" : dir_prefix
         File.open(File.join(Rho::RhoFSConnector.get_base_app_path,'app',prefix,'fixtures',filename+'.txt')).each do |line|
-          if row_index == 0
-            columns = line.chomp.split('|')
-            row_index += 1
-            next
+          if first_row
+            columns = line.chomp.split('|'); first_row = false; next;
           end
-          
           parts = line.chomp.split('|')
 
           row = {}
@@ -39,8 +35,47 @@ module Rho
             end
           end
           Rhom::RhomDbAdapter.insert_into_table(filename,row)
+        end
 
-          if row_index%commitRecCount == 0          
+        Rhom::RhomDbAdapter.commit
+        columns = []
+      end
+    end
+    
+    def self.load_offline_data_ex(tables=[], dir_prefix=nil, commit_count=10000)
+      columns = []
+      tables.each do |filename|
+    
+        Rhom::RhomDbAdapter.destroy_table(filename)
+        Rhom::RhomDbAdapter.start_transaction
+        
+        row_index=0
+        prefix = dir_prefix.nil? ? "" : dir_prefix
+        query = ""
+        
+        File.open(File.join(Rho::RhoFSConnector.get_base_app_path,'app',prefix,'fixtures',filename+'.txt')).each do |line|
+          if row_index == 0
+            columns = line.chomp.split('|')
+            quests = ""
+            columns.each do |col|             
+                quests += ',' if quests.length() > 0
+                quests += '?'
+            end
+            query = "insert into #{filename} (#{columns.join(',')}) values (#{quests})"
+            row_index += 1
+            next
+          end
+          
+          parts = line.chomp.split('|')
+          
+          begin
+              Rhom::RhomDbAdapter.execute_sql query, parts
+          rescue Exception => e
+            puts "load_offline_data : exception insert data: #{e}; data : #{parts}; line : #{row_index}"
+          end
+          
+          if row_index%commit_count == 0          
+            puts "commit start"
             Rhom::RhomDbAdapter.commit
             Rhom::RhomDbAdapter.start_transaction
             puts "commit : #{row_index}"
@@ -53,6 +88,6 @@ module Rho
         puts "commit : #{row_index}"
         columns = []
       end
-    end
+    end    
   end
 end
