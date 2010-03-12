80ff50c1d42c79faf4af5ef6e44eb49d2c9f9d07
Dmitry Moskalchuk <dmitry@rhomobile.com>
2010-03-12 15:53:51 +0300

    Remove net-http tests from specs

--- spec/framework_spec/app/spec/library/net/http/httpresponse/entity_spec.rb
+++
@@ -1,7 +0,0 @@
-require File.dirname(File.join(__rhoGetCurrentDir(), __FILE__)) + '/../../../../spec_helper'
-require 'net/http'
-require File.dirname(File.join(__rhoGetCurrentDir(), __FILE__)) + "/shared/body"
-
-describe "Net::HTTPResponse#entity" do
-  it_behaves_like :net_httpresponse_body, :entity
-end
