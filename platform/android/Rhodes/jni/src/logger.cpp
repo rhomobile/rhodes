cd4fbfa06ac1652353347d1c79d3d2ee0e2ba654
Dmitry Moskalchuk <dmitry@rhomobile.com>
2010-03-16 17:08:39 +0300

    Fix issue #2814281 - Android: empty log on first application run

--- platform/android/Rhodes/jni/src/logger.cpp
+++ platform/android/Rhodes/jni/src/logger.cpp
@@ -8,30 +8,30 @@
 RHO_GLOBAL void JNICALL Java_com_rhomobile_rhodes_Logger_E
   (JNIEnv *env, jclass, jstring tag, jstring msg)
 {
-    RAWLOGC_ERROR(rho_cast<std::string>(tag).c_str(), rho_cast<std::string>(msg).c_str());
+    RAWLOGC_ERROR(rho_cast<std::string>(env, tag).c_str(), rho_cast<std::string>(env, msg).c_str());
 }
 
 RHO_GLOBAL void JNICALL Java_com_rhomobile_rhodes_Logger_W
   (JNIEnv *env, jclass, jstring tag, jstring msg)
 {
-    RAWLOGC_ERROR(rho_cast<std::string>(tag).c_str(), rho_cast<std::string>(msg).c_str());
+    RAWLOGC_ERROR(rho_cast<std::string>(env, tag).c_str(), rho_cast<std::string>(env, msg).c_str());
 }
 
 RHO_GLOBAL void JNICALL Java_com_rhomobile_rhodes_Logger_I
   (JNIEnv *env, jclass, jstring tag, jstring msg)
 {
-    RAWLOGC_INFO(rho_cast<std::string>(tag).c_str(), rho_cast<std::string>(msg).c_str());
+    RAWLOGC_INFO(rho_cast<std::string>(env, tag).c_str(), rho_cast<std::string>(env, msg).c_str());
 }
 
 RHO_GLOBAL void JNICALL Java_com_rhomobile_rhodes_Logger_D
   (JNIEnv *env, jclass, jstring tag, jstring msg)
 {
-    RAWLOGC_INFO(rho_cast<std::string>(tag).c_str(), rho_cast<std::string>(msg).c_str());
+    RAWLOGC_INFO(rho_cast<std::string>(env, tag).c_str(), rho_cast<std::string>(env, msg).c_str());
 }
 
 RHO_GLOBAL void JNICALL Java_com_rhomobile_rhodes_Logger_T
   (JNIEnv *env, jclass, jstring tag, jstring msg)
 {
-    RAWTRACEC(rho_cast<std::string>(tag).c_str(), rho_cast<std::string>(msg).c_str());
+    RAWTRACEC(rho_cast<std::string>(env, tag).c_str(), rho_cast<std::string>(env, msg).c_str());
 }
 
