a2a5e91511a7dd390fa686f79d4689d1aae22878
Lee Lundrigan <leelundrigan@gmail.com>
2010-03-25 21:02:08 -0700

    updated css with bug fixes and consolidated some of the code. also styled settings with new css and html

--- res/generators/templates/application/app/application.rb
+++ res/generators/templates/application/app/application.rb
@@ -1,4 +1,12 @@
 require 'rho/rhoapplication'
 
 class AppApplication < Rho::RhoApplication
+  def initialize
+    # Tab items are loaded left->right, @tabs[0] is leftmost tab in the tab-bar
+    @tabs = [{ :label => "Index", :action => '/app', :icon => "/public/images/tabs/dashboard.png", :reload => true },
+             { :label => "Settings", :action => '/app/Settings', :icon => "/public/images/tabs/accounts.png" },
+             { :label => "About", :action => '/app/About', :icon => "/public/images/tabs/contacts.png" }]
+    # Important to call super _after_ you define @tabs!
+    super
+  end
 end
