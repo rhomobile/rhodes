4254d35b24e6e2b48548936d5a622001be6947e9
Lee Lundrigan <leelundrigan@gmail.com>
2010-03-29 13:05:24 -0700

    adding in an icon lib, added icons to tabs, and fixed a bug with the model edit screen

--- res/generators/templates/application/app/application.rb
+++ res/generators/templates/application/app/application.rb
@@ -3,9 +3,9 @@ require 'rho/rhoapplication'
 class AppApplication < Rho::RhoApplication
   def initialize
     # Tab items are loaded left->right, @tabs[0] is leftmost tab in the tab-bar
-    @tabs = [{ :label => "Index", :action => '/app', :icon => "/public/images/tabs/dashboard.png", :reload => true },
-             { :label => "Settings", :action => '/app/Settings', :icon => "/public/images/tabs/accounts.png" },
-             { :label => "About", :action => '/app/About', :icon => "/public/images/tabs/contacts.png" }]
+    @tabs = [{ :label => "Home", :action => '/app', :icon => "/public/icons/house.png", :reload => true },
+             { :label => "Settings", :action => '/app/Settings', :icon => "/public/icons/gear2.png" },
+             { :label => "About", :action => '/app/About', :icon => "/public/icons/about.png" }]
     # Important to call super _after_ you define @tabs!
     super
   end
