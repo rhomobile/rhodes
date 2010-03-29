754930547e53e73d47d22ccb34d552a4bfcccead
lars <larsburgess@gmail.com>
2010-03-29 11:02:04 -0700

    bump version to 1.5.2, added relevant section to changelog

--- lib/framework/rhodes.rb
+++ lib/framework/rhodes.rb
@@ -1,9 +1,9 @@
 module Rhodes
   unless defined? Rhodes::VERSION
-    VERSION = '1.5.1'
+    VERSION = '1.5.2'
   end
   unless defined? Rhodes::DBVERSION
-    DBVERSION = '1.5.1'
+    DBVERSION = '1.5.2'
   end
   
 end
