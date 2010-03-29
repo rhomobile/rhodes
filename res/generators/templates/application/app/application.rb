b21b456c434cbb110bf30c95dbfa4ca0027ade0f
Lee Lundrigan <leelundrigan@gmail.com>
2010-03-29 16:31:14 -0700

    removed tabs by default and added a class variable called tabbar

--- res/generators/templates/application/app/application.rb
+++ res/generators/templates/application/app/application.rb
@@ -3,9 +3,9 @@ require 'rho/rhoapplication'
 class AppApplication < Rho::RhoApplication
   def initialize
     # Tab items are loaded left->right, @tabs[0] is leftmost tab in the tab-bar
-    @tabs = [{ :label => "Home", :action => '/app', :icon => "/public/icons/house.png", :reload => true },
-             { :label => "Settings", :action => '/app/Settings', :icon => "/public/icons/gear2.png" },
-             { :label => "About", :action => '/app/About', :icon => "/public/icons/about.png" }]
+    @tabs = nil
+
+    @@tabbar = nil
     # Important to call super _after_ you define @tabs!
     super
   end
