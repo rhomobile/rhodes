48ab2a0f5f57cc0d2f19794a6553a50ed3eccde6
Igor Tarasov <igor@rhomobile.com>
2010-04-01 20:42:32 +0400

    RhodesApp: add method to get menu

--- platform/shared/common/RhodesApp.h
+++ platform/shared/common/RhodesApp.h
@@ -82,6 +82,7 @@ public:
 
     void setViewMenu(unsigned long valMenu);
     void addViewMenuItem( const String& strLabel, const String& strLink );
+	const Hashtable<String,String>& getViewMenu (void) {return m_hashViewMenuItems;}; 
 
     boolean sendLog();
 
