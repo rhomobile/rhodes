3cb59592bd71bdbdcd571f3cbe98f88b03f1631f
lars <larsburgess@gmail.com>
2010-04-07 21:32:34 -0700

    merging w/ macboypro

--- res/generators/templates/application/app/Settings/controller.rb
+++ res/generators/templates/application/app/Settings/controller.rb
@@ -1,8 +1,10 @@
 require 'rho'
 require 'rho/rhocontroller'
 require 'rho/rhoerror'
+require 'helpers/browser_helper'
 
 class SettingsController < Rho::RhoController
+  include BrowserHelper
   
   def index
     @msg = @params['msg']
