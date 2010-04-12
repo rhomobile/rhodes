336ccba529e39193d3806822aaf093ef9c847d82
lars <larsburgess@gmail.com>
2010-04-12 16:17:22 -0700

    setting 1.5.3 version

--- lib/framework/rhodes.rb
+++ lib/framework/rhodes.rb
@@ -1,9 +1,9 @@
 module Rhodes
   unless defined? Rhodes::VERSION
-    VERSION = '1.5.2'
+    VERSION = '1.5.3'
   end
   unless defined? Rhodes::DBVERSION
-    DBVERSION = '1.5.2'
+    DBVERSION = '1.5.3'
   end
   
 end
--- lib/framework/version.rb
+++ lib/framework/version.rb
@@ -1,8 +1,8 @@
 module RhodesFramework
   unless defined? RhodesFramework::VERSION
-    VERSION = '1.5.2'
+    VERSION = '1.5.3'
   end
   unless defined? RhodesFramework::DBVERSION
-    DBVERSION = '1.5.2'
+    DBVERSION = '1.5.3'
   end
 end
