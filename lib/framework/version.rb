7dcb81176d20469629252857f1d84e27a2b9b466
lars <larsburgess@gmail.com>
2010-04-12 18:41:38 -0700

    bumping version, getting fix for clean:iphone

--- lib/framework/version.rb
+++ lib/framework/version.rb
@@ -1,8 +1,8 @@
 module RhodesFramework
   unless defined? RhodesFramework::VERSION
-    VERSION = '1.5.3'
+    VERSION = '1.5.4'
   end
   unless defined? RhodesFramework::DBVERSION
-    DBVERSION = '1.5.3'
+    DBVERSION = '1.5.4'
   end
 end
