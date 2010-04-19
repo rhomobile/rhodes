a118ef35c015d6ca84290928411a195296c3aba7
genywind <evgeny@rhomobile.com>
2010-04-19 16:53:27 +0400

    add BrowserField(field2) support to BB 5.0

---
+++ platform/bb/rhodes/src/com/rho/BrowserAdapter5.java
@@ -0,0 +1,28 @@
+package com.rho;
+
+import javax.microedition.io.HttpConnection;
+import rhomobile.RhodesApplication;
+
+public class BrowserAdapter5 implements IBrowserAdapter 
+{
+	public BrowserAdapter5(RhoMainScreen oMainScreen, RhodesApplication app) 
+	{
+	}
+
+	public void setFullBrowser()
+	{
+	}
+	
+    public void processConnection(HttpConnection connection, Object e) 
+    {
+    }
+
+    public void executeJavascript(String strJavascript)
+    {
+    }
+    
+    public boolean navigationMovement(int dx, int dy, int status, int time)
+    {
+    	return false;
+    }
+}
