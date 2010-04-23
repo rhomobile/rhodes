a2cfac7cc88e6495a128a52e5a053dcb9fcd12bc
genywind <evgeny@rhomobile.com>
2010-04-23 12:59:59 +0400

    Fix WM app menu

--- platform/wm/rhodes/rho/rubyext/WebView.cpp
+++ platform/wm/rhodes/rho/rubyext/WebView.cpp
@@ -13,13 +13,18 @@ HWND getMainWnd();
 
 void rho_webview_refresh(int index) 
 {
-	::PostMessage(getMainWnd(),WM_COMMAND,IDM_REFRESH,0);
+    rho_webview_navigate(RHODESAPP().getCurrentUrl().c_str(),0);
 }
 
 void rho_webview_navigate(const char* url, int index) 
 {
     String strUrl = RHODESAPP().canonicalizeRhoUrl(url);
     ::PostMessage( getMainWnd(), WM_COMMAND, IDM_NAVIGATE, (LPARAM)_tcsdup(convertToStringW(strUrl).c_str()) );
+}
+
+void rho_webview_navigate_back()
+{
+    ::PostMessage( getMainWnd(), WM_COMMAND, IDM_NAVIGATE_BACK, (LPARAM)0 );
 }
 
 const char* rho_webview_execute_js(const char* js, int index) 
@@ -44,6 +49,12 @@ int rho_webview_active_tab()
 void rho_webview_set_menu_items(VALUE valMenu) 
 {
     rho_rhodesapp_setViewMenu(valMenu); 
+}
+
+
+void rho_webview_full_screen_mode(int enable)
+{
+    ::PostMessage( getMainWnd(), WM_COMMAND, ID_FULLSCREEN, (LPARAM)enable );
 }
 
 }
