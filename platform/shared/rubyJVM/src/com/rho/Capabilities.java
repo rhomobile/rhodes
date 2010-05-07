ffd8999202454904499a209eca0ccfb8bf43862a
genywind <evgeny@rhomobile.com>
2010-05-07 17:44:52 +0400

    turn on push and services

--- platform/shared/rubyJVM/src/com/rho/Capabilities.java
+++ platform/shared/rubyJVM/src/com/rho/Capabilities.java
@@ -1,6 +1,6 @@
 package com.rho;
 
 public class Capabilities {
-  public static final boolean ENABLE_PUSH = false;
-  public static final boolean RUNAS_SERVICE = false;
+  public static final boolean ENABLE_PUSH = true;
+  public static final boolean RUNAS_SERVICE = true;
 }
