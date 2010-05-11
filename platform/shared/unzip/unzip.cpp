4a6377bacbb4a5632ee7f948acc99f82ef9070d1
Dmitry Moskalchuk <dmitry@rhomobile.com>
2010-05-11 13:19:07 +0400

    Fix symbols conflict in unzip

--- platform/shared/unzip/unzip.cpp
+++ platform/shared/unzip/unzip.cpp
@@ -1696,7 +1696,7 @@ int inflate_blocks_free(inflate_blocks_statef *s, z_streamp z)
 
 
 
-extern const char inflate_copyright[] =
+extern const char zlib_inflate_copyright[] =
    " inflate 1.1.3 Copyright 1995-1998 Mark Adler ";
 // If you use the zlib library in a product, an acknowledgment is welcome
 // in the documentation of your product. If for some reason you cannot
