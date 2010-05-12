904ee57efc861ecc74b057c96e7118630eb28e4f
Igor Tarasov <igor@rhomobile.com>
2010-05-12 20:02:00 +0400

    Win32:    - fix for issue #1975439 (Win32: Add error handling to unzip of rhobundle)    - fix for issue #1975348 (error dowload rhobundle from rhodes client)    - AppManager refactoring;

--- platform/wm/rhodes/AppManager.cpp
+++ platform/wm/rhodes/AppManager.cpp
@@ -10,6 +10,9 @@
 #include "unzip/unzip.h"
 #include "ext/rho/rhoruby.h"
 #include "common/AutoPointer.h"
+#include "common/StringConverter.h"
+
+using namespace rho::common;
 
 extern "C" wchar_t* wce_mbtowc(const char* a);
 
@@ -21,24 +24,65 @@ CAppManager::~CAppManager(void)
 {
 }
 
-bool CAppManager::RemoveFolder(LPCTSTR szPath) {
+bool CAppManager::RemoveFolder(String pathname)
+{
+	if (pathname.length() > 0) {
+		LPWSTR  swPath = wce_mbtowc(pathname.c_str());
+		TCHAR name[MAX_PATH+2];
+		wsprintf(name, L"%s%c", swPath, '\0');
 
-	if (szPath) {
 		SHFILEOPSTRUCT fop;
-		
+
 		fop.hwnd = NULL;
 		fop.wFunc = FO_DELETE;		
-		fop.pFrom = szPath;
+		fop.pFrom = name;
 		fop.pTo = NULL;
 		fop.fFlags = FOF_SILENT | FOF_NOCONFIRMATION | FOF_NOERRORUI | FOF_NOCONFIRMMKDIR;
-		
 		int result = SHFileOperation(&fop);
+
+		if (swPath)
+			free (swPath);
+
 		return result == 0;
 	}
 	return false;
 }
 
-bool CAppManager::RestartClient(HWND hwnd) {
+bool CAppManager::MoveFolder(const String& pathFrom, const String &pathTo)
+{
+	if (pathFrom.length() > 0 && pathTo.length() > 0) {
+		
+		LPWSTR  swPathFrom = wce_mbtowc(pathFrom.c_str());
+		LPWSTR  swPathTo   = wce_mbtowc(pathTo.c_str());
+
+		TCHAR tcPathFrom[MAX_PATH+2];
+		TCHAR tcPathTo[MAX_PATH+2];
+
+		wsprintf(tcPathFrom, L"%s%c", swPathFrom,'\0');
+		wsprintf(tcPathTo, L"%s%c", swPathTo,'\0');
+
+		SHFILEOPSTRUCT fop;
+		
+		fop.hwnd = NULL;
+		fop.wFunc = FO_MOVE;
+		fop.pFrom = tcPathFrom;
+		fop.pTo   = tcPathTo;
+		fop.fFlags = FOF_SILENT | FOF_NOCONFIRMATION | FOF_NOERRORUI | FOF_NOCONFIRMMKDIR;
+		
+		int result = SHFileOperation(&fop);
+
+		if (swPathFrom)
+			free (swPathFrom);
+		if (swPathTo)
+			free (swPathTo);
+
+		return result == 0;
+	}
+	return false;
+}
+
+bool CAppManager::RestartClient(HWND hwnd) 
+{
 	TCHAR module[MAX_PATH];
 	GetModuleFileName(NULL,module,MAX_PATH);
 	HINSTANCE res = ShellExecute(hwnd, L"open", module, L"-restarting", NULL, SW_SHOWNORMAL);
@@ -46,84 +90,180 @@ bool CAppManager::RestartClient(HWND hwnd) {
 }
 
 void CAppManager::ReloadRhoBundle(HWND hwnd, const char* szUrl, const char* szZipPassword)
-{
-	if ( szUrl )
-	{
-		//get zip file with rhodes
-		//DWORD dwDataSize = 0;
-        rho::net::CNetRequest request;
-        NetResponse(resp, request.pullData(szUrl, null));
-        DWORD dwDataSize = resp.getDataSize();
-		//char* zipData = request.doRequest( L"GET",const_cast<char*>(szUrl),NULL,0,NULL,0,false,true,false,&dwDataSize);
-        const char* zipData = resp.getCharData();
-		if ( zipData && dwDataSize > 0 )
-		{
-			LPWSTR rootw = wce_mbtowc(rho_native_rhopath());
-			rootw[wcslen(rootw)-1] = '\\';
-			bool ret = false;
-			if (wcslen(rootw)<MAX_PATH) {
-				TCHAR name[MAX_PATH+2];
-				wsprintf(name,L"%sapps%c",rootw,'\0');
-				if( RemoveFolder(name) ) {
-					wsprintf(name,L"%slib%c",rootw,'\0');
+{	
+	const char  *tmp_dir_name = "_tmp_";
+	const char	*loadData = NULL;
+	unsigned int loadSize = 0;
+	String		 root_dir = rho_native_rhopath();
+	String		 tmp_unzip_dir;
+	int          errCode = RRB_UNKNOWN_ERR;
 
-					ret = RemoveFolder(name);
-				}
-			}
+	root_dir.at(root_dir.length()-1) = '\\';
+	tmp_unzip_dir = root_dir + tmp_dir_name;
 
-			//TODO: Add error handling to unzip code
-			if( ret ) {
-				ZIPENTRY ze; 
-				// Open zip file
-				HZIP hz = OpenZip(const_cast<char*>(zipData), dwDataSize, szZipPassword);
-			
-				if ( hz ) {
-					//Stop HTTP Server
-					RHODESAPP().stopApp();
-
-					// Set base for unziping
-					SetUnzipBaseDir(hz, rootw);
-				
-					// Get info about the zip
-					// -1 gives overall information about the zipfile
-					GetZipItem(hz,-1,&ze);
-					int numitems = ze.index;
-				
-					// Iterate through items and unzip them
-					for (int zi = 0; zi<numitems; zi++)
-					{ 
-						// fetch individual details, e.g. the item's name.
-						GetZipItem(hz,zi,&ze); 
-						// unzip item
-						UnzipItem(hz, zi, ze.name);         
-					}
-				
-					CloseZip(hz);
-					
-					//Show MessageBox
-					MessageBox(hwnd, _T("Rhobundle has been updated successfully.\nApplication will be restarted."), _T("Information"), MB_OK | MB_ICONINFORMATION );
-					//Restart client
-					RestartClient(hwnd); 
-					//Close main window and client
-					::PostMessage(hwnd,WM_CLOSE,0,0);
-				} else {
-					MessageBox(hwnd, _T("Can't unzip loaded rhobundle."), _T("Stop"), MB_OK | MB_ICONSTOP );
-				}
-			} else {
-				MessageBox(hwnd, _T("Can't remove old version of rhobundle. However, it may be corrupted. Exit application and reinstall rhobundle manualy."), _T("Stop"), MB_OK | MB_ICONSTOP );
-			}
-
-			if ( rootw )
-				free(rootw);
-
-		} else {
-			MessageBox(hwnd, _T("Error loading rhobundle."), _T("Stop"), MB_OK | MB_ICONSTOP );
-		}
-
-//		if ( zipData )
-//			free( zipData );
+	if (szUrl == NULL) {
+		LOG(ERROR) + "null url passed to ReloadRhoBundle";
+		return;
 	}
 
+	//trying to load file
+	rho::net::CNetRequest request;
+	//NB: for mobile devices should use filesystem instead of RAM
+	NetResponse (resp, request.pullData(szUrl, null));
+	if (resp.getDataSize() > 0 && resp.getCharData()) {
+		loadData = resp.getCharData();
+		loadSize = resp.getDataSize();
+		errCode = RRB_NONE_ERR;
+	} else {
+		errCode = RRB_LOADING_ERR;
+	}
+
+	//trying to unzip loaded file
+	if (errCode == RRB_NONE_ERR)
+		errCode = unzipRhoBundle(loadData, loadSize, szZipPassword, tmp_unzip_dir);
+	
+	RHODESAPP().stopApp();
+
+	//trying to delete old rhobundle
+	if (errCode == RRB_NONE_ERR)
+		errCode = removeOldRhoBundle();
+
+	//move new rhobundle from temporary dir
+	if (errCode == RRB_NONE_ERR) {
+		errCode = updateRhoBundle (root_dir, tmp_unzip_dir);
+	}
+
+	showMessage(hwnd, errCode);
+}
+
+int CAppManager::unzipRhoBundle (const char *zipData, unsigned int zipDataSize, const char *zipPassword, const String &targetPath)
+{
+	int err_code = RRB_NONE_ERR;
+	HANDLE hFind = 0;
+
+	HZIP   hz = OpenZip((void *)zipData, zipDataSize, zipPassword);
+	if (!hz) {
+		LOG(ERROR) + "Failed to open zipfile";
+		err_code = RRB_UNZIP_ERR;
+	}
+
+	if (err_code == RRB_NONE_ERR) {
+		//check for target dir
+		WIN32_FIND_DATA wfd;
+		hFind = FindFirstFile(convertToStringW(targetPath).c_str(), &wfd);
+		if (INVALID_HANDLE_VALUE == hFind) {
+			if (CreateDirectory(convertToStringW(targetPath).c_str(), NULL) != TRUE) {
+				LOG(ERROR) + "Failed to create temporary dir";
+				err_code = RRB_UNZIP_ERR;
+			}
+		} else {
+			LOG(WARNING) + "Temporary dir exists";
+			if (!RemoveFolder (targetPath)) {
+				LOG(ERROR) + "Failed to delete old temporary dir";
+				err_code = RRB_UNZIP_ERR;
+			}
+		}
+	}
+
+	if (err_code == RRB_NONE_ERR) {
+		// Set base for unziping
+		SetUnzipBaseDir(hz, convertToStringW(targetPath).c_str());
+					
+		// Get info about the zip
+		// -1 gives overall information about the zipfile
+		ZIPENTRY ze;
+		GetZipItem(hz,-1, &ze);
+		int numitems = ze.index;
+					
+		// Iterate through items and unzip them
+		for (int zi = 0; zi<numitems; zi++) { 
+			// fetch individual details, e.g. the item's name.
+			if (ZR_OK != GetZipItem(hz,zi,&ze)) {
+				LOG(ERROR) + "Failed to unzip item";
+				RemoveFolder (targetPath);
+				err_code = RRB_UNZIP_ERR;
+				break;
+			}
+			UnzipItem(hz, zi, ze.name);
+		}
+	}
+
+	if (hz)
+		CloseZip(hz);
+
+	return err_code;
+}
+
+int CAppManager::removeOldRhoBundle (void)
+{
+	String root = rho_native_rhopath();
+	root.at(root.length()-1) = '\\';
+
+	if (!RemoveFolder(root + "apps")) {
+		LOG(ERROR) + "Failed to remove" + "\"" + (root + "apps") + "\"";
+		return RRB_REMOVEOLD_ERR;
+	}
+
+	if (!RemoveFolder(root + "lib")) {
+		LOG(ERROR) + "Failed to remove" + "\"" + (root + "lib	") + "\"";
+		return RRB_REMOVEOLD_ERR;
+	}
+	
+	return RRB_NONE_ERR;
+}
+
+
+int CAppManager::updateRhoBundle (const String &root_path, const String &from_path)
+{
+	int err_code = RRB_NONE_ERR;
+
+	if (!MoveFolder (from_path + "\\*.*", root_path)) {
+		LOG(ERROR) + "Failed to move new rhobundle content";
+		err_code = RRB_UPDATE_ERR;
+	}
+
+	if (err_code == RRB_NONE_ERR) {
+		if (!RemoveFolder (from_path)) {
+			LOG(ERROR) + "Failed to remove temporary dir";
+		}
+	}
+
+	return err_code;
+}
+
+void CAppManager::showMessage(HWND hWnd, int rrbErrCode) 
+{
+	String messageText;
+
+	if (rrbErrCode == RRB_NONE_ERR) {
+		MessageBox(hWnd, 
+					_T("Rhobundle has been updated successfully.\nApplication will be restarted."), 
+					_T("Information"), 
+					MB_OK | MB_ICONINFORMATION );
+		RestartClient(hWnd); 
+		//Close main window and client
+		::PostMessage(hWnd,WM_CLOSE,0,0);
+	}
+
+	switch (rrbErrCode) {
+	case RRB_LOADING_ERR:
+		messageText = "Error loading rhobundle.";
+		break;
+	case RRB_UNZIP_ERR:
+		messageText= "Can't unzip loaded rhobundle.";
+		break;
+	case RRB_REMOVEOLD_ERR:
+		messageText = "Can't remove old version of rhobundle. However, it may be corrupted. Exit application and reinstall rhobundle manualy.";
+		break;
+	case RRB_UPDATE_ERR:
+		messageText= "Error updating rhobundle. Exit application and reinstall rhobundle manualy.";
+		break;
+	default:
+		messageText = "Error reloading rhobundle.";
+		break;
+	}
+
+	MessageBox(hWnd, convertToStringW(messageText).c_str(), _T("Stop"), MB_OK | MB_ICONSTOP );
 }
 
 #endif
--- platform/wm/rhodes/AppManager.h
+++ platform/wm/rhodes/AppManager.h
@@ -1,19 +1,40 @@
 #pragma once
 
+#include "common/RhoStd.h"
+
+using namespace rho;
+
 #ifdef ENABLE_DYNAMIC_RHOBUNDLE
 class CAppManager
 {
+	enum {
+		RRB_NONE_ERR,
+		RRB_LOADING_ERR,
+		RRB_UNZIP_ERR,
+		RRB_REMOVEOLD_ERR,
+		RRB_UPDATE_ERR,
+		RRB_UNKNOWN_ERR
+	};
+
 public:
-	CAppManager(void);
-	~CAppManager(void);
+	CAppManager  (void);
+	~CAppManager (void);
 
 	/**
 	 * @param szUrl - url of the zip file with rhobundle
 	 * @param szZipPassword - zip password if required
 	 */
 	static void ReloadRhoBundle(HWND hwnd, const char* szUrl, const char* szZipPassword);
-	static bool RemoveFolder(LPCTSTR szPath);
+	static bool RemoveFolder(String pathname);
+	static bool MoveFolder(const String& pathFrom, const String &pathTo);
 	static bool RestartClient(HWND hwnd);
+
+private:
+
+	static int unzipRhoBundle (const char *zipData, unsigned int zipDataSize, const char *zipPassword, const String &targetPath);
+	static int removeOldRhoBundle (void);
+	static int updateRhoBundle(const String &root_path, const String &from_path);
+	static void showMessage (HWND hWnd, int rrbErrCode);
 };
 
 #endif
