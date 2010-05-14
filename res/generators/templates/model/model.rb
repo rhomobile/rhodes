da434ecf7ca8a41149d75c3d970713f8e54816bc
genywind <evgeny@rhomobile.com>
2010-05-14 22:48:54 +0400

    migrate rhogen to new model definition

--- res/generators/templates/model/model.rb
+++ res/generators/templates/model/model.rb
@@ -1,9 +1,7 @@
 # The model has already been created by the framework, and extends Rhom::RhomObject
 # You can add more methods here
-require 'rhom/rhom_model'
-
 class <%= class_name %>
-  extend Rhom::PropertyBag
+  include Rhom::PropertyBag
   
   #add model specifc code here
 end
