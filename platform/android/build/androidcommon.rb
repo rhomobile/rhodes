0a4b5992aaba86af795e7d0e985832bbc2a0df18
Dmitry Moskalchuk <dmitry@rhomobile.com>
2010-05-17 16:32:13 +0400

    Fix android build script according to the latest changed in Android NDK

--- platform/android/build/androidcommon.rb
+++ platform/android/build/androidcommon.rb
@@ -163,13 +163,13 @@ def cc_link(outname, objects, additional = nil, deps = nil)
   args << outname
   args += objects
   args += additional if additional.is_a? Array and not additional.empty?
-  args << "#{$ndksysroot}/usr/lib/libstdc++.so"
-  args << "#{$ndksysroot}/usr/lib/libsupc++.so" unless USE_STLPORT
-  args << "#{$ndksysroot}/usr/lib/libc.so"
-  args << "#{$ndksysroot}/usr/lib/libm.so"
   args << "-L#{$ndksysroot}/usr/lib"
   args << "-Wl,-rpath-link=#{$ndksysroot}/usr/lib"
+  args << "#{$ndksysroot}/usr/lib/libstdc++.so"
+  args << "#{$ndksysroot}/usr/lib/libsupc++.so" unless USE_STLPORT
   args << "#{$ndktools}/lib/gcc/arm-eabi/#{$ndkgccver}/interwork/libgcc.a"
+  args << "#{$ndksysroot}/usr/lib/libc.so"
+  args << "#{$ndksysroot}/usr/lib/libm.so"
   cc_run($gccbin, args)
 end
 
