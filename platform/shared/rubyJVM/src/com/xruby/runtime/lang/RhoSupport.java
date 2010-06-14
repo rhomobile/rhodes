097507af0209b734fee6ce3a5b27b7bae35a87c4
genywind <evgeny@rhomobile.com>
2010-06-14 16:35:34 +0400

    fix create class name

--- platform/shared/rubyJVM/src/com/xruby/runtime/lang/RhoSupport.java
+++ platform/shared/rubyJVM/src/com/xruby/runtime/lang/RhoSupport.java
@@ -181,6 +181,13 @@ public class RhoSupport {
         required_file = required_file.replace('\\', '.');
         required_file = StringMe.replaceAll(required_file,"-", "minus");
         
+        int nDot = required_file.lastIndexOf('.');
+        if (nDot>0)
+        {
+        	String fName = required_file.substring(nDot+1);
+        	required_file = required_file.substring(0, nDot+1) + fName.toLowerCase();
+        }
+        
         required_file += ".main";
         return getAppName() + "." + required_file;
     }
