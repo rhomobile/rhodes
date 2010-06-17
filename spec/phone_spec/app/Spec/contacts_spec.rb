abb2b8d0e4f05b10d25d537f8f12874224bd8c7e
Dmitry Moskalchuk <dmitry@rhomobile.com>
2010-06-17 17:53:16 +0400

    Contacts create_test done

--- spec/phone_spec/app/Spec/contacts_spec.rb
+++ spec/phone_spec/app/Spec/contacts_spec.rb
@@ -1,23 +1,46 @@
 require 'rho/rhocontact'
 
+class Hash
+  def diff(rhs)
+    diff = {}
+    self.each do |k,v|
+      next if rhs.has_key?(k) and rhs[k] == v
+      diff[k] = v
+    end
+    rhs.each do |k,v|
+      next if self.has_key?(k) and self[k] == v
+      diff[k] = v
+    end
+    diff
+  end
+end
+
 class ContactsTest
   def initialize
   end
 
   def create_test
+    first_name = 'Random'
+    last_name = 'Newbie'
+    mobile_number = '+1222333444'
+
     contacts = Rho::RhoContact.find(:all)
 
     contact = {}
-    contact['first_name'] = 'Random'
-    contact['last_name'] = 'Newbie'
-    contact['mobile_number'] = '+1222333444'
+    contact['first_name'] = first_name
+    contact['last_name'] = last_name
+    contact['mobile_number'] = mobile_number
     Rho::RhoContact.create!(contact)
 
     newcontacts = Rho::RhoContact.find(:all)
 
-    puts "contacts: #{contacts.inspect.to_s}"
-    puts "newcontacts: #{contacts.inspect.to_s}"
-    #Test_equal( parsed.is_a?(Array), true )
+    diff = newcontacts.diff(contacts)
+    Test_equal( diff.size, 1 )
+    Test_equal( diff.keys.size, 1 )
+    c = diff[diff.keys.first]
+    Test_equal( c['first_name'], first_name )
+    Test_equal( c['last_name'], last_name )
+    Test_equal( c['mobile_number'], mobile_number )
   end
   
 end    
