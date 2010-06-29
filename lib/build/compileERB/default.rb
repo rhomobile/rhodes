c8291aa576ab023cfe5bd81396e28e98cceea400
genywind <evgeny@rhomobile.com>
2010-06-30 01:09:10 +0400

    fix encoding issue

--- lib/build/compileERB/default.rb
+++ lib/build/compileERB/default.rb
@@ -8,7 +8,7 @@ Find.find(dir) do |path|
   if File.extname(path) == ext
     begin
         strFile = IO.read(path)
-        strFile.force_encoding('utf-8')
+        #strFile.force_encoding('utf-8')
 
       rbText = ERB.new(strFile).src
 	  
