2ebb40e4ca3d8f7d86df00275aa917c3af9a0a8b
Dmitry Moskalchuk <dmitry@rhomobile.com>
2010-07-06 20:08:57 +0400

    Add runtime selection of ContactAccessor

---
+++ platform/android/Rhodes/src/com/rhomobile/rhodes/phonebook/ContactAccessorNew.java
@@ -0,0 +1,33 @@
+package com.rhomobile.rhodes.phonebook;
+
+import java.util.Map;
+
+public class ContactAccessorNew implements ContactAccessor {
+	
+	private static final String TAG = "ContactsAccessorNew";
+	
+	private static final String PB_ID = Phonebook.PB_ID;
+	private static final String PB_FIRST_NAME = Phonebook.PB_FIRST_NAME;
+	private static final String PB_LAST_NAME = Phonebook.PB_LAST_NAME;
+	private static final String PB_MOBILE_NUMBER = Phonebook.PB_MOBILE_NUMBER;
+	private static final String PB_HOME_NUMBER = Phonebook.PB_HOME_NUMBER;
+	private static final String PB_BUSINESS_NUMBER = Phonebook.PB_BUSINESS_NUMBER;
+	private static final String PB_EMAIL_ADDRESS = Phonebook.PB_EMAIL_ADDRESS;
+	private static final String PB_COMPANY_NAME = Phonebook.PB_COMPANY_NAME;
+
+	public Map<String, Contact> getAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void remove(Contact contact) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void save(Contact contact) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+}
