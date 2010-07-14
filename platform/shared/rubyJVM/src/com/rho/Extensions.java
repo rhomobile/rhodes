b86804a0765022c427c0f058eebb057ac0f8d92e
dmitrys <dmitrys@rhomobile.com>
2010-07-14 04:37:48 +0400

    refactoring of extension mechanizm in BB platform

---
+++ platform/shared/rubyJVM/src/com/rho/Extensions.java
@@ -0,0 +1,11 @@
+// WARNING! THIS FILE IS GENERATED AUTOMATICALLY! DO NOT EDIT IT MANUALLY!
+// Generated Wed Jul 14 04:31:28 +0400 2010
+package com.rho; 
+ 
+public class Extensions {
+ 
+public static String[] extensions = {
+""
+};
+ 
+}
