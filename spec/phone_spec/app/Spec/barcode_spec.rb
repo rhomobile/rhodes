58ead36ce8ea29ed52a19014fc79d9f67ab8fc1b
dmitrys <dmitrys@rhomobile.com>
2010-07-16 15:37:54 +0400

    change barcode test images to UPC images

--- spec/phone_spec/app/Spec/barcode_spec.rb
+++ spec/phone_spec/app/Spec/barcode_spec.rb
@@ -2,10 +2,10 @@ class BarcodeSpec
  
   def barcode_test
 
-    barcode_01_filename = File.join(Rho::RhoApplication::get_model_path('app','Barcode'), 'barcode_01.png')	
-    barcode_01_correct_code = '9876543210128'	
-    barcode_02_filename = File.join(Rho::RhoApplication::get_model_path('app','Barcode'), 'barcode_02.jpg')	
-    barcode_02_correct_code = '4006170005209'
+    barcode_01_filename = File.join(Rho::RhoApplication::get_model_path('app','Barcode'), 'Barcode_UPC_01.png')	
+    barcode_01_correct_code = '0718122850617'	
+    barcode_02_filename = File.join(Rho::RhoApplication::get_model_path('app','Barcode'), 'Barcode_UPC_02.jpg')	
+    barcode_02_correct_code = '0123456789012'
 	
 
     barcode_01_recognized_code = Barcode.barcode_recognize(barcode_01_filename)
