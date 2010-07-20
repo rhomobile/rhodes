97d060628bd4264eef8dbf26388b621d4ad926fe
unknown <Dima@.(none)>
2010-07-20 06:31:27 +0400

    Fix incorrect Barcode recognize by ZBar

--- spec/phone_spec/app/Spec/barcode_spec.rb
+++ spec/phone_spec/app/Spec/barcode_spec.rb
@@ -3,9 +3,9 @@ class BarcodeSpec
   def barcode_test
 
     barcode_01_filename = File.join(Rho::RhoApplication::get_model_path('app','Barcode'), 'Barcode_UPC_01.png')	
-    barcode_01_correct_code = '0718122850617'	
+    barcode_01_correct_code = '718122850617'	
     barcode_02_filename = File.join(Rho::RhoApplication::get_model_path('app','Barcode'), 'Barcode_UPC_02.jpg')	
-    barcode_02_correct_code = '0123456789012'
+    barcode_02_correct_code = '123456789012'
 	
 
     barcode_01_recognized_code = Barcode.barcode_recognize(barcode_01_filename)
