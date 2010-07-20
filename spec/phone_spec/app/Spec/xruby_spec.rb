e557b55781ed81b0856458e5bd815d07b5e1c19b
genywind <evgeny@rhomobile.com>
2010-07-21 00:15:56 +0400

    fixed metadata issues for BB

--- spec/phone_spec/app/Spec/xruby_spec.rb
+++ spec/phone_spec/app/Spec/xruby_spec.rb
@@ -5,6 +5,22 @@ describe "Xruby" do
     #[:action => :back] - Does not compile
     [{:action => :back}]
   end
+  
+  #https://www.pivotaltracker.com/story/show/4365686
+  def test_method(p)
+    p == 1 || p == 2
+  end
+  
+  it "if with function parameters" do
+    test = false
+    #if test_method 1 and test_method 2
+    if test_method( 1 ) and test_method 2
+        test = true
+    end
+    
+    test.should == true
+  end
+  
 =begin
   def elsif_test
     cond = 'false'
