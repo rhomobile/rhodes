e59b69269dbb38d3f101feab8e4c5d91bb7274ee
dmitrys <dmitrys@rhomobile.com>
2010-07-28 18:40:16 +0400

    Add Signature to WM and Android (only compilation fix)

---
+++ platform/wm/rhodes/signature/Signature.cpp
@@ -0,0 +1,28 @@
+#include "stdafx.h"
+
+#if defined(_WIN32_WCE)
+#include <aygshell.h>
+#endif
+#include <atltime.h>
+#include "ext/rho/rhoruby.h"
+#include "../MainWindow.h"
+#include "Signature.h"
+#include "common/RhodesApp.h"
+
+#ifdef _MSC_VER
+// warning C4800: 'int' : forcing to bool 'true' or 'false' (performance warning)
+#pragma warning ( disable : 4800 )
+#endif
+
+extern "C" HWND getMainWnd();
+
+#if defined(_WIN32_WCE)
+// strdup is implemented as part of ruby CE port
+extern "C" char *strdup(const char * str);
+extern "C" wchar_t* wce_mbtowc(const char* a);
+#endif
+
+
+
+void rho_signature_take_signature(char* callback_url) {
+}
