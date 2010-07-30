09410d68d2e646f912c0927663c139d8331b32c2
dmitrys <dmitrys@rhomobile.com>
2010-07-30 21:02:29 +0400

    Add Signature capture to Android

--- platform/android/Rhodes/jni/src/signature.cpp
+++ platform/android/Rhodes/jni/src/signature.cpp
@@ -1,24 +1,31 @@
 #include "rhodes/JNIRhodes.h"
 
-//#include "rhodes/jni/com_rhomobile_rhodes_camera_Camera.h"
+#include "rhodes/jni/com_rhomobile_rhodes_signature_Signature.h"
 
 #include <common/RhodesApp.h>
 
 #undef DEFAULT_LOGCATEGORY
 #define DEFAULT_LOGCATEGORY "Signature"
 
+RHO_GLOBAL void JNICALL Java_com_rhomobile_rhodes_signature_Signature_callback
+  (JNIEnv *env, jclass, jstring callback, jstring filePath, jstring error, jboolean cancelled)
+{
+    rho_rhodesapp_callSignatureCallback(rho_cast<std::string>(callback).c_str(),
+        rho_cast<std::string>(filePath).c_str(), rho_cast<std::string>(error).c_str(), cancelled);
+}
+
 
 RHO_GLOBAL void rho_signature_take_signature(char* callback_url)
 {
-/*
+///*
     JNIEnv *env = jnienv();
-    jclass cls = getJNIClass(RHODES_JAVA_CLASS_CAMERA);
+    jclass cls = getJNIClass(RHODES_JAVA_CLASS_SIGNATURE);
     if (!cls) return;
-    jmethodID mid = getJNIClassStaticMethod(env, cls, "takePicture", "(Ljava/lang/String;)V");
+    jmethodID mid = getJNIClassStaticMethod(env, cls, "takeSignature", "(Ljava/lang/String;)V");
     if (!mid) return;
     jstring objCallback = rho_cast<jstring>(callback_url);
     env->CallStaticVoidMethod(cls, mid, objCallback);
     env->DeleteLocalRef(objCallback);
-*/
+//*/
 }
 
