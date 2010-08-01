dfa65bc0d276f870e9223863c937e18a91fadb89
Pablo Kang <pablokang@gmail.com>
2010-08-01 09:58:57 -0700

    Add commented out lines to implement application with sync

--- res/generators/templates/application/app/application.rb
+++ res/generators/templates/application/app/application.rb
@@ -3,10 +3,14 @@ require 'rho/rhoapplication'
 class AppApplication < Rho::RhoApplication
   def initialize
     # Tab items are loaded left->right, @tabs[0] is leftmost tab in the tab-bar
+    # Super must be called *after* settings @tabs!
     @tabs = nil
-
     @@tabbar = nil
-    # Important to call super _after_ you define @tabs!
+
     super
+
+    # Uncomment to set sync notification callback to /app/Settings/sync_notify.
+    # SyncEngine::set_objectnotify_url("/app/Settings/sync_notify")
+    # SyncEngine.set_notification(-1, "/app/Settings/sync_notify", '')
   end
 end
--- res/generators/templates/model/model.rb
+++ res/generators/templates/model/model.rb
@@ -2,6 +2,9 @@
 # You can add more methods here
 class <%= class_name %>
   include Rhom::PropertyBag
-  
+
+  # Uncomment the following line to enable sync with <%= class_name %>.
+  # enable :sync
+
   #add model specifc code here
 end
