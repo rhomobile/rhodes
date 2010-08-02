40a613f54bd0a434f978e50991dd85a5500303f8
genywind <evgeny@rhomobile.com>
2010-08-03 00:57:07 +0400

    add test for sync schema sources(commented out)

--- spec/phone_spec/app/Product/product.rb
+++ spec/phone_spec/app/Product/product.rb
@@ -1,9 +1,24 @@
 require 'rhom'
 
 class Product
+
     include Rhom::PropertyBag
 
     enable :sync
     #set :partition, :application
 
+=begin
+    include Rhom::FixedSchema
+
+    enable :sync
+    set :schema_version, '1.0'
+    
+    property :brand, :string
+    property :created_at, :string
+    property :name, :string
+    property :price, :string
+    property :quantity, :string
+    property :sku, :string
+    property :updated_at,  :string
+=end
 end
--- spec/phone_spec/app/Product_s/product_s.rb
+++ spec/phone_spec/app/Product_s/product_s.rb
@@ -1,9 +1,17 @@
+require 'rhom'
+
 class Product_s
     include Rhom::FixedSchema
 
     enable :sync
     set :schema_version, '1.0'
     
-    property :billing_address_country, :string
+    property :brand, :string
+    property :created_at, :string
+    property :name, :string
+    property :price, :string
+    property :quantity, :string
+    property :sku, :string
+    property :updated_at,  :string
 
 end
