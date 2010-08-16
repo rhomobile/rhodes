3650f1a2e835e80a482366388e44b9d1b40238ac
genywind <evgeny@rhomobile.com>
2010-08-16 18:31:56 +0400

    add belongs_to test support new sync status complete

--- spec/phone_spec/app/Product_s/product_s.rb
+++ spec/phone_spec/app/Product_s/product_s.rb
@@ -4,7 +4,12 @@ class Product_s
     include Rhom::FixedSchema
 
     enable :sync
+    set :sync_priority, 2 #should sync after Customer    
+    
     set :schema_version, '1.0'
+    
+    belongs_to :quantity, 'Customer_s'
+    belongs_to :sku, 'Customer_s'
     
     property :brand, :string
     property :created_at, :string
