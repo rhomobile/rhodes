a51f766ad71b20d5a7b6030672a8e128fa138929
genywind <evgeny@rhomobile.com>
2010-08-30 23:58:47 +0400

    add spanish files

---
+++ lib/framework/rholang/rhomsg_es.rb
@@ -0,0 +1,28 @@
+module Rho
+  class RhoMessages
+    LocalMessages = {
+      'sync_failed_for' => "Error de sincronización  ",
+      'details'         => " Detalles: ",
+      'sync_completed'  => "Sincronización completada.",
+      'syncronizing'    => "Sincronizando ",
+      'syncronizing_data' => "Sincronizando datos...",
+      
+	  'home_menu' => "Inicio",
+	  'refresh_menu' => "Actualizar",
+	  'back_menu' => "Atrás <Esc>",
+	  'sync_menu' => "Sincronizar",
+	  'options_menu' => "Opciones",
+	  'log_menu' => "Log",
+	  'close_menu' => "Cerrar",
+      'get_link_menu' => "Obtener vínculo",
+      'open_link_menu' => "Obtener vínculo",
+      'email_menu' => "Email",
+      'call_menu' => "Llamar",
+      'sms_menu' => "SMS",
+      'add_contacts_menu' => "Agregar a libreta",
+      
+      'hide' => 'Ocultar'
+    }
+    
+  end # RhoMessages
+end # Rho
