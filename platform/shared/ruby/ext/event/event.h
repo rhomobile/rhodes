63a9aa6dedae3889a06c47751f95310b7be2cfae
Dmitry Moskalchuk <dmitry@rhomobile.com>
2010-09-26 20:53:59 +0400

    Initial add of Event ruby extension (calendar)

---
+++ platform/shared/ruby/ext/event/event.h
@@ -0,0 +1,23 @@
+#ifndef RUBY_EVENT_H
+#define RUBY_EVENT_H 1
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/********************************************
+ * List of supported event's fields (visible in ruby)
+ ********************************************/
+
+#define RUBY_EV_ID "id"
+#define RUBY_EV_TITLE "title"
+#define RUBY_EV_CANCELED "canceled"
+#define RUBY_EV_ORGANIZER "organizer"
+#define RUBY_EV_START_DATE "start_date"
+#define RUBY_EV_END_DATE "end_date"
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
