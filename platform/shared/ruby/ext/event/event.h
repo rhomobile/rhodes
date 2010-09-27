d98c371cc0dcfbf7964702387c4f8855ccb191ec
Dmitry Moskalchuk <dmitry@rhomobile.com>
2010-09-27 17:48:05 +0400

    Fix date conversion (iPhone calendar)

--- platform/shared/ruby/ext/event/event.h
+++ platform/shared/ruby/ext/event/event.h
@@ -15,6 +15,9 @@ extern "C" {
 #define RUBY_EV_ORGANIZER "organizer"
 #define RUBY_EV_START_DATE "start_date"
 #define RUBY_EV_END_DATE "end_date"
+#define RUBY_EV_LAST_MODIFIED "last_modified"
+#define RUBY_EV_LOCATION "location"
+#define RUBY_EV_NOTES "notes"
 
 #ifdef __cplusplus
 }
