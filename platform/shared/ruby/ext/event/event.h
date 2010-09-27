b2a052517947edb0932f27f7a76f03a942edf8db
Dmitry Moskalchuk <dmitry@rhomobile.com>
2010-09-27 18:18:56 +0400

    Add attendees field

--- platform/shared/ruby/ext/event/event.h
+++ platform/shared/ruby/ext/event/event.h
@@ -13,11 +13,13 @@ extern "C" {
 #define RUBY_EV_TITLE "title"
 #define RUBY_EV_CANCELED "canceled"
 #define RUBY_EV_ORGANIZER "organizer"
+#define RUBY_EV_ATTENDEES "attendees"
 #define RUBY_EV_START_DATE "start_date"
 #define RUBY_EV_END_DATE "end_date"
 #define RUBY_EV_LAST_MODIFIED "last_modified"
 #define RUBY_EV_LOCATION "location"
 #define RUBY_EV_NOTES "notes"
+#define RUBY_EV_RECURRENCE "recurrence"
 
 #ifdef __cplusplus
 }
