2e0bf0eee0b36c706bf292dcbc9f63adb1d48124
Dmitry Moskalchuk <dmitry@rhomobile.com>
2010-09-28 18:56:32 +0400

    All event adapters (java <-> C, ruby <-> C) for Android done

---
+++ platform/android/Rhodes/src/com/rhomobile/rhodes/event/Event.java
@@ -0,0 +1,15 @@
+package com.rhomobile.rhodes.event;
+
+import java.util.Date;
+
+public class Event {
+	
+	public String id;
+	public String title;
+	public Date startDate;
+	public Date endDate;
+	public Date lastModified;
+	public String location;
+	public String notes;
+	
+}
