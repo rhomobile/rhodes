7648a696a07a78362a1c87eea5e43ad72d55c1b8
genywind <evgeny@rhomobile.com>
2010-10-05 12:58:36 +0400

    support cancel login in logout

--- platform/wm/rhodes/rho/net/NetRequest.cpp
+++ platform/wm/rhodes/rho/net/NetRequest.cpp
@@ -32,6 +32,7 @@ INetResponse* CNetRequest::pullCookies(const String& strUrl, const String& strBo
 
 INetResponse* CNetRequest::doRequest( const char* method, const String& strUrl, const String& strBody, IRhoSession* oSession, Hashtable<String,String>* pHeaders )
 {
+    m_bCancel = false;
     CNetRequestImpl oImpl(this, method,strUrl,oSession,pHeaders, m_sslVerifyPeer);
     return oImpl.sendString(strBody);
 }
@@ -51,6 +52,7 @@ void CNetRequest::cancel()
 
 INetResponse* CNetRequest::pushMultipartData(const String& strUrl, VectorPtr<CMultipartItem*>& arItems, IRhoSession* oSession, Hashtable<String,String>* pHeaders)
 {
+    m_bCancel = false;
     CNetRequestImpl oImpl(this, "POST",strUrl,oSession,pHeaders,m_sslVerifyPeer);
     CNetResponseImpl* pResp = oImpl.sendMultipartData(arItems);
     return pResp;
@@ -58,6 +60,7 @@ INetResponse* CNetRequest::pushMultipartData(const String& strUrl, VectorPtr<CMu
 
 INetResponse* CNetRequest::pushMultipartData(const String& strUrl, CMultipartItem& oItem, IRhoSession* oSession, Hashtable<String,String>* pHeaders)
 {
+    m_bCancel = false;
     VectorPtr<CMultipartItem*> arItems;
     arItems.addElement(&oItem);
 
@@ -69,6 +72,7 @@ INetResponse* CNetRequest::pushMultipartData(const String& strUrl, CMultipartIte
 
 INetResponse* CNetRequest::pullFile(const String& strUrl, const String& strFilePath, IRhoSession* oSession, Hashtable<String,String>* pHeaders)
 {
+    m_bCancel = false;
     common::CRhoFile oFile;
     if ( !oFile.open(strFilePath.c_str(),common::CRhoFile::OpenForAppend) ) 
     {
