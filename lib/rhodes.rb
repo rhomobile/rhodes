e5467fe3b7fa2c0e14c9aed3c4ac4879ff2218a9
lars <larsburgess@gmail.com>
2010-10-13 11:56:14 -0700

    prepping version for 2.2.0.beta.1

--- lib/rhodes.rb
+++ lib/rhodes.rb
@@ -1,9 +1,9 @@
 module Rhodes
   unless defined? Rhodes::VERSION
-    VERSION = '2.1.0'
+    VERSION = '2.2.0'
   end
   unless defined? Rhodes::DBVERSION
-    DBVERSION = '2.1.0'
+    DBVERSION = '2.2.0'
   end
   
 end
