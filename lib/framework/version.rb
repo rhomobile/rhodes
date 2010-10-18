366a5d37540f8947c14e9deb8990b1ea46e31953
lars <larsburgess@gmail.com>
2010-10-18 10:24:01 -0700

    prepping for 2.2.1

--- lib/framework/version.rb
+++ lib/framework/version.rb
@@ -1,8 +1,8 @@
 module RhodesFramework
   unless defined? RhodesFramework::VERSION
-    VERSION = '2.2.0'
+    VERSION = '2.2.1'
   end
   unless defined? RhodesFramework::DBVERSION
-    DBVERSION = '2.2.0'
+    DBVERSION = '2.2.1'
   end
 end
