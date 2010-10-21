d46ddb96de2169a5d0b3c0fd8d1d841c5229d781
dmitrys <dmitrys@rhomobile.com>
2010-10-21 22:21:31 +0400

    add SplitView for Tabs (only for iPad)

---
+++ platform/iphone/Classes/SplitView/SplittedMainView.h
@@ -0,0 +1,54 @@
+//
+//  TabbedMainView.h
+//  rhorunner
+//
+//  Created by Dmitry Moskalchuk on 26.03.10.
+//  Copyright 2010 Rhomobile. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "RhoMainView.h"
+#import "RhoViewController.h"
+
+
+@interface SplittedMainView : UISplitViewController<RhoMainView> {
+
+
+}
+
+
+- (id)initWithMainView:(id<RhoMainView>)v parent:(UIWindow*)p tabs:(NSArray*)items;
+
+
+
+
+
+
+//- (UIView*)view;
+
+- (UIWebView*)detachWebView;
+
+- (void)loadHTMLString:(NSString*)data;
+
+- (void)back:(int)index;
+- (void)forward:(int)index;
+- (void)navigate:(NSString*)url tab:(int)index;
+- (void)navigateRedirect:(NSString*)url tab:(int)index;
+- (void)reload:(int)index;
+
+- (void)executeJs:(NSString*)js tab:(int)index;
+
+- (NSString*)currentLocation:(int)index;
+
+- (void)switchTab:(int)index;
+- (int)activeTab;
+
+- (void)addNavBar:(NSString*)title left:(NSArray*)left right:(NSArray*)right;
+- (void)removeNavBar;
+
+- (UIWebView*)getWebView:(int)tab_index;
+
+
+
+
+@end
