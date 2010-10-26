2aa0be380a0489dea52d7f7548dea89cc37c3418
genywind <evgeny@rhomobile.com>
2010-10-26 17:38:12 +0400

    fix issue with bulk filename

--- platform/shared/net/URI.h
+++ platform/shared/net/URI.h
@@ -16,7 +16,9 @@ public:
     static String urlEncode(const String& fullPath);
     static String urlDecode(const String& url);	
 	static void urlDecode(const String &url, String& ret );
-	
+    static String urlEscapeSymbols(const String& url);	
+    static void urlEscapeSymbols(const String& fullPath, String& strRes);
+
     String getPath();
     String getQueryString();
     String getScheme();
