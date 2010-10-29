4507e6f3253f6ff17d6886b032dc5160e910f59e
genywind <evgeny@rhomobile.com>
2010-10-30 01:25:28 +0400

    save local chanages while run bulk sync - C-platform

--- platform/shared/sync/SyncEngine.cpp
+++ platform/shared/sync/SyncEngine.cpp
@@ -302,6 +302,22 @@ CSyncSource* CSyncEngine::findSourceByName(const String& strSrcName)
     return findSource(CSourceID(strSrcName));
 }
 
+void CSyncEngine::applyChangedValues(db::CDBAdapter& db)
+{
+    DBResult( resSrc , db.executeSQL( "SELECT DISTINCT(source_id) FROM changed_values" ) );
+    for ( ; !resSrc.isEnd(); resSrc.next() )
+    {
+        int nSrcID = resSrc.getIntByIdx(0);
+        DBResult( res, db.executeSQL("SELECT source_id,sync_type,name, partition from sources WHERE source_id=?", nSrcID) );
+        if ( res.isEnd() )
+            continue;
+
+        CSyncSource src( res.getIntByIdx(0), res.getStringByIdx(2), "none", db, *this );
+
+        src.applyChangedValues();
+    }
+}
+
 void CSyncEngine::loadAllSources()
 {
     m_sources.removeAllElements();
