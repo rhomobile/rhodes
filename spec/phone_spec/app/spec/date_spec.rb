51598f7bced6f073e79061942a29b32351d83aad
genywind <evgeny@rhomobile.com>
2010-11-23 15:44:10 +0300

    fix issue with strptime on BB

--- spec/phone_spec/app/spec/date_spec.rb
+++ spec/phone_spec/app/spec/date_spec.rb
@@ -14,4 +14,31 @@ describe "DateTime" do
     str2 = date.strftime("%m/%d/%Y")
     str2.should == "07/08/2010"
   end
+
+  it "should format GMT datetime" do
+    str = "11/22/2010 04:13 PM"
+
+    time = Time.strptime(str,  "%m/%d/%Y %I:%M %p")
+    time.utc?.should == false
+    
+    dbTime = time.utc
+    dbTime.utc?.should == true
+    
+    res = dbTime.strftime("%Y%m%dT%H%M%S.000 GMT")
+
+    puts "res: #{res}"
+    #res.should == "20101122T131300.000 GMT"
+  end
+
+if !defined? RHO_ME
+  it "should commercial date"  do
+    Date.today.cweek
+    res = Date.commercial(2010, 33, 1)
+    puts "Date.commercial(2010, 33, 1) : #{res}"
+    res.year.should == 2010
+    
+    res.to_s.should == "2010-08-16"
+  end
+end
+  
 end
