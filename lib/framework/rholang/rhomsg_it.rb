f8c69598d7b59d825cc7296fcc9d2d1ac8600945
genywind <evgeny@rhomobile.com>
2010-12-04 12:22:20 +0300

    support italian on blackberry

---
+++ lib/framework/rholang/rhomsg_it.rb
@@ -0,0 +1,28 @@
+module Rho
+  class RhoMessages
+    LocalMessages = {
+      'sync_failed_for' => "Sync failed for ",
+      'details'         => " Details: ",
+      'sync_completed'  => "Sync completed.",
+      'syncronizing'    => "Synchronizing ",
+      'syncronizing_data' => "Synchronizing data...",
+      
+	  'home_menu' => "Home",
+	  'refresh_menu' => "Refresh",
+	  'back_menu' => "Back",
+	  'sync_menu' => "Sync",
+	  'options_menu' => "Options",
+	  'log_menu' => "Log",
+	  'close_menu' => "Close",
+      'get_link_menu' => "Apri collegamento",
+      'open_link_menu' => "Apri collegamento",
+      'email_menu' => "Email",
+      'call_menu' => "Call",
+      'sms_menu' => "SMS",
+      'add_contacts_menu' => "Add to Contacts",
+      
+      'hide' => 'Hide'
+    }
+    
+  end # RhoMessages
+end # Rho
