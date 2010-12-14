c0b84c50bb44ee8a0b84c51ba046bb9cf9eae9cc
lars <larsburgess@gmail.com>
2010-12-14 14:40:11 -0800

    #7466905 - 'rhodes' command doesn't work on windows * bumping version

--- lib/framework/version.rb
+++ lib/framework/version.rb
@@ -1,6 +1,6 @@
 module RhodesFramework
   unless defined? RhodesFramework::VERSION
-    VERSION = '2.2.5'
+    VERSION = '2.2.6'
   end
   unless defined? RhodesFramework::DBVERSION
     DBVERSION = '2.2.2'
