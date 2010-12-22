b304ab277f30723d8a879dee33a9c9e6fe05b7ed
Dmitry Moskalchuk <dmitry@rhomobile.com>
2010-12-22 19:37:55 +0300

    Fix service usage from PushReceiver (Android)

--- platform/android/Rhodes/src/com/rhomobile/rhodes/PushReceiver.java
+++ platform/android/Rhodes/src/com/rhomobile/rhodes/PushReceiver.java
@@ -8,9 +8,21 @@ import android.util.Log;
 
 public class PushReceiver extends BroadcastReceiver {
 	
-	private static final String TAG = "PushReceiver";
+	private static final String TAG = PushReceiver.class.getSimpleName();
 	
 	private static final String REG_ID = "registration_id";
+	
+	public static final String INTENT_SOURCE = PushReceiver.class.getName();
+	
+	private static final String INTENT_PREFIX = PushReceiver.class.getPackage().getName();
+	
+	public static final String INTENT_TYPE = INTENT_PREFIX + ".type";
+	public static final String INTENT_REGISTRATION_ID = INTENT_PREFIX + ".registration_id";
+	public static final String INTENT_EXTRAS = INTENT_PREFIX + ".extras";
+	
+	public static final int INTENT_TYPE_UNKNOWN = 0;
+	public static final int INTENT_TYPE_REGISTRATION_ID = 1;
+	public static final int INTENT_TYPE_MESSAGE = 2;
 
 	private void handleRegistration(Context context, Intent intent) {
 		String id = intent.getStringExtra(REG_ID);
@@ -23,26 +35,22 @@ public class PushReceiver extends BroadcastReceiver {
 			Log.d(TAG, "Unregistered: " + unregistered);
 		}
 		else if (id != null) {
-			// TODO: store it in reg_id variable
-			Log.d(TAG, "Registered: id: " + id);
-			RhodesService r = RhodesService.getInstance();
-			if (r == null) {
-				Log.e(TAG, "RhodesService instance is null");
-				return;
-			}
-			r.setPushRegistrationId(id);
+			Intent serviceIntent = new Intent(context, RhodesService.class);
+			serviceIntent.putExtra(RhodesService.INTENT_SOURCE, INTENT_SOURCE);
+			serviceIntent.putExtra(INTENT_TYPE, INTENT_TYPE_REGISTRATION_ID);
+			serviceIntent.putExtra(INTENT_REGISTRATION_ID, id);
+			context.startService(serviceIntent);
 		}
 		else
 			Log.w(TAG, "Unknown registration event");
 	}
 	
 	private void handleMessage(Context context, Intent intent) {
-		RhodesService r = RhodesService.getInstance();
-		if (r == null) {
-			Log.e(TAG, "Rhodes instance is null");
-			return;
-		}
-		r.handlePushMessage(intent);
+		Intent serviceIntent = new Intent(context, RhodesService.class);
+		serviceIntent.putExtra(RhodesService.INTENT_SOURCE, INTENT_SOURCE);
+		serviceIntent.putExtra(INTENT_TYPE, INTENT_TYPE_MESSAGE);
+		serviceIntent.putExtra(INTENT_EXTRAS, intent.getExtras());
+		context.startService(serviceIntent);
 	}
 	
 	@Override
