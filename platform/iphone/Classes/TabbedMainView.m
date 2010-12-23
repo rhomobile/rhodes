8130107127d7f1dec8db81b74b6a6bb2b8c00171
dmitrys <dmitrys@rhomobile.com>
2010-12-23 07:09:28 +0300

    iPhone: support bkgColor for Native Toolbar and Tabbar, support custom selected color for tabbar, support disabled tabs in tabbar

--- platform/iphone/Classes/TabbedMainView.m
+++ platform/iphone/Classes/TabbedMainView.m
@@ -52,17 +52,265 @@
 @end
 
 
+
+@implementation RhoUITabBarController
+
+@synthesize bkgColor;
+
+- (UIImage*)makeUIImageWithGradient:(int)height colorR:(int)colorR colorG:(int)colorG colorB:(int)colorB {
+	// firstly make context
+    CGContextRef    context = NULL;
+    CGColorSpaceRef colorSpace;
+    void *          bitmapData;
+    int             bitmapByteCount;
+    int             bitmapBytesPerRow;
+	
+    size_t pixelsWide = 1;
+    size_t pixelsHigh = height;
+	
+    bitmapBytesPerRow   = (pixelsWide) << 2;
+    bitmapByteCount     = (bitmapBytesPerRow * pixelsHigh);
+	
+    colorSpace = CGColorSpaceCreateDeviceRGB();//CGColorSpaceCreateDeviceGray();//(kCGColorSpaceGenericRGB);
+	
+    bitmapData = malloc( bitmapByteCount );
+	
+    context = CGBitmapContextCreate (bitmapData,
+									 pixelsWide,
+									 pixelsHigh,
+									 8,      // bits per component
+									 bitmapBytesPerRow,
+									 colorSpace,
+									 kCGImageAlphaNoneSkipLast);
+	
+    CGColorSpaceRelease( colorSpace );
+	CGRect rect;
+	
+	float color12_r = ((float)colorR)/255.0;
+	float color12_g = ((float)colorG)/255.0;
+	float color12_b = ((float)colorB)/255.0;
+
+	float color21_r = color12_r;
+	float color21_g = color12_g;
+	float color21_b = color12_b;
+	
+	
+	float color11_r = color12_r + 0.3;
+	float color11_g = color12_g + 0.3;
+	float color11_b = color12_b + 0.3;
+	if (color11_r > 1) color11_r = 1.0;
+	if (color11_g > 1) color11_g = 1.0;
+	if (color11_b > 1) color11_b = 1.0;
+	
+	float color22_r = color21_r - 0.2;
+	float color22_g = color21_g - 0.2;
+	float color22_b = color21_b - 0.2;
+	if (color22_r < 0) color22_r = 0.0;
+	if (color22_g < 0) color22_g = 0.0;
+	if (color22_b < 0) color22_b = 0.0;
+
+	color12_r = color12_r+0.07;
+	color12_g = color12_g+0.07;
+	color12_b = color12_b+0.07;
+	if (color12_r > 1) color12_r = 1;
+	if (color12_g > 1) color12_g = 1;
+	if (color12_b > 1) color12_b = 1;
+	
+	int  y0 =0;
+	int  y1 = ((float)height)*0.5;
+	int  y2 = ((float)height)*0.5;
+	int  y3 = (float)height;
+	
+	rect.origin.x = 0;
+	rect.origin.y = y0;
+	rect.size.width = pixelsWide;
+	rect.size.height = y1-y0;
+	
+	
+	int i;
+			
+	for (i = y0; i <= y1; i++ ) {
+		float k = ((float)i - y0)/((float)(y1 - y0));
+		k = sqrt(k);
+		float curR = color22_r*(1-k) + color21_r*k; 
+		float curG = color22_g*(1-k) + color21_g*k; 
+		float curB = color22_b*(1-k) + color21_b*k; 
+		rect.origin.y = i;
+		rect.size.height = 1;
+		CGContextSetRGBFillColor(context, curR, curG, curB, 1);
+		CGContextFillRect(context, rect);
+
+	
+	}
+
+	for (i = y2; i <= y3; i++ ) {
+		float k = ((float)i - y2)/((float)(y3 - y2));
+		k= k*k;
+		float curR = color12_r*(1-k) + color11_r*k; 
+		float curG = color12_g*(1-k) + color11_g*k; 
+		float curB = color12_b*(1-k) + color11_b*k; 
+		rect.origin.y = i;
+		rect.size.height = 1;
+		CGContextSetRGBFillColor(context, curR, curG, curB, 1);
+		CGContextFillRect(context, rect);
+		
+		
+	}
+
+	rect.origin.y = y3-1;
+	rect.size.height = 1;
+	CGContextSetRGBFillColor(context, 0, 0, 0, 1);
+	CGContextFillRect(context, rect);
+	
+	CGImageRef cgImage = CGBitmapContextCreateImage(context);
+	
+	UIImage* ui = [UIImage imageWithCGImage:cgImage];
+	
+	CGContextRelease(context);
+	
+	return ui;
+}
+
+- (void)viewDidLoad {
+	
+    [super viewDidLoad]; 
+	
+    CGRect frame = self.tabBar.frame;
+	frame.origin.x = 0;
+	frame.origin.y = 0;
+    UIView *v = [[UIView alloc] initWithFrame:frame];
+    
+	int cR = (self.bkgColor & 0xFF0000) >> 16;
+	int cG = (self.bkgColor & 0xFF00) >> 8;
+	int cB = (self.bkgColor & 0xFF);
+	
+	UIImage *i = [self makeUIImageWithGradient:(int)frame.size.height colorR:cR colorG:cG colorB:cB];
+	
+    UIColor *c = [[UIColor alloc] initWithPatternImage:i];
+    v.backgroundColor = c;
+	v.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
+    [c release];
+    [[self tabBar] insertSubview:v atIndex:0];
+    [v release];
+	
+}
+
+
+@end
+
+
+@implementation RhoCustomTabBarItem
+
+@synthesize customHighlightedImage;
+@synthesize customStdImage;
+
+- (void) dealloc
+{
+    [customHighlightedImage release]; customHighlightedImage=nil;
+    [customStdImage release]; customStdImage=nil;   
+    [super dealloc];
+}
+
+-(UIImage *) selectedImage
+{
+    return self.customHighlightedImage;
+}
+
+/*
+-(UIImage *) unselectedImage
+{
+    return self.customStdImage;
+}
+*/
+@end
+
+
+
 @implementation TabbedMainView
 
 @synthesize tabbar, tabbarData, tabindex;
+
+
+
+- (UIImage*)recolorImageWithColor:(UIImage*)image color:(UIColor *)color shadowColor:(UIColor *)shadowColor shadowOffset:(CGSize)shadowOffset shadowBlur:(CGFloat)shadowBlur
+{
+    CGColorRef cgColor = [color CGColor];
+    CGColorRef cgShadowColor = [shadowColor CGColor];
+
+	CGRect contextRect;
+    contextRect.origin.x = 0.0f;
+    contextRect.origin.y = 0.0f;
+    contextRect.size = [image size];
+	contextRect.size.width += 6;
+	contextRect.size.height += 6;
+
+    UIImage *itemImage = image;
+    CGSize itemImageSize = [itemImage size];
+    CGPoint itemImagePosition; 
+    itemImagePosition.x = ceilf((contextRect.size.width - itemImageSize.width) / 2);
+    itemImagePosition.y = ceilf((contextRect.size.height - itemImageSize.height) / 2);
+    UIGraphicsBeginImageContext(contextRect.size);
+    CGContextRef c = UIGraphicsGetCurrentContext();
+
+    CGContextSetShadowWithColor(c, shadowOffset, shadowBlur, cgShadowColor);
+
+    CGContextBeginTransparencyLayer(c, NULL);
+    CGContextScaleCTM(c, 1.0, -1.0);
+    CGContextClipToMask(c, CGRectMake(itemImagePosition.x, -itemImagePosition.y, itemImageSize.width, -itemImageSize.height), [itemImage CGImage]);
+
+    CGContextSetFillColorWithColor(c, cgColor);
+    contextRect.size.height = -contextRect.size.height;
+	
+	float* colorComponents = CGColorGetComponents(cgColor);
+	float color0R = colorComponents[0];
+	float color0G = colorComponents[1];
+	float color0B = colorComponents[2];
+	
+	float color1R = color0R+0.6;
+	float color1G = color0G+0.6;
+	float color1B = color0B+0.6;
+	if (color1R > 1) color1R = 1; 
+	if (color1G > 1) color1G = 1; 
+	if (color1B > 1) color1B = 1; 
+	
+	CGFloat components[16] = {	color0R, color0G, color0B, 1.0, 
+								color0R, color0G, color0B, 1.0,
+								color1R, color1G, color1B, 1.0,
+								1.0, 1.0, 1.0, 1.0};
+	CGFloat locations[4] = {	0, 0.3, 0.8, 1.0 }; 
+	CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceRGB();  
+	CGGradientRef colorGradient = CGGradientCreateWithColorComponents(colorSpace, components, locations, 4);
+	CGContextDrawLinearGradient(c, colorGradient, CGPointMake(0,contextRect.size.height), CGPointMake(0,0), 0);	
+	CGContextDrawLinearGradient(c, colorGradient, CGPointMake(contextRect.size.width*0.75,contextRect.size.height), CGPointMake(contextRect.size.width*0.25, 0), 0);	
+	
+    //CGContextFillRect(c, contextRect);
+    CGContextEndTransparencyLayer(c);
+
+    UIImage* resImage = UIGraphicsGetImageFromCurrentImageContext();
+	UIGraphicsEndImageContext();
+	return resImage;
+}
+
+
+
 
 - (id)initWithMainView:(id<RhoMainView>)v parent:(UIWindow*)p tabs:(NSArray *)items {
 	[SimpleMainView disableHiddenOnStart];
     CGRect frame = [[v view] frame];
     
+	NSString *background_color_enable = [items objectAtIndex:0];
+	NSString *background_color = [items objectAtIndex:1];
 	
-    tabbar = [[UITabBarController alloc] initWithNibName:nil bundle:nil];
-    tabbar.delegate = [Rhodes sharedInstance];
+	if ([background_color_enable isEqualToString:@"true"]) {
+		RhoUITabBarController* rc = [RhoUITabBarController alloc];
+		rc.bkgColor = [background_color intValue];
+		rc = [rc initWithNibName:nil bundle:nil];
+		tabbar = rc;
+	}
+	else {
+		tabbar = [[UITabBarController alloc] initWithNibName:nil bundle:nil];
+    }
+	tabbar.delegate = [Rhodes sharedInstance];
     tabbar.view.frame = frame;
     tabbar.selectedIndex = 0;
     //tabbar.tabBar.autoresizingMask = UIViewAutoresizingFlexibleLeftMargin | UIViewAutoresizingFlexibleTopMargin | UIViewAutoresizingFlexibleWidth;
@@ -80,19 +328,23 @@
 	//else {
 		childFrame.size.height -= tbFrame.size.height;
 	//}
-    int count = [items count]/5;
+    int count = ([items count] -2 )/8;
     NSMutableArray *views = [NSMutableArray arrayWithCapacity:count];
     NSMutableArray *tabs = [[NSMutableArray alloc] initWithCapacity:count];
     
     NSString *initUrl = nil;
     
     for (int i = 0; i < count; ++i) {
-        int index = i*5 - 1;
+        int index = i*8 - 1 + 2;
         NSString *label = [items objectAtIndex:++index];
         NSString *url = [items objectAtIndex:++index];
         NSString *icon = [items objectAtIndex:++index];
         NSString *reload = [items objectAtIndex:++index];
         NSString *colored_icon = [items objectAtIndex:++index];
+		
+		NSString *selected_color = (NSString*)[items objectAtIndex:++index];  
+		NSString *selected_color_enable = (NSString*)[items objectAtIndex:++index];  
+		NSString *disabled = (NSString*)[items objectAtIndex:++index];  
         
 		colored_icon = nil;
 		
@@ -107,7 +359,42 @@
             SimpleMainView *subController = [[SimpleMainView alloc] initWithParentView:tabbar.view frame:childFrame];
             subController.title = label;
             NSString *imagePath = [[AppManager getApplicationsRootPath] stringByAppendingPathComponent:icon];
-            subController.tabBarItem.image = [UIImage imageWithContentsOfFile:imagePath];
+			
+			
+			if ([selected_color_enable isEqualToString:@"true"]) {
+				
+				int sel_col = [selected_color intValue];
+				int cR = (sel_col & 0xFF0000) >> 16;
+				int cG = (sel_col & 0xFF00) >> 8;
+				int cB = (sel_col & 0xFF);
+				
+				RhoCustomTabBarItem *tabItem = [[RhoCustomTabBarItem alloc]
+												initWithTitle:label image:nil tag:0];
+			
+				tabItem.image = [UIImage imageWithContentsOfFile:imagePath];
+				tabItem.badgeValue = nil;
+
+				//subController.tabBarItem.image = [UIImage imageWithContentsOfFile:imagePath];
+				//subController.tabBarItem.badgeValue = nil;
+				
+				UIImage* img = [UIImage imageWithContentsOfFile:imagePath];
+				//img = [self recolorImageWithColor:img color:[UIColor colorWithRed:(43.0 / 255.0) green:(143.0 / 255.0) blue:(230.0 / 255.0) alpha:1.0] shadowColor:[UIColor blackColor] shadowOffset:CGSizeMake(0.5f, 1.0f) shadowBlur:3.0f];
+				img = [self recolorImageWithColor:img color:[UIColor colorWithRed:( ((float)(cR)) / 255.0) green:(((float)(cG)) / 255.0) blue:(((float)(cB)) / 255.0) alpha:1.0] shadowColor:[UIColor blackColor] shadowOffset:CGSizeMake(0.5f, 1.0f) shadowBlur:3.0f];
+				tabItem.customHighlightedImage = img;
+				tabItem.customStdImage=nil;       
+			
+				subController.tabBarItem=tabItem;
+				[tabItem release]; 
+			}
+			else {
+				subController.tabBarItem.image = [UIImage imageWithContentsOfFile:imagePath];
+				subController.tabBarItem.badgeValue = nil;
+			}
+			
+			if ([disabled isEqualToString:@"true"]) {
+				subController.tabBarItem.enabled = NO;
+			}
+			
 			subController.mTabBarCallback = self;
 	    //[subController navigateRedirect:url tab:0];
             
@@ -139,6 +426,7 @@
     
     // Release any cached data, images, etc that aren't in use.
 }
+
 
 - (void)viewDidUnload {
     [super viewDidUnload];
