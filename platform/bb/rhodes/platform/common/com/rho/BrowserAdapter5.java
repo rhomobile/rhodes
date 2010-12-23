79f3bf6d694a652b375f3c37d025d76feb12b029
genywind <evgeny@rhomobile.com>
2010-12-23 22:59:32 +0300

    fix bb4.6 build

--- platform/bb/rhodes/platform/common/com/rho/BrowserAdapter5.java
+++ platform/bb/rhodes/platform/common/com/rho/BrowserAdapter5.java
@@ -20,6 +20,9 @@ public class BrowserAdapter5 implements IBrowserAdapter
     public void executeJavascript(String strJavascript)
     {
     }
+    public void setCookie(String url, String cookie)
+    {
+    }
     
     public boolean navigationMovement(int dx, int dy, int status, int time)
     {
