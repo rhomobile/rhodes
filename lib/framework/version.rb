61df5ea67b21edc82a6ce9f36baee08811bf3f3f
lars <larsburgess@gmail.com>
2010-12-27 17:47:48 -0800

    * Fixed 'rhodes' command to work via relative path * prepped master branch version for 2.3.0

--- lib/framework/version.rb
+++ lib/framework/version.rb
@@ -1,6 +1,6 @@
 module RhodesFramework
   unless defined? RhodesFramework::VERSION
-    VERSION = '2.2.6'
+    VERSION = '2.3.0'
   end
   unless defined? RhodesFramework::DBVERSION
     DBVERSION = '2.2.2'
