04b1d82c10a6d51017e6b0a175a33a6cd210b091
genywind <evgeny@rhomobile.com>
2011-01-17 23:47:19 +0300

    support db encryption for bb with sqlite db

--- platform/bb/rhodes/src/rhomobile/NativeBar.java
+++ platform/bb/rhodes/src/rhomobile/NativeBar.java
@@ -10,13 +10,45 @@ public class NativeBar
 				return RubyConstant.QNIL;
 			}
 		});
+		klass.getSingletonClass().defineMethod("create_native_toolbar", new RubyTwoArgMethod() {
+			protected RubyValue run(RubyValue receiver, RubyValue arg0, RubyValue arg1, RubyBlock block) {
+				return RubyConstant.QNIL;
+			}
+		});
+		klass.getSingletonClass().defineMethod("create_native_tabbar", new RubyTwoArgMethod() {
+			protected RubyValue run(RubyValue receiver, RubyValue arg0, RubyValue arg1, RubyBlock block) {
+				return RubyConstant.QNIL;
+			}
+		});
+		
 		klass.getSingletonClass().defineMethod("remove", new RubyNoArgMethod() {
 			protected RubyValue run(RubyValue receiver, RubyBlock block) {
 				return RubyConstant.QNIL;
 			}
 		});
+		klass.getSingletonClass().defineMethod("remove_native_toolbar", new RubyNoArgMethod() {
+			protected RubyValue run(RubyValue receiver, RubyBlock block) {
+				return RubyConstant.QNIL;
+			}
+		});
+		klass.getSingletonClass().defineMethod("remove_native_tabbar", new RubyNoArgMethod() {
+			protected RubyValue run(RubyValue receiver, RubyBlock block) {
+				return RubyConstant.QNIL;
+			}
+		});
+		
 		klass.getSingletonClass().defineMethod("switch_tab", new RubyOneArgMethod() {
 			protected RubyValue run(RubyValue receiver, RubyValue arg0, RubyBlock block) {
+				return RubyConstant.QNIL;
+			}
+		});
+		klass.getSingletonClass().defineMethod("native_tabbar_switch_tab", new RubyOneArgMethod() {
+			protected RubyValue run(RubyValue receiver, RubyValue arg0, RubyBlock block) {
+				return RubyConstant.QNIL;
+			}
+		});
+		klass.getSingletonClass().defineMethod("native_tabbar_set_tab_badge", new RubyTwoArgMethod() {
+			protected RubyValue run(RubyValue receiver, RubyValue arg0, RubyValue arg1, RubyBlock block) {
 				return RubyConstant.QNIL;
 			}
 		});
