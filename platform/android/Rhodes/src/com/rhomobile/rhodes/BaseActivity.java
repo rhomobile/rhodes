f67b2626b7545450dec11f0b6869a2b72f660ce3
Dmitry Moskalchuk <dmitry@rhomobile.com>
2011-01-29 01:54:55 +0300

    Fix freeze on splash screen if main activity was killed in background (Android)

--- platform/android/Rhodes/src/com/rhomobile/rhodes/BaseActivity.java
+++ platform/android/Rhodes/src/com/rhomobile/rhodes/BaseActivity.java
@@ -2,15 +2,42 @@ package com.rhomobile.rhodes;
 
 import android.app.Activity;
 import android.app.Application;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
 import android.content.res.Configuration;
+import android.os.Bundle;
+import android.os.IBinder;
 import android.util.Log;
 
-public class BaseActivity extends Activity {
+public class BaseActivity extends Activity implements ServiceConnection {
 	
 	private static final String TAG = BaseActivity.class.getName();
 	
 	private static final boolean DEBUG = false;
+	
+	protected RhodesService mRhodesService;
+	private boolean mBoundToService;
 
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		Intent intent = new Intent(this, RhodesService.class);
+		bindService(intent, this, Context.BIND_AUTO_CREATE);
+		mBoundToService = true;
+	}
+	
+	@Override
+	protected void onDestroy() {
+		if (mBoundToService) {
+			unbindService(this);
+			mBoundToService = false;
+		}
+		super.onDestroy();
+	}
+	
 	@Override
 	protected void onStart() {
 		super.onStart();
@@ -35,5 +62,19 @@ public class BaseActivity extends Activity {
 		if (DEBUG)
 			Log.d(TAG, "getApplication: " + app);
 		return (RhodesApplication)app;
+	}
+
+	@Override
+	public void onServiceConnected(ComponentName name, IBinder service) {
+		mRhodesService = ((RhodesService.LocalBinder)service).getService();
+		if (DEBUG)
+			Log.d(TAG, "Connected to service: " + mRhodesService);
+	}
+
+	@Override
+	public void onServiceDisconnected(ComponentName name) {
+		if (DEBUG)
+			Log.d(TAG, "Disconnected from service: " + mRhodesService);
+		mRhodesService = null;
 	}
 }
