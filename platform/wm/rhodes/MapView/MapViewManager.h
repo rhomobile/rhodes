9c48def285611ba02b504211bca2ed2269825756
dmitrys <dmitrys@rhomobile.com>
2011-02-04 18:39:33 +0300

    add MapView WM first

---
+++ platform/wm/rhodes/MapView/MapViewManager.h
@@ -0,0 +1,154 @@
+
+#pragma once
+
+#include <windows.h>
+
+#include "stdafx.h"
+
+#include "Graphics.h"
+#include <string>
+#include "ext/rho/rhoruby.h"
+#include "common/StringConverter.h"
+
+#include "resource.h"
+
+
+using namespace rho::common;
+using namespace rho::common::map;
+using namespace rho;
+using namespace stdext;
+
+/*
+class MapAnnotation {
+public:
+	MapAnnotation(String _title, String _subtitle, String _adress, String _url, double _longitude, double _latitude, bool adressBased) {
+		mTitle = _title;
+		mSubtitle = _subtitle;
+		mAdress = _adress;
+		mUrl = _url;
+		mLongitude = _longitude;
+		mLatitude = _latitude;
+		mIsAdressBased = adressBased;
+	}
+	String const &title() {
+		return mTitle;
+	}
+	String const &subtitle() {
+		return mSubtitle;
+	}
+	String const &address() {
+		return mAdress;
+	}
+	String const &url() {
+		return mUrl;
+	}
+	double const latitude() {
+		return mLatitude;
+	}
+	double const longitude() {
+		return mLongitude;
+	}
+	bool const isAdressBased() {
+		return mIsAdressBased;
+	}
+private:
+	String mTitle;
+	String mSubtitle;
+	String mAdress;
+	String mUrl;
+	double mLongitude;
+	double mLatitude;
+	bool mIsAdressBased;
+};
+*/
+
+/*
+class IMapView
+{
+public:
+	virtual ~IMapView(){}
+
+	virtual void setSize(int width, int height) = 0;
+	virtual int width() = 0;
+	virtual int height() = 0;
+
+	virtual void setMapType(String const &type) = 0;
+	virtual String const mapType() = 0;
+
+	virtual void setZoom(int zoom) = 0;
+	virtual void setZoom(double latDelta, double lonDelta) = 0;
+	virtual int zoom() = 0;
+	virtual int minZoom() = 0;
+	virtual int maxZoom() = 0;
+
+	virtual void moveTo(double latitude, double longitude) = 0;
+	virtual void moveTo(String geoCode) = 0;
+	virtual void move(int dx, int dy) = 0;
+	virtual double latitude() = 0;
+	virtual double longitude() = 0;
+
+	virtual void addAnnotation(Annotation* ann) = 0;
+
+	// Return true if this MapView need to be closed
+	virtual bool handleClick(int x, int y) = 0;
+
+	virtual void paint(IDrawingContext* context) = 0;
+
+	virtual void setGraphicsDevice(IDrawingDevice* device) = 0;
+};
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class CRhoMapViewDlg : public //CDialog
+#if defined(OS_WINDOWS)
+	COkCancelModalDialog<CRhoMapViewDlg>
+#else
+	CDialogImpl <CRhoMapViewDlg>
+#endif	
+{
+public:
+	CRhoMapViewDlg  ();
+	~CRhoMapViewDlg ();
+
+	enum { IDD = IDD_MAPVIEW };
+
+	BEGIN_MSG_MAP(CRhoBluetoothDiscoverDlg)
+		MESSAGE_HANDLER(WM_INITDIALOG, OnInitDialog)
+		MESSAGE_HANDLER(WM_PAINT, OnDraw)
+		MESSAGE_HANDLER(WM_VSCROLL, OnSliderScroll)
+		COMMAND_ID_HANDLER(IDOK, OnOK)
+		COMMAND_ID_HANDLER(IDCANCEL, OnCancel)
+		COMMAND_ID_HANDLER(IDC_BUTTON_ZOOM_IN, OnZoomIn)
+		COMMAND_ID_HANDLER(IDC_BUTTON_ZOOM_OUT, OnZoomOut)
+	END_MSG_MAP()
+
+public:
+	LRESULT OnInitDialog(UINT /*uMsg*/, WPARAM /*wParam*/, LPARAM /*lParam*/, BOOL& /*bHandled*/);
+	LRESULT OnDraw(UINT /*uMsg*/, WPARAM /*wParam*/, LPARAM /*lParam*/, BOOL& /*bHandled*/);
+	LRESULT OnSliderScroll(UINT /*uMsg*/, WPARAM /*wParam*/, LPARAM /*lParam*/, BOOL& /*bHandled*/);
+
+	LRESULT OnOK(WORD /*wNotifyCode*/, WORD wID, HWND /*hWndCtl*/, BOOL& /*bHandled*/);
+	LRESULT OnCancel(WORD /*wNotifyCode*/, WORD wID, HWND /*hWndCtl*/, BOOL& /*bHandled*/);
+	LRESULT OnZoomIn(WORD /*wNotifyCode*/, WORD wID, HWND /*hWndCtl*/, BOOL& /*bHandled*/);
+	LRESULT OnZoomOut(WORD /*wNotifyCode*/, WORD wID, HWND /*hWndCtl*/, BOOL& /*bHandled*/);
+
+	void doOpen();
+	void doClose();
+
+	void requestRedraw();
+
+
+	rho_param* mParams;
+};
