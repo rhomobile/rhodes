57760fd977935b511e845fa967c4a4b4ba47f443
Steven Moore <stave177@gmail.com>
2011-02-08 17:48:34 -0800

    Adding contributing.txt with info about automated testing

--- platform/shared/ruby/ext/rho/extensions.c
+++ platform/shared/ruby/ext/rho/extensions.c
@@ -1,3 +1,9 @@
 // WARNING! THIS FILE IS GENERATED AUTOMATICALLY! DO NOT EDIT IT MANUALLY!
+extern void Init_digest(void);
+extern void Init_digest(void);
+extern void Init_md5(void);
 void Init_Extensions(void) {
+    Init_digest();
+    Init_digest();
+    Init_md5();
 }
