73dfa0052c3fef3151dabd0ef5c7b14fbed250e6
Steven Moore <stave177@gmail.com>
2011-02-08 17:51:02 -0800

    Reverting extensions.c back to how it was before my last commit...

--- platform/shared/ruby/ext/rho/extensions.c
+++ platform/shared/ruby/ext/rho/extensions.c
@@ -1,9 +1,3 @@
 // WARNING! THIS FILE IS GENERATED AUTOMATICALLY! DO NOT EDIT IT MANUALLY!
-extern void Init_digest(void);
-extern void Init_digest(void);
-extern void Init_md5(void);
 void Init_Extensions(void) {
-    Init_digest();
-    Init_digest();
-    Init_md5();
 }
