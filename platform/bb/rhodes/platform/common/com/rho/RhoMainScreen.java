04ba45a14e5a952ec43b989ffd3b2934b95eba62
genywind <evgeny@rhomobile.com>
2011-02-10 19:08:03 +0300

    fix bb 4.6

--- platform/bb/rhodes/platform/common/com/rho/RhoMainScreen.java
+++ platform/bb/rhodes/platform/common/com/rho/RhoMainScreen.java
@@ -2,7 +2,17 @@ package com.rho;
 
 import net.rim.device.api.ui.container.MainScreen;
 
-public abstract class RhoMainScreen extends MainScreen {
+public abstract class RhoMainScreen extends MainScreen 
+{
+	public RhoMainScreen()
+	{
+		super();
+	}
+
+	public RhoMainScreen(long style)
+	{
+		super(style);
+	}
 	
 	protected boolean onTouchUnclick(){ return false; }
 	
