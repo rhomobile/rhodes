dbb185c0771a97ed7a4845135b9873e6cd001572
unknown <geny@.(none)>
2011-02-10 18:58:31 +0300

    support bb 4.6

--- platform/bb/rhodes/platform/4.7/com/rho/RhoMainScreen.java
+++ platform/bb/rhodes/platform/4.7/com/rho/RhoMainScreen.java
@@ -5,20 +5,52 @@ import net.rim.device.api.system.Display;
 import net.rim.device.api.ui.TouchEvent;
 import net.rim.device.api.ui.container.MainScreen;
 
-public abstract class RhoMainScreen extends MainScreen {
+public abstract class RhoMainScreen extends MainScreen 
+{
+	public RhoMainScreen()
+	{
+		super();
+	}
+
+	public RhoMainScreen(long style)
+	{
+		super(style);
+	}
 	
-	protected abstract boolean onTouchUnclick();
+	protected boolean onTouchUnclick(int x, int y){ return false; }
+	protected boolean onTouchClick(int x, int y){ return false; }
+	protected boolean onTouchDown(int x, int y){ return false; }
+	protected boolean onTouchUp(int x, int y){ return false; }
+	protected boolean onTouchMove(int x, int y){ return false; }
 	
 	protected boolean touchEvent(TouchEvent message) {
 		if ( RhodesApplication.getInstance().isInputDisabled() )
 			return true;
 		
 		int nEvent = message.getEvent();
-		
-		if ( nEvent == TouchEvent.UNCLICK )
+		switch( nEvent )
 		{
-			if ( onTouchUnclick() )
+		case TouchEvent.CLICK:
+			if ( onTouchClick(message.getX(1), message.getY(1)) )
 				return true;
+			break;
+		case TouchEvent.UNCLICK:
+			if ( onTouchUnclick(message.getX(1), message.getY(1)) )
+				return true;
+			break;
+			
+		case TouchEvent.DOWN:
+			if ( onTouchDown(message.getX(1), message.getY(1)) )
+				return true;
+			break;
+		case TouchEvent.UP:
+			if ( onTouchUp(message.getX(1), message.getY(1)) )
+				return true;
+			break;
+		case TouchEvent.MOVE:
+			if ( onTouchMove(message.getX(1), message.getY(1)) )
+				return true;
+			break;
 		}
 		
 		return super.touchEvent(message);
@@ -30,7 +62,7 @@ public abstract class RhoMainScreen extends MainScreen {
 	    return net.rim.device.api.ui.Touchscreen.isSupported();
 	}
 	
-	protected abstract void onChangeOrientation(int x, int y, int nOrientation);
+	protected void onChangeOrientation(int x, int y, int nOrientation){}
 	protected void sublayout(int arg0, int arg1) 
 	{
 		onChangeOrientation(arg0, arg1, Display.getOrientation()== Display.ORIENTATION_LANDSCAPE ? 1 : 2);
