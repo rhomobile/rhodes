c3c7bb9084e855b1bfba53620f93bd9865af4fbd
unknown <User@.(none)>
2011-02-12 05:27:48 +0300

    add support click on Annotation to WM and Android

--- platform/android/Rhodes/src/com/rhomobile/rhodes/mapview/OneTouchHandler.java
+++ platform/android/Rhodes/src/com/rhomobile/rhodes/mapview/OneTouchHandler.java
@@ -17,6 +17,11 @@ public class OneTouchHandler implements TouchHandler {
 		mMapTouch = mapTouch;
 	}
 	
+	private static final float CLICK_TOLERANCE = 8;
+	private float mFirstTouchX;
+	private float mFirstTouchY;
+	private boolean mIsClickPossible;
+	
 	private void dumpEvent(MotionEvent event) {
 		String names[] = { "DOWN", "UP", "MOVE", "CANCEL", "OUTSIDE",
 				"5?", "6?", "7?", "8?", "9?" };
@@ -38,17 +43,35 @@ public class OneTouchHandler implements TouchHandler {
 		int action = event.getAction();
 		switch (action) {
 		case MotionEvent.ACTION_DOWN:
+			mIsClickPossible = true;
+			mFirstTouchX = event.getX();
+			mFirstTouchY = event.getY();
 			mMapTouch.touchDown(new Touch(event.getX(), event.getY()), null);
 			break;
 		case MotionEvent.ACTION_UP:
 			mMapTouch.touchUp(new Touch(event.getX(), event.getY()), null);
+			// check for click
+			if (checkDistance(event.getX(), event.getY(), mFirstTouchX, mFirstTouchY, CLICK_TOLERANCE) && mIsClickPossible) {
+				mMapTouch.touchClick(new Touch(event.getX(), event.getY()));
+			}
 			break;
 		case MotionEvent.ACTION_MOVE:
+			if (!checkDistance(event.getX(), event.getY(), mFirstTouchX, mFirstTouchY, CLICK_TOLERANCE)) {
+				mIsClickPossible = false;
+			}
 			mMapTouch.touchMove(new Touch(event.getX(), event.getY()), null);
 			break;
 		}
 		
 		return true;
+	}
+	
+	private boolean checkDistance(float x1, float y1, float x2, float y2, float delta) {
+		float dx = x1 - x2;
+		if (dx < 0) dx = -dx;
+		float dy = y1 - y2;
+		if (dy < 0) dy = -dy;
+		return ((dx <= delta) && (dy <= delta));
 	}
 
 }
