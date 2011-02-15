a99b9259c6484259f5bfd7051a9b70752c7bc056
genywind <geny@.(none)>
2011-02-15 21:25:30 +0300

    optimize strftime

--- lib/framework/dateME.rb
+++ lib/framework/dateME.rb
@@ -11,26 +11,65 @@ class Time
     str    
   end
   
-  def strftime(fmt='%F')
+  def __getPM()
+    strPM = 'AM'
+    nHour = hour();
+    if nHour >= 12
+        strPM = 'PM'
+        nHour = nHour-12 if nHour > 12
+    end
 
+	strPM, nHour
+  end
+
+  def strftime(fmt='%F')
     if fmt == "%m/%d/%Y, %I:%M%p"
         strRes = "" 
-        strPM = 'AM'
-        nHour = hour();
-        if nHour >= 12
-            strPM = 'PM'
-            nHour = nHour-12 if nHour > 12
-        end
-        
+        strPM, nHour = __getPM()
+
         strRes += __makeTwoDigit(mon()) + '/' + __makeTwoDigit(mday()) + '/' + year().to_s + ', ' + __makeTwoDigit(nHour) + ':' + __makeTwoDigit(min()) + strPM;
         return strRes 
     elsif fmt == "%Y%m%dT%H%M%S.000 GMT"
         strRes = year().to_s() +  __makeTwoDigit(mon()) + __makeTwoDigit(mday()) + "T" + __makeTwoDigit(hour()) + __makeTwoDigit(min()) + __makeTwoDigit(sec()) +
             ".000 GMT"
         
-        return strRes 
+        return strRes
+	elsif fmt == '%a %b %d %Y'		 
+        strRes = LocalizationSimplified::DateHelper::AbbrDaynames[wday()] + ' ' + LocalizationSimplified::DateHelper::AbbrMonthnames[mon()] + ' ' + __makeTwoDigit(mday()) + ' ' + year().to_s()
+		return strRes 
+	elsif fmt == '%d %Y'		 
+        strRes = "Test"
+		return strRes 
+    elsif fmt == '%Y%m%d'
+        strRes = year().to_s() +  __makeTwoDigit(mon()) + __makeTwoDigit(mday())
+		return strRes 
+    elsif fmt == '%a'
+		strRes = LocalizationSimplified::DateHelper::AbbrDaynames[wday()]
+		return strRes 
+    elsif fmt == '%b'
+		strRes = LocalizationSimplified::DateHelper::AbbrMonthnames[mon()]
+		return strRes 
+    elsif fmt == '%d'
+		strRes = __makeTwoDigit(mday())
+		return strRes 
+    elsif fmt == '%Y'
+		strRes = year().to_s()
+		return strRes 
+    elsif fmt == '%M'
+		strRes =  __makeTwoDigit(min())
+		return strRes 
+    elsif fmt == '%H'
+		strRes =  __makeTwoDigit(hour())
+		return strRes 
+    elsif fmt == '%p'
+        strPM, nHour = __getPM()
+		strRes =  strPM
+		return strRes 
+
     end
-    
+
+	puts "strftime: " + fmt
+
     DateTime.new(self).strftime(fmt)
   end
 
@@ -46,7 +85,9 @@ class Date
     Date.new( Time.strptime(str,fmt) )
   end
 
-  def to_s() format('%.4d-%02d-%02d', year(), mon(), mday()) end # 4p
+  def to_s() 
+	format('%.4d-%02d-%02d', year(), mon(), mday()) 
+  end # 4p
   
   def self.civil(y=-4712, m=1, d=1, sg=2299161)
     Date.new( Time.new(y,m,d) )
