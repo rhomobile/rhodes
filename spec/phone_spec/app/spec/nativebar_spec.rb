27a0aa941285ad28445c67dacd202f4daf764ed5
rognar <kostyar2004@bk.ru>
2011-02-25 20:21:03 +0300

    3 ui tests r failed each time on the CiJoe server

--- spec/phone_spec/app/spec/nativebar_spec.rb
+++ spec/phone_spec/app/spec/nativebar_spec.rb
@@ -2,15 +2,15 @@ describe "NativeBar" do
 
   it "should create" do
     NativeBar.create Rho::RhoApplication::NOBAR_TYPE, []
-    sleep 2
+    sleep 20
     NativeBar.started.should ==  false
 
     NativeBar.create Rho::RhoApplication::TOOLBAR_TYPE, [{:action => :back}]
-    sleep 2
+    sleep 20
     NativeBar.started.should ==  true
 
     NativeBar.remove
-    sleep 2
+    sleep 20
     NativeBar.started.should ==  false
 
   end
