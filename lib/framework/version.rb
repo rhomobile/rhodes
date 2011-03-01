c117fdcf83d30975cb473bf83ece2d36780decb9
lars <larsburgess@gmail.com>
2011-03-01 11:19:53 -0800

    bumping version for release

--- lib/framework/version.rb
+++ lib/framework/version.rb
@@ -1,6 +1,6 @@
 module RhodesFramework
   unless defined? RhodesFramework::VERSION
-    VERSION = '2.3.0'
+    VERSION = '2.3.1'
   end
   unless defined? RhodesFramework::DBVERSION
     DBVERSION = '2.2.2'
