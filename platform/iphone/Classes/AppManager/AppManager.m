bbb5e6d08878969a0522d662975fcba49844f0d8
dmitrys <dmitrys@rhomobile.com>
2011-03-02 21:14:05 +0300

    revert last 4 commits

--- platform/iphone/Classes/AppManager/AppManager.m
+++ platform/iphone/Classes/AppManager/AppManager.m
@@ -250,20 +250,12 @@ VALUE rho_sys_get_locale()
 int rho_sys_get_screen_width()
 {
     CGRect rect = [[UIScreen mainScreen] bounds];
-    UIInterfaceOrientation current_orientation = [[UIApplication sharedApplication] statusBarOrientation];
-	if ((current_orientation == UIInterfaceOrientationLandscapeLeft) || (current_orientation == UIInterfaceOrientationLandscapeRight)) {
-        return rect.size.height;
-	}
     return rect.size.width;
 }
 
 int rho_sys_get_screen_height()
 {
     CGRect rect = [[UIScreen mainScreen] bounds];
-    UIInterfaceOrientation current_orientation = [[UIApplication sharedApplication] statusBarOrientation];
-	if ((current_orientation == UIInterfaceOrientationLandscapeLeft) || (current_orientation == UIInterfaceOrientationLandscapeRight)) {
-        return rect.size.width;
-	}
     return rect.size.height;
 }
 
@@ -354,16 +346,6 @@ int rho_sysimpl_get_property(char* szPropName, VALUE* resValue)
         {*resValue = rho_ruby_create_integer(rho_sys_get_screen_width()); return 1; }
     else if (strcasecmp("screen_height", szPropName) == 0)
         {*resValue = rho_ruby_create_integer(rho_sys_get_screen_height()); return 1; }
-    else if (strcasecmp("orientation", szPropName) == 0) {
-        UIInterfaceOrientation current_orientation = [[UIApplication sharedApplication] statusBarOrientation];
-        if ((current_orientation == UIInterfaceOrientationLandscapeLeft) || (current_orientation == UIInterfaceOrientationLandscapeRight)) {
-            *resValue = rho_ruby_create_string("landscape");
-        }
-        else {
-            *resValue = rho_ruby_create_string("portrait");
-        }
-        return 1;
-    }
     else if (strcasecmp("has_network", szPropName) == 0)
         {*resValue = rho_sys_has_network(); return 1; }
     else if (strcasecmp("has_camera", szPropName) == 0) {
