77c3ab62cee87fe0796748db8a75d4f5fab5a104
rognar <kostyar2004@bk.ru>
2011-03-04 19:08:50 +0300

    Rhodes projec tfor WP 7

---
+++ platform/wp7/rhodes/Rhodes/App.xaml.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+
+using Microsoft.Scripting.Hosting;
+using Microsoft.Scripting;
+using System.Windows.Resources;
+using System.IO;
+
+namespace Rhodes
+{
+    public partial class App : Application
+    {
+        /// <summary>
+        /// Provides easy access to the root frame of the Phone Application.
+        /// </summary>
+        /// <returns>The root frame of the Phone Application.</returns>
+        public PhoneApplicationFrame RootFrame { get; private set; }
+
+        /// <summary>
+        /// Constructor for the Application object.
+        /// </summary>
+        public App()
+        {
+            // Global handler for uncaught exceptions. 
+            UnhandledException += Application_UnhandledException;
+
+            // Show graphics profiling information while debugging.
+            if (System.Diagnostics.Debugger.IsAttached)
+            {
+                // Display the current frame rate counters.
+                Application.Current.Host.Settings.EnableFrameRateCounter = true;
+
+                // Show the areas of the app that are being redrawn in each frame.
+                //Application.Current.Host.Settings.EnableRedrawRegions = true;
+
+                // Enable non-production analysis visualization mode, 
+                // which shows areas of a page that are being GPU accelerated with a colored overlay.
+                //Application.Current.Host.Settings.EnableCacheVisualization = true;
+            }
+
+            // Standard Silverlight initialization
+            InitializeComponent();
+
+            // Phone-specific initialization
+            InitializePhoneApplication();
+        }
+
+        public class RhoHost : ScriptHost
+        {
+            private readonly PlatformAdaptationLayer/*!*/ _pal;
+
+            public RhoHost()
+            {
+                _pal = new WP_PlatformAdaptationLayer();
+            }
+
+            public override PlatformAdaptationLayer PlatformAdaptationLayer
+            {
+                get { return _pal; }
+            }
+        }
+
+        ScriptRuntime _runtime;
+        ScriptEngine _engine;
+
+        private void initRuby()
+        {
+            ScriptRuntimeSetup runtimeSetup = ScriptRuntimeSetup.ReadConfiguration();
+            var languageSetup = IronRuby.RubyHostingExtensions.AddRubySetup(runtimeSetup);
+
+            runtimeSetup.DebugMode = false;
+            runtimeSetup.PrivateBinding = false;
+            runtimeSetup.HostType = typeof(RhoHost);
+
+            //runtimeSetup.HostArguments = new object[] { testCase.Options };
+            languageSetup.Options["NoAdaptiveCompilation"] = false;
+            languageSetup.Options["CompilationThreshold"] = 0;
+            languageSetup.Options["Verbosity"] = 2;
+
+            _runtime = IronRuby.Ruby.CreateRuntime(runtimeSetup);
+            _engine = IronRuby.Ruby.GetEngine(_runtime);
+
+            System.Collections.ObjectModel.Collection<string> paths = new System.Collections.ObjectModel.Collection<string>();
+            paths.Add("lib");
+            paths.Add("apps/app");
+            _engine.SetSearchPaths(paths);
+
+            //IronRuby.Runtime.Loader
+            //context.Loader.LoadAssembly(assemblyName.ConvertToString(), initializer, true, true);
+            //IronRuby.Libraries
+            //IronRuby.StandardLibrary.StringScanner.StringScannerLibraryInitializer
+        }
+
+        object _rhoframework;
+        private void RhoRubyStart()
+        {
+            string code = "def foo; 'haha'; end; foo()";
+            //string code = "class MyClass; def initialize(arg1); end; end; MyClass.new('');";
+
+            StreamResourceInfo sr = Application.GetResourceStream(new Uri("rho/lib/rhoframework.rb", UriKind.Relative));
+            using (System.IO.BinaryReader br = new BinaryReader(sr.Stream))
+            {
+                char[] str = br.ReadChars((int)sr.Stream.Length);
+                code = new string(str);
+            }
+
+            ScriptSource src = _engine.CreateScriptSourceFromString(code);
+            if (src == null)
+                return;
+
+            _rhoframework = src.Execute(_engine.CreateScope());
+            if (_rhoframework == null)
+                return;
+
+        }
+
+        // Code to execute when the application is launching (eg, from Start)
+        // This code will not execute when the application is reactivated
+        private void Application_Launching(object sender, LaunchingEventArgs e)
+        {
+            initRuby();
+            RhoRubyStart();
+        }
+
+        // Code to execute when the application is activated (brought to foreground)
+        // This code will not execute when the application is first launched
+        private void Application_Activated(object sender, ActivatedEventArgs e)
+        {
+        }
+
+        // Code to execute when the application is deactivated (sent to background)
+        // This code will not execute when the application is closing
+        private void Application_Deactivated(object sender, DeactivatedEventArgs e)
+        {
+        }
+
+        // Code to execute when the application is closing (eg, user hit Back)
+        // This code will not execute when the application is deactivated
+        private void Application_Closing(object sender, ClosingEventArgs e)
+        {
+        }
+
+        // Code to execute if a navigation fails
+        private void RootFrame_NavigationFailed(object sender, NavigationFailedEventArgs e)
+        {
+            if (System.Diagnostics.Debugger.IsAttached)
+            {
+                // A navigation has failed; break into the debugger
+                System.Diagnostics.Debugger.Break();
+            }
+        }
+
+        // Code to execute on Unhandled Exceptions
+        private void Application_UnhandledException(object sender, ApplicationUnhandledExceptionEventArgs e)
+        {
+            if (System.Diagnostics.Debugger.IsAttached)
+            {
+                // An unhandled exception has occurred; break into the debugger
+                System.Diagnostics.Debugger.Break();
+            }
+        }
+
+        #region Phone application initialization
+
+        // Avoid double-initialization
+        private bool phoneApplicationInitialized = false;
+
+        // Do not add any additional code to this method
+        private void InitializePhoneApplication()
+        {
+            if (phoneApplicationInitialized)
+                return;
+
+            // Create the frame but don't set it as RootVisual yet; this allows the splash
+            // screen to remain active until the application is ready to render.
+            RootFrame = new PhoneApplicationFrame();
+            RootFrame.Navigated += CompleteInitializePhoneApplication;
+
+            // Handle navigation failures
+            RootFrame.NavigationFailed += RootFrame_NavigationFailed;
+
+            // Ensure we don't initialize again
+            phoneApplicationInitialized = true;
+        }
+
+        // Do not add any additional code to this method
+        private void CompleteInitializePhoneApplication(object sender, NavigationEventArgs e)
+        {
+            // Set the root visual to allow the application to render
+            if (RootVisual != RootFrame)
+                RootVisual = RootFrame;
+
+            // Remove this handler since it is no longer needed
+            RootFrame.Navigated -= CompleteInitializePhoneApplication;
+        }
+
+        #endregion
+    }
+}
