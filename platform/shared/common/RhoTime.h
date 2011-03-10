a5706754e03a8dd4e1b3ed2fc3ebf3fa9e6d46c3
genywind <evgeny@rhomobile.com>
2011-03-11 00:20:10 +0300

    add RhoTimer

--- platform/shared/common/RhoTime.h
+++ platform/shared/common/RhoTime.h
@@ -1,7 +1,7 @@
 #ifndef _RHOTIME_H_
 #define _RHOTIME_H_
 
-#include "RhoPort.h"
+#include "RhoStd.h"
 
 #if !defined( OS_WINDOWS ) && !defined(OS_WINCE)
 #include <sys/time.h>
@@ -113,6 +113,11 @@ public:
         return *this;
     }
 
+    void addMillis(int nMs)
+    {
+        m_nativeTime += nMs;
+    }
+
     static CTimeInterval getCurrentTime(){
         CTimeInterval res;
 
@@ -137,67 +142,35 @@ inline CTimeInterval operator-(const CTimeInterval& time1, const CTimeInterval& 
     return time1.minus(time2);
 }
 
-#if 0
-class CBaseTime{
-#if defined(OS_WINDOWS) || defined(OS_WINCE) 
-typedef SYSTEMTIME TimeType;
-#else
-#endif
+class CRhoTimer
+{
+    struct CTimerItem
+    {
+        int m_nInterval;
+        CTimeInterval m_oFireTime;
+        String m_strCallback;
+        String m_strCallbackData;
 
+        CTimerItem(int nInterval, const char* szCallback, const char* szCallbackData);
+    };
+
+    Vector<CTimerItem> m_arItems;
+
+    void callTimerCallback(CTimerItem& oItem);
 public:
-    CBaseTime(){ memset( &m_nativeTime, 0, sizeof(m_nativeTime) ); }
-/*
-    static int64 getTickCount() {
-        #if defined( OS_WINDOWS ) || defined(OS_WINCE)
-          SYSTEMTIME now;
-          GetSystemTime(&now);
-          return (static_cast<int64>(now.wSecond) * 1000000 +
-                  static_cast<int64>(now.wMilliseconds) * 1000);
-        #else
-          struct timeval tv;
-          gettimeofday(&tv, NULL);
-          return static_cast<int64>(tv.tv_sec) * 1000000 + tv.tv_usec;
-        #endif
-    }
-*/
-    void setToLocalTime(){
-#if defined( OS_WINDOWS ) || defined(OS_WINCE)
-          GetLocalTime(&m_nativeTime);
-#else
-#endif
-    }
-    void setToUTCTime(){
-#if defined( OS_WINDOWS ) || defined(OS_WINCE)
-          GetSystemTime(&m_nativeTime);
-#else
-#endif
+    CRhoTimer(){}
+
+    void addTimer(int nInterval, const char* szCallback, const char* szCallbackData)
+    {
+        m_arItems.addElement(CTimerItem(nInterval, szCallback, szCallbackData));
     }
 
-protected:
-
-    TimeType m_nativeTime;
+    timeval getNextTimeout();
+    boolean checkTimers();
+    void stopTimer(const char* szCallback);
 };
 
-class CLocalTime : public CBaseTime{
-public:
-    CLocalTime(){ setToLocalTime(); }
-
-    String toString(){ 
-        char timeBuf[100];
-
-#if defined( OS_WINDOWS ) || defined(OS_WINCE)
-        sprintf(timeBuf, "%02d/%02d/%04d %02d:%02d:%02d", m_nativeTime.wMonth, m_nativeTime.wDay, m_nativeTime.wYear,
-            m_nativeTime.wHour, m_nativeTime.wMinute, m_nativeTime.wSecond );
-#else
-#endif
-
-        return String(timeBuf); 
-    }
-
-};
-
-#endif //0
 }
 }
 
-#endif //_RHOFILEPATH_H_
+#endif //_RHOTIME_H_
