1b8d7a863961fc388c2c1a3123aa4af171962d7f
genywind <evgeny@rhomobile.com>
2011-03-17 11:04:53 +0300

    add HttpServer to WP7

---
+++ platform/wp7/rhodes/rho/common/RhoFile.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO.IsolatedStorage;
+using System.Windows;
+using System.Windows.Resources;
+using System.IO;
+
+namespace rho.common
+{
+    public class CRhoFile
+    {
+        public static bool isResourceFileExist(String path)
+        {
+            if (path.StartsWith("/"))
+                path = path.Substring(1);
+
+            StreamResourceInfo sr = Application.GetResourceStream(new Uri(path, UriKind.Relative));
+            if (sr == null)
+                return false;
+
+            return sr != null;
+        }
+
+        public static bool isFileExist(String path)
+        {
+            IsolatedStorageFile isoStore = IsolatedStorageFile.GetUserStoreForApplication();
+            return isoStore.FileExists(path);
+        }
+    }
+}
