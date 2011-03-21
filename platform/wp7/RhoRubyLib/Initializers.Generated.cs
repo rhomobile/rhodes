b5581cb36c91eced07440dfb0fea7911e42a9136
rognar <kostyar2004@bk.ru>
2011-03-21 19:52:26 +0300

    IronRuby Wrapper for DB layer

--- platform/wp7/RhoRubyLib/Initializers.Generated.cs
+++ platform/wp7/RhoRubyLib/Initializers.Generated.cs
@@ -23,15 +23,78 @@ namespace rho.rubyext {
     
     public sealed class rubyextLibraryInitializer : IronRuby.Builtins.LibraryInitializer {
         protected override void LoadModules() {
+            IronRuby.Builtins.RubyClass classRef0 = GetClass(typeof(System.Object));
             
             
+            IronRuby.Builtins.RubyModule def1 = DefineGlobalModule("SQLite3", typeof(rho.rubyext.RhoSQLite3), 0x00000008, null, null, null, IronRuby.Builtins.RubyModule.EmptyArray);
             DefineGlobalModule("WebView", typeof(rho.rubyext.RhoWebView), 0x00000008, LoadWebView_Instance, LoadWebView_Class, null, IronRuby.Builtins.RubyModule.EmptyArray);
+            DefineGlobalClass("Database", typeof(rho.rubyext.RhoDatabase), 0x00000008, classRef0, LoadDatabase_Instance, null, null, new IronRuby.Builtins.RubyModule[] {def1}, 
+                new Func<IronRuby.Builtins.RubyClass, IronRuby.Builtins.MutableString, rho.rubyext.RhoDatabase>(rho.rubyext.RhoDatabase.Create)
+            );
+        }
+        
+        private static void LoadDatabase_Instance(IronRuby.Builtins.RubyModule/*!*/ module) {
+            DefineLibraryMethod(module, "close", 0x11, 
+                0x00000000U, 
+                new Func<rho.rubyext.RhoDatabase, System.Int32>(rho.rubyext.RhoDatabase.Close)
+            );
+            
+            DefineLibraryMethod(module, "commit", 0x11, 
+                0x00000000U, 
+                new Func<rho.rubyext.RhoDatabase, System.Int32>(rho.rubyext.RhoDatabase.Commit)
+            );
+            
+            DefineLibraryMethod(module, "destroy_tables", 0x11, 
+                0x00000000U, 
+                new Func<rho.rubyext.RhoDatabase, System.Int64, System.Int64, System.Int32>(rho.rubyext.RhoDatabase.destroyTables)
+            );
+            
+            DefineLibraryMethod(module, "execute", 0x11, 
+                0x00000000U, 
+                new Func<rho.rubyext.RhoDatabase, IronRuby.Builtins.MutableString, IronRuby.Builtins.RubyArray>(rho.rubyext.RhoDatabase.Execute)
+            );
+            
+            DefineLibraryMethod(module, "initialize", 0x12, 
+                0x00000002U, 
+                new Func<rho.rubyext.RhoDatabase, IronRuby.Builtins.MutableString, rho.rubyext.RhoDatabase>(rho.rubyext.RhoDatabase.Reinitialize)
+            );
+            
+            DefineLibraryMethod(module, "is_ui_waitfordb", 0x11, 
+                0x00000000U, 
+                new Func<rho.rubyext.RhoDatabase, System.Boolean>(rho.rubyext.RhoDatabase.isUiWaitForDb)
+            );
+            
+            DefineLibraryMethod(module, "lock_db", 0x11, 
+                0x00000000U, 
+                new Func<rho.rubyext.RhoDatabase, System.Int32>(rho.rubyext.RhoDatabase.Lock)
+            );
+            
+            DefineLibraryMethod(module, "rollback", 0x11, 
+                0x00000000U, 
+                new Func<rho.rubyext.RhoDatabase, System.Int32>(rho.rubyext.RhoDatabase.Rollback)
+            );
+            
+            DefineLibraryMethod(module, "start_transaction", 0x11, 
+                0x00000000U, 
+                new Func<rho.rubyext.RhoDatabase, System.Int32>(rho.rubyext.RhoDatabase.startTransaction)
+            );
+            
+            DefineLibraryMethod(module, "table_exist?", 0x11, 
+                0x00000000U, 
+                new Func<rho.rubyext.RhoDatabase, IronRuby.Builtins.MutableString, System.Boolean>(rho.rubyext.RhoDatabase.isTableExist)
+            );
+            
+            DefineLibraryMethod(module, "unlock_db", 0x11, 
+                0x00000000U, 
+                new Func<rho.rubyext.RhoDatabase, System.Int32>(rho.rubyext.RhoDatabase.Unlock)
+            );
+            
         }
         
         private static void LoadWebView_Instance(IronRuby.Builtins.RubyModule/*!*/ module) {
             DefineLibraryMethod(module, "navigate", 0x12, 
                 0x00010000U, 
-                new Func<System.Object, System.String, System.Boolean>(rho.rubyext.RhoWebView.Navigate)
+                new Action<System.Object, System.String>(rho.rubyext.RhoWebView.Navigate)
             );
             
         }
@@ -39,7 +102,7 @@ namespace rho.rubyext {
         private static void LoadWebView_Class(IronRuby.Builtins.RubyModule/*!*/ module) {
             DefineLibraryMethod(module, "navigate", 0x21, 
                 0x00010000U, 
-                new Func<System.Object, System.String, System.Boolean>(rho.rubyext.RhoWebView.Navigate)
+                new Action<System.Object, System.String>(rho.rubyext.RhoWebView.Navigate)
             );
             
         }
---
+++ platform/wp7/RhoRubyLib/rubyext/RhoDatabase.cs
@@ -0,0 +1,106 @@
+﻿using Microsoft.Scripting.Utils;
+using Microsoft.Scripting.Runtime;
+using IronRuby.Runtime;
+using IronRuby.Builtins;
+using System;
+
+namespace rho.rubyext
+{
+    [RubyClass("Database", Inherits = typeof(object)), Includes(typeof(RhoSQLite3))]
+    public class RhoDatabase : RubyObject
+    {
+        #region Construction
+
+        public RhoDatabase(RubyClass/*!*/ cls) 
+            : base(cls) {
+        }
+
+        public RhoDatabase(RubyClass/*!*/ cls, MutableString/*!*/ dbName) 
+            : base(cls) {
+            Reinitialize(this, dbName);
+        }
+
+        [RubyConstructor]
+        public static RhoDatabase/*!*/ Create(RubyClass/*!*/ self, [NotNull]MutableString/*!*/ dbName)
+        {
+            return new RhoDatabase(self, dbName);
+        }
+
+        [RubyMethod("initialize", RubyMethodAttributes.PrivateInstance)]
+        public static RhoDatabase/*!*/ Reinitialize(RhoDatabase/*!*/ self, [NotNull]MutableString/*!*/ dbName)
+        {  
+            return self;
+        }
+
+        #endregion
+
+        #region Public Instance Methods
+
+        [RubyMethod("close")]
+        public static int Close(RhoDatabase/*!*/ self)
+        {
+            return 1;
+        }
+
+        [RubyMethod("commit")]
+        public static int Commit(RhoDatabase/*!*/ self)
+        {
+            return 1;
+        }
+
+        [RubyMethod("destroy_tables")]
+        public static int destroyTables(RhoDatabase/*!*/ self, long arInclude, long arExclude)
+        {
+            return 1;
+        }
+
+        [RubyMethod("execute")]
+        public static RubyArray Execute(RhoDatabase/*!*/ self, MutableString/*!*/ sqlStatement)
+        {
+            RubyArray ret = new RubyArray();
+            return ret;
+        }
+
+        [RubyMethod("is_ui_waitfordb")]
+        public static Boolean isUiWaitForDb(RhoDatabase/*!*/ self)
+        {
+            return true;
+        }
+
+        [RubyMethod("lock_db")]
+        public static int Lock(RhoDatabase/*!*/ self)
+        {
+            return 1;
+        }
+
+        [RubyMethod("rollback")]
+        public static int Rollback(RhoDatabase/*!*/ self)
+        {
+            return 1;
+        }
+
+        [RubyMethod("start_transaction")]
+        public static int startTransaction(RhoDatabase/*!*/ self)
+        {
+            return 1;
+        }
+
+        [RubyMethod("table_exist?")]
+        public static Boolean isTableExist(RhoDatabase/*!*/ self, MutableString/*!*/ tblName)
+        {
+            return true;
+        }
+
+        [RubyMethod("unlock_db")]
+        public static int Unlock(RhoDatabase/*!*/ self)
+        {
+            return 1;
+        }
+
+        #endregion
+
+        #region Helpers
+
+        #endregion
+    }
+}
