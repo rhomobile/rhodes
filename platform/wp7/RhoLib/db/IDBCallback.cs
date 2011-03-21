add9ca1b6c408500dece6e48db127164ca34d087
genywind <evgeny@rhomobile.com>
2011-03-21 23:26:56 +0300

    add DB interfaces

---
+++ platform/wp7/RhoLib/db/IDBCallback.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace rho.db
+{
+    interface IDBCallback
+    {
+        void onBeforeDelete(String tableName, IDBResult rows2Delete);
+        void onBeforeUpdate(String tableName, IDBResult rows2Insert, int[] cols);
+    }
+}
