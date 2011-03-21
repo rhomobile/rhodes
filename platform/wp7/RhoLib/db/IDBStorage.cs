80424ded8f4774741ef03b198e86b6e6cc02e53c
genywind <evgeny@rhomobile.com>
2011-03-21 23:26:56 +0300

    add DB interfaces

---
+++ platform/wp7/RhoLib/db/IDBStorage.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using rho.common;
+
+namespace rho.db
+{
+    interface IDBStorage
+    {
+        void open(String strPath, String strSqlScript, String strEncryptionInfo);
+        void close();
+	
+        IDBResult executeSQL(String strStatement, Object[] values, boolean bReportNonUnique);
+	
+        void executeBatchSQL(String strStatement);
+	
+        IDBResult createResult();
+	
+        void deleteAllFiles(String strPath);
+	
+        void startTransaction();
+        void commit();
+        void rollback();
+        void onBeforeCommit();
+	
+        void setDbCallback(IDBCallback callback);
+	
+        String[] getAllTableNames();
+        boolean isTableExists(String strName);
+	
+        boolean isDbFileExists(String strPath);
+        void createTriggers();
+    }
+}
