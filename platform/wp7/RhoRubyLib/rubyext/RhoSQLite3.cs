cd6013844b79601895eb231f1c9e0152ddf82d8d
rognar <kostyar2004@bk.ru>
2011-03-21 19:52:26 +0300

    IronRuby Wrapper for DB layer

---
+++ platform/wp7/RhoRubyLib/rubyext/RhoSQLite3.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Scripting.Utils;
+using IronRuby.Runtime;
+using System;
+
+namespace rho.rubyext
+{
+    [RubyModule("SQLite3")]
+    public static class RhoSQLite3
+    {
+        #region Private Implementation Details
+
+        #endregion
+
+        #region Private Instance & Singleton Methods
+
+        #endregion
+    }
+}
