e51a1793da132c47d9729482b031febaa803eee2
genywind <evgeny@rhomobile.com>
2011-03-21 23:26:56 +0300

    add DB interfaces

---
+++ platform/wp7/RhoLib/common/RhoStd.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace rho.common
+{
+    struct boolean
+    {
+        private bool m_bValue;
+        public boolean(bool b) { m_bValue = b; }
+        public static implicit operator boolean(bool b) { return new boolean(b); }
+    };
+
+    public class Hashtable<TKey, TValue> : System.Collections.Generic.Dictionary<TKey, TValue>
+    {
+    }
+
+    public class Mutex
+    {
+    }
+}
