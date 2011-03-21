32ce6649eb8e5c216b8ca6a547ddeb8e820417a5
genywind <evgeny@rhomobile.com>
2011-03-21 23:26:56 +0300

    add DB interfaces

---
+++ platform/wp7/RhoLib/db/IDBResult.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using rho.common;
+
+namespace rho.db
+{
+    interface IDBResult
+    {
+    int getColCount();
+	String getColName(int nCol);
+	String getOrigColName(int nCol);
+	
+	//New
+    boolean isEnd();
+    boolean isOneEnd();
+    void next();
+	String getStringByIdx(int nCol);
+	int getIntByIdx(int nCol);
+	long getLongByIdx(int nCol);
+	String getUInt64ByIdx(int nCol);
+
+	Object /*RubyValue*/ getRubyValueByIdx(int nCol);
+	boolean isNullByIdx(int nCol);
+	
+	Object /*RubyValue*/ getRubyValue(String colname);
+	int getInt(String colname);
+	String getString(String colname);
+	
+	Object[] getCurData();
+	
+    boolean isNonUnique();
+    
+    void close();
+    }
+}
