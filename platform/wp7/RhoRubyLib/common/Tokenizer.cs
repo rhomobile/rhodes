5d0e9a2bd5599225fa995fa0bb6f85b418739ed0
genywind <evgeny@rhomobile.com>
2011-03-22 11:07:42 +0300

    remove RhoLib

---
+++ platform/wp7/RhoRubyLib/common/Tokenizer.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using rho.common;
+
+namespace rho.common
+{
+    public class Tokenizer
+    {
+    String m_str;
+	String m_delims;
+	int m_length;
+	int m_position;
+	
+	public Tokenizer(String str,String delims) {
+		m_delims = delims;
+		m_str = str;
+		m_length = str.length();
+		m_position = 0;
+	}
+	
+	public String nextToken() {
+		//eatDelimeters();
+		int start = m_position;
+		while (m_position<m_length && m_delims.indexOf(m_str.charAt(m_position))==-1) {
+			m_position++;
+		}
+		
+		String strToken = m_str.substring(start,m_position);
+		eatDelimeters();
+		return strToken;
+	}
+	
+	public void eatDelimeters() 
+	{
+		if ( m_position == m_length )
+			m_position++;
+		else if (m_position<m_length) {
+			char c = m_str.charAt(m_position);
+			if (m_delims.indexOf(c)>=0) {
+				m_position++;		
+			} else {
+				return;
+			}
+			
+		}
+	}
+	
+    public boolean hasMoreTokens() {
+    	//eatDelimeters();
+    	return (m_position <= m_length);
+    }
+    }
+}
