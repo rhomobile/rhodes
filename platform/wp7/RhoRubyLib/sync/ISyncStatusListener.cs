c61a896cc97116e2232f8bb62fcd46c9c1722bad
genywind <evgeny@rhomobile.com>
2011-03-23 13:53:57 +0300

    wp7 refactor

---
+++ platform/wp7/RhoRubyLib/sync/ISyncStatusListener.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace rho.sync
+{
+    interface ISyncStatusListener
+    {
+        void createStatusPopup(String status);
+        void reportStatus(String status, int error);
+    }
+}
