68c4c19c774f784351eea5be4405183ee7a2f7f3
rognar <kostyar2004@bk.ru>
2011-03-24 01:56:56 +0300

    sqlresult is implemented

--- platform/wp7/RhoRubyLib/db/SqliteResult.cs
+++ platform/wp7/RhoRubyLib/db/SqliteResult.cs
@@ -1,116 +1,178 @@
 ﻿using System;
 using System.Net;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Documents;
-using System.Windows.Ink;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Animation;
 using System.Windows.Shapes;
 using Community.CsharpSqlite;
+using System.Collections;
 using rho.common;
 
 namespace rho.db
 {
     public class CSqliteResult : IDBResult
     {
-
+        private Sqlite3.Vdbe m_st;
+        //private SqliteCopyResult m_resCopy;
+        private boolean m_bNonUnique = false;
         public CSqliteResult()
         {
         }
 
         public CSqliteResult(Sqlite3.Vdbe stmt)
         {
+            m_st = stmt;
+            if (m_st != null)
+                executeStatement(stmt);
         }
 
         public int getColCount()
         {
-            return 1;
+            return Sqlite3.sqlite3_data_count(m_st);
         }
 
         public String getColName(int nCol)
         {
-            return null;
+            return Sqlite3.sqlite3_column_name(m_st, nCol);
         }
 
         public String getOrigColName(int nCol)
         {
-            return null;
+            return Sqlite3.sqlite3_column_name(m_st, nCol);
         }
 
         //New
         public boolean isEnd()
         {
-            return true;
+            return m_st == null;
         }
 
         public boolean isOneEnd()
         {
-            return true;
+            return m_st == null;
         }
 
         public void next()
         {
+            if (m_st != null)
+                executeStatement(m_st);
         }
 
         public String getStringByIdx(int nCol)
         {
-            return null;
+            return Sqlite3.sqlite3_column_text(m_st, nCol);
         }
 
         public int getIntByIdx(int nCol)
         {
-            return 1;
+            return Sqlite3.sqlite3_column_int(m_st, nCol);
         }
 
         public long getLongByIdx(int nCol)
         {
-            return 1;
+            return Sqlite3.sqlite3_column_int64(m_st, nCol);
         }
 
         public String getUInt64ByIdx(int nCol)
         {
-            return null;
+            return getStringByIdx(nCol);
         }
 
         public Object /*RubyValue*/ getRubyValueByIdx(int nCol)
         {
-            return null;
+            return Sqlite3.sqlite3_column_value(m_st, nCol);
         }
 
         public boolean isNullByIdx(int nCol)
         {
-            return true;
+            return Sqlite3.sqlite3_column_type(m_st, nCol) == Sqlite3.SQLITE_NULL;
         }
 
         public Object /*RubyValue*/ getRubyValue(String colname)
         {
-            return null;
+            return getRubyValueByIdx( findColIndex(colname) );
         }
 
         public int getInt(String colname)
         {
-            return 1;
+            return getIntByIdx(findColIndex(colname));
         }
 
         public String getString(String colname)
         {
-            return null;
+            return getStringByIdx(findColIndex(colname));
         }
 
         public Object[] getCurData()
         {
-            return null;
+            String[] cols = getColumnNames();
+            Object[] res = new Object[cols.Length];
+            for (int i = 0; i < cols.Length; i++)
+                res[i] = Sqlite3.sqlite3_column_name(m_st, i);
+
+            return res;
         }
 
         public boolean isNonUnique()
         {
-            return true;
+            return m_bNonUnique;
         }
 
         public void close()
         {
+            if (m_st != null)
+            {
+                int res = Sqlite3.sqlite3_finalize( ref m_st );
+                if (res != Sqlite3.SQLITE_OK)
+                    throw new DBException(res, DBLastError());
+
+                m_st = null;
+            }
         }
+
+        #region Helpers
+
+        private String[] getColumnNames()
+        {
+            int cnt = Sqlite3.sqlite3_data_count(m_st);
+            String[] res = new String[cnt];
+            for (int i = 0; i < cnt; i++)
+            {
+                res[i] = Sqlite3.sqlite3_column_name(m_st, i);
+            }
+
+            return res;
+        }
+
+        private string DBLastError()
+        {
+            return Sqlite3.sqlite3_errmsg(m_st.db);
+        }
+
+        private bool executeStatement(Sqlite3.Vdbe pStmt)
+        {
+            int res = Sqlite3.sqlite3_step(pStmt);
+
+            if (res != Sqlite3.SQLITE_ROW)
+            {
+                close();
+                if (res != Sqlite3.SQLITE_OK && res != Sqlite3.SQLITE_ROW && res != Sqlite3.SQLITE_DONE)
+                    throw new DBException(res, DBLastError());
+            }
+
+            return res == Sqlite3.SQLITE_ROW;
+        }
+
+        private int findColIndex(String colname)
+        {
+            int cnt = Sqlite3.sqlite3_data_count(m_st);
+
+            for (int i = 0; i < cnt; i++)
+            {
+                if (colname == Sqlite3.sqlite3_column_name(m_st, i))
+                    return i;
+            }
+
+            return - 1;
+        }
+
+        #endregion
     }
 }
