62ae0dcf90844f38b1dee38ef0fc6c830d120242
Maris Krivtezs <marisks@gmail.com>
2011-04-01 11:33:29 +0300

    Initial commit of WM signature feature - Created signature dialog for signature capture - Created signature screen capture - Created signature saving as GIF, BMP, PNG

--- platform/wm/rhodes/signature/Signature.h
+++ platform/wm/rhodes/signature/Signature.h
@@ -1,3 +1,54 @@
+#pragma once
+#include <msinkaut.h>
 
+class CRhoTakeSignatureDlg : public 
+	CDialogImpl <CRhoTakeSignatureDlg>
+{
+public:
+	CRhoTakeSignatureDlg();
+	~CRhoTakeSignatureDlg();
+	
+	enum { IDD = IDD_TAKE_SIGNATURE };
+
+	BEGIN_MSG_MAP(CRhoTakeSignatureDlg)
+		MESSAGE_HANDLER(WM_INITDIALOG, OnInitDialog)
+		COMMAND_ID_HANDLER(IDOK, OnOK)
+		COMMAND_ID_HANDLER(IDCANCEL, OnCancel)
+	END_MSG_MAP()
+
+public:
+	LRESULT OnInitDialog(UINT /*uMsg*/, WPARAM /*wParam*/, LPARAM /*lParam*/, BOOL& /*bHandled*/);
+	LRESULT OnOK(WORD /*wNotifyCode*/, WORD wID, HWND /*hWndCtl*/, BOOL& /*bHandled*/);
+	LRESULT OnCancel(WORD /*wNotifyCode*/, WORD wID, HWND /*hWndCtl*/, BOOL& /*bHandled*/);
+
+	IInkOverlay*        m_pInkOverlay;
+};
+
+class Signature
+{
+public:
+	class Params {
+	public:
+		Params(String &callback_url, String &image_format){
+			m_callback_url = callback_url;
+			m_image_format = image_format;
+		}
+		String m_callback_url;
+		String m_image_format;
+	};
+public:
+	Signature(void);
+	virtual ~Signature(void);
+
+public: 
+	HRESULT takeSignature(HWND hwndOwner,LPTSTR pszFilename,LPCTSTR szFormat);
+
+private:
+	HRESULT saveInkOverlay(LPTSTR pszFilename,LPCTSTR szFormat);
+	HBITMAP getScreenHBITMAP();
+	HRESULT saveScreen(LPTSTR pszFilename,LPCTSTR szFormat);
+private:
+	CRhoTakeSignatureDlg dlg;
+};
 
 extern "C" void rho_signature_take_signature(char* callback_url, char* image_format);
