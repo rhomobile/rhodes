ff8f4ee5c101d766f71662e63de0e365bc434fa8
rognar <kostyar2004@bk.ru>
2011-04-05 19:55:39 +0400

    added NativeBar module

---
+++ platform/wp7/RhoRubyLib/rubyext/RhoNativeBar.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.Scripting.Utils;
+using Microsoft.Scripting.Runtime;
+using IronRuby.Runtime;
+using IronRuby.Builtins;
+using System;
+using System.Runtime.InteropServices;
+
+namespace rho.rubyext
+{
+    [RubyModule("NativeBar")]
+    public static class RhoNativeBar
+    {
+        private static CRhoRuby RhoRuby { get { return CRhoRuby.Instance; } }
+
+        #region Private Implementation Details
+
+        #endregion
+
+        #region Private Instance & Singleton Methods
+
+        [RubyMethodAttribute("create", RubyMethodAttributes.PublicSingleton)]
+        public static void Create(RubyModule/*!*/ self, int barType, RubyArray args)
+        {
+
+        }
+
+        [RubyMethodAttribute("remove", RubyMethodAttributes.PublicSingleton)]
+        public static void Remove(RubyModule/*!*/ self)
+        {
+
+        }
+
+        [RubyMethodAttribute("switch_tab", RubyMethodAttributes.PublicSingleton)]
+        public static void switchTab(RubyModule/*!*/ self, int index)
+        {
+
+        }
+
+        [RubyMethodAttribute("started", RubyMethodAttributes.PublicSingleton)]
+        public static Boolean isStarted(RubyModule/*!*/ self)
+        {
+            return true;
+        }
+
+        [RubyMethodAttribute("set_tab_badge", RubyMethodAttributes.PublicSingleton)]
+        public static void setTabBadge(RubyModule/*!*/ self, int index, MutableString/*!*/ val)
+        {
+
+        }
+
+        [RubyMethodAttribute("create_native_toolbar", RubyMethodAttributes.PublicSingleton)]
+        public static void createNativeToolBar(RubyModule/*!*/ self, int barType, RubyArray args)
+        {
+
+        }
+
+        [RubyMethodAttribute("remove_native_toolbar", RubyMethodAttributes.PublicSingleton)]
+        public static void removeNativeToolBar(RubyModule/*!*/ self)
+        {
+
+        }
+
+        [RubyMethodAttribute("create_native_tabbar", RubyMethodAttributes.PublicSingleton)]
+        public static void createNativeTabBar(RubyModule/*!*/ self, int barType, RubyArray args)
+        {
+
+        }
+
+        [RubyMethodAttribute("remove_native_tabbar", RubyMethodAttributes.PublicSingleton)]
+        public static void removeNativeTabBar(RubyModule/*!*/ self)
+        {
+
+        }
+
+        [RubyMethodAttribute("native_tabbar_switch_tab", RubyMethodAttributes.PublicSingleton)]
+        public static void nativeTabBarSwitchTab(RubyModule/*!*/ self, int index)
+        {
+
+        }
+
+        [RubyMethodAttribute("native_tabbar_set_tab_badge", RubyMethodAttributes.PublicSingleton)]
+        public static void nativeTabBarSetTabBadge(RubyModule/*!*/ self, int index, MutableString/*!*/ val)
+        {
+
+        }
+
+        [RubyMethodAttribute("native_tabbar_get_current_tab", RubyMethodAttributes.PublicSingleton)]
+        public static void nativeTabBarGetCurrentTab(RubyModule/*!*/ self)
+        {
+
+        }
+
+        #endregion
+    }
+}
