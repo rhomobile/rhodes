14e5a1ffa800ed42e30434ce9ed4326870b5feee
rognar <kostyar2004@bk.ru>
2011-04-11 01:46:50 +0400

    add callback to AsyncHttp

---
+++ platform/wp7/RhoRubyLib/rubyext/RhoKernelOps.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.Scripting.Utils;
+using Microsoft.Scripting.Runtime;
+using IronRuby.Runtime;
+using IronRuby.Builtins;
+using rho.common;
+
+namespace rho.rubyext
+{
+    public static class RhoKernelOps
+    {
+        private static CRhodesApp RhodesApp { get { return CRhodesApp.Instance; } }
+
+        public static Object __rhoGetCallbackObject(Object/*!*/ self, int index)
+        {
+            return RhodesApp.getCallbackObject(index); ;
+        }
+    }
+}
