f32df8025de6a0295aeed1c88f28f4529a2b7bb7
Dmitry Soldatenkov <dmitrys@rhomobile.com>
2011-04-21 20:55:22 +0400

    support title and initial time in DateTimePicker on WM

--- platform/wm/rhodes/DateTimePicker.cpp
+++ platform/wm/rhodes/DateTimePicker.cpp
@@ -41,8 +41,16 @@ CDateTimePickerDialog::~CDateTimePickerDialog ()
 
 LRESULT CDateTimePickerDialog::OnInitDialog(UINT /*uMsg*/, WPARAM /*wParam*/, LPARAM /*lParam*/, BOOL& /*bHandled*/)
 {
-	SetWindowText(_T("Date"));
-
+	if ((m_title != NULL) && (strlen(m_title) > 0)) {
+		String title = m_title;
+		StringW title_w = convertToStringW(title);
+		//SetWindowText(title_w.c_str());
+		SetDlgItemText(ID_DATETIME_CAPTION, title_w.c_str());
+		//GetDlgItem(ID_DATETIME_CAPTION).SetWindowText(title_w.c_str());
+	}
+	else {
+		SetWindowText(_T("Date"));
+	}
 #if defined(_WIN32_WCE)
 
     SHINITDLGINFO shidi = { SHIDIM_FLAGS, m_hWnd, SHIDIF_SIZEDLGFULLSCREEN };
@@ -69,6 +77,12 @@ LRESULT CDateTimePickerDialog::OnInitDialog(UINT /*uMsg*/, WPARAM /*wParam*/, LP
 	}
 	DateTime_SetRange( GetDlgItem(IDC_DATE_CTRL), flags, times);
 
+	SYSTEMTIME start_time;
+	if (m_initialTime != 0) {
+		UnixTimeToSystemTime(m_initialTime, &start_time);
+		DateTime_SetSystemtime( GetDlgItem(IDC_DATE_CTRL), GDT_VALID, &start_time);
+		DateTime_SetSystemtime( GetDlgItem(IDC_TIME_CTRL), GDT_VALID, &start_time);
+	}
 
 	GotoDlgCtrl(GetDlgItem(IDC_DATE_CTRL));
 
@@ -81,6 +95,8 @@ LRESULT CDateTimePickerDialog::OnInitDialog(UINT /*uMsg*/, WPARAM /*wParam*/, LP
 
 	DLG_ITEM_SET_FONT_BOLD (IDC_DATE_STATIC);
 	DLG_ITEM_SET_FONT_BOLD (IDC_TIME_STATIC);
+	DLG_ITEM_SET_FONT_BOLD (ID_DATETIME_CAPTION);
+	
 
 	if (m_format == CDateTimeMessage::FORMAT_DATE) {
 		GetDlgItem(IDC_TIME_CTRL).ShowWindow(SW_HIDE);
@@ -155,7 +171,16 @@ CTimePickerDialog::~CTimePickerDialog ()
 
 LRESULT CTimePickerDialog::OnInitDialog(UINT /*uMsg*/, WPARAM /*wParam*/, LPARAM /*lParam*/, BOOL& /*bHandled*/)
 {
-	SetWindowText(_T("Date"));
+	if ((m_title != NULL) && (strlen(m_title) > 0)) {
+		String title = m_title;
+		StringW title_w = convertToStringW(title);
+		//SetWindowText(title_w.c_str());
+		//GetDlgItem(IDC_TIME_CTRL).SetWindowText(title_w.c_str());
+		SetDlgItemText(ID_TIME_CAPTION, title_w.c_str());
+	}
+	else {
+		SetWindowText(_T("Date"));
+	}
 
 #if defined(_WIN32_WCE)
 
@@ -169,6 +194,12 @@ LRESULT CTimePickerDialog::OnInitDialog(UINT /*uMsg*/, WPARAM /*wParam*/, LPARAM
     RHO_ASSERT(SHCreateMenuBar(&mbi));
 	GotoDlgCtrl(GetDlgItem(IDC_TIME_CTRL));
 
+	SYSTEMTIME start_time;
+	if (m_initialTime != 0) {
+		UnixTimeToSystemTime(m_initialTime, &start_time);
+		DateTime_SetSystemtime( GetDlgItem(IDC_TIME_CTRL), GDT_VALID, &start_time);
+	}
+
 #else
 
 	CreateButtons();
@@ -177,6 +208,7 @@ LRESULT CTimePickerDialog::OnInitDialog(UINT /*uMsg*/, WPARAM /*wParam*/, LPARAM
 #endif
 
 	DLG_ITEM_SET_FONT_BOLD (IDC_TIME_STATIC);
+	DLG_ITEM_SET_FONT_BOLD (ID_TIME_CAPTION);
 
     return FALSE;
 }
@@ -203,15 +235,24 @@ LRESULT CTimePickerDialog::OnCancel(WORD /*wNotifyCode*/, WORD wID, HWND /*hWndC
 }
 
 
+static char ourTitle[1024];
+
 void  choose_datetime_with_range(char* callback, char* title, 
 					  long initial_time, int format, char* data,
 					  long min_time, long max_time)
 {
 	LOG(INFO) + __FUNCTION__ + "callback = " + callback + " title = " + title;
 
+	if (title != NULL) {
+		strcpy(ourTitle, title);
+	}
+	else {
+		strcpy(ourTitle, "");
+	}
+
 	HWND main_wnd = getMainWnd();
 	::PostMessage(main_wnd, WM_DATETIME_PICKER, 0, 
-					(LPARAM)new CDateTimeMessage(callback, title, initial_time, format, data, min_time, max_time));
+					(LPARAM)new CDateTimeMessage(callback, ourTitle, initial_time, format, data, min_time, max_time));
 }
 
 
