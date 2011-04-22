aab83048fdaf681c4795c0499217114585305d38
Albert R. Timashev <albert@timashev.com>
2011-04-22 13:17:44 +0400

    preparing for Qt Rhodes emulator merge

---
+++ platform/wm/rhodes/rho/rubyext/NativeToolbarExt.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "NativeToolbar.h"
+#include "MainWindow.h"
+
+#define TOOLBAR_TYPE		0
+#define TABBAR_TYPE			1
+#define NOBAR_TYPE			2
+#define VTABBAR_TYPE		3
+
+extern CMainWindow& getAppWindow();
