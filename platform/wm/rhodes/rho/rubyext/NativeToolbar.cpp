47cd30e95067aa1ffb49d3377fd389af0ec08ea3
Albert R. Timashev <albert@timashev.com>
2011-04-22 13:17:44 +0400

    preparing for Qt Rhodes emulator merge

--- platform/wm/rhodes/rho/rubyext/NativeToolbar.cpp
+++ platform/wm/rhodes/rho/rubyext/NativeToolbar.cpp
@@ -5,17 +5,12 @@
 #include "MainWindow.h"
 #include "common/RhoFilePath.h"
 #include "rubyext/WebView.h"
+#include "NativeToolbarExt.h"
 
 #ifndef max
 #define max(a,b)            (((a) > (b)) ? (a) : (b))
 #endif
 
-#define TOOLBAR_TYPE		0
-#define TABBAR_TYPE			1
-#define NOBAR_TYPE			2
-#define VTABBAR_TYPE		3
-
-extern CMainWindow& getAppWindow();
 IMPLEMENT_LOGCLASS(CNativeToolbar,"NativeToolbar");
 extern "C" int rho_wmsys_has_touchscreen();
 
@@ -434,85 +429,4 @@ void CNativeToolbar::removeToolbar()
 bool CNativeToolbar::isStarted()
 {
     return m_hWnd && IsWindowVisible();
-}
-
-extern "C"
-{
-void remove_native_toolbar();
-void create_native_toolbar(int bar_type, rho_param *p) 
-{
-    if ( bar_type == NOBAR_TYPE )
-        remove_native_toolbar();
-    else if ( bar_type == TOOLBAR_TYPE )
-    {
-        getAppWindow().performOnUiThread(new CNativeToolbar::CCreateTask(p) );
-    }else
-    {
-    	RAWLOGC_ERROR("NativeBar", "Only Toolbar control is supported.");
-    }
-}
-
-void create_nativebar(int bar_type, rho_param *p) 
-{
-	RAWLOGC_INFO("NativeBar", "NativeBar.create() is DEPRECATED. Use Rho::NativeToolbar.create() or Rho::NativeTabbar.create().");
-    create_native_toolbar(bar_type, p);
-}
-
-void remove_native_toolbar() 
-{
-    getAppWindow().performOnUiThread(new CNativeToolbar::CRemoveTask() );
-}
-
-void remove_nativebar() 
-{
-	RAWLOGC_INFO("NativeBar", "NativeBar.remove() is DEPRECATED API ! Please use Rho::NativeToolbar.remove() or Rho::NativeTabbar.remove().");
-	remove_native_toolbar();
-}
-
-VALUE nativebar_started() 
-{
-    bool bStarted = CNativeToolbar::getInstance().isStarted();
-    return rho_ruby_create_boolean(bStarted?1:0);
-}
-
-//Tabbar
-void create_native_tabbar(int bar_type, rho_param *p) {
-}
-
-void remove_native_tabbar() {
-}
-
-void native_tabbar_switch_tab(int index) {
-}
-
-void native_tabbar_set_tab_badge(int index,char *val) {
-}
-
-void nativebar_set_tab_badge(int index,char* val)
-{
-}
-
-int native_tabbar_get_current_tab() 
-{
-	return 0;
-}
-
-void nativebar_switch_tab(int index) {
-	//TODO: Implement me!
-}
-
-//NavBar - iphone only
-void create_navbar(rho_param *p)
-{
-}
-
-void remove_navbar()
-{
-}
-
-VALUE navbar_started()
-{
-    return rho_ruby_create_boolean(0);
-}
-
 }
