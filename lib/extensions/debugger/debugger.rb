787283bb9543d6cc94d92494469342b56d2103bd
Albert R. Timashev <albert@timashev.com>
2011-05-02 00:18:50 +0400

    debugger extension fixed for Windows

--- lib/extensions/debugger/debugger.rb
+++ lib/extensions/debugger/debugger.rb
@@ -1,17 +1,15 @@
 require 'timeout'
 
-def debug_read_cmd(io)
-    outbuf = ""
-    begin
-      r,w,e = IO.select([io],nil,nil,0)
-       while r
-         outbuf << io.read(1)
-         r,w,e = IO.select([io],nil,nil,0)
-       end
-    rescue
-      puts $!.inspect
+def debug_read_cmd(io,wait)
+  begin
+    if wait
+      $_cmd << io.readpartial(4096)
+    else
+      $_cmd << io.read_nonblock(4096)
     end
-    outbuf
+  rescue
+    # puts $!.inspect
+  end
 end
 
 def execute_cmd(cmd) 
@@ -27,31 +25,33 @@ def execute_cmd(cmd)
   $_s.write("EV:" + result + "\n")
 end
 
-def debug_handle_cmd(cmd)
-  puts "Received command: #{cmd}"
-  wait = true
-  if cmd =~/CONNECTED/
-    puts "Connected to debugger"
-    wait = false
-  elsif cmd =~/^BP/
-    ary = cmd.split(":")
-    $_file = ary[1]
-    $_line = ary[2].chomp
+def debug_handle_cmd()
+  cmd = $_cmd.match(/^([^\n\r])*([\n\r]+|$)/)[0]
+  $_cmd = $_cmd.sub(/^([^\n\r])*([\n\r]+|$)(.*$)/, "\\3")
+  if cmd != ""
+    puts "Received command: #{cmd}"
     wait = true
-    puts "Breakpoint received:#{$_file}:#{$_line}"
-  elsif cmd =~ /STEP/
-    $_step = true
-    wait = false
-    puts "Received STEP"
-  elsif cmd =~ /CONT/
-    wait = false
-    $_step = false
-  elsif cmd =~ /^EV:/
-    cmd = cmd.gsub(/^EV:/,"")
-    execute_cmd cmd
+    if cmd =~/^CONNECTED/
+      puts "Connected to debugger"
+      wait = false
+    elsif cmd =~/^BP/
+      ary = cmd.split(":")
+      $_file = ary[1].gsub(/\|/,':')
+      $_line = ary[2].chomp
+      wait = true
+      puts "Breakpoint received: #{$_file}:#{$_line}"
+    elsif cmd =~ /^STEP/
+      $_step = true
+      wait = false
+      puts "Received STEP"
+    elsif cmd =~ /^CONT/
+      wait = false
+      $_step = false
+    elsif cmd =~ /^EV:/
+      cmd = cmd.gsub(/^EV:/,"")
+      execute_cmd cmd
+    end
   end
-
-
   wait
 end
 
@@ -62,18 +62,19 @@ $_tracefunc = lambda{|event, file, line, id, bind, classname|
 
     if ($_file or $_step)
       if ((file =~ Regexp.new(Regexp.escape($_file) + '$') and line.to_i == $_line.to_i) or $_step)
-        $_s.write("BP:#{file}:#{line}\n")
+        filename = file.gsub(/:/, '|')
+        $_s.write("BP:#{filename}:#{line}\n")
 
         wait = true
         while wait
-          cmd = debug_read_cmd $_s
-          wait = debug_handle_cmd(cmd) if cmd != ""
+          debug_read_cmd($_s,wait)
+          wait = debug_handle_cmd()
         end
       end
     end
 
-    cmd = debug_read_cmd $_s
-    debug_handle_cmd(cmd) if cmd != ""
+    debug_read_cmd($_s,false)
+    debug_handle_cmd()
   end
 }
 
@@ -82,9 +83,9 @@ $_s = nil
 
 begin
   puts "opening connection"
-  raise "debug host not set" if (Rho::RhoConfig.debug_host.nil? or Rho::RhoConfig.debug_host == "")
-  raise "debug port not set" if (Rho::RhoConfig.debug_port.nil? or Rho::RhoConfig.debug_port == "")
-  $_s = timeout(30) { TCPSocket.open(Rho::RhoConfig.debug_host, Rho::RhoConfig.debug_port) }
+  debug_host = (Rho::RhoConfig.debug_host.nil? or Rho::RhoConfig.debug_host == "") ? '127.0.0.1' : Rho::RhoConfig.debug_host
+  debug_port = (Rho::RhoConfig.debug_port.nil? or Rho::RhoConfig.debug_port == "") ? 9000 : Rho::RhoConfig.debug_port
+  $_s = timeout(30) { TCPSocket.open(debug_host, debug_port) }
 
   puts "connected: " + $_s.to_s
   $_s.write("CONNECT\n")
@@ -92,6 +93,7 @@ begin
   $_file = nil
   $_line = nil
   $_step = false
+  $_cmd = ""
 
   set_trace_func $_tracefunc
 rescue
