4ef02baa36b8e099e4520d07af5ac9c71c49fd5f
rognar <kostyar2004@bk.ru>
2011-05-05 19:37:02 +0400

    log collection on the host is implemented

---
+++ platform/wp7/RhoLogServer/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+
+namespace RhoLogServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String path = args.Length > 0 ? args[0] : ""; 
+            SocketServer server = new SocketServer(path);
+            while (true)
+            {
+                Thread.Sleep(100);
+            }
+        }
+    }
+}
