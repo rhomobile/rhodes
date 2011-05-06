8cd44034e4c10b3501e3b4a35cfea8cc2866755c
rognar <kostyar2004@bk.ru>
2011-05-06 12:02:42 +0400

    added logserversinker

--- platform/wp7/rhodes/App.xaml.cs
+++ platform/wp7/rhodes/App.xaml.cs
@@ -24,7 +24,7 @@ namespace Rhodes
     public partial class App : Application
     {
         private static CRhodesApp RHODESAPP() { return CRhodesApp.Instance; }
-        private static BackgroundWorker m_logThread = new BackgroundWorker();
+        //private static BackgroundWorker m_logThread = new BackgroundWorker();
 
         /// <summary>
         /// Provides easy access to the root frame of the Phone Application.
@@ -70,9 +70,9 @@ namespace Rhodes
             //initRuby();
             //RhoRubyStart();
             //IsolatedStorageExplorer.Explorer.Start("localhost");
-            m_logThread.DoWork += RhoLogSender.logWorkerDoWork;
-            m_logThread.RunWorkerCompleted += RhoLogSender.logWorkerRunWorkerCompleted;
-            m_logThread.RunWorkerAsync();
+            //m_logThread.DoWork += RhoLogSender.logWorkerDoWork;
+            //m_logThread.RunWorkerCompleted += RhoLogSender.logWorkerRunWorkerCompleted;
+            //m_logThread.RunWorkerAsync();
         }
 
         // Code to execute when the application is activated (brought to foreground)
@@ -92,7 +92,7 @@ namespace Rhodes
         private void Application_Closing(object sender, ClosingEventArgs e)
         {
             RHODESAPP().stopApp();
-            m_logThread.CancelAsync();
+           // m_logThread.CancelAsync();
         }
 
         // Code to execute if a navigation fails
