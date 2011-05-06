6c097c280a1505f58e0aaea9c36d680d0bc5a91f
rognar <kostyar2004@bk.ru>
2011-05-06 12:07:32 +0400

    logserversinker is added

---
+++ platform/wp7/RhoRubyLib/logging/RhoLogServerSink.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Net;
+using System.Threading;
+using System.ComponentModel;
+using IronRuby.Builtins;
+using System.Collections.Generic;
+using rho.net;
+using rho.common;
+
+namespace rho.logging
+{
+    public class RhoLogServerSink : IRhoLogSink
+    {
+        private static RhoLogConf m_oLogConf;
+        CAsyncHttp m_aHttp = new CAsyncHttp(true);
+
+        public RhoLogServerSink(RhoLogConf conf)
+        {
+            m_oLogConf = conf;
+        }
+
+        public void close()
+        {
+            if (m_aHttp != null)
+                m_aHttp.stop(2);
+        }
+
+        private RhoLogConf getLogConf()
+        {
+            return m_oLogConf;
+        }
+
+        public int getCurPos()
+        {
+            return 0;
+        }
+
+        public void writeLogMessage(String strMsg)
+        {
+            IDictionary<object, object> map = new Dictionary<object, object>();
+            Hash values = new Hash(map);
+            values.Add(MutableString.Create("url"), MutableString.Create("http://localhost:8000"));
+            values.Add(MutableString.Create("body"), MutableString.Create(strMsg));
+            RhoParams p = new RhoParams(values);
+            m_aHttp.addHttpCommand(new CAsyncHttp.HttpCommand("POST", p));
+        }
+
+        public void clear()
+        {
+
+        }
+    }
+}
