4738103ae840cec284ae7530eb96a1a49459cb5e
genywind <evgeny@rhomobile.com>
2011-05-08 00:16:26 +0400

    rhosimulator: add caption

--- platform/wm/rhodes/emulator/MainWindowQt.cpp
+++ platform/wm/rhodes/emulator/MainWindowQt.cpp
@@ -49,8 +49,6 @@ HWND CMainWindow::Initialize(const wchar_t* title)
     HWND hWnd = (HWND)m_mainWindowProxy.init(this, title);
     SubclassWindow(hWnd);
 
-    SetWindowText(L"RhoSimulator");
-
     //rho_rhodesapp_callAppActiveCallback(1);
     rho_rhodesapp_callUiCreatedCallback();
     return hWnd;
