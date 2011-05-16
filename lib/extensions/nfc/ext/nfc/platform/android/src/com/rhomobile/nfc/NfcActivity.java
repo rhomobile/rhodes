2b3532f1a6556be971338c829948c96c96a6d2bd
dmitrys <dmitrys@rhomobile.com>
2011-05-17 00:47:43 +0400

    Read NDE messages from any Tag Read info and content from Mifare Tag

--- lib/extensions/nfc/ext/nfc/platform/android/src/com/rhomobile/nfc/NfcActivity.java
+++ lib/extensions/nfc/ext/nfc/platform/android/src/com/rhomobile/nfc/NfcActivity.java
@@ -4,9 +4,11 @@ import android.app.Activity;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.nfc.NdefMessage;
 import android.nfc.NdefRecord;
 import android.nfc.NfcAdapter;
+import android.nfc.Tag;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Parcelable;
@@ -28,7 +30,11 @@ public class NfcActivity  extends Activity {
 		Intent intent = getIntent();
 		String action = intent.getAction();
 		
+		Utils.platformLog(TAG, " $$$$$$$$$ NfcActivity STARTED !!! Action = "+action);
+		
+		
 		if (NfcAdapter.ACTION_TAG_DISCOVERED.equals(action)) {
+			Utils.platformLog(TAG, "ACTION_TAG_DISCOVERED !");
             Parcelable[] rawMsgs = intent.getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES);
             NdefMessage[] msgs;
             if (rawMsgs != null) {
@@ -38,6 +44,45 @@ public class NfcActivity  extends Activity {
                 }
             } else {
                 // Unknown tag type
+                byte[] empty = new byte[] {};
+                NdefRecord record = new NdefRecord(NdefRecord.TNF_UNKNOWN, empty, empty, empty);
+                NdefMessage msg = new NdefMessage(new NdefRecord[] {record});
+                msgs = new NdefMessage[] {msg};
+            }
+            Nfc.getInstance().onReceiveMessages(msgs);
+        }
+		if (NfcAdapter.ACTION_NDEF_DISCOVERED.equals(action)) {
+			Utils.platformLog(TAG, "ACTION_NDEF_DISCOVERED !");
+			Uri u = intent.getData();
+			
+			Utils.platformLog(TAG, "     Data = "+u.toString());
+			String t = intent.getType();
+			if (t != null) {
+				Utils.platformLog(TAG, "     Type = "+t);
+			}
+			else {
+				Utils.platformLog(TAG, "     Type is NULL= ");
+			}
+			
+			Tag tag = (Tag)intent.getExtras().get(NfcAdapter.EXTRA_TAG);
+			if (tag != null) {
+				Utils.platformLog(TAG, "     Tag found in extars ! ");
+			}
+			else {
+				Utils.platformLog(TAG, "     Tag not found in extars ! ");
+			}
+			
+			
+            Parcelable[] rawMsgs = intent.getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES);
+            NdefMessage[] msgs;
+            if (rawMsgs != null) {
+                msgs = new NdefMessage[rawMsgs.length];
+                for (int i = 0; i < rawMsgs.length; i++) {
+                    msgs[i] = (NdefMessage) rawMsgs[i];
+                }
+            } else {
+                // Unknown tag type
+    			Utils.platformLog(TAG, "     Unknown Tag type !!!");
                 byte[] empty = new byte[] {};
                 NdefRecord record = new NdefRecord(NdefRecord.TNF_UNKNOWN, empty, empty, empty);
                 NdefMessage msg = new NdefMessage(new NdefRecord[] {record});
