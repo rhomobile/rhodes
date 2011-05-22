a2844aa378e40b227537c794605f58cb6b4ad029
rognar <kostyar2004@bk.ru>
2011-05-23 00:48:07 +0400

    added Exception processing into RhoDatabase

--- platform/wp7/RhoRubyLib/rubyext/RhoKernelOps.cs
+++ platform/wp7/RhoRubyLib/rubyext/RhoKernelOps.cs
@@ -9,16 +9,41 @@ namespace rho.rubyext
 {
     public static class RhoKernelOps
     {
+        private static RhoLogger LOG = RhoLogger.RHO_STRIP_LOG ? new RhoEmptyLogger() :
+            new RhoLogger("RhoKernelOps");
+
+        static CRhoRuby RhoRuby { get { return CRhoRuby.Instance; } }
+
         private static CRhodesApp RhodesApp { get { return CRhodesApp.Instance; } }
 
         public static Object __rhoGetCallbackObject(Object/*!*/ self, int index)
         {
-            return RhodesApp.getCallbackObject(index); ;
+            Object res = null;
+            try
+            {
+                res = RhodesApp.getCallbackObject(index);
+            }
+            catch (Exception ex)
+            {
+                LOG.HandleRubyException(ex, RhoRuby.rubyContext.CurrentException, "__rhoGetCallbackObject");
+            }
+
+            return res;
         }
 
         public static Object __rho_exist_in_resources(Object/*!*/ self, String path)
         {
-            return CRhoFile.isResourceFileExist(path);
+            Object res = null;
+            try
+            {
+                res = CRhoFile.isResourceFileExist(path);
+            }
+            catch (Exception ex)
+            {
+                LOG.HandleRubyException(ex, RhoRuby.rubyContext.CurrentException, "__rho_exist_in_resources");
+            }
+
+            return res;
         }
 
     }
