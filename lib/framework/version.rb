9ff9ff1059c4cdf3751eccfe25fe64f72250a491
genywind <evgeny@rhomobile.com>
2011-05-23 23:04:34 +0400

    bumping version for release 3.0.1.beta.1

--- lib/framework/version.rb
+++ lib/framework/version.rb
@@ -1,6 +1,6 @@
 module RhodesFramework
   unless defined? RhodesFramework::VERSION
-    VERSION = '3.0.0'
+    VERSION = '3.0.1'
   end
   unless defined? RhodesFramework::DBVERSION
     DBVERSION = '2.2.2'
