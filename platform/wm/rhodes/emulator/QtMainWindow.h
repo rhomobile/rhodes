6040f19ffd29b8de1216c7f02df3e70c42baec0c
Albert R. Timashev <albert@timashev.com>
2011-06-02 20:56:28 +0300

    RhoSimulator: added Qt UI implementation (phase 4)

--- platform/wm/rhodes/emulator/QtMainWindow.h
+++ platform/wm/rhodes/emulator/QtMainWindow.h
@@ -1,9 +1,11 @@
 #ifndef QTMAINWINDOW_H
 #define QTMAINWINDOW_H
 
+#include <vector>
 #include <QMainWindow>
 #include <QUrl>
-#include <QWebInspector>
+#include <QtWebKit/QWebView>
+#include <QtWebKit/QWebInspector>
 #include <QAction>
 #include "MainWindowCallback.h"
 #include "common/IRhoThreadImpl.h"
@@ -15,6 +17,9 @@ namespace Ui {
 class QtMainWindow : public QMainWindow
 {
     Q_OBJECT
+
+public:
+    typedef QHash<QString, QVariant> QTabBarRuntimeParams;
 
 public:
     explicit QtMainWindow(QWidget *parent = 0);
@@ -36,20 +41,25 @@ public:
     void toolbarAddAction(const QString & text);
     void toolbarAddAction(const QIcon & icon, const QString & text, const char* action, bool rightAlign = false);
     void toolbarAddSeparator(void);
-	void setToolbarStyle(bool border, QString background);
+    void setToolbarStyle(bool border, QString background);
     // menu
     void menuClear(void);
     void menuAddAction(const QString & text, int item);
     void menuAddSeparator(void);
-	// tabbar
+    // tabbar
     void tabbarRemoveAllTabs(void);
     void tabbarShow(void);
     void tabbarHide(void);
     int tabbarGetHeight(void);
+	void tabbarSwitch(int index);
+	int tabbarGetCurrent();
+    int tabbarAddTab(const QString& label, const char* icon, bool disabled, QTabBarRuntimeParams& tbri);
+    void setTabbarStyle(QString background);
 private:
     Ui::QtMainWindow *ui;
     QWebInspector *wi;
     IMainWindowCallback* cb;
+    std::vector<QWebView*> tabViews;
 
 private slots:
     void on_webView_urlChanged(QUrl );
@@ -57,6 +67,7 @@ private slots:
     void on_webView_loadStarted();
     void on_webView_linkClicked(const QUrl&);
     void on_actionBack_triggered();
+    void on_tabBar_currentChanged(int index);
     void on_toolbarAction_triggered(bool);
     void on_menuAction_triggered(bool);
     void on_menuMain_aboutToShow();
