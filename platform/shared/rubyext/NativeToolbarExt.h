4232922d2701b4d1ad1175e46876e6971ca10206
Albert R. Timashev <albert@rhomobile.com>
2011-06-13 19:56:27 +0300

    rhosimulator: wm dependency extracted to platform/shared

--- platform/shared/rubyext/NativeToolbarExt.h
+++ platform/shared/rubyext/NativeToolbarExt.h
@@ -1,6 +1,9 @@
-#pragma once
+#ifndef _NATIVETOOLBAREXT_H_
+#define _NATIVETOOLBAREXT_H_
 
 #define TOOLBAR_TYPE		0
 #define TABBAR_TYPE			1
 #define NOBAR_TYPE			2
 #define VTABBAR_TYPE		3
+
+#endif //_NATIVETOOLBAREXT_H_
