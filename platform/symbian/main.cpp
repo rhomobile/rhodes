61b8d29e9dc61d6d1439387f100cf89b6b7ffb97
rognar <kostyar2004@bk.ru>
2011-06-13 21:53:21 +0400

    added web link support

--- platform/symbian/main.cpp
+++ platform/symbian/main.cpp
@@ -7,7 +7,7 @@ int main(int argc, char *argv[])
 
     QmlApplicationViewer viewer;
     viewer.setOrientation(QmlApplicationViewer::ScreenOrientationAuto);
-    viewer.setMainQmlFile(QLatin1String("qml/qml/inlinehtml.qml"));
+    viewer.setMainQmlFile(QLatin1String("qml/rhodes.qml"));
     viewer.showExpanded();
 
     return app.exec();
