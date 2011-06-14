918552415116a19e6f0cd767a00c2a0b25fafb17
Albert R. Timashev <albert@rhomobile.com>
2011-06-14 22:04:49 +0300

    osx: initial Qt projects commit

---
+++ platform/osx/RhoSimulator/build.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+cd ../../shared/qt/rubylib
+qmake -o Makefile -spec macx-g++ rubylib.pro
+make all
+cd ../rholib
+qmake -o Makefile -spec macx-g++ rholib.pro
+make all
+cd ../sqlite3
+qmake -o Makefile -spec macx-g++ sqlite3.pro
+make all
+cd ../syncengine
+qmake -o Makefile -spec macx-g++ syncengine.pro
+make all
+cd ../rhodes
+qmake -o Makefile -spec macx-g++ RhoSimulator.pro
+make all
+cd ../../../osx/
