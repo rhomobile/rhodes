9853000f74e0fe68b1eadcc02e96a3e59caec4e4
Alexey Tikhvinsky <alexey@rhomobile.com>
2011-06-15 16:22:49 +0400

    Android: introduce NoSSLVerifyPeer parameter in rhoconfig for WebView.

--- platform/android/Rhodes/src/com/rhomobile/rhodes/webview/RhoWebViewClient.java
+++ platform/android/Rhodes/src/com/rhomobile/rhodes/webview/RhoWebViewClient.java
@@ -1,11 +1,14 @@
 package com.rhomobile.rhodes.webview;
 
 import com.rhomobile.rhodes.Logger;
+import com.rhomobile.rhodes.RhoConf;
 import com.rhomobile.rhodes.RhodesActivity;
 import com.rhomobile.rhodes.RhodesService;
 
 import android.graphics.Bitmap;
+import android.net.http.SslError;
 import android.view.Window;
+import android.webkit.SslErrorHandler;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
@@ -15,7 +18,7 @@ public class RhoWebViewClient extends WebViewClient
     
     @Override
     public boolean shouldOverrideUrlLoading(WebView view, String url) {
-        Logger.T(TAG, "Loading URL: " + url);
+        Logger.I(TAG, "Loading URL: " + url);
         return RhodesService.getInstance().handleUrlLoading(url);
     }
     
@@ -47,5 +50,32 @@ public class RhoWebViewClient extends WebViewClient
         Logger.E(TAG, msg.toString());
     }
 
-
+    @Override
+    public void onReceivedSslError(WebView view, SslErrorHandler handler, SslError error) {
+        
+        if(RhoConf.getBool("NoSSLVerifyPeer")) {
+            Logger.D(TAG, "Skip SSL error.");// + error.toString());
+            handler.proceed();
+        } else {
+            StringBuilder msg = new StringBuilder();
+            msg.append("SSL error - ");
+            switch(error.getPrimaryError()) {
+            case SslError.SSL_NOTYETVALID:
+                msg.append("The certificate is not yet valid: ");
+                break;
+            case SslError.SSL_EXPIRED:
+                msg.append("The certificate has expired: ");
+                break;
+            case SslError.SSL_IDMISMATCH:
+                msg.append("Hostname mismatch: ");
+                break;
+            case SslError.SSL_UNTRUSTED:
+                msg.append("The certificate authority is not trusted: ");
+                break;
+            }
+            msg.append(error.getCertificate().toString());
+            Logger.W(TAG, msg.toString());
+            handler.cancel();
+        }
+    }
 }
