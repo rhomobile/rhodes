8b42fe0580cf982466e5ed5119aabe317780530b
Albert R. Timashev <albert@rhomobile.com>
2011-06-24 17:21:13 +0300

    rhosimulator: minor source code fixes

--- platform/shared/qt/rhodes/impl/RhoThreadImpl.cpp
+++ platform/shared/qt/rhodes/impl/RhoThreadImpl.cpp
@@ -1,21 +1,16 @@
 #include "common/RhoPort.h"
-//#include <QThread>
 
 extern "C" {
 
 void* rho_nativethread_start()
 {
     //TODO: rho_nativethread_start
-    //QThread* thread = new QThread();
-    //thread->start();
-    //return (void*)thread;
     return 0;
 }
 
 void rho_nativethread_end(void* pData)
 {
     //TODO: rho_nativethread_end
-    //((QThread*)pData)->quit();
 }
 
 } //extern "C"
