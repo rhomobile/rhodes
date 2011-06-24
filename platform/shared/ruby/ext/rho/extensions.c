bf55580754a76b2f1b1974358a8e0e8e26943388
rognar <kostyar2004@bk.ru>
2011-06-24 21:26:45 +0400

    added some changes into rhodes for symbian

--- platform/shared/ruby/ext/rho/extensions.c
+++ platform/shared/ruby/ext/rho/extensions.c
@@ -1,3 +1,9 @@
 // WARNING! THIS FILE IS GENERATED AUTOMATICALLY! DO NOT EDIT IT MANUALLY!
+#pragma comment(lib, "Barcode.lib")
+#pragma comment(lib, "Rainbow.lib")
+extern void Init_Barcode(void);
+extern void Init_Rainbow(void);
 void Init_Extensions(void) {
+    Init_Barcode();
+    Init_Rainbow();
 }
