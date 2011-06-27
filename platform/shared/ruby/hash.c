cd0fb2a6c2eb37d54def0be4ba76daa53f6a42d2
genywind <evgeny@rhomobile.com>
2011-06-27 16:15:15 +0400

    fix android build

--- platform/shared/ruby/hash.c
+++ platform/shared/ruby/hash.c
@@ -1802,7 +1802,7 @@ rb_hash_compare_by_id_p(VALUE hash)
 static int path_tainted = -1;
 //RHO
 //static char **origenviron;
-#if defined(OS_WINCE) || defined(OS_MACOSX)
+#ifndef RHO_SYMBIAN
 static char * __environ = "";
 static char ** environ = &__environ;
 #endif
