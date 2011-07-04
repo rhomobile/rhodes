309edea1c82e6c26f66c4e575b1ab8a599e7bff6
Alexey Tikhvinsky <alexey@rhomobile.com>
2011-07-05 00:32:46 +0400

    Rhosync-client-Java: Rhosync client initialization.

--- rhosync-client/Java/RhoSync/jni/src/RhoSyncClient.cpp
+++ rhosync-client/Java/RhoSync/jni/src/RhoSyncClient.cpp
@@ -1,7 +1,105 @@
+/*
+ ============================================================================
+ Author     : Alexey Tikhvinsky
+ Copyright  : Copyright (C) 2011 Rhomobile (http://www.rhomobile.com).
+              All rights reserved.
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ ============================================================================
+ */
+
+#include <vector>
+
+#include "common/RhoStd.h"
 #include "rhodes/JNIRhodes.h"
 #include "sync/SyncThread.h"
+#include "SyncClient/SyncClient.h"
 
 #include "com_rhomobile_rhosync_RhoSyncClient.h"
+#include "com_rhomobile_rhosync_RhomModel.h"
+
+typedef std::vector<RHOM_MODEL> model_vector;
+typedef std::vector<rho::String> string_vector;
+
+RHO_GLOBAL void JNICALL Java_com_rhomobile_rhosync_RhomModel_init
+  (JNIEnv * env, jobject jmodel)
+{
+    jclass clsmodel = getJNIClass(RHOSYNC_JAVA_CLASS_RHOMMODEL);
+    if (!clsmodel) return;
+
+    jmethodID midmodeltype = getJNIClassMethod(env, clsmodel, "setModelType", "(I)V");
+    if (!midmodeltype) return;
+    jmethodID midsynctype = getJNIClassMethod(env, clsmodel, "setSyncType", "(I)V");
+    if (!midsynctype) return;
+    jmethodID midsyncpri = getJNIClassMethod(env, clsmodel, "setSyncPriority", "(I)V");
+    if (!midsyncpri) return;
+    jmethodID midpart = getJNIClassMethod(env, clsmodel, "setPartition", "(Ljava/lang/String;)V");
+    if (!midpart) return;
+
+    RHOM_MODEL model;
+    rho_syncclient_initmodel(&model);
+
+    env->CallVoidMethod(jmodel, midmodeltype, model.type);
+    env->CallVoidMethod(jmodel, midsynctype, model.sync_type);
+    env->CallVoidMethod(jmodel, midsyncpri, model.sync_priority);
+    env->CallVoidMethod(jmodel, midpart, rho_cast<jhstring>(env, model.partition).get());
+}
+
+
+RHO_GLOBAL void JNICALL Java_com_rhomobile_rhosync_RhoSyncClient_initialize
+  (JNIEnv * env, jobject, jobjectArray jmodels)
+{
+    jclass clsmodel = getJNIClass(RHOSYNC_JAVA_CLASS_RHOMMODEL);
+    if (!clsmodel) return;
+    jmethodID midname = getJNIClassMethod(env, clsmodel, "getName", "()Ljava/lang/String;");
+    if (!midname) return;
+    jmethodID midmodeltype = getJNIClassMethod(env, clsmodel, "getModelType", "()I");
+    if (!midmodeltype) return;
+    jmethodID midsynctype = getJNIClassMethod(env, clsmodel, "getSyncType", "()I");
+    if (!midsynctype) return;
+    jmethodID midsyncpri = getJNIClassMethod(env, clsmodel, "getSyncPriority", "()I");
+    if (!midsyncpri) return;
+    jmethodID midpart = getJNIClassMethod(env, clsmodel, "getPartition", "()Ljava/lang/String;");
+    if (!midpart) return;
+
+    size_t cnt = static_cast<model_vector::size_type>(env->GetArrayLength(jmodels));
+    model_vector models(cnt);
+    string_vector names(cnt);
+    string_vector partitions(cnt);
+
+    for(model_vector::size_type i = 0, cnt = models.size(); i < cnt; ++i)
+    {
+        jobject jmodel = env->GetObjectArrayElement(jmodels, i);
+        RHOM_MODEL& model = models[i];
+        rho::String& name = names[i];
+        rho::String& partition = partitions[i];
+        
+        jhobject jname = env->CallObjectMethod(jmodel, midname);
+        name = rho_cast<rho::String>(env, static_cast<jstring>(jname.get()));
+        model.name = const_cast<char*>(name.c_str());
+
+        model.type = static_cast<RHOM_MODEL_TYPE>(env->CallIntMethod(jmodel, midmodeltype));
+        model.sync_type = static_cast<RHOM_SYNC_TYPE>(env->CallIntMethod(jmodel, midsynctype));
+        model.sync_priority = env->CallIntMethod(jmodel, midsyncpri);
+
+        jhobject jpart = env->CallObjectMethod(jmodel, midpart);
+        partition = rho_cast<rho::String>(env, static_cast<jstring>(jpart.get()));
+        model.partition = const_cast<char*>(partition.c_str());
+    }
+
+    rho_syncclient_init(&models.front(), models.size());
+}
 
 RHO_GLOBAL void JNICALL Java_com_rhomobile_rhosync_RhoSyncClient_setSyncServer
   (JNIEnv *, jobject, jstring)
--- rhosync-client/Java/RhoSync/src/com/rhomobile/rhosync/RhomModel.java
+++ rhosync-client/Java/RhoSync/src/com/rhomobile/rhosync/RhomModel.java
@@ -1,7 +1,60 @@
+/*
+ ============================================================================
+ Author	    : Alexey Tikhvinsky
+ Copyright  : Copyright (C) 2011 Rhomobile. All rights reserved.
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ ============================================================================
+ */
+
 package com.rhomobile.rhosync;
 
-public interface RhomModel {
-	RhoSyncNotify sync();
+public class RhomModel {
+	public final static int MODEL_TYPE_PROPERTY_BAG = 0;
+	public final static int MODEL_TYPE_FIXED_SCHEMA = 1;
+
+	public final static int SYNC_TYPE_NONE = 0;
+	public final static int SYNC_TYPE_INCREMENTAL = 1;
+	public final static int SYNC_TYPE_BULK_ONLY = 2;
+	
+    private String mName;
+    private int mModelType;
+
+    private int mSyncType;
+    private int mSyncPriority;
+
+    private String mPartition;
+
+    private native void init();
+
+    public RhomModel(String name, int syncType) {
+    	init();
+    	mName = name;
+    	mSyncType = syncType;
+    }
+    
+    public String getName() { return mName; }
+    public int getModelType() { return mModelType; }
+    public void setModelType(int type) { mModelType = type; }
+    public int getSyncType() { return mSyncType; } 
+    public void setSyncType(int type) { mSyncType = type; }
+    public int getSyncPriority() { return mSyncPriority; }
+    public void setSyncPriority(int prio) { mSyncPriority = prio; }
+    public String getPartition() { return mPartition; }
+    public void setPartition(String part) { mPartition = part; }
+	
+	//RhoSyncNotify sync() {}
 	/* TODO:
 	create();
 	find(String text);
@@ -10,7 +63,7 @@ public interface RhomModel {
 	save();
 	destroy();
 	*/
-	void startBulkUpdate();
-	void stopBulkUpdate();
+	void startBulkUpdate() {}
+	void stopBulkUpdate() {}
 
 }
