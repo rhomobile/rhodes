f1a117aa29837977005343d2ea089b9c0c03c6be
Albert R. Timashev <albert@rhomobile.com>
2011-07-06 18:24:15 +0400

    rhosimulator/macosx: Camera simulation implemented

--- platform/shared/qt/rhodes/impl/CameraImpl.cpp
+++ platform/shared/qt/rhodes/impl/CameraImpl.cpp
@@ -10,14 +10,19 @@ void choose_picture(char* callback_url)
     CMainWindow::getInstance()->selectPicture(strdup(callback_url));
 }
 
-void take_picture(char* callback_url, rho_param * options_hash)
+void take_picture(char* callback_url, rho_param* options_hash)
 {
     CMainWindow::getInstance()->takePicture(strdup(callback_url));
 }
 
 VALUE get_camera_info(const char* camera_type)
 {
-    return rho_ruby_get_NIL();
+    VALUE info = rho_ruby_createHash();
+    VALUE max_res = rho_ruby_createHash();
+    rho_ruby_add_to_hash(max_res, rho_ruby_create_string("width"), rho_ruby_create_integer(2048));
+    rho_ruby_add_to_hash(max_res, rho_ruby_create_string("height"), rho_ruby_create_integer(1152));
+    rho_ruby_add_to_hash(info, rho_ruby_create_string("max_resolution"), max_res);
+    return info;
 }
 
 } //extern "C"
