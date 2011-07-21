7126877770c0e687f5f840f0867f71152dfbfc7e
genywind <evgeny@rhomobile.com>
2011-07-21 09:41:36 +0400

    ruby: add methods from prelude

--- lib/framework/rhoframework.rb
+++ lib/framework/rhoframework.rb
@@ -72,6 +72,41 @@ end
 
 end
 
+# Mutex
+class Mutex
+  def synchronize
+    self.lock
+    begin
+      yield
+    ensure
+      self.unlock rescue nil
+    end
+  end
+end
+
+# Thread
+class Thread
+  MUTEX_FOR_THREAD_EXCLUSIVE = Mutex.new
+  def self.exclusive
+    MUTEX_FOR_THREAD_EXCLUSIVE.synchronize{
+      yield
+    }
+  end
+end
+
+module Kernel   
+    def require_relative(relative_feature)
+      c = caller.first
+      e = c.rindex(/:\\d+:in /)
+      file = $`
+      if /\\A\\((.*)\\)/ =~ file # eval, etc.
+        raise LoadError, "require_relative is called in #{$1}"
+      end
+      absolute_feature = File.expand_path(File.join(File.dirname(file), relative_feature))
+      require absolute_feature
+    end
+end
+
 begin
 	if defined? RHO_WP7
 		require 'rhosystem'
