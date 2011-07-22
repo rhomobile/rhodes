4905ad5f0c14b2e8b30bed2794dc0c2f4b462aca
genywind <evgeny@rhomobile.com>
2011-07-22 17:46:39 +0400

    log: apply exclude filters for all log sync: json encode values for login and clientregister

--- platform/shared/logging/RhoLog.cpp
+++ platform/shared/logging/RhoLog.cpp
@@ -65,4 +65,68 @@ void LogMessage::flushLog(){
         common::CRhoFatalError().processFatalError();
 }
 
+void LogMessage::applyExcludeFilter(String& strMsg)
+{
+    Vector<String>& arSecure = LOGCONF().getExcludeAttribs();
+    if ( arSecure.size() == 0 )
+        return;
+
+    for ( int i = 0; i < strMsg.length(); i++ )
+    {
+        rho::boolean bFound = false;
+        for ( int j = 0; j < arSecure.size(); j++ )
+        {
+            const String& strExclude = arSecure.elementAt(j);
+            if ( strncmp(strMsg.c_str() + i, strExclude.c_str(), strExclude.length()) == 0 )
+            {
+                rho::boolean bSlash = false;
+                int nRemoveStart = i + strExclude.length(); 
+                int nEndSep = '"';
+
+                if ( strncmp(strMsg.c_str() + nRemoveStart, "\":\"", 3) == 0 )
+                    nRemoveStart += 3;
+                else if ( strncmp(strMsg.c_str() + nRemoveStart, "\"=>\"", 4) == 0 )
+                    nRemoveStart += 4;
+                else if ( strncmp(strMsg.c_str() + nRemoveStart, "=", 1) == 0 )
+                {
+                    nRemoveStart += 1;
+                    nEndSep = '&';
+                }
+                else
+                    break;
+
+                int nFill = nRemoveStart;
+                for ( ; nFill < strMsg.length(); nFill++ )
+                {
+                    if ( bSlash && strMsg[nFill] == '\\' ) 
+                    {
+                        bSlash = false;
+                        continue;
+                    }
+                    else if ( nEndSep != '&' && strMsg[nFill] == '\\' )
+                        bSlash = true;
+                    else
+                    {
+                        if ( strMsg[nFill] == nEndSep && !bSlash )
+                        {
+                            bFound = true;
+                            break;
+                        }
+
+                        bSlash = false;
+                    }
+                }
+                if ( bFound || nEndSep == '&')
+                {
+                    strMsg.erase(nRemoveStart, nFill-nRemoveStart );
+                    i += strExclude.length();
+                }
+            }
+            if ( bFound)
+                break;
+        }
+    }
+
+}
+
 }
