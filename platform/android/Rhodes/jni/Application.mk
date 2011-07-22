756528d332a5cde8d8f9862191303deab20f6181
Alexey Tikhvinsky <alexey@rhomobile.com>
2011-07-22 14:54:24 +0400

    Android: Native part build using NDK build system and Eclipse integration.

---
+++ platform/android/Rhodes/jni/Application.mk
@@ -0,0 +1,3 @@
+APP_STL := stlport_static
+APP_CFLAGS += --sysroot $(NDK_ROOT)/platforms/android-9/arch-arm -DOS_ANDROID
+#APP_CFLAGS += --sysroot /home/lexis/android/android-ndk-r6/platforms/android-9/arch-arm -DOS_ANDROID
