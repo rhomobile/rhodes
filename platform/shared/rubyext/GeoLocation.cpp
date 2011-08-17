5b153362de55ade670640dd125d68cab96d6c5c0
Alexey Tikhvinsky <alexey@rhomobile.com>
2011-08-18 02:36:45 +0400

    Make CGeoNotification assignment thread safe.

--- platform/shared/rubyext/GeoLocation.cpp
+++ platform/shared/rubyext/GeoLocation.cpp
@@ -87,10 +87,13 @@ void CGeoLocation::callGeoCallback(boolean bError, boolean bRunInThread)
 
 void CGeoLocation::setGeoCallback(const char *url, char* params, int timeout_sec, boolean bView)
 {
-    if ( bView)
-        m_ViewNotify = CGeoNotification(url?url:"",params?params:"");
-    else
-        m_Notify = CGeoNotification(url?url:"",params?params:"");
+    synchronized(m_mxNotify)
+    {
+        if ( bView)
+            m_ViewNotify = CGeoNotification(url?url:"",params?params:"");
+        else
+            m_Notify = CGeoNotification(url?url:"",params?params:"");
+    }
 
     if ( url && *url )
     {
