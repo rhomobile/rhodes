86f14d3da9fe4b34b73c71dbe1c53b24c3b82058
genywind <evgeny@rhomobile.com>
2011-09-05 12:28:33 +0400

    fix android config

--- platform/shared/ruby/android/ruby/config.h
+++ platform/shared/ruby/android/ruby/config.h
@@ -245,11 +245,7 @@
 #define HAVE_SOCKADDR_STORAGE 1
 #define HAVE_GAI_STRERROR 1
 
-#ifndef RUBYLINUX
-    #define LOAD_RELATIVE 1
-#else
-    #define LOAD_RELATIVE 0
-#endif
+#define LOAD_RELATIVE 1
 
 #define HAVE_PTHREAD_H 1
 #define RUBY_LIB_VERSION_STYLE 3	/* full */
