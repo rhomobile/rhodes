477d8f163de0d4b57de7512a52f0b186b87a7bf5
genywind <evgeny@rhomobile.com>
2011-09-16 15:51:14 +0400

    rhoconnect-client:c++:support associations

--- rhoconnect-client/C++/Tests/RhoConnectClientTest.cpp
+++ rhoconnect-client/C++/Tests/RhoConnectClientTest.cpp
@@ -41,6 +41,8 @@
 using namespace rho;
 using namespace rho::common;
 const char* g_szProduct = "Product";
+const char* g_szCustomer = "Customer";
+String g_product_test_name;
 
 #ifdef WIN32
 int _tmain(int argc, _TCHAR* argv[])
@@ -52,7 +54,7 @@ extern "C" int runSyncClientTests()
 	char** argv = 0;
 #endif
 
-    RHOM_MODEL models[1] = {0};
+    RHOM_MODEL models[2] = {0};
     rho_connectclient_initmodel(&models[0]);
     models[0].name = g_szProduct;
     //models[0].type = RMT_PROPERTY_FIXEDSCHEMA;
@@ -70,11 +72,15 @@ extern "C" int runSyncClientTests()
     */
     models[0].sync_type = RST_INCREMENTAL;
 
-    //rho_connectclient_initmodel(&models[1]);
-    //models[1].name = "Product";
-    //models[1].sync_type = RST_INCREMENTAL;
+    models[0].associations = rho_connectclient_hash_create();
+    rho_connectclient_hash_put( models[0].associations, "quantity", g_szCustomer );
+    rho_connectclient_hash_put( models[0].associations, "sku", g_szCustomer );
 
-    rho_connectclient_init(models, 1);
+    rho_connectclient_initmodel(&models[1]);
+    models[1].name = g_szCustomer;
+    models[1].sync_type = RST_INCREMENTAL;
+
+    rho_connectclient_init(models, 2);
 
     rho_sync_set_threaded_mode(0);
     rho_sync_set_pollinterval(0);
@@ -206,9 +212,64 @@ TEST(SyncClient, shouldCreateNewProduct)
 
     rho_connectclient_hash_delete(item);
     rho_connectclient_hash_delete(item2);
+    rho_connectclient_hash_delete(item3);
 
     rho_connectclient_free_syncnotify(&oNotify);
     rho_sync_free_string(szRes);
+}
+
+TEST(SyncClient, shouldCreateNewProductWithCustomers) 
+{
+    EXPECT_EQ(rho_sync_logged_in(), 1);
+
+    unsigned long cust1 = rho_connectclient_hash_create();
+    rho_connectclient_hash_put(cust1, "first", "CustTest1");
+    rho_connectclient_create_object(g_szCustomer, cust1);
+
+    unsigned long cust2 = rho_connectclient_hash_create();
+    rho_connectclient_hash_put(cust2, "first", "CustTest2");
+    rho_connectclient_create_object(g_szCustomer, cust2);
+
+    g_product_test_name = CLocalTime().toString(true, true);
+    unsigned long item = rho_connectclient_hash_create();
+    rho_connectclient_hash_put(item, "name", g_product_test_name.c_str());
+    rho_connectclient_hash_put(item, "quantity", rho_connectclient_hash_get(cust1, "object"));
+    rho_connectclient_hash_put(item, "sku", rho_connectclient_hash_get(cust2, "object"));
+    rho_connectclient_create_object(g_szProduct, item);
+
+    char* szRes = (char*)rho_sync_doSyncAllSources(0,0);
+    RHO_CONNECT_NOTIFY oNotify = {0};
+    rho_connectclient_parsenotify(szRes, &oNotify);
+
+    EXPECT_EQ(String(oNotify.status), "complete");
+    EXPECT_EQ(oNotify.error_code, RHO_ERR_NONE);
+    rho_connectclient_free_syncnotify(&oNotify);
+    rho_sync_free_string(szRes);
+
+    unsigned long prodCond = rho_connectclient_hash_create();
+    rho_connectclient_hash_put(prodCond, "name", g_product_test_name.c_str());
+    unsigned long prod = rho_connectclient_find_first(g_szProduct, prodCond );
+    rho_connectclient_hash_delete(prodCond);
+
+    EXPECT_NE( prod, 0 );
+    EXPECT_NE( String(rho_connectclient_hash_get(prod, "object")), rho_connectclient_hash_get(item, "object") );
+    EXPECT_NE( String(rho_connectclient_hash_get(prod, "quantity")), rho_connectclient_hash_get(cust1, "object") );
+    EXPECT_NE( String(rho_connectclient_hash_get(prod, "sku")), rho_connectclient_hash_get(cust2, "object") );
+
+    unsigned long cust11 = rho_connectclient_find(g_szCustomer, rho_connectclient_hash_get(prod, "quantity") );
+    EXPECT_NE( cust11, 0 );
+    EXPECT_EQ( String(rho_connectclient_hash_get(cust11, "first")), rho_connectclient_hash_get(cust1, "first") );
+
+    unsigned long cust22 = rho_connectclient_find(g_szCustomer, rho_connectclient_hash_get(prod, "sku") );
+    EXPECT_NE( cust22, 0 );
+    EXPECT_EQ( String(rho_connectclient_hash_get(cust22, "first")), rho_connectclient_hash_get(cust2, "first") );
+
+    rho_connectclient_hash_delete(cust22);
+    rho_connectclient_hash_delete(cust11);
+    rho_connectclient_hash_delete(cust1);
+    rho_connectclient_hash_delete(cust2);
+    rho_connectclient_hash_delete(item);
+    rho_connectclient_hash_delete(prod);
 }
 
 TEST(SyncClient, shouldModifyProduct) 
@@ -264,6 +325,25 @@ TEST(SyncClient, shouldDeleteAllTestProduct)
         rho_connectclient_itemdestroy(g_szProduct, rho_connectclient_strhasharray_get(items, i));
     }
 
+    unsigned long cond2 = rho_connectclient_hash_create();
+    rho_connectclient_hash_put(cond2, "name", g_product_test_name.c_str());
+
+    unsigned long items2 = rho_connectclient_find_all(g_szProduct, cond2 );
+    EXPECT_NE( items2, 0 );
+  
+    for ( int i = 0; i < rho_connectclient_strhasharray_size(items2); i++ )
+    {
+        rho_connectclient_itemdestroy(g_szProduct, rho_connectclient_strhasharray_get(items2, i));
+    }
+
+    unsigned long cond3 = rho_connectclient_hash_create();
+    rho_connectclient_hash_put(cond3, "first", "CustTest1");
+    rho_connectclient_itemdestroy(g_szCustomer, cond3);
+
+    unsigned long cond4 = rho_connectclient_hash_create();
+    rho_connectclient_hash_put(cond4, "first", "CustTest2");
+    rho_connectclient_itemdestroy(g_szCustomer, cond4);
+
     char* szRes = (char*)rho_sync_doSyncSourceByName(g_szProduct);
     RHO_CONNECT_NOTIFY oNotify = {0};
     rho_connectclient_parsenotify(szRes, &oNotify);
@@ -275,6 +355,9 @@ TEST(SyncClient, shouldDeleteAllTestProduct)
     EXPECT_EQ(item2, 0);
 
     rho_connectclient_hash_delete(cond);
+    rho_connectclient_hash_delete(cond2);
+    rho_connectclient_hash_delete(cond3);
+    rho_connectclient_hash_delete(cond4);
     rho_connectclient_strhasharray_delete(items);
 }
 
