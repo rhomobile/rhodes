44e5a75714e080f658318e4c467e41b274f10de6
genywind <evgeny@rhomobile.com>
2011-09-19 18:28:53 +0400

    wm: add support for webkit_browser capability

---
+++ platform/wm/rhodes/IBrowserEngine.h
@@ -0,0 +1,23 @@
+#pragma once
+
+class CMainWindow;
+namespace rho
+{
+
+struct IBrowserEngine
+{
+public:
+    virtual ~IBrowserEngine(void){};
+
+    virtual BOOL Navigate(LPCTSTR szURL) = 0;
+    virtual HWND GetHTMLWND() = 0;
+    virtual BOOL ResizeOnTab(int iInstID,RECT rcNewSize) = 0;
+    virtual BOOL BackOnTab(int iInstID,int iPagesBack = 1) = 0;
+    virtual BOOL ForwardOnTab(int iInstID) = 0;
+    virtual BOOL Reload(bool bFromCache) = 0;
+    virtual BOOL NavigateToHtml(LPCTSTR szHtml) = 0;
+    virtual LRESULT OnWebKitMessages(UINT uMsg, WPARAM wParam, LPARAM lParam, BOOL& bHandled) = 0;
+    virtual void RunMessageLoop(CMainWindow& mainWnd) = 0;
+};
+
+}
