805f276872ef5db1b280be8dbfb80e0bf6339ec0
genywind <evgeny@rhomobile.com>
2011-09-20 12:56:26 +0400

    wm: refactor rhoelements code

--- platform/shared/ruby/ext/rho/extensions.c
+++ platform/shared/ruby/ext/rho/extensions.c
@@ -1,6 +1,3 @@
 // WARNING! THIS FILE IS GENERATED AUTOMATICALLY! DO NOT EDIT IT MANUALLY!
-#pragma comment(lib, "Barcode.lib")
-extern void Init_Barcode(void);
 void Init_Extensions(void) {
-    Init_Barcode();
 }
---
+++ platform/wm/rhodes/rhoelements/RhoWKBrowserEngine.h
@@ -0,0 +1,40 @@
+#pragma once
+
+#include "IBrowserEngine.h"
+#include "rhoelements/PBCore/PBCore/Config.h"
+
+class CEng;
+
+namespace rho
+{
+
+class CRhoWKBrowserEngine :  public rho::IBrowserEngine
+{
+    static WNDPROC m_WebKitOwnerProc;
+    static CConfig* g_pConfig;
+    static WCHAR CRhoWKBrowserEngine::g_szConfigFilePath[MAX_PATH + 1];
+    static WCHAR CRhoWKBrowserEngine::g_szInstallDirectory[MAX_PATH + 1];
+    CEng* m_pEngine;
+
+    static LRESULT CALLBACK WK_HTMLWndProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam);
+    static LRESULT CALLBACK WK_GetEngineConfig(int iInstID, LPCTSTR tcSetting, TCHAR* tcValue);
+
+    LRESULT ProcessOnTopMostWnd(HWND hWnd, int tabIndex);
+public:
+    CRhoWKBrowserEngine(HWND hParentWnd, HINSTANCE hInstance);
+    virtual ~CRhoWKBrowserEngine(void);
+
+    virtual BOOL Navigate(LPCTSTR szURL);
+    virtual HWND GetHTMLWND();
+    virtual BOOL ResizeOnTab(int iInstID,RECT rcNewSize);
+    virtual BOOL BackOnTab(int iInstID,int iPagesBack = 1);
+    virtual BOOL ForwardOnTab(int iInstID);
+    virtual BOOL ReloadOnTab(bool bFromCache, UINT iTab);
+    virtual BOOL NavigateToHtml(LPCTSTR szHtml);
+    virtual LRESULT OnWebKitMessages(UINT uMsg, WPARAM wParam, LPARAM lParam, BOOL& bHandled);
+    virtual void RunMessageLoop(CMainWindow& mainWnd);
+
+    CEng* getWebKitEngine() const;
+};
+
+}
