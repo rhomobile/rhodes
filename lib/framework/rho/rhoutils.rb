e838012b2989fb1da06d008bdd844bec52ddfe57
genywind <evgeny@rhomobile.com>
2011-10-01 01:33:10 +0400

    bb: fix spec

--- lib/framework/rho/rhoutils.rb
+++ lib/framework/rho/rhoutils.rb
@@ -55,65 +55,65 @@ module Rho
         cur_src = nil
         hashItem = {}
         cur_objid = nil
-        File.open(File.join(Rho::RhoFSConnector.get_base_app_path(),'app',prefix,'fixtures',filename+'.txt')) do |f|
-          f.each do |line|
-              if first_row
-                columns = line.chomp.split('|'); first_row = false; next;
-              end
-              parts = line.chomp.split('|')
+        f = File.open(File.join(Rho::RhoFSConnector.get_base_app_path(),'app',prefix,'fixtures',filename+'.txt'))
+        f.each do |line|
+          if first_row
+            columns = line.chomp.split('|'); first_row = false; next;
+          end
+          parts = line.chomp.split('|')
 
-              row = {}
-              columns.each_with_index do |col,idx|
-                col.strip! 
-                if col == 'source_name'
-                  src_name = parts[idx]
-                  src_name = source_map[src_name] if source_map
-                  row['source_id'] = @@mapSrc[src_name].source_id()
-                else
-                  row[col] = parts[idx]
-                end
-              end
-              
-              src = @@mapSrcByIdx[ row['source_id'].to_i ]
-              src_db = src ? ::Rho::RHO.get_src_db(src.name() ) :  ::Rho::RHO.get_user_db()
-              if src_db != db
-                db.commit if db
-                db = src_db
-                db.start_transaction 
-                
-                if !mapPartDeleted[db]
-                    #puts  "delete_all_from_table: #{row}"
-                    db.delete_all_from_table(filename)
-                    mapPartDeleted[db] = 1
-                end
-              end
-              
-              if ( cur_objid != row['object'] )
-                if cur_src && hashItem && hashItem.length() > 0            
-                
-                    if !mapDeleted[cur_src.name()]
-                        db.delete_all_from_table(cur_src.name())
-                        mapDeleted[cur_src.name()] = 1
-                    end
-                    
-                    hashItem['object'] = cur_objid
-                    db.insert_into_table(cur_src.name(),hashItem) 
+          row = {}
+          columns.each_with_index do |col,idx|
+            col.strip! 
+            if col == 'source_name'
+              src_name = parts[idx]
+              src_name = source_map[src_name] if source_map
+              row['source_id'] = @@mapSrc[src_name].source_id()
+            else
+              row[col] = parts[idx]
+            end
+          end
+          
+          src = @@mapSrcByIdx[ row['source_id'].to_i ]
+          src_db = src ? ::Rho::RHO.get_src_db(src.name() ) :  ::Rho::RHO.get_user_db()
+          if src_db != db
+            db.commit if db
+            db = src_db
+            db.start_transaction 
+            
+            if !mapPartDeleted[db]
+                #puts  "delete_all_from_table: #{row}"
+                db.delete_all_from_table(filename)
+                mapPartDeleted[db] = 1
+            end
+          end
+          
+          if ( cur_objid != row['object'] )
+            if cur_src && hashItem && hashItem.length() > 0            
+            
+                if !mapDeleted[cur_src.name()]
+                    db.delete_all_from_table(cur_src.name())
+                    mapDeleted[cur_src.name()] = 1
                 end
                 
-                hashItem = {}
-                cur_objid = row['object']            
-              end
-              
-              cur_src = @@mapSrcByIdx[ row['source_id'].to_i ]
-              #puts "cur_src: #{cur_src.schema()}" if cur_src
-              if ( cur_src && cur_src.schema() )  
-                hashItem[ row['attrib'] ] = row['value'] if row['value']
-              else
-                db.insert_into_table(filename,row)
-              end  
-            end                
-        end
-
+                hashItem['object'] = cur_objid
+                db.insert_into_table(cur_src.name(),hashItem) 
+            end
+            
+            hashItem = {}
+            cur_objid = row['object']            
+          end
+          
+          cur_src = @@mapSrcByIdx[ row['source_id'].to_i ]
+          #puts "cur_src: #{cur_src.schema()}" if cur_src
+          if ( cur_src && cur_src.schema() )  
+            hashItem[ row['attrib'] ] = row['value'] if row['value']
+          else
+            db.insert_into_table(filename,row)
+          end  
+        end                
+        f.close
+        
         if cur_src && hashItem && hashItem.length() > 0            
         
             if !mapDeleted[cur_src.name()]
@@ -142,39 +142,39 @@ module Rho
         prefix = dir_prefix.nil? ? "" : dir_prefix
         query = ""
         
-        File.open(File.join(Rho::RhoFSConnector.get_base_app_path(),'app',prefix,'fixtures',filename+'.txt')) do |f|
-          f.each do |line|
-              if row_index == 0
-                columns = line.chomp.split('|')
-                quests = ""
-                columns.each do |col|             
-                    quests += ',' if quests.length() > 0
-                    quests += '?'
-                end
-                query = "insert into #{filename} (#{columns.join(',')}) values (#{quests})"
-                row_index += 1
-                next
-              end
-              
-              parts = line.chomp.split('|')
-              
-              begin
-                  db.execute_sql query, parts
-              rescue Exception => e
-                #puts "load_offline_data : exception insert data: #{e}; data : #{parts}; line : #{row_index}"
-              end
-              
-              if row_index%commit_count == 0          
-                puts "commit start"
-                db.commit
-                db.start_transaction
-                puts "commit : #{row_index}"
-              end  
-              
-              row_index += 1
-          end                  
-        end
-
+        f = File.open(File.join(Rho::RhoFSConnector.get_base_app_path(),'app',prefix,'fixtures',filename+'.txt'))
+        f.each do |line|
+          if row_index == 0
+            columns = line.chomp.split('|')
+            quests = ""
+            columns.each do |col|             
+                quests += ',' if quests.length() > 0
+                quests += '?'
+            end
+            query = "insert into #{filename} (#{columns.join(',')}) values (#{quests})"
+            row_index += 1
+            next
+          end
+          
+          parts = line.chomp.split('|')
+          
+          begin
+              db.execute_sql query, parts
+          rescue Exception => e
+            #puts "load_offline_data : exception insert data: #{e}; data : #{parts}; line : #{row_index}"
+          end
+          
+          if row_index%commit_count == 0          
+            puts "commit start"
+            db.commit
+            db.start_transaction
+            puts "commit : #{row_index}"
+          end  
+          
+          row_index += 1
+        end                  
+        f.close
+        
         db.commit
         puts "commit : #{row_index}"
         columns = []
