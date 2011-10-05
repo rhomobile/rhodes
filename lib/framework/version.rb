b038db1cdf9dde6d59ecf39c5de67ce6b18665d0
genywind <evgeny@rhomobile.com>
2011-10-05 16:40:01 +0400

    bumping version for release

--- lib/framework/version.rb
+++ lib/framework/version.rb
@@ -1,6 +1,6 @@
 module RhodesFramework
   unless defined? RhodesFramework::VERSION
-    VERSION = '3.1.0'
+    VERSION = '3.2.0'
   end
   unless defined? RhodesFramework::DBVERSION
     DBVERSION = '2.2.2'
--- lib/rhodes.rb
+++ lib/rhodes.rb
@@ -1,6 +1,6 @@
 module Rhodes
   unless defined? Rhodes::VERSION
-    VERSION = '3.1.0'
+    VERSION = '3.2.0'
   end
   unless defined? Rhodes::DBVERSION
     DBVERSION = '2.2.0'
