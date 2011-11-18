567200a0b5612dd3f0e1ea484fde9b0d678fa8e8
genywind <evgeny@rhomobile.com>
2011-11-19 01:39:27 +0400

    fix spec

--- spec/phone_spec/app/spec/syncengine_spec.rb
+++ spec/phone_spec/app/spec/syncengine_spec.rb
@@ -196,7 +196,11 @@ end
     dbRes.length.should == 1
     dbRes[0]['token_sent'].should == 0
     dbRes[0]['token'].should be_nil
-    dbRes[0]['client_id'].should be_nil
+    if $spec_settings[:schema_model]
+        dbRes[0]['client_id'].should be_nil
+    else
+        dbRes[0]['client_id'].should == ""
+    end    
   
     res = ::Rho::RhoSupport::parse_query_parameters getProduct.sync( "/app/Settings/sync_notify")
     res['status'].should == 'ok'
