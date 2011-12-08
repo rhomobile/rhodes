7862ef0aa7f5168469bcbbe9b7b2dbf51c4cafad
larsburgess <larsburgess@gmail.com>
2011-12-07 17:51:10 -0800

    #21979037 - added `metadata=` helper to rhom objects
    This allows simple assignment of metadata inside of your application
    (i.e. by not using rhoconnect): Account.metadata = {"foo" =>
    "bar"}.to_json

--- lib/framework/rhom/rhom_object_factory.rb
+++ lib/framework/rhom/rhom_object_factory.rb
@@ -116,6 +116,14 @@ module Rhom
                   
                   Rho::RhoController.cached_metadata[src_name]
                 end
+                
+                def metadata=(metadata_doc)
+                  src_name = get_source_name
+                  db = ::Rho::RHO.get_src_db(src_name)
+                  db.start_transaction
+                  db.execute_sql("UPDATE sources set metadata=? where name=?", metadata_doc, src_name)
+                  db.commit
+                end
 
                 def count
                   db = ::Rho::RHO.get_src_db(get_source_name)
