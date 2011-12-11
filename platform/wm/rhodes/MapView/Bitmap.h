d87474b22f4b7c6a7aeb11146e4c7f41f36deeb2
Anton Vishnevski <antonv@rhomobile.com>
2011-12-11 22:20:13 +0400

    separate mapview graphs engine for wm/win32

--- platform/wm/rhodes/MapView/Bitmap.h
+++ platform/wm/rhodes/MapView/Bitmap.h
@@ -52,9 +52,8 @@ using namespace rho::common::map;
 using namespace rho;
 using namespace stdext;
 
-
-
-class WMAlphaBitmap {
+class WMAlphaBitmap 
+{
 public:
 	WMAlphaBitmap(IImage* img);
 	virtual ~WMAlphaBitmap();
@@ -69,7 +68,8 @@ private:
 	int mHeight;
 };
 
-class WMBitmap {
+class WMBitmap 
+{
 public:
 	WMBitmap(IImage* img, bool useAlpha);
 	virtual public ~WMBitmap();
@@ -94,79 +94,3 @@ private:
 };
 
 
-class DrawingImageImpl : public IDrawingImage {
-public:
-	DrawingImageImpl(void const *p, int size, bool useAlpha);
-	DrawingImageImpl(const char* path, bool useAlpha);
-	DrawingImageImpl(WMBitmap* bitmap);
-	virtual ~DrawingImageImpl();
-	
-	virtual void draw(HDC hdc, int x, int y);
-
-	virtual IDrawingImage* clone();
-
-	virtual int width() const {return mWidth;}
-	virtual int height() const {return mHeight;}
-
-private:
-	void init(const char* path, void const *p, int size, WMBitmap* bitmap, bool useAlpha);
-	//IImage* mImage;
-	WMBitmap* mBitmap;
-	int mWidth;
-	int mHeight;
-	int mID;
-};
-
-
-class DrawingContextImpl : public IDrawingContext{
-public:
-	DrawingContextImpl(HDC hdc, int width, int height);
-	virtual int getWidth();
-	virtual int getHeight();
-	virtual void drawImage(int x, int y, IDrawingImage* image);
-	virtual void drawText(int x, int y, int nWidth, int nHeight, String const &text, int color);
-	virtual void getTextRect(int x, int y, String &text, RECT* resultRect);
-	virtual void fillRect(int x, int y, int width, int height, int color);
-	virtual void drawLine(int x1, int y1, int x2, int y2, int color);
-
-
-private:
-	HDC mHDC;
-	int mWidth;
-	int mHeight;
-};
-
-class DrawingDeviceImpl : public IDrawingDevice
-{
-public:
-
-	virtual IDrawingImage* createImage(String const &path, bool useAlpha) {
-		return new DrawingImageImpl(path.c_str(), useAlpha);
-	}
-	virtual IDrawingImage* createImage(void const *p, size_t s, bool useAlpha) {
-		return new DrawingImageImpl(p, s, useAlpha);
-	}
-
-	virtual IDrawingImage* createImageEx(void const *p, size_t s, int x, int y, int w, int h, bool useAlpha) {
-		return new DrawingImageImpl(p, s, useAlpha);
-	}
-
-
-	virtual IDrawingImage* cloneImage(IDrawingImage *image) {
-		if (image == NULL) {
-			return NULL;
-		}
-		return ((DrawingImageImpl*)image)->clone();
-	}
-
-	virtual void destroyImage(IDrawingImage* image) {
-		delete image;
-	};
-
-
-    	virtual IDrawingImage* createCalloutImage(String const &title, String const &subtitle, String const& url, int* x_offset, int* y_offset)  {
-		return NULL;
-	}
-
-	virtual void requestRedraw(){}
-};
