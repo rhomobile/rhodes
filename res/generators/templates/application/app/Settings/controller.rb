af3ff117c8921332afbcff842c9dda8a8777a6c8
genywind <evgeny@rhomobile.com>
2011-12-16 19:32:04 +0400

    generator: remove back from login page

--- res/generators/templates/application/app/Settings/controller.rb
+++ res/generators/templates/application/app/Settings/controller.rb
@@ -13,7 +13,7 @@ class SettingsController < Rho::RhoController
 
   def login
     @msg = @params['msg']
-    render :action => :login, :back => '/app'
+    render :action => :login
   end
 
   def login_callback
