13350ae81f4f52c959c4f4fd7c790e509f6f543c
Albert R. Timashev <albert@rhomobile.com>
2012-01-11 18:43:07 +0400

    rhosimulator: Rho::RhoConfig.app_version fixed

--- platform/shared/qt/rhodes/main.cpp
+++ platform/shared/qt/rhodes/main.cpp
@@ -149,6 +149,7 @@ int main(int argc, char *argv[])
     if ( m_strRhodesPath.length() > 0 )
         RHOSIMCONF().setString("rhodes_path", m_strRhodesPath, false );
     RHOCONF().setString( "rhosim_platform", RHOSIMCONF().getString( "platform"), false);
+    RHOCONF().setString( "app_version", RHOSIMCONF().getString( "app_version"), false);
     RHOSIMCONF().setString( "ext_path", RHOSIMCONF().getString( "ext_path") + CFilePath::join( m_strRhodesPath, "/lib/extensions/debugger;"), false);
     RHOSIMCONF().setString( "ext_path", RHOSIMCONF().getString( "ext_path") + CFilePath::join( m_strRhodesPath, "/lib/extensions/uri;"), false);
     RHOSIMCONF().setString( "ext_path", RHOSIMCONF().getString( "ext_path") + CFilePath::join( m_strRhodesPath, "/lib/extensions/timeout;"), false);
