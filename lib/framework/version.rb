2e8709483ea071552dbd75008e5da46b8dda1e4b
genywind <evgeny@rhomobile.com>
2012-02-27 22:59:06 +0400

    rhodes 3.3.3.beta.2

--- lib/framework/version.rb
+++ lib/framework/version.rb
@@ -1,6 +1,6 @@
 module RhodesFramework
   unless defined? RhodesFramework::VERSION
-    VERSION = '3.3.2'
+    VERSION = '3.3.3'
   end
   unless defined? RhodesFramework::DBVERSION
     DBVERSION = '2.2.2'
