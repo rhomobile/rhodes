db91d48c3adebbe98e172ed5eabb7e17cc18367d
dmitrys <dmitrys@rhomobile.com>
2012-03-27 21:12:16 +0400

    Support Moto Barcode API in Rhodes Barcode extension

--- lib/extensions/barcode/ext/barcode/platform/android/jni/src/barcode.cpp
+++ lib/extensions/barcode/ext/barcode/platform/android/jni/src/barcode.cpp
@@ -1,6 +1,6 @@
 //#include "rhodes/JNIRhodes.h"
 #include <rhodes.h>
-//#include <android/log.h>
+#include <android/log.h>
 #include <common/RhodesApp.h>
 #include <logging/RhoLog.h>
 #include <stdlib.h>
@@ -85,3 +85,83 @@ RHO_GLOBAL void JNICALL Java_com_rhomobile_barcode_Barcode_callback
     rho_net_request_with_data(url, rho_cast<std::string>(env, body).c_str());
     rho_http_free(url);
 }
+
+
+
+class RhoScannersListContainer : public rho::ICallbackObject
+{
+public:
+	RhoScannersListContainer() {
+	}
+    
+	virtual ~RhoScannersListContainer() {
+	}
+    
+	virtual unsigned long getObjectValue() {
+        VALUE b_array = rho_ruby_create_array();
+        VALUE b_hash = rho_ruby_createHash();
+        rho_ruby_add_to_hash(b_hash, rho_ruby_create_string("friendlyName"), rho_ruby_create_string("RhoBarcode based on ZXing"));
+        rho_ruby_add_to_hash(b_hash, rho_ruby_create_string("deviceName"), rho_ruby_create_string("RhoBarcode"));
+        rho_ruby_add_to_array(b_array, b_hash);
+        return b_array;
+	}
+};
+
+
+
+
+
+RHO_GLOBAL void JNICALL Java_com_rhomobile_barcode_Barcode_enumeratecallback
+(JNIEnv *env, jclass, jstring callback_url) {
+    char* url = rho_http_normalizeurl(rho_cast<std::string>(env, callback_url).c_str());
+    
+    char body[2048];
+    
+	strcpy(body, "&status=ok&rho_callback=1&");
+	strcat(body, (RHODESAPP().addCallbackObject( new RhoScannersListContainer(), "scannerArray")).c_str());
+    char* norm_url = rho_http_normalizeurl(url);
+    
+    rho_net_request_with_data(norm_url, body);
+    rho_http_free(norm_url);    
+    
+    
+    
+}
+
+extern "C" void rho_motobarcode_enumerate(const char* callback) {
+    
+    JNIEnv *env = jnienv();
+    jclass cls = rho_find_class(env, "com/rhomobile/barcode/Barcode");
+    if (!cls) return;
+    jmethodID mid = env->GetStaticMethodID( cls, "callMotoEnumerateCallback", "(Ljava/lang/String;)V");
+    if (!mid) return;
+    
+    jstring objCallback = env->NewStringUTF(callback);
+    if(objCallback != NULL)
+    {
+        env->CallStaticObjectMethod(cls, mid, objCallback);
+        env->DeleteLocalRef(objCallback);
+    }
+
+    //VALUE b_array = rho_ruby_create_array();
+    //rho_ruby_add_to_array(b_array, rho_ruby_create_string("RhoBarcode based on ZXing"));
+	//return b_array;
+   
+}
+
+extern "C" void  rho_motobarcode_enable(const char* callback, rho_param* p) {
+    __android_log_write(ANDROID_LOG_INFO, "RhoBarcode", "Barcode::enable() DO NOT SUPPORT BY RHODES SW SCANNER !");    
+}
+
+extern "C" void  rho_motobarcode_disable() {
+    __android_log_write(ANDROID_LOG_INFO, "RhoBarcode", "Barcode::disable() DO NOT SUPPORT BY RHODES SW SCANNER !");    
+}
+
+extern "C" void  rho_motobarcode_start() {
+    __android_log_write(ANDROID_LOG_INFO, "RhoBarcode", "Barcode::start() DO NOT SUPPORT BY RHODES SW SCANNER !");    
+}
+
+extern "C" void  rho_motobarcode_stop() {
+    __android_log_write(ANDROID_LOG_INFO, "RhoBarcode", "Barcode::stop() DO NOT SUPPORT BY RHODES SW SCANNER !");    
+}
+
