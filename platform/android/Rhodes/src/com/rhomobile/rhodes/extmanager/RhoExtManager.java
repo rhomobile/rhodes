897825ae22b96dc028c29c49d6b4994e17d210d6
Alexey Tikhvinsky <alexey@rhomobile.com>
2012-04-11 17:51:50 +0400

    Android: Provide common convenient way to print messages to rhodes log for extensions

--- platform/android/Rhodes/src/com/rhomobile/rhodes/extmanager/RhoExtManager.java
+++ platform/android/Rhodes/src/com/rhomobile/rhodes/extmanager/RhoExtManager.java
@@ -1,7 +1,8 @@
 package com.rhomobile.rhodes.extmanager;
 
-public class RhoExtManager extends Object {
+import com.rhomobile.rhodes.Logger;
 
+public class RhoExtManager extends Object {
     private static RhoExtManagerImpl ourRhoExtManager = null;
 
     public static RhoExtManagerImpl getImplementationInstance() {
@@ -11,8 +12,47 @@ public class RhoExtManager extends Object {
         return ourRhoExtManager;
     }
 
+    /**
+     * @return IRhoExtManager instance
+     */
     public static IRhoExtManager getInstance() {
         return getImplementationInstance();
+    }
+
+    /**
+     * Convenient way to print trace message to rhodes log
+     * @param tag - log source
+     * @param msg - log message
+     */
+    public static void logT(String tag, String msg) {
+        Logger.T(tag, msg);
+    }
+
+    /**
+     * Convenient way to print info message to rhodes log
+     * @param tag - log source
+     * @param msg - log message
+     */
+    public static void logI(String tag, String msg) {
+        Logger.I(tag, msg);
+    }
+
+    /**
+     * Convenient way to print warning message to rhodes log
+     * @param tag - log source
+     * @param msg - log message
+     */
+    public static void logW(String tag, String msg) {
+        Logger.W(tag, msg);
+    }
+
+    /**
+     * Convenient way to print error message to rhodes log
+     * @param tag - log source
+     * @param msg - log message
+     */
+    public static void logE(String tag, String msg) {
+        Logger.E(tag, msg);
     }
 
 }
