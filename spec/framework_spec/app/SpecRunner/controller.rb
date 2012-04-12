a2fff2ab5c55b0752b2838452d5c5e19b1ae8ae5
genywind <evgeny@rhomobile.com>
2012-04-12 20:34:54 +0400

    specs: add GC.enable

--- spec/framework_spec/app/SpecRunner/controller.rb
+++ spec/framework_spec/app/SpecRunner/controller.rb
@@ -5,6 +5,8 @@ class SpecRunnerController < Rho::RhoController
 
   #GET /SpecRunner
   def index
+    GC.enable()
+  
     @msg = 'MSpec version: '+MSpec::VERSION
     @runner = SpecRunner.new
     @code = @runner.run
