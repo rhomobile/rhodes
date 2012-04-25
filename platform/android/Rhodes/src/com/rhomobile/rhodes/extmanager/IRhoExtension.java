8d1fc4a37b6d7ed0906b0209e330527c4157c682
Alexey Tikhvinsky <alexey@rhomobile.com>
2012-04-25 20:50:00 +0400

    Android: integrate with new version of Motorola web view

--- platform/android/Rhodes/src/com/rhomobile/rhodes/extmanager/IRhoExtension.java
+++ platform/android/Rhodes/src/com/rhomobile/rhodes/extmanager/IRhoExtension.java
@@ -1,5 +1,7 @@
 package com.rhomobile.rhodes.extmanager;
 
+import android.graphics.Rect;
+
 public interface IRhoExtension {
 
     void onSetPropertiesData(IRhoExtManager extManager, String propId, String data, int pos, int total, IRhoExtData ext);
@@ -19,14 +21,15 @@ public interface IRhoExtension {
     void onStatus(IRhoExtManager extManager, String status, IRhoExtData ext);
     void onTitle(IRhoExtManager extManager, String title, IRhoExtData ext);
     void onConsole(IRhoExtManager extManager, String message, IRhoExtData ext);
-    void onInputMethod(IRhoExtManager extManager, boolean enabled, IRhoExtData ext);
+    void onInputMethod(IRhoExtManager extManager, boolean enabled, String type, Rect area, IRhoExtData ext);
     void onNavigateError(IRhoExtManager extManager, String url, IRhoExtData ext);
 
     void onAppActivate(IRhoExtManager extManager, boolean bActivate);
 
     //EkiohLocation getCachedLocation(IRhoExtManager extManager, IRhoExtData ext);
     void startLocationUpdates(IRhoExtManager extManager, boolean highAccuracy, IRhoExtData ext); 
-    void stopLocationUpdates(IRhoExtManager extManager, IRhoExtData ext); 
+    void stopLocationUpdates(IRhoExtManager extManager, IRhoExtData ext);
+    void onAuthRequired(IRhoExtManager extManager, String type, String url, String realm, IRhoExtData ext); 
 
 }
 
