de96e5f7cd0a9fa9a2bb20b7f56a914da34443e9
rognar <kostyar2004@bk.ru>
2012-04-28 18:02:45 +0400

    added license stub

---
+++ platform/wm/rhodes/LicenseStub.cpp
@@ -0,0 +1,39 @@
+#include "stdafx.h"
+#include "common/app_build_capabilities.h"
+
+#ifdef APP_BUILD_CAPABILITY_WEBKIT_BROWSER && APP_BUILD_CAPABILITY_NON_MOTOROLA_DEVICE
+class CConfig
+{
+};
+
+class CSync
+{
+public:	
+	int Run(int a, long b,long c);
+};
+
+int CSync::Run(int a, long b,long c)
+{
+	return 1;
+}
+
+class CAppManager
+{
+public:	
+	long SwitchAppInst(int iInstID);
+};
+
+long CAppManager::SwitchAppInst(int iInstID)
+{
+	return 1;
+}
+
+struct PBCoreStruct
+{
+};
+
+CConfig *g_pConfig = 0;
+CSync	*g_pEventSync = 0;
+CAppManager	*g_pAppManager = 0;	
+PBCoreStruct *g_pPBCore = 0;
+#endif
