8caca006515b243961a8d9c0f07223a92806a0fe
Alex Epifanoff <alex.epifanoff@gmail.com>
2012-06-09 20:06:38 +0400

    phone_spec: database export spec

---
+++ spec/phone_spec/app/spec/database_spec.rb
@@ -0,0 +1,72 @@
+#
+#  database_spec.rb
+#  rhodes
+#
+#  Copyright (C) 2012 Rhomobile, Inc. All rights reserved.
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#require 'spec/spec_helper'
+require 'rho/rho'
+#require 'fileutils'
+
+describe "Database_test" do
+	
+	before(:all)  do
+		SyncEngine.set_threaded_mode(false)
+		
+		::Rhom::Rhom.database_full_reset_and_logout
+		
+	end
+	
+	after(:all)  do
+		
+	end
+	
+def copy_file(src, dst_dir)
+	if !defined?(RHO_WP7)  
+		content = File.binread(src)  
+	else
+		content = File.read(src)  
+	end    
+    File.open(File.join( dst_dir, File.basename(src) ), "wb"){|f| f.write(content) }
+end
+	  
+		
+	
+	it "should export database Database spec" do		
+		file_name = File.join(Rho::RhoApplication::get_model_path('app','BlobBulkTest'), 'test3.png')
+		copy_file(file_name, Rho::RhoApplication::get_blob_folder() )
+		file_name = File.join(Rho::RhoApplication::get_blob_folder(), 'test3.png')
+		File.exists?(file_name).should == true
+		if !defined?(RHO_WP7)   
+			file_size = File.size(file_name)
+		end    
+		file_content = File.read(file_name)
+		
+		item = BlobBulkTest.new
+		puts "item = #{item.inspect}"
+		item.name = 'BlobTestItem'
+		item.image_uri = 'test3.png'
+		item.save
+		
+		File.exists?(file_name).should == true
+
+		exportPath = ::Rhom::Rhom.database_export('user')
+		exportPath.should_not be_nil		
+		File.exists?(exportPath).should == true
+		File.size(exportPath).should_not == 0
+
+	end
+end
