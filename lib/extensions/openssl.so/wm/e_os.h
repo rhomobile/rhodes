cb44efd765a7f6f59d408fb2ed7af9127312dda0
genywind <evgeny@rhomobile.com>
2012-07-06 02:51:21 +0400

    wm: fix openssl build

--- lib/extensions/openssl.so/wm/e_os.h
+++ lib/extensions/openssl.so/wm/e_os.h
@@ -308,13 +308,14 @@ static unsigned int _strlen31(const char *str)
 #    endif
 #    if defined(_MSC_VER) && !defined(_DLL) && defined(stdin)
 #      if _MSC_VER>=1300
-#        undef stdin
+/*#        undef stdin
 #        undef stdout
 #        undef stderr
-         FILE *__iob_func();
+         //FILE *__iob_func();
+        _CRTIMP FILE * __cdecl __iob_func(void);
 #        define stdin  (&__iob_func()[0])
 #        define stdout (&__iob_func()[1])
-#        define stderr (&__iob_func()[2])
+#        define stderr (&__iob_func()[2])*/
 #      elif defined(I_CAN_LIVE_WITH_LNK4049)
 #        undef stdin
 #        undef stdout
