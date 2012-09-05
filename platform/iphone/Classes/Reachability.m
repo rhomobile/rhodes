9640320dd16ab06ffe8312e707020feb60ebfd4b
Alex Epifanoff <alex.epifanoff@gmail.com>
2012-09-05 19:45:50 +0400

    iphone: removed annoying logging

--- platform/iphone/Classes/Reachability.m
+++ platform/iphone/Classes/Reachability.m
@@ -9,7 +9,7 @@
 
 #import "Reachability.h"
 
-#define kShouldPrintReachabilityFlags 1
+//#define kShouldPrintReachabilityFlags 1
 
 static void PrintReachabilityFlags(SCNetworkReachabilityFlags    flags, const char* comment)
 {
