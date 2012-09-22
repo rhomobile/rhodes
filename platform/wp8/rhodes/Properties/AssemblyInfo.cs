a97f3d6b7d9060d2c6ad4a14c8997ed7024bc48d
Albert R. Timashev <albert@rhomobile.com>
2012-09-22 13:10:09 -0700

    Windows Phone 8 platform added

---
+++ platform/wp8/rhodes/Properties/AssemblyInfo.cs
@@ -0,0 +1,37 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Resources;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("rhodes")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Motorola Solutions, Inc.")]
+[assembly: AssemblyProduct("rhodes")]
+[assembly: AssemblyCopyright("Copyright © 2012")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("d8f11645-2628-4d05-a2dc-2ef1d7f9a57f")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("3.4.0.0")]
+[assembly: AssemblyFileVersion("3.4.0.0")]
+[assembly: NeutralResourcesLanguageAttribute("en-US")]
---
+++ platform/wp8/rhoruntime/rhoruntime.h
@@ -0,0 +1,16 @@
+﻿#pragma once
+
+namespace rhoruntime
+{
+	public delegate void CallbackPointer();
+
+    public ref class CRhoRuntime sealed
+    {
+    public:
+		CRhoRuntime();
+		void SetCallback(CallbackPointer^ callback);
+		void DoCallback();
+	private:
+		CallbackPointer^ m_Callback;
+    };
+}
