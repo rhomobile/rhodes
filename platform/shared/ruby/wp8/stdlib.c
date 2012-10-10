b8bbd609542b7a0555c60a24434d85842a6b6877
anton vishnevski <antonv@rhomobile.com>
2012-10-10 19:14:10 +0400

    final fix linker issuew

--- platform/shared/ruby/wp8/stdlib.c
+++ platform/shared/ruby/wp8/stdlib.c
@@ -20,40 +20,40 @@ char *_fullpath(char *absPath, const char *relPath,
 	return absPath;
 }
 
-int mblen(const char *mbstr, size_t count)
-{
-	const char *p = mbstr;
-	size_t i;
-	int    n=0;
+//int mblen(const char *mbstr, size_t count)
+//{
+//	const char *p = mbstr;
+//	size_t i;
+//	int    n=0;
+//
+//	for( i=0; i<count; i++ )
+//	{
+//		if( *p=='\0' ) break;
+//		if( IsDBCSLeadByteEx( CP_UTF8, *p ) )
+//			n+=2, p+=2;
+//		else
+//			n+=1, p+=1;
+//	}
+//
+//	return n;
+//}
 
-	for( i=0; i<count; i++ )
-	{
-		if( *p=='\0' ) break;
-		if( IsDBCSLeadByteEx( CP_UTF8, *p ) )
-			n+=2, p+=2;
-		else
-			n+=1, p+=1;
-	}
-
-	return n;
-}
-
-void *bsearch( const void *key, const void *base,
-			   size_t num, size_t width,
-			   int ( __cdecl *compare )(const void *, const void *))
-{
-	size_t i;
-	const void* p = base;
-	const char* px;
-
-	for( i=0; i<num; i++ )
-	{
-		if( 0==compare( key, p ) )
-			return (void*)p;
-		px = (const char*)p; px+=width; p=(const void*)px;
-	}
-	return NULL;
-}
+//void *bsearch( const void *key, const void *base,
+//			   size_t num, size_t width,
+//			   int ( __cdecl *compare )(const void *, const void *))
+//{
+//	size_t i;
+//	const void* p = base;
+//	const char* px;
+//
+//	for( i=0; i<num; i++ )
+//	{
+//		if( 0==compare( key, p ) )
+//			return (void*)p;
+//		px = (const char*)p; px+=width; p=(const void*)px;
+//	}
+//	return NULL;
+//}
 
 int __cdecl strcmpi(const char * _Str1, const char * _Str2)
 {
