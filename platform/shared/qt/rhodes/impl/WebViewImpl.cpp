2494b6da4c75ebe4a744cbc4be7ac7f8e15e4717
Alexey Tikhvinsky <alexey@rhomobile.com>
2012-10-24 18:33:50 +0400

    Fix build

--- platform/shared/qt/rhodes/impl/WebViewImpl.cpp
+++ platform/shared/qt/rhodes/impl/WebViewImpl.cpp
@@ -113,7 +113,7 @@ void rho_webview_set_cookie(const char *url, const char *cookie)
 
 void rho_webview_save(const char* format, const char* path, int tab_index)
 {
-    RAWLOG_ERROR("rho_webview_save is not implemented")
+    RAWLOG_ERROR("rho_webview_save is not implemented");
 }
 
 } //extern "C"
