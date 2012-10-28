e0db59db3f78a501ebb0c9fdeffdae324057efc6
dmitrys <dmitrys@rhomobile.com>
2012-10-28 20:37:57 +0400

    fix rhodes build

--- platform/shared/qt/rhodes/impl/WebViewImpl.cpp
+++ platform/shared/qt/rhodes/impl/WebViewImpl.cpp
@@ -119,6 +119,7 @@ void rho_webview_save(const char* format, const char* path, int tab_index)
 VALUE rho_webview_get_current_url(int tab_index)
 {
     RAWLOG_ERROR("rho_webview_get_current_url is not implemented");
+    return rho_ruby_get_NIL();
 }
    
 
