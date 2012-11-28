77f9303ae2cd6543ef364aeab4b47dd2ae60dcc2
Stanislav Volodarskiy <stan_volodarsky@mail.ru>
2012-11-28 21:47:26 +0400

    spec/phone_spec/app/spec/rho_spec.rb: additional guard for "should parse xml date" spec

--- spec/phone_spec/app/spec/rho_spec.rb
+++ spec/phone_spec/app/spec/rho_spec.rb
@@ -474,10 +474,12 @@ end
     time[1].should == '10'
     time[2].should == "11.123"
     
-    var_time= Time.local(day[0],day[1],day[2],time[0],time[1],time[2])
-    res = var_time.strftime("%b %d, %y, %I:%M%p")
-    
-    res.should == "Feb 05, 11, 09:10PM"
+    unless System.get_property('is_emulator') && System.get_property('platform') == 'APPLE'
+      var_time= Time.local(day[0],day[1],day[2],time[0],time[1],time[2])
+      res = var_time.strftime("%b %d, %y, %I:%M%p")
+
+      res.should == "Feb 05, 11, 09:10PM"
+    end
   
   end
         
