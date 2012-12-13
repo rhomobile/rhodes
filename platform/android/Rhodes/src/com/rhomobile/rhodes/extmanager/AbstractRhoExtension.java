7a3e1e7621d13f66bc005066a3aa62d8c0d2e257
Alexey Tikhvinsky <alexey@rhomobile.com>
2012-12-13 22:44:50 +0400

    Android: Refactor IRhoExtension handlers to return status flag

--- platform/android/Rhodes/src/com/rhomobile/rhodes/extmanager/AbstractRhoExtension.java
+++ platform/android/Rhodes/src/com/rhomobile/rhodes/extmanager/AbstractRhoExtension.java
@@ -10,68 +10,84 @@ public abstract class AbstractRhoExtension implements IRhoExtension {
     }
 
     @Override
-    public void onSetPropertiesData(IRhoExtManager extManager, String propId,
-            String data, int pos, int total, IRhoWebView ext) {
+    public boolean onSetPropertiesData(IRhoExtManager extManager, String propId,
+            String data, int pos, int total, IRhoWebView ext, boolean res) {
+        return res;
     }
 
     @Override
-    public void onSetPropertiesDataEnd(IRhoExtManager extManager, IRhoWebView ext) {
+    public boolean onSetPropertiesDataEnd(IRhoExtManager extManager, IRhoWebView ext, boolean res) {
+        return res;
     }
 
     @Override
-    public void onSetProperty(IRhoExtManager extManager, String name, String value, IRhoWebView ext) {
+    public boolean onSetProperty(IRhoExtManager extManager, String name, String value, IRhoWebView ext, boolean res) {
+        return res;
     }
 
     @Override
-    public void onBeforeNavigate(IRhoExtManager extManager, String url, IRhoWebView ext) {
+    public boolean onBeforeNavigate(IRhoExtManager extManager, String url, IRhoWebView ext, boolean res) {
+        return res;
     }
 
     @Override
-    public void onNavigateProgress(IRhoExtManager extManager, String url, int pos, int total, IRhoWebView ext) {
+    public boolean onNavigateProgress(IRhoExtManager extManager, String url, int pos, int total, IRhoWebView ext, boolean res) {
+        return res;
     }
 
     @Override
-    public void onNavigateComplete(IRhoExtManager extManager, String url, IRhoWebView ext) {
+    public boolean onNavigateComplete(IRhoExtManager extManager, String url, IRhoWebView ext, boolean res) {
+        return res;
     }
 
     @Override
-    public void onDocumentComplete(IRhoExtManager extManager, String urlOfDocument, IRhoWebView ext) {
+    public boolean onDocumentComplete(IRhoExtManager extManager, String urlOfDocument, IRhoWebView ext, boolean res) {
+        return res;
     }
 
     @Override
-    public void onAlert(IRhoExtManager extManager, String message, IRhoWebView ext) {
+    public boolean onAlert(IRhoExtManager extManager, String message, IRhoWebView ext, boolean res) {
+        return res;
     }
 
     @Override
-    public void onConfirm(IRhoExtManager extManager, String message, IRhoWebView ext) {
+    public boolean onConfirm(IRhoExtManager extManager, String message, IRhoWebView ext, boolean res) {
+        return res;
     }
 
     @Override
-    public void onPrompt(IRhoExtManager extManager, String message, String defaultResponse, IRhoWebView ext) {
+    public boolean onPrompt(IRhoExtManager extManager, String message, String defaultResponse, IRhoWebView ext, boolean res) {
+        return res;
     }
 
     @Override
-    public void onSelect(IRhoExtManager extManager, String[] items, int selected, IRhoWebView ext) {
+    public boolean onSelect(IRhoExtManager extManager, String[] items, int selected, IRhoWebView ext, boolean res) {
+        return res;
     }
 
     @Override
-    public void onStatus(IRhoExtManager extManager, String status, IRhoWebView ext) {
+    public boolean onStatus(IRhoExtManager extManager, String status, IRhoWebView ext, boolean res) {
+        return res;
     }
 
     @Override
-    public void onTitle(IRhoExtManager extManager, String title, IRhoWebView ext) {
+    public boolean onTitle(IRhoExtManager extManager, String title, IRhoWebView ext, boolean res) {
+        return res;
     }
 
     @Override
-    public void onConsole(IRhoExtManager extManager, String message, IRhoWebView ext) {
+    public boolean onConsole(IRhoExtManager extManager, String message, IRhoWebView ext, boolean res) {
+        return res;
     }
 
     @Override
-    public void onInputMethod(IRhoExtManager extManager, boolean enabled, String type, Rect rect, IRhoWebView ext) {
+    public boolean onInputMethod(IRhoExtManager extManager, boolean enabled, String type, Rect rect, IRhoWebView ext, boolean res) {
+        return res;
     }
 
     @Override
-    public void onNavigateError(IRhoExtManager extManager, String url, LoadErrorReason reason, IRhoWebView ext) {
+    public boolean onNavigateError(IRhoExtManager extManager, String url, LoadErrorReason reason, IRhoWebView ext, boolean res) {
+        return res;
     }
 
     @Override
@@ -82,15 +98,18 @@ public abstract class AbstractRhoExtension implements IRhoExtension {
     //EkiohLocation getCachedLocation(IRhoExtManager extManager, IRhoExtData ext);
 
     @Override
-    public void startLocationUpdates(IRhoExtManager extManager, boolean highAccuracy, IRhoWebView ext) {
+    public boolean startLocationUpdates(IRhoExtManager extManager, boolean highAccuracy, IRhoWebView ext, boolean res) {
+        return res;
     }
 
     @Override
-    public void stopLocationUpdates(IRhoExtManager extManager, IRhoWebView ext) {
+    public boolean stopLocationUpdates(IRhoExtManager extManager, IRhoWebView ext, boolean res) {
+        return res;
     }
 
     @Override
-    public void onAuthRequired(IRhoExtManager extManager, String type, String url, String realm, IRhoWebView ext) {
+    public boolean onAuthRequired(IRhoExtManager extManager, String type, String url, String realm, IRhoWebView ext, boolean res) {
+        return res;
     }
 
 }
--- platform/android/Rhodes/src/com/rhomobile/rhodes/extmanager/IRhoExtension.java
+++ platform/android/Rhodes/src/com/rhomobile/rhodes/extmanager/IRhoExtension.java
@@ -7,32 +7,29 @@ public interface IRhoExtension {
     public enum LoadErrorReason { STOP, INTERNAL_ERROR, BAD_LICENSE }
 
     IRhoWebView onCreateWebView(IRhoExtManager extManager, int tabIndex);
-    void onSetPropertiesData(IRhoExtManager extManager, String propId, String data, int pos, int total, IRhoWebView ext);
-    void onSetPropertiesDataEnd(IRhoExtManager extManager, IRhoWebView ext);
-    void onSetProperty(IRhoExtManager extManager, String name, String value, IRhoWebView ext);
-    void onBeforeNavigate(IRhoExtManager extManager, String url, IRhoWebView ext);
-    void onNavigateProgress(IRhoExtManager extManager, String url, int pos, int total, IRhoWebView ext);
-    void onNavigateComplete(IRhoExtManager extManager, String url, IRhoWebView ext);
-    void onDocumentComplete(IRhoExtManager extManager, String urlOfDocument, IRhoWebView ext);
-    //bool onWndMsg(MSG& oMsg){return false;}
-    //long onNavigateTimeout(IRhoExtManager extManager, String urlBeingNavigatedTo, IRhoExtData ext);
-    //long onSIPState(IRhoExtManager extManager, boolean bSIPState, IRhoExtData ext);
-    void onAlert(IRhoExtManager extManager, String message, IRhoWebView ext);
-    void onConfirm(IRhoExtManager extManager, String message, IRhoWebView ext);
-    void onPrompt(IRhoExtManager extManager, String message, String defaultResponse, IRhoWebView ext);
-    void onSelect(IRhoExtManager extManager, String[] items, int selected, IRhoWebView ext);
-    void onStatus(IRhoExtManager extManager, String status, IRhoWebView ext);
-    void onTitle(IRhoExtManager extManager, String title, IRhoWebView ext);
-    void onConsole(IRhoExtManager extManager, String message, IRhoWebView ext);
-    void onInputMethod(IRhoExtManager extManager, boolean enabled, String type, Rect area, IRhoWebView ext);
-    void onNavigateError(IRhoExtManager extManager, String url, LoadErrorReason reason, IRhoWebView ext);
+    boolean onSetPropertiesData(IRhoExtManager extManager, String propId, String data, int pos, int total, IRhoWebView ext, boolean res);
+    boolean onSetPropertiesDataEnd(IRhoExtManager extManager, IRhoWebView ext, boolean res);
+    boolean onSetProperty(IRhoExtManager extManager, String name, String value, IRhoWebView ext, boolean res);
+    boolean onBeforeNavigate(IRhoExtManager extManager, String url, IRhoWebView ext, boolean res);
+    boolean onNavigateProgress(IRhoExtManager extManager, String url, int pos, int total, IRhoWebView ext, boolean res);
+    boolean onNavigateComplete(IRhoExtManager extManager, String url, IRhoWebView ext, boolean res);
+    boolean onDocumentComplete(IRhoExtManager extManager, String url, IRhoWebView ext, boolean res);
+    boolean onAlert(IRhoExtManager extManager, String message, IRhoWebView ext, boolean res);
+    boolean onConfirm(IRhoExtManager extManager, String message, IRhoWebView ext, boolean res);
+    boolean onPrompt(IRhoExtManager extManager, String message, String defaultResponse, IRhoWebView ext, boolean res);
+    boolean onSelect(IRhoExtManager extManager, String[] items, int selected, IRhoWebView ext, boolean res);
+    boolean onStatus(IRhoExtManager extManager, String status, IRhoWebView ext, boolean res);
+    boolean onTitle(IRhoExtManager extManager, String title, IRhoWebView ext, boolean res);
+    boolean onConsole(IRhoExtManager extManager, String message, IRhoWebView ext, boolean res);
+    boolean onInputMethod(IRhoExtManager extManager, boolean enabled, String type, Rect area, IRhoWebView ext, boolean res);
+    boolean onNavigateError(IRhoExtManager extManager, String url, LoadErrorReason reason, IRhoWebView ext, boolean res);
+    boolean onAuthRequired(IRhoExtManager extManager, String type, String url, String realm, IRhoWebView ext, boolean res); 
 
     void onAppActivate(IRhoExtManager extManager, boolean bActivate);
 
     //EkiohLocation getCachedLocation(IRhoExtManager extManager, IRhoExtData ext);
-    void startLocationUpdates(IRhoExtManager extManager, boolean highAccuracy, IRhoWebView ext); 
-    void stopLocationUpdates(IRhoExtManager extManager, IRhoWebView ext);
-    void onAuthRequired(IRhoExtManager extManager, String type, String url, String realm, IRhoWebView ext); 
+    boolean startLocationUpdates(IRhoExtManager extManager, boolean highAccuracy, IRhoWebView ext, boolean res); 
+    boolean stopLocationUpdates(IRhoExtManager extManager, IRhoWebView ext, boolean res);
 
 }
 
