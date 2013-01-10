47cee6aaaa9deb0d3a88ca9397ed59ed0a3f5c97
genywind <evgeny@rhomobile.com>
2013-01-10 15:58:15 +0400

    api_generator:c++: support singleton

--- platform/shared/api_generator/sample/windows/Barcode1.h
+++ platform/shared/api_generator/sample/windows/Barcode1.h
@@ -21,8 +21,6 @@ struct IBarcode1Singleton
     virtual void enumerate(CMethodResult& oResult) = 0;
 };
 
-class CBarcode1SingletonBase;
-CBarcode1SingletonBase* barcode1_impl_createSingleton();
 
 class CBarcode1SingletonBase : public CModuleSingletonBase<IBarcode1, IBarcode1Singleton>
 {
@@ -30,13 +28,10 @@ protected:
     static rho::common::CAutoPtr<CBarcode1SingletonBase> m_pInstance;
 
 public:
-    static CBarcode1SingletonBase* getInstance()
-    {
-        if (!m_pInstance)
-            m_pInstance = barcode1_impl_createSingleton();
-
-        return m_pInstance;
-    }
+    static void setInstance(CBarcode1SingletonBase* pInstance){ m_pInstance = pInstance; }
+    static CBarcode1SingletonBase* getInstance(){ return m_pInstance; }
 
 };
 
+extern "C" void Init_RubyAPI_Barcode1();
+extern "C" void Init_JSAPI_Barcode1();
