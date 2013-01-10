517195ff20157e2a58358644f088390b20931f5b
genywind <evgeny@rhomobile.com>
2013-01-10 12:36:54 +0400

    api_generator: add singleton interface

--- platform/shared/api_generator/sample/windows/Barcode1Base_impl.cpp
+++ platform/shared/api_generator/sample/windows/Barcode1Base_impl.cpp
@@ -1,5 +1,3 @@
 #include "Barcode1.h"
 
-rho::Hashtable<rho::String,IBarcode1*> CBarcode1FactoryBase::m_hashBarcodes;
-rho::String CBarcode1FactoryBase::m_strDefaultID;
-rho::common::CAutoPtr<rho::common::CThreadQueue> CBarcode1FactoryBase::m_pCommandQueue;
+rho::common::CAutoPtr<CBarcode1SingletonBase> CBarcode1SingletonBase::m_pInstance;
