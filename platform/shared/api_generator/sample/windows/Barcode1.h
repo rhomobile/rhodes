a93ecbeb9270865436787388514cdc12138bef62
genywind <evgeny@rhomobile.com>
2013-01-10 13:06:27 +0400

    api_generator: c++: move base classes to common code

--- platform/shared/api_generator/sample/windows/Barcode1.h
+++ platform/shared/api_generator/sample/windows/Barcode1.h
@@ -2,7 +2,7 @@
 
 #include "common/RhoStd.h"
 #include "../../common/MethodResult.h"
-#include "../../common/GeneratorQueue.h"
+#include "../../common/BaseClasses.h"
 
 ///////////////////////////////////////////////////////////
 struct IBarcode1
@@ -11,35 +11,6 @@ struct IBarcode1
     virtual void getProps(CMethodResult& oResult) = 0;
     virtual void getProps(const rho::String& strName, CMethodResult& oResult) = 0;
     virtual void getProps(const rho::Vector<rho::String>& arNames, CMethodResult& oResult) = 0;
-};
-
-class CBarcode1ImplBase: public IBarcode1
-{
-protected:
-
-    rho::String m_strID;
-    rho::Hashtable<rho::String, rho::String> m_hashProps;
-
-public:
-    CBarcode1ImplBase(const rho::String& strID)
-    {
-        m_strID = strID;
-    }
-
-    virtual void getProps(CMethodResult& oResult)
-    { 
-        oResult.set(m_hashProps);
-    }
-
-    virtual void getProps(const rho::String& strName, CMethodResult& oResult)
-    {
-        oResult.set(m_hashProps[strName]);
-    }
-    virtual void getProps(const rho::Vector<rho::String>& arNames, CMethodResult& oResult)
-    {
-        oResult.set(m_hashProps);
-    }
-
 };
 
 struct IBarcode1Singleton
@@ -53,12 +24,9 @@ struct IBarcode1Singleton
 class CBarcode1SingletonBase;
 CBarcode1SingletonBase* barcode1_impl_createSingleton();
 
-class CBarcode1SingletonBase : public IBarcode1Singleton
+class CBarcode1SingletonBase : public CModuleSingletonBase<IBarcode1, IBarcode1Singleton>
 {
 protected:
-    rho::String m_strDefaultID;
-    rho::Hashtable<rho::String,IBarcode1*> m_hashBarcodes;
-    rho::common::CAutoPtr<rho::common::CThreadQueue> m_pCommandQueue;
     static rho::common::CAutoPtr<CBarcode1SingletonBase> m_pInstance;
 
 public:
@@ -70,33 +38,5 @@ public:
         return m_pInstance;
     }
 
-    rho::Hashtable<rho::String,IBarcode1*>& getBarcodes(){ return m_hashBarcodes; }
-
-    void setDefaultID(const rho::String& strDefaultID){ m_strDefaultID = strDefaultID; }
-
-    void setCommandQueue( rho::common::CThreadQueue* pQueue){ m_pCommandQueue = pQueue; }
-    void addCommandToQueue(rho::common::IRhoRunnable* pFunctor)
-    {
-        if ( !m_pCommandQueue )
-        {
-            m_pCommandQueue = new CGeneratorQueue();
-            m_pCommandQueue->start(rho::common::CThreadQueue::epLow);
-        }
-
-        m_pCommandQueue->addQueueCommand( new CGeneratorQueue::CGeneratorQueueCommand(pFunctor) );
-    }
-
-    rho::String getDefaultIDEx()
-    { 
-        if ( m_strDefaultID.length() == 0 )
-            setDefaultID(getDefaultID());
-
-        if ( !getBarcodes().containsKey(m_strDefaultID) )
-        {
-            IBarcode1* pObj = create(m_strDefaultID);
-            getBarcodes().put(m_strDefaultID, pObj );
-        }
-        return m_strDefaultID; 
-    }
 };
 
