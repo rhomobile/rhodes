5ecddb2fabf5664fc935dc1aecf89c573f75281d
genywind <evgeny@rhomobile.com>
2013-01-17 13:11:45 +0400

    api_generator: move common code to RhoLib

--- platform/shared/ruby/ext/rho/rhoruby.c
+++ platform/shared/ruby/ext/rho/rhoruby.c
@@ -964,3 +964,45 @@ int rho_is_eval_disabled()
 {
     return !rho_conf_getBool("enable_eval");
 }
+
+static void
+obj_free(void *p)
+{
+    ruby_xfree(p);
+}
+
+static VALUE obj_allocate(VALUE klass)
+{
+	return Data_Wrap_Struct(klass, 0, obj_free, ALLOC(void*));
+}
+
+VALUE rho_ruby_create_object_with_id( VALUE klass, const char* szID )
+{
+    const char ** ppString = NULL;
+
+    VALUE valObj = obj_allocate(klass);
+    Data_Get_Struct(valObj, void *, ppString);
+    *ppString = xmalloc(strlen(szID)+1);
+    strcpy(*ppString, szID);
+
+    return valObj;
+}
+
+int rho_ruby_is_string(VALUE val)
+{
+    return (TYPE(val) == T_STRING) ? 1 : 0;
+}
+
+int rho_ruby_is_array(VALUE val)
+{
+    return (TYPE(val) == T_ARRAY) ? 1 : 0;
+}
+
+const char* rho_ruby_get_object_id( VALUE valObj )
+{
+    const char ** ppString = NULL;
+
+    Data_Get_Struct(valObj, void *, ppString);
+
+    return *ppString;
+}
