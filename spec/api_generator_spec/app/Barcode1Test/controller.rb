48f0932ef44b3f31091ff09b630c92c47ed29ffd
genywind <evgeny@rhomobile.com>
2013-01-17 13:33:11 +0400

    api_generator:sample: move test to spec app

--- spec/api_generator_spec/app/Barcode1Test/controller.rb
+++ spec/api_generator_spec/app/Barcode1Test/controller.rb
@@ -1,20 +1,46 @@
 require 'rho/rhocontroller'
-require 'helpers/application_helper'
-require 'helpers/browser_helper'
-require 'barcode1'
 
 class Barcode1TestController < Rho::RhoController
-  include BrowserHelper
-  include ApplicationHelper
+  #@layout = :simplelayout
+  @layout = 'Barcode1Test/layout'
   
   def index
+    puts "Barcode1Test index controller"
     render :back => '/app'
   end
 
+
   def run_test
-    sum = Barcode1.calc_summ(3,7) 
-    Alert.show_popup Barcode1::Barcode1StringHelper::process_string('test')+sum.to_s
+   Rho::Barcode1.getProps("display", url_for(:action => :test_callback1))
+   arBarcodes = Rho::Barcode1.enumerate
+   puts "arBarcodes : #{arBarcodes}"
+   Alert.show_popup "arBarcodes : #{arBarcodes}"
+   id = arBarcodes[0].getProps("ID")
+   Alert.show_popup "first ID =  : #{id}"
+
+   arBarcodes[1].getProps("sound", url_for(:action => :test_callback2))
+   
+   arBarcodes[1].getProps(nil, url_for(:action => :test_callback3), "param1=123")
+
     render :action => :index, :back => '/app'
+   
   end
-  
+
+  def test_callback1
+      puts "callback1 = #{@params}"
+      Alert.show_popup "callback1 = #{@params}"
+  end
+
+  def test_callback2
+      puts "callback2 = #{@params}"
+      Alert.show_popup "callback2 = #{@params}"
+  end
+
+  def test_callback3
+      puts "callback3 = #{@params}"
+      Alert.show_popup "callback3 = #{@params}"
+  end
+
+
+
 end
