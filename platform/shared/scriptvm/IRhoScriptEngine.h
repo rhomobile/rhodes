47c6fadd80814d186f0d9529d6b764e9f0376cfd
Mikhail Malinin <malinin.work@gmail.com>
2013-02-07 21:10:22 +0400

    iOS JS VM initial commit

--- platform/shared/scriptvm/IRhoScriptEngine.h
+++ platform/shared/scriptvm/IRhoScriptEngine.h
@@ -1,18 +1,18 @@
 /*------------------------------------------------------------------------
 * (The MIT License)
-*
+* 
 * Copyright (c) 2008-2011 Rhomobile, Inc.
-*
+* 
 * Permission is hereby granted, free of charge, to any person obtaining a copy
 * of this software and associated documentation files (the "Software"), to deal
 * in the Software without restriction, including without limitation the rights
 * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 * copies of the Software, and to permit persons to whom the Software is
 * furnished to do so, subject to the following conditions:
-*
+* 
 * The above copyright notice and this permission notice shall be included in
 * all copies or substantial portions of the Software.
-*
+* 
 * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -20,45 +20,31 @@
 * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 * THE SOFTWARE.
-*
+* 
 * http://rhomobile.com
 *------------------------------------------------------------------------*/
 
 #pragma once
 
 namespace rho {
-namespace net{
-    struct INetRequestImpl;
-    struct ISSL;
-}
-
-struct ICallbackObject
-{
-    virtual ~ICallbackObject(){};
-    virtual unsigned long getObjectValue() = 0;
-};
-
 namespace common {
-struct IRhoThreadImpl;
-struct IRhoCrypt;
-struct IRhoScriptVM;
 
-struct IRhoClassFactory
+enum eScriptEngine {
+	SCRIPT_ENGINE_JAVASCRIPT = 0,
+    NUM_SCRIPT_ENGINES
+};
+
+struct IRhoScriptEngine
 {
-    virtual ~IRhoClassFactory(void){;}
+    virtual ~IRhoScriptEngine(void){;}
 
-    virtual IRhoThreadImpl* createThreadImpl() = 0;
-    virtual net::ISSL* createSSLEngine() = 0;
+    virtual int getEngineId() = 0;
 
-    virtual IRhoCrypt* createRhoCrypt() = 0;
+    virtual bool loadScript(const String& buffer, const String& tag) = 0;
+    virtual bool unloadScript(const String& tag) = 0;
 
-    virtual net::INetRequestImpl* createNetRequestImpl() = 0;
-
-    virtual IRhoScriptVM* createScriptVM( int vmId ) = 0;
+    virtual String performAction(const String& actionName, const String& params) = 0;
 };
 
 }
 }
-
-rho::common::IRhoClassFactory* rho_get_RhoClassFactory();
-
---
+++ platform/shared/scriptvm/ScriptVM.h
@@ -0,0 +1,60 @@
+/*------------------------------------------------------------------------
+ * (The MIT License)
+ *
+ * Copyright (c) 2008-2011 Rhomobile, Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ * http://rhomobile.com
+ *------------------------------------------------------------------------*/
+
+#pragma once
+
+#include "common/RhoStd.h"
+#include "common/AutoPointer.h"
+
+namespace rho {
+namespace common {
+    
+enum eVM {
+    VM_JAVASCRIPT = 0,
+    NUM_VIRTUAL_MACHINES
+};
+
+struct IRhoScriptVM
+{
+    virtual ~IRhoScriptVM(void){}
+    
+    virtual int getVMId() = 0;
+    
+    virtual bool loadScript(const rho::String& buffer, const rho::String& tag) = 0;
+    virtual bool unloadScript(const rho::String& tag) = 0;
+    
+    virtual rho::String performAction(const rho::String& actionName, const rho::String& params) = 0;
+};
+    
+}
+}
+
+extern "C" {
+    
+    bool rho_scriptvm_load_script(int vmId, const rho::String& buffer, const rho::String& tag);
+    bool rho_scriptvm_unload_script(int vmId, const rho::String& tag);
+    void rho_scriptvm_perform_action(int vmId, const rho::String& actionName, const rho::String& params, rho::String& result);
+}
