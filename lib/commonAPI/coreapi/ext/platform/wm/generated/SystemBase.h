d03462f5654e5c977bf3c72aed7af7faa2ac9329
genywind <evgeny@rhomobile.com>
2013-02-09 13:23:39 +0400

    api_generator:cpp:support property bat to accessor mapping

--- lib/commonAPI/coreapi/ext/platform/wm/generated/SystemBase.h
+++ lib/commonAPI/coreapi/ext/platform/wm/generated/SystemBase.h
@@ -15,110 +15,12 @@ public:
     CSystemBase(){}
 
 
-    virtual void getProperty( const rho::StringW& propertyName, CMethodResult& oResult)
-    {
-        oResult.set(m_hashProps[propertyName]);
-    }
-    virtual void getProperties( const rho::Vector<::rho::StringW>& arrayofNames, CMethodResult& oResult)
-    {
-        rho::Hashtable<::rho::StringW, rho::StringW> res;
-        oResult.setCollectionMode(true);
-        for ( int i = 0; i < (int)arrayofNames.size(); i++ )
-        {
-            getProperty(arrayofNames[i], oResult);
-
-            if ( oResult.isError() )
-                break;
-
-            res[arrayofNames[i]] = oResult.toStringW();
-        }
-
-        oResult.setCollectionMode(false);
-        if ( oResult.isError() )
-            oResult.callCallback();
-        else
-            oResult.set(res);
-    }
-    virtual void getAllProperties(CMethodResult& oResult)
-    {
-        oResult.set(m_hashProps);
-    }
-    virtual void setProperty( const rho::StringW& propertyName,  const rho::StringW& propertyValue, CMethodResult& oResult)
-    {
-
-        if (false){}
-
-
-        else if (_wcsicmp(L"applicationIconBadge",propertyName.c_str()) == 0){
-
-            int64 arg;
-            rho::common::convertFromStringW(propertyValue.c_str(), arg);
-            setApplicationIconBadge( arg, oResult);
-
-        }
-
-        else if (_wcsicmp(L"httpProxyURI",propertyName.c_str()) == 0){
-
-            rho::StringW arg;
-            rho::common::convertFromStringW(propertyValue.c_str(), arg);
-            setHttpProxyURI( arg, oResult);
-
-        }
-
-        else if (_wcsicmp(L"lockWindowSize",propertyName.c_str()) == 0){
-
-            bool arg;
-            rho::common::convertFromStringW(propertyValue.c_str(), arg);
-            setLockWindowSize( arg, oResult);
-
-        }
-
-        else if (_wcsicmp(L"showKeyboard",propertyName.c_str()) == 0){
-
-            bool arg;
-            rho::common::convertFromStringW(propertyValue.c_str(), arg);
-            setShowKeyboard( arg, oResult);
-
-        }
-
-        else if (_wcsicmp(L"fullScreen",propertyName.c_str()) == 0){
-
-            bool arg;
-            rho::common::convertFromStringW(propertyValue.c_str(), arg);
-            setFullScreen( arg, oResult);
-
-        }
-
-        else if (_wcsicmp(L"localServerPort",propertyName.c_str()) == 0){
-
-            int64 arg;
-            rho::common::convertFromStringW(propertyValue.c_str(), arg);
-            setLocalServerPort( arg, oResult);
-
-        }
-
-        else if (_wcsicmp(L"screenAutoRotate",propertyName.c_str()) == 0){
-
-            bool arg;
-            rho::common::convertFromStringW(propertyValue.c_str(), arg);
-            setScreenAutoRotate( arg, oResult);
-
-        }
-    }
-    virtual void setProperties( const rho::Hashtable<::rho::StringW, rho::StringW>& propertyMap, CMethodResult& oResult)
-    {
-        for ( rho::Hashtable<rho::StringW, rho::StringW>::const_iterator it = propertyMap.begin();  it != propertyMap.end(); ++it )
-        {
-            setProperty( it->first, it->second, oResult );
-            if ( oResult.isError() )
-                break;
-        }
-    }
-    virtual void clearAllProperties(CMethodResult& oResult)
-    {
-        m_hashProps.clear();
-    }
-
+    virtual void getProperty( const rho::StringW& propertyName, CMethodResult& oResult);
+    virtual void getProperties( const rho::Vector<::rho::StringW>& arrayofNames, CMethodResult& oResult);
+    virtual void getAllProperties(CMethodResult& oResult);
+    virtual void setProperty( const rho::StringW& propertyName,  const rho::StringW& propertyValue, CMethodResult& oResult);
+    virtual void setProperties( const rho::Hashtable<::rho::StringW, rho::StringW>& propertyMap, CMethodResult& oResult);
+    virtual void clearAllProperties(CMethodResult& oResult);
 
 
 
--- res/generators/templates/api/cpp/MontanaBase.h
+++ res/generators/templates/api/cpp/MontanaBase.h
@@ -16,82 +16,20 @@ public:
     C<%= $cur_module.name %>Base(){}
 
 <% if $cur_module.is_template_propertybag %>
-    virtual void getProperty( const rho::StringW& propertyName, CMethodResult& oResult)
-    {
-        oResult.set(m_hashProps[propertyName]);
-    }
-    virtual void getProperties( const rho::Vector<::rho::StringW>& arrayofNames, CMethodResult& oResult)
-    {
-        rho::Hashtable<::rho::StringW, rho::StringW> res;
-        oResult.setCollectionMode(true);
-        for ( int i = 0; i < (int)arrayofNames.size(); i++ )
-        {
-            getProperty(arrayofNames[i], oResult);
-
-            if ( oResult.isError() )
-                break;
-
-            res[arrayofNames[i]] = oResult.toStringW();
-        }
-
-        oResult.setCollectionMode(false);
-        if ( oResult.isError() )
-            oResult.callCallback();
-        else
-            oResult.set(res);
-    }
-    virtual void getAllProperties(CMethodResult& oResult)
-    {
-        oResult.set(m_hashProps);
-    }
-    virtual void setProperty( const rho::StringW& propertyName,  const rho::StringW& propertyValue, CMethodResult& oResult)
-    {
-<% if $cur_module.use_property_bag_mode == ModuleProperty::USE_PROPERTY_BAG_MODE_PROPERTY_BAG_VIA_ACCESSORS%>
-        if (false){}
-<%   $cur_module.methods.each do |module_method|
-        next if module_method.access != ModuleMethod::ACCESS_INSTANCE
-        next if module_method.special_behaviour != ModuleMethod::SPECIAL_BEHAVIOUR_SETTER%>
-
-        else if (_wcsicmp(L"<%= module_method.linked_property.native_name %>",propertyName.c_str()) == 0){
-<%if module_method.linked_property.use_property_bag_mode == ModuleProperty::USE_PROPERTY_BAG_MODE_PROPERTY_BAG_VIA_ACCESSORS %>
-            <%= api_generator_cpp_makeNativeType(module_method.linked_property.type) %> arg;
-            rho::common::convertFromStringW(propertyValue.c_str(), arg);
-            <%= module_method.native_name%>( arg, oResult);
-<%else%>
-            m_hashProps.put(propertyName, propertyValue);<%
-end;%>
-        }<%
-end; else%>
-        m_hashProps.put(propertyName, propertyValue);<%
-end%>
-    }
-    virtual void setProperties( const rho::Hashtable<::rho::StringW, rho::StringW>& propertyMap, CMethodResult& oResult)
-    {
-        for ( rho::Hashtable<rho::StringW, rho::StringW>::const_iterator it = propertyMap.begin();  it != propertyMap.end(); ++it )
-        {
-            setProperty( it->first, it->second, oResult );
-            if ( oResult.isError() )
-                break;
-        }
-    }
-    virtual void clearAllProperties(CMethodResult& oResult)
-    {
-        m_hashProps.clear();
-    }
+    virtual void getProperty( const rho::StringW& propertyName, CMethodResult& oResult);
+    virtual void getProperties( const rho::Vector<::rho::StringW>& arrayofNames, CMethodResult& oResult);
+    virtual void getAllProperties(CMethodResult& oResult);
+    virtual void setProperty( const rho::StringW& propertyName,  const rho::StringW& propertyValue, CMethodResult& oResult);
+    virtual void setProperties( const rho::Hashtable<::rho::StringW, rho::StringW>& propertyMap, CMethodResult& oResult);
+    virtual void clearAllProperties(CMethodResult& oResult);
 
 <% $cur_module.methods.each do |module_method|
     next if module_method.access != ModuleMethod::ACCESS_INSTANCE
     next if module_method.special_behaviour != ModuleMethod::SPECIAL_BEHAVIOUR_GETTER && module_method.special_behaviour != ModuleMethod::SPECIAL_BEHAVIOUR_SETTER
     next if module_method.linked_property.use_property_bag_mode != ModuleProperty::USE_PROPERTY_BAG_MODE_ACCESSORS_VIA_PROPERTY_BAG %>
 
-    virtual void <%= module_method.native_name%>(<%= module_method.cached_data["cpp_params"] %>)
-    { <% if module_method.special_behaviour == ModuleMethod::SPECIAL_BEHAVIOUR_GETTER %>
-        getProperty( L"<%= module_method.linked_property.native_name %>", oResult); <%
-elsif module_method.special_behaviour == ModuleMethod::SPECIAL_BEHAVIOUR_SETTER %>
-        setProperty( L"<%= module_method.linked_property.native_name %>", rho::common::convertToStringW(value), oResult );<%
-end %>
-    }<% end %>
-
+    virtual void <%= module_method.native_name%>(<%= module_method.cached_data["cpp_params"] %>);
+<% end %>
 <% end %>
 };
 
