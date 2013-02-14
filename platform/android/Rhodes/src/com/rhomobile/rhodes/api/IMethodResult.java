16aa326e420ad7413e32a60223acb7bdf26a946f
Alexey Tikhvinsky <alexey@rhomobile.com>
2013-02-15 00:53:56 +0400

    Api generator: Android: property bag model implementation

--- platform/android/Rhodes/src/com/rhomobile/rhodes/api/IMethodResult.java
+++ platform/android/Rhodes/src/com/rhomobile/rhodes/api/IMethodResult.java
@@ -7,4 +7,7 @@ public interface IMethodResult {
     void set(final String res);
     void set(final List<String> res);
     void set(final Map<String, String> res);
+    
+    void setError(String mesage);
+    void setArgError(String message);
 }
