9a3ef397e86c2a9bbc3397856ec332a5b76d83f9
genywind <evgeny@rhomobile.com>
2013-03-02 17:06:39 +0400

    wp8: fix build

--- lib/commonAPI/coreapi/ext/shared/InitExtension.cpp
+++ lib/commonAPI/coreapi/ext/shared/InitExtension.cpp
@@ -6,7 +6,7 @@ extern "C" void Init_SQLite3();
 extern "C" void Init_CoreAPI_Extension()
 {
     Init_System();
-#if defined(WINDOWS_PLATFORM)
+#if defined(OS_WINDOWS_DESKTOP) || defined(OS_WINCE)
     Init_NetworkAccess();
     Init_SQLite3();
 #endif
