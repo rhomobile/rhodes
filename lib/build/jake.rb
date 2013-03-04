8e0e77a690cf2d177afb8a345814e9282aaad532
Stanislav Volodarskiy <stan_volodarsky@mail.ru>
2013-03-04 13:52:38 +0400

    lib/build/jake.rb: Jake.run3 method is added.

--- lib/build/jake.rb
+++ lib/build/jake.rb
@@ -371,6 +371,36 @@ class Jake
     self.run2(command, args, {:directory => wd, :system => system, :hiderrors => hideerrors})
   end
   
+  def self.run3(command, cd = nil, env = {})
+    set_list = []
+    env.each_pair do |k, v|
+      if RUBY_PLATFORM =~ /(win|w)32$/
+        set_list << "set \"#{k}=#{v}\"&&"
+      else
+        set_list << "export #{k}=#{v}&&"
+      end
+    end
+
+    to_print = "CMD: #{command}"
+    to_run = set_list.join('') + command
+    if !cd.nil?
+      to_print = "PWD: #{cd}\n#{to_print}"
+
+      if RUBY_PLATFORM =~ /(win|w)32$/
+        cd_ = cd.gsub('/', "\\")
+        to_run = "cd /d \"#{cd_}\"&&#{to_run}"
+      else
+        to_run = "cd '#{cd}'&&#{to_run}"
+      end
+    end
+
+    puts
+    puts to_print
+    STDOUT.flush
+
+    fail "[#{command}]" unless system(to_run)
+  end
+
   def self.unjar(src,targetdir)
     jpath = $config["env"]["paths"]["java"]   
     cmd = jpath && jpath.length()>0 ? File.join(jpath, "jar" ) : "jar"
