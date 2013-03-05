d89deac367c906214ba7fd4a5c95a36f61d8b1bd
Alexey Tikhvinsky <alexey@rhomobile.com>
2013-03-05 18:01:13 +0400

    Android: coreapi: NetworkAccess

--- lib/commonAPI/coreapi/ext/shared/InitExtension.cpp
+++ lib/commonAPI/coreapi/ext/shared/InitExtension.cpp
@@ -7,7 +7,7 @@ extern "C" void Init_CoreAPI_Extension()
 {
     Init_System();
 
-#if defined(OS_WINDOWS_DESKTOP) || defined(OS_WINCE) || defined(OS_WP8) || defined(OS_MACOSX)
+#if defined(OS_WINDOWS_DESKTOP) || defined(OS_WINCE) || defined(OS_WP8) || defined(OS_MACOSX) || defined(OS_ANDROID)
 	Init_NetworkAccess();
 #endif
 
