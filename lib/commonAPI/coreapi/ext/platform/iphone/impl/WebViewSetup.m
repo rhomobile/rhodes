d66a74008979485261a72e31b5da4cd0fc0d8adf
dmitrys <dmitrys@rhomobile.com>
2013-03-15 02:10:39 +0400

    fix iOS WebView

--- lib/commonAPI/coreapi/ext/platform/iphone/impl/WebViewSetup.m
+++ lib/commonAPI/coreapi/ext/platform/iphone/impl/WebViewSetup.m
@@ -2,7 +2,14 @@
 
 extern void Init_WebView_API();
 
+#ifndef RHO_NO_RUBY_API
+extern unsigned long rb_require(const char *fname);
+#endif
+
 void Init_WebView()
 {
     Init_WebView_API();
+#ifndef RHO_NO_RUBY_API
+    rb_require("WebView");
+#endif
 }
