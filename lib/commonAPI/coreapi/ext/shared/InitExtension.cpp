d560f8f8b2250f8f6ab9319002e0618a648dc91d
genywind <evgeny@rhomobile.com>
2013-03-21 20:42:48 +0400

    api: wm: application module

--- lib/commonAPI/coreapi/ext/shared/InitExtension.cpp
+++ lib/commonAPI/coreapi/ext/shared/InitExtension.cpp
@@ -4,10 +4,16 @@ extern "C" void Init_Network();
 extern "C" void Init_SQLite3();
 extern "C" void Init_Log();
 extern "C" void Init_WebView();
+extern "C" void Init_Application();
 
 extern "C" void Init_CoreAPI_Extension()
 {
     Init_System();
+
+#if defined(OS_WINDOWS_DESKTOP) || defined(OS_WINCE)
+    Init_Application();
+#endif
+
 	Init_Network();
     Init_SQLite3();
 #if defined(OS_MACOSX) || defined(OS_WINDOWS_DESKTOP) || defined(OS_WINCE) || defined(OS_WP8)
@@ -16,4 +22,5 @@ extern "C" void Init_CoreAPI_Extension()
 #if defined(OS_MACOSX) || defined(OS_WINDOWS_DESKTOP) || defined(OS_WINCE)
     Init_WebView();
 #endif
+
 }
