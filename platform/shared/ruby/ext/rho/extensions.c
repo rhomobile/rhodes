96aa586ad90cc3ec007b7d8ef472bd78e99e7fd4
Stanislav Volodarskiy <stan_volodarsky@mail.ru>
2013-03-26 19:02:38 +0400

    spec-rhoapi.js: SQLite3 coverage tests are added

--- platform/shared/ruby/ext/rho/extensions.c
+++ platform/shared/ruby/ext/rho/extensions.c
@@ -1,5 +1,4 @@
 // WARNING! THIS FILE IS GENERATED AUTOMATICALLY! DO NOT EDIT IT MANUALLY!
-#pragma comment(lib, "CoreAPI.lib")
 extern void Init_CoreAPI_Extension(void);
 void Init_Extensions(void) {
     Init_CoreAPI_Extension();
