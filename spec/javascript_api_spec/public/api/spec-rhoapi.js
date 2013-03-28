405fb254476657ccebec39f6329b5acc6a7ac4f9
Stanislav Volodarskiy <stan_volodarsky@mail.ru>
2013-03-28 10:42:17 +0400

    Sync tests for Rho.Database and Rho.Database.SQLite3 is done

--- spec/javascript_api_spec/public/api/spec-rhoapi.js
+++ spec/javascript_api_spec/public/api/spec-rhoapi.js
@@ -241,15 +241,9 @@ describe("Rho common API", function () {
 
     });
 
-    describe('Rho.Database.SQLite3 module', function () {
-        it('passes test by Dmitry P.', function() {
-            var queryInit = 'DROP TABLE IF EXISTS todo ; CREATE TABLE todo (id INT, task VARCHAR(128))';
-
-            var db = new Rho.Database.SQLite3("path", "local");
-            db.execute(queryInit, true, []);
-            db.close();
-
-            dbInit();
+    describe('Rho.Application module', function() {
+        it('returns path to database file', function() {
+            expect(Rho.Application.databaseFilePath('local')).toMatch(/.*syncdblocal\.sqlite$/);
         });
     });
 
@@ -257,7 +251,7 @@ describe("Rho common API", function () {
         var db = null;
 
         beforeEach(function() {
-            db = new Rho.Database.SQLite3('path', 'local');
+            db = new Rho.Database.SQLite3(Rho.Application.databaseFilePath('local'), 'local');
         });
 
         afterEach(function() {
@@ -274,91 +268,104 @@ describe("Rho common API", function () {
         // SQLite3.prototype.startTransaction = function(/* optional function */ oResult)
         // SQLite3.prototype.commitTransaction = function(/* optional function */ oResult)
         it('starts and commits transaction', function() {
+            db.execute('DROP TABLE IF EXISTS t; CREATE TABLE t(x INTEGER, y TEXT, z VARCHAR(10));', true);
+
             db.startTransaction();
+            db.execute('INSERT INTO t (x, y, z) VALUES (?, ?, ?);', false, [10, 'ten', 'TEN']);
+            expect(db.execute('SELECT * FROM t;')).toEqual([{x: '10', y: 'ten', z: 'TEN'}]);
             db.commitTransaction();
+            expect(db.execute('SELECT * FROM t;')).toEqual([{x: '10', y: 'ten', z: 'TEN'}]);
         });
 
         // SQLite3.prototype.startTransaction = function(/* optional function */ oResult)
         // SQLite3.prototype.rollbackTransaction = function(/* optional function */ oResult)
         it('starts and rollbacks transaction', function() {
+            db.execute('DROP TABLE IF EXISTS t; CREATE TABLE t(x INTEGER, y TEXT, z VARCHAR(10));', true);
+
             db.startTransaction();
+            db.execute('INSERT INTO t (x, y, z) VALUES (?, ?, ?);', false, [10, 'ten', 'TEN']);
+            expect(db.execute('SELECT * FROM t;')).toEqual([{x: '10', y: 'ten', z: 'TEN'}]);
             db.rollbackTransaction();
+            expect(db.execute('SELECT * FROM t;')).toEqual([]);
         });
 
         // SQLite3.prototype.lockDb = function(/* optional function */ oResult)
         // SQLite3.prototype.unlockDb = function(/* optional function */ oResult)
         it('locks and unlocks database', function() {
             db.lockDb();
+            db.execute('DROP TABLE IF EXISTS t; CREATE TABLE t(x INTEGER, y TEXT, z VARCHAR(10));', true);
+            db.execute('INSERT INTO t (x, y, z) VALUES (?, ?, ?);', false, [10, 'ten', 'TEN']);
             db.unlockDb();
-        });
-
-        // SQLite3.prototype.import = function(/* const rho::String& */ zipName, /* optional function */ oResult)
-        it('imports zip', function() {
-            db.import('');
-        });
-
-        // SQLite3.prototype.export = function(/* optional function */ oResult)
-        it('exports zip', function() {
-            db.export();
+            expect(db.execute('SELECT * FROM t;')).toEqual([{x: '10', y: 'ten', z: 'TEN'}]);
         });
 
         // SQLite3.prototype.destroyTables = function(/* const rho::Vector<rho::String>& */ include, /* const rho::Vector<rho::String>& */ exclude, /* optional function */ oResult)
-        it('destroys tables', function() {
-            db.destroyTables('', '');
+        describe('destroys tables', function() {
+            var makeTest = function(name, include, exclude, remaining) {
+                var contains = function(array, item) {
+                    for (var i = 0; i < array.length; ++i) {
+                        if (array[i] === item) {
+                            return true;
+                        }
+                    }
+                    return false;
+                };
+
+                it(name, function() {
+                    for (var i = 1; i < 5; ++i) {
+                        db.execute('CREATE TABLE IF NOT EXISTS t' + i + '(x INTEGER);');
+                    }
+
+                    db.destroyTables(include, exclude);
+
+                    var actual = [];
+                    for (var i = 1; i < 5; ++i) {
+                        if (db.isTableExist('t' + i)) {
+                            actual.push('t' + i);
+                        }
+                    }
+                    expect(actual).toEqual(remaining);
+                });
+            };
+
+            // names of tables to delete            included      excluded      remaining
+            makeTest('all'                       , [          ], [          ], [                ]);
+            makeTest('all but excluded'          , [          ], ['t2', 't1'], ['t1', 't2'      ]);
+            makeTest('included only'             , ['t2', 't1'], [          ], ['t3', 't4'      ]);
+            makeTest('included only but excluded', ['t2', 't1'], ['t2', 't3'], ['t2', 't3', 't4']);
         });
 
         // SQLite3.prototype.isTableExist = function(/* const rho::String& */ tableName, /* optional function */ oResult)
         it('checks for table existence', function() {
-            expect(db.isTableExist('')).toBe(false);
+            db.execute('DROP TABLE IF EXISTS t;');
+            expect(db.isTableExist('t')).toBe(false);
+
+            db.execute('CREATE TABLE t(x INTEGER, y TEXT, z VARCHAR(10));');
+            expect(db.isTableExist('t')).toBe(true);
+
+            db.execute('DROP TABLE IF EXISTS t;');
+            expect(db.isTableExist('t')).toBe(false);
         });
 
         // SQLite3.prototype.isUiWaitForDb = function(/* optional function */ oResult)
         it('checks something strange', function() {
-            expect(db.isUiWaitForDb('')).toBe(false);
+            expect(db.isUiWaitForDb()).toBe(false);
         });
 
         // SQLite3.prototype.execute = function(/* const rho::String& */ sqlStmt, /* bool */ isBatch, /* const rho::Vector<rho::String>& */ args, /* optional function */ oResult)
         it('executes SQL statements', function() {
-            db.execute('select * from table;');
+            db.execute('DROP TABLE IF EXISTS t; CREATE TABLE t(x INTEGER, y TEXT, z VARCHAR(10));', true);
+            db.execute('INSERT INTO t (x, y, z) VALUES (?, ?, ?);', false, [10, 'ten', 'TEN']);
+            db.execute('INSERT INTO t (x, y, z) VALUES (?, ?, ?);', false, [11, 'eleven', 'ELEVEN']);
+            expect(db.execute('SELECT * FROM t ORDER BY x DESC;')).toEqual([{x: 11, y: 'eleven', z: 'ELEVEN'}, {x: 10, y: 'ten', z: 'TEN'}]);
         });
-
-        // SQLite3.prototype.import = function(/* const rho::String& */ zipName, /* optional function */ oResult)
-        it('imports zip', function() {
-            db.import('');
-        });
-
-        // SQLite3.prototype.export = function(/* optional function */ oResult)
-        it('exports zip', function() {
-            db.export();
-        });
-
-        // SQLite3.prototype.destroyTables = function(/* const rho::Vector<rho::String>& */ include, /* const rho::Vector<rho::String>& */ exclude, /* optional function */ oResult)
-        it('destroys tables', function() {
-            db.destroyTables('', '');
-        });
-
-        // SQLite3.prototype.isTableExist = function(/* const rho::String& */ tableName, /* optional function */ oResult)
-        it('checks for table existence', function() {
-            expect(db.isTableExist('')).toBe(false);
-        });
-
-        // SQLite3.prototype.isUiWaitForDb = function(/* optional function */ oResult)
-        it('checks something strange', function() {
-            expect(db.isUiWaitForDb('')).toBe(false);
-        });
-
-        // SQLite3.prototype.execute = function(/* const rho::String& */ sqlStmt, /* bool */ isBatch, /* const rho::Vector<rho::String>& */ args, /* optional function */ oResult)
-        it('executes SQL statements', function() {
-            db.execute('DROP TABLE IF EXISTS todo', true, []);
-        });
-
     });
 
     describe('Rho.Database module', function() {
         var db = null;
 
         beforeEach(function() {
-            db = new Rho.Database_('path', 'local');
+            db = new Rho.Database(Rho.Application.databaseFilePath('local'), 'local');
         });
 
         afterEach(function() {
@@ -370,74 +377,128 @@ describe("Rho common API", function () {
         // function Database()
         // Database.prototype.close = function()
         it('creates Database object and closes it', function() {
+            // look at beforeEach and afterEach calls above
         });
 
         // Database.prototype.startTransaction = function()
         // Database.prototype.commitTransaction = function()
         it('starts and commits transaction', function() {
+            db.executeBatchSql('DROP TABLE IF EXISTS t; CREATE TABLE t(x INTEGER, y TEXT, z VARCHAR(10));');
+
             db.startTransaction();
+            db.executeSql('INSERT INTO t (x, y, z) VALUES (?, ?, ?);', [10, 'ten', 'TEN']);
+            expect(db.executeSql('SELECT * FROM t;')).toEqual([{x: '10', y: 'ten', z: 'TEN'}]);
             db.commitTransaction();
+            expect(db.executeSql('SELECT * FROM t;')).toEqual([{x: '10', y: 'ten', z: 'TEN'}]);
         });
 
         // Database.prototype.startTransaction = function()
         // Database.prototype.rollbackTransaction = function()
         it('starts and rollbacks transaction', function() {
+            db.executeBatchSql('DROP TABLE IF EXISTS t; CREATE TABLE t(x INTEGER, y TEXT, z VARCHAR(10));');
+
             db.startTransaction();
+            db.executeSql('INSERT INTO t (x, y, z) VALUES (?, ?, ?);', [10, 'ten', 'TEN']);
+            expect(db.executeSql('SELECT * FROM t;')).toEqual([{x: '10', y: 'ten', z: 'TEN'}]);
             db.rollbackTransaction();
+            expect(db.executeSql('SELECT * FROM t;')).toEqual([]);
         });
 
         // Database.prototype.lockDb = function()
         // Database.prototype.unlockDb = function()
         it('locks and unlocks database', function() {
             db.lockDb();
+            db.executeBatchSql('DROP TABLE IF EXISTS t; CREATE TABLE t(x INTEGER, y TEXT, z VARCHAR(10));');
+            db.executeSql('INSERT INTO t (x, y, z) VALUES (?, ?, ?);', [10, 'ten', 'TEN']);
             db.unlockDb();
+            expect(db.executeSql('SELECT * FROM t;')).toEqual([{x: '10', y: 'ten', z: 'TEN'}]);
         });
 
         // Database.prototype.isUiWaitForDb = function()
         it('calls isUiWaitForDb', function() {
-            db.isUiWaitForDb();
+            expect(db.isUiWaitForDb()).toBe(false);
         });
 
         // Database.prototype.executeSql = function(/* const rho::String& */ sqlStmt, /* const rho::Vector<rho::String>& */ args)
         it('executes SQL statements', function() {
-            db.executeSql('select * from table;');
+            db.executeSql('DROP TABLE IF EXISTS t;');
+            db.executeSql('CREATE TABLE t(x INTEGER, y TEXT, z VARCHAR(10));');
+            db.executeSql('INSERT INTO t (x, y, z) VALUES (?, ?, ?);', [10, 'ten', 'TEN']);
+            db.executeSql('INSERT INTO t (x, y, z) VALUES (?, ?, ?);', [11, 'eleven', 'ELEVEN']);
+            expect(db.executeSql('SELECT * FROM t ORDER BY x DESC;')).toEqual([{x: '11', y: 'eleven', z: 'ELEVEN'}, {x: '10', y: 'ten', z: 'TEN'}]);
         });
 
         // Database.prototype.executeBatchSql = function(/* const rho::String& */ sqlStmt, /* const rho::Vector<rho::String>& */ args)
         it('executes SQL statements as batch', function() {
-            db.executeBatchSql ('select * from table;');
-        });
-
-        // Database.prototype.import = function(/* const rho::String& */ zipName)
-        it('imports zip', function() {
-            db.import('');
-        });
-
-        // Database.prototype.export = function()
-        it('exports zip', function() {
-            db.export();
+            db.executeBatchSql('DROP TABLE IF EXISTS t; CREATE TABLE t(x INTEGER, y TEXT, z VARCHAR(10));');
+            db.executeSql('INSERT INTO t (x, y, z) VALUES (?, ?, ?);', [10, 'ten', 'TEN']);
+            db.executeSql('INSERT INTO t (x, y, z) VALUES (?, ?, ?);', [11, 'eleven', 'ELEVEN']);
+            expect(db.executeSql('SELECT * FROM t ORDER BY x DESC;')).toEqual([{x: 11, y: 'eleven', z: 'ELEVEN'}, {x: 10, y: 'ten', z: 'TEN'}]);
         });
 
         // Database.prototype.destroyTable = function(/* const rho::String& */ tableName)
         it('destroys table', function() {
-            db.destroyTable('');
+            db.executeBatchSql('CREATE TABLE IF NOT EXISTS t1(x INTEGER); CREATE TABLE IF NOT EXISTS t2(x INTEGER);');
+
+            db.destroyTable('t1');
+            db.destroyTable('t3');
+
+            expect(db.isTableExist('t1')).toBe(false);
+            expect(db.isTableExist('t2')).toBe(true);
+            expect(db.isTableExist('t3')).toBe(false);
         });
 
         // Database.prototype.destroyTables = function(/* const rho::Hashtable<rho::String, rho::String>& */ propertyMap)
-        it('destroys multiple tables', function() {
-            db.destroyTable({'include': ''});
+        describe('destroys multiple tables', function() {
+            var makeTest = function(name, include, exclude, remaining) {
+                var contains = function(array, item) {
+                    for (var i = 0; i < array.length; ++i) {
+                        if (array[i] === item) {
+                            return true;
+                        }
+                    }
+                    return false;
+                };
+
+                it(name, function() {
+                    for (var i = 1; i < 5; ++i) {
+                        db.executeSql('CREATE TABLE IF NOT EXISTS t' + i + '(x INTEGER);');
+                    }
+
+                    db.destroyTables({'include': include, 'exclude': exclude});
+
+                    var actual = [];
+                    for (var i = 1; i < 5; ++i) {
+                        if (db.isTableExist('t' + i)) {
+                            actual.push('t' + i);
+                        }
+                    }
+                    expect(actual).toEqual(remaining);
+                });
+            };
+
+            // names of tables to delete            included      excluded      remaining
+            makeTest('all'                       , [          ], [          ], [                ]);
+            makeTest('all but excluded'          , [          ], ['t2', 't1'], ['t1', 't2'      ]);
+            makeTest('included only'             , ['t2', 't1'], [          ], ['t3', 't4'      ]);
+            makeTest('included only but excluded', ['t2', 't1'], ['t2', 't3'], ['t2', 't3', 't4']);
         });
 
         // Database.prototype.isTableExist = function(/* const rho::String& */ tableName)
-        it('check for table existence', function() {
-            expect(db.isTableExist('')).toEqual(true);
+        it('checks for table existence', function() {
+            db.executeSql('DROP TABLE IF EXISTS t;');
+            expect(db.isTableExist('t')).toBe(false);
+
+            db.executeSql('CREATE TABLE t(x INTEGER, y TEXT, z VARCHAR(10));');
+            expect(db.isTableExist('t')).toBe(true);
+
+            db.executeSql('DROP TABLE IF EXISTS t;');
+            expect(db.isTableExist('t')).toBe(false);
         });
 
         // Database.prototype.setDoNotBackupAttribute = function(/* bool */ setFlag)
         it('sets "don\'t backup attribute"', function() {
             db.setDoNotBackupAttribute(true);
         });
-
     });
-
 });
