5958dc9e24d5c3a7f383d5e35453415ada41e6bf
Alex Epifanoff <alex.epifanoff@gmail.com>
2013-03-29 19:15:03 +0400

    implemented eventually block for mspec and rhospec

--- lib/extensions/rhospec/rhospec.rb
+++ lib/extensions/rhospec/rhospec.rb
@@ -188,10 +188,41 @@ class Object
     def it_should_behave_like(desc)
         raise "it_should_behave_like not implemented"
     end
+    
+    def call_block(&block)
+        block.call
+        return nil
+        rescue Exception => e
+        return e
+    end
+    
+    
+    NO_BLOCK_PASSED = "you must pass a block to the eventually method"
+    
+    def eventually options = {}, &block
+        raise NO_BLOCK_PASSED if block.nil?
+        timeout = options[:timeout] || 5
+        delay = options[:delay] || 0.25
+        last_error = nil
+        begin_time = Time.now
+        
+        while (Time.now - begin_time) < timeout
+            e = call_block(&block)
+            if e then
+                last_error = e
+                sleep delay
+                else
+                return
+            end        
+        end
+        
+        raise last_error
+    end
+
 
     alias_method :context, :describe
     alias_method :specify, :it
-  
+
 end
 
 class PositiveOperatorMatcher
