7be5261126da7c3f47d6a602209f9354db1e3206
anton <antonv@rhomobile.com>
2013-03-29 22:41:18 +0400

    fix shows toolbar

--- platform/wm/rhodes/rho/rubyext/NativeToolbarExt.cpp
+++ platform/wm/rhodes/rho/rubyext/NativeToolbarExt.cpp
@@ -30,6 +30,7 @@
 #include "rubyext/NativeToolbarExt.h"
 
 extern IMainWindow& getAppWindow();
+extern IMainWindow* getMainWindowObject();
 
 extern "C"
 {
@@ -39,39 +40,44 @@ void remove_native_toolbar();
 
 void create_native_toolbar(int bar_type, rho_param *p) 
 {
-    //if ( bar_type == NOBAR_TYPE )
-    //    remove_native_toolbar();
-    //else if ( bar_type == TOOLBAR_TYPE )
-    //{
-    //    getAppWindow().performOnUiThread(new CNativeToolbar::CCreateTask(p) );
-    //}
-    //else
-    //{
-    //	RAWLOGC_ERROR("NativeBar", "Only Toolbar control is supported.");
-    //}
+    if ( bar_type == NOBAR_TYPE )
+        remove_native_toolbar();
+    else if ( bar_type == TOOLBAR_TYPE )
+    {
+        getMainWindowObject()->performOnUiThread(getMainWindowObject()->makeCreateToolbarTask(p));
+    }
+    else
+    {
+    	RAWLOGC_ERROR("NativeBar", "Only Toolbar control is supported.");
+    }
 }
 
 void create_nativebar(int bar_type, rho_param *p) 
 {
-	//RAWLOGC_INFO("NativeBar", "NativeBar.create() is DEPRECATED. Use Rho::NativeToolbar.create() or Rho::NativeTabbar.create().");
- //   create_native_toolbar(bar_type, p);
+	RAWLOGC_INFO("NativeBar", "NativeBar.create() is DEPRECATED. Use Rho::NativeToolbar.create() or Rho::NativeTabbar.create().");
+    create_native_toolbar(bar_type, p);
 }
 
 void remove_native_toolbar() 
 {
-    //getAppWindow().performOnUiThread(new CNativeToolbar::CRemoveTask() );
+    getMainWindowObject()->performOnUiThread(getMainWindowObject()->makeRemoveToolbarTask());
 }
 
 void remove_nativebar() 
 {
-	//RAWLOGC_INFO("NativeBar", "NativeBar.remove() is DEPRECATED API ! Please use Rho::NativeToolbar.remove() or Rho::NativeTabbar.remove().");
-	//remove_native_toolbar();
+	RAWLOGC_INFO("NativeBar", "NativeBar.remove() is DEPRECATED API ! Please use Rho::NativeToolbar.remove() or Rho::NativeTabbar.remove().");
+	remove_native_toolbar();
 }
 
 VALUE nativebar_started() 
 {
-    //bool bStarted = CNativeToolbar::getInstance().isStarted();
-    //return rho_ruby_create_boolean(bStarted?1:0);
+    IToolbar *toolBar = getMainWindowObject()->getToolbar();
+
+    if (toolBar)
+    {
+        return rho_ruby_create_boolean(toolBar->isStarted() ? 1 : 0);
+    }
+
     return 0;
 }
 
@@ -85,22 +91,22 @@ void remove_native_tabbar()
 
 void create_native_tabbar(int bar_type, rho_param *p)
 {
-//#if defined(OS_WINDOWS_DESKTOP)
-//    // check for iPad SplitTabBar type -> redirect to TabBar
-//    if (bar_type == VTABBAR_TYPE) {
-//        bar_type = TABBAR_TYPE;
-//    }
-//
-//	if ( bar_type == NOBAR_TYPE )
-//        remove_native_tabbar();
-//    else if ( bar_type == TABBAR_TYPE )
-//    {
-//        getAppWindow().performOnUiThread(new CNativeTabbarQt::CCreateTask(bar_type, p) );
-//    } else
-//    {
-//    	RAWLOGC_ERROR("NativeTabbar", "Only Tabbar control is supported.");
-//    }
-//#endif
+#if defined(OS_WINDOWS_DESKTOP)
+    // check for iPad SplitTabBar type -> redirect to TabBar
+    if (bar_type == VTABBAR_TYPE) {
+        bar_type = TABBAR_TYPE;
+    }
+
+	if ( bar_type == NOBAR_TYPE )
+        remove_native_tabbar();
+    else if ( bar_type == TABBAR_TYPE )
+    {
+        getMainWindowObject()->performOnUiThread(new CNativeTabbarQt::CCreateTask(bar_type, p) );
+    } else
+    {
+    	RAWLOGC_ERROR("NativeTabbar", "Only Tabbar control is supported.");
+    }
+#endif
 }
 
 void native_tabbar_switch_tab(int index)
@@ -119,8 +125,8 @@ void native_tabbar_set_tab_badge(int index,char *val)
 
 void nativebar_set_tab_badge(int index,char* val)
 {
-//  RAWLOGC_INFO("NativeBar", "NativeBar.set_tab_badge() is DEPRECATED. Use Rho::NativeTabbar.set_tab_badge().");
-//  native_tabbar_set_tab_badge(index, val);
+  RAWLOGC_INFO("NativeBar", "NativeBar.set_tab_badge() is DEPRECATED. Use Rho::NativeTabbar.set_tab_badge().");
+  native_tabbar_set_tab_badge(index, val);
 }
 
 int native_tabbar_get_current_tab() 
@@ -135,8 +141,8 @@ int native_tabbar_get_current_tab()
 
 void nativebar_switch_tab(int index)
 {
-	//RAWLOGC_INFO("NativeBar", "NativeBar.switch_tab() is DEPRECATED. Use Rho::NativeTabbar.switch_tab().");
-	//native_tabbar_switch_tab(index);
+	RAWLOGC_INFO("NativeBar", "NativeBar.switch_tab() is DEPRECATED. Use Rho::NativeTabbar.switch_tab().");
+	native_tabbar_switch_tab(index);
 }
 
 //NavBar - iphone only
