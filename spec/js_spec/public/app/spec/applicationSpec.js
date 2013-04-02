cd522a68ea13385d88cf255fb767f8846f6ecc86
Vladimir Musulainen <vmusulainen@gmail.com>
2013-04-02 14:00:42 +0400

    Modified js spec - they use get*** or set*** syntax

--- spec/js_spec/public/app/spec/applicationSpec.js
+++ spec/js_spec/public/app/spec/applicationSpec.js
@@ -1,70 +1,75 @@
  describe("<application specs>", function(){
 
-	it("Path to apps/app folder inside application bundle.", function(){
-		expect(Rho.Application.appBundleFolder()).isNotEmptyString();
+	it("Path to apps/app folder inside application bundle", function(){
+		expect(Rho.Application.getAppBundleFolder()).isNotEmptyString();
 	});
 
-	it("Path to apps folder inside application bundle.", function(){
-        expect(Rho.Application.appsBundleFolder()).isNotEmptyString();
+	it("Path to apps folder inside application bundle", function(){
+        expect(Rho.Application.getAppsBundleFolder()).isNotEmptyString();
 	});
 
-	it("Path to folder where application can write files and create subfolders.", function(){
-		expect(Rho.Application.userFolder()).isNotEmptyString();
+	it("Path to folder where application can write files and create subfolders", function(){
+		expect(Rho.Application.getUserFolder()).isNotEmptyString();
 	});
 
-	xit("Path to configuration file.", function(){
-		expect(Rho.Application.configPath()).isNotEmptyString();
+	xit("Path to configuration file", function(){
+		expect(Rho.Application.getConfigPath()).isNotEmptyString();
 	});
 
-	it("Path to file with model list.", function(){
-		expect(Rho.Application.modelsManifestPath()).isNotEmptyString(0);
+	it("Path to file with model list", function(){
+		expect(Rho.Application.getModelsManifestPath()).isNotEmptyString(0);
 	});
 
-	it("Path to folder where database blob files are stored.", function(){
-		expect(Rho.Application.databaseBlobFolder()).isNotEmptyString(0);
+	it("Path to folder where database blob files are stored", function(){
+		expect(Rho.Application.getDatabaseBlobFolder()).isNotEmptyString(0);
 	});
 
-	it("Path to application public folder.", function(){
-		expect(Rho.Application.publicFolder()).isNotEmptyString();
+	it("Path to application public folder", function(){
+		expect(Rho.Application.getPublicFolder()).isNotEmptyString();
 	});
 
-	it("Startup page for your application.", function(){
+	it("Startup page for your application", function(){
         Rho.Application.setStartURI('index.html');
-		expect(Rho.Application.startURI()).toEqual('index.html');
+		expect(Rho.Application.getStartURI()).toEqual('index.html');
 	});
 
-	it("Settings page URI.", function(){
+	it("Settings page URI", function(){
         Rho.Application.setSettingsPageURI('settings.html');
-		expect(Rho.Application.settingsPageURI()).toEqual('settings.html');
+		expect(Rho.Application.getSettingsPageURI()).toEqual('settings.html');
 	});
 
-	it("Splash screen image display options. This a string with several parameters divided by ';': delay=5;center;hcenter;vcenter;vzoom;hzoom;zoom", function(){
-		expect(Rho.Application.splash()).isNotEmptyString();
+	it("Splash screen image display options", function(){
+		expect(Rho.Application.getSplash()).isNotEmptyString();
 	});
 
-	it("Version from build time configuration file.", function(){
-		expect(Rho.Application.version()).isNotEmptyString();
+	it("Version from build time configuration file", function(){
+		expect(Rho.Application.getVersion()).isNotEmptyString();
 	});
 
-	it("Define Window caption text.If missed - caption from page used.", function(){
-		expect(Rho.Application.title()).isNotEmptyString();
+    //TODO: Move to platform dependent tests
+	it("Define Window caption text.If missed - caption from page used", function(){
+        expect(Rho.Application.getTitle()).isNotEmptyString();
+
+        Rho.Application.setTitle('Title');
+		expect(Rho.Application.getTitle()).toEqual('Title');
 	});
 
-	it("Application name.", function(){
-		expect(Rho.Application.name()).isNotEmptyString();
+	it("Application name", function(){
+		expect(Rho.Application.getName()).isNotEmptyString();
 	});
 
-	it("Current application locale.Like 'en', 'ru' etc.", function(){
-		expect(Rho.Application.locale()).isNotEmptyString();
+	it("Current application locale.Like 'en', 'ru' etc", function(){
+		expect(Rho.Application.getLocale()).isNotEmptyString();
 	});
 
-	it("Current application country code.", function(){
-		expect(Rho.Application.country()).isNotEmptyString();
+	it("Current application country code", function(){
+		expect(Rho.Application.getCountry()).isNotEmptyString();
 	});
 
-	it("Bad link URI to navigate in browser.", function(){
+     //TODO: Move to platform dependent tests
+	it("Bad link URI to navigate in browser", function(){
         Rho.Application.setBadLinkURI('badLink.html');
-		expect(Rho.Application.badLinkURI()).toEqual('badLink.html');
+		expect(Rho.Application.getBadLinkURI()).toEqual('badLink.html');
 	});
 
 })
--- spec/js_spec/public/app/spec/systemSpec.js
+++ spec/js_spec/public/app/spec/systemSpec.js
@@ -1,207 +1,210 @@
-describe("<system specs>", function(){
+describe("<system specs>", function () {
 
     /* System property specs */
 
-	it("Platform name", function(){
-        expect( Rho.System.platform()).isNotEmptyString();
-	});
-
-	it("Is camera available on the device", function(){
-        expect(typeof Rho.System.hasCamera()).toEqual('boolean');
-	});
-
-	it("Screen width in logical pixels", function(){
-        expect(Rho.System.screenWidth()).isNumberGreaterThenZero();
-	});
-
-	it("Screen height in logical pixels", function(){
-        expect(Rho.System.screenHeight()).isNumberGreaterThenZero();
-	});
-
-	it("Screen width in real pixels", function(){
-        expect(Rho.System.realScreenWidth()).isNumberGreaterThenZero();
-	});
-
-	it("Screen height in real pixels", function(){
-        expect(Rho.System.realScreenHeight()).isNumberGreaterThenZero();
-	});
-
-	it("Current screen orientation", function(){
-        expect(Rho.System.screenOrientation()).isNotEmptyString();
-	});
-
-	it("Horizontal PPI (Pixels Per Inch)", function(){
-        expect(Rho.System.ppiX()).isNumberGreaterThenZero();
-	});
-
-	it("Vertical PPI (Pixels Per Inch)", function(){
-        expect(Rho.System.ppiY()).isNumberGreaterThenZero();
-	});
-
-	it("Device phone number", function(){
-        expect(Rho.System.phoneNumber()).isNotEmptyString();
-	});
-
-	it("Push notifications device ID which may be used to receive push messages", function(){
-        expect(Rho.System.devicePushId()).isNotEmptyString();
-	});
-
-	it("Hardware based ID", function(){
-        expect(Rho.System.phoneId()).isNotEmptyString();
-	});
-
-	it("Name of device", function(){
-        expect(Rho.System.deviceName()).isNotEmptyString();
-	});
-
-	it("Version OS of device", function(){
-        expect(Rho.System.osVersion()).isNotEmptyString();
-	});
-
-	it("Current device locale", function(){
-        expect(Rho.System.locale()).isNotEmptyString();
-	});
-
-	it("Current device country code", function(){
-        expect(Rho.System.country()).isNotEmptyString();
-	});
-
-	it("Is application is running on emulator", function(){
-		expect(Rho.System.isEmulator()).isBoolean;
-	});
-
-	it("Is calendar support available", function(){
-		expect(Rho.System.hasCalendar()).isBoolean();
-	});
-
-	it("Is device supports Motorola device capabilities", function(){
-		expect(Rho.System.isMotorolaDevice()).isBoolean();
-	});
-
-	it("The OEM Information string for the terminal", function(){
-        expect(Rho.System.oemInfo()).isNotEmptyString();
-	});
-
-	it("The Unique Unit IDentifier for the terminal", function(){
-        expect(Rho.System.uuid()).isNotEmptyString();
-	});
-
-	it("Setting HTTP proxy URI", function(){
-        Rho.System.set_http_proxy_url('http://localhost');
-        expect(Rho.System.httpProxyURI()).toEqual('http://localhost');
-	});
-
-	it("Displaying software keyboard", function(){
-		Rho.System.showKeyboard(true);
-		expect(Rho.System.showKeyboard()).toEqual(true);
-	});
-
-    it("Hiding software keyboard", function(){
-        Rho.System.showKeyboard(false);
-        expect(Rho.System.showKeyboard()).toEqual(false);
+    it("Platform name", function () {
+        expect(Rho.System.getPlatform()).isNotEmptyString();
     });
 
-    it("Default port of the local (embedded) HTTP server", function(){
-        expect(Rho.System.localServerPort()).toEqual(8080);
+    it("Is camera available on the device", function () {
+        expect(typeof Rho.System.getHasCamera()).toEqual('boolean');
     });
 
-	it("Setting port of the local (embedded) HTTP server", function(){
-		Rho.System.localServerPort(7777);
-		expect(Rho.System.localServerPort()).toEqual(7777);
-	});
-
-	it("Get free local server port", function(){
-        expect(Rho.System.freeServerPort()).isNumberGreaterThenZero();
-	});
-
-    it("Setting screen auto rotate", function(){
-        expect(Rho.System.screenAutoRotate()).toEqual(true);
-
-        Rho.System.screenAutoRotate(false);
-        expect(Rho.System.screenAutoRotate()).toEqual(false);
+    it("Screen width in logical pixels", function () {
+        expect(Rho.System.getScreenWidth).isNumberGreaterThenZero();
     });
 
-	it("Is device has touch screen", function(){
-		expect(Rho.System.hasTouchscreen()).isBoolean();
-	});
+    it("Screen height in logical pixels", function () {
+        expect(Rho.System.getScreenHeight()).isNumberGreaterThenZero();
+    });
 
-	it("Is the security token check was failed", function(){
-		expect(Rho.System.securityTokenNotPassed()).isBoolean();
-	});
+    it("Screen width in real pixels", function () {
+        expect(Rho.System.getRealScreenWidth()).isNumberGreaterThenZero();
+    });
 
-	it("Browser framework identity string", function(){
-		expect(Rho.System.webviewFramework()).isNotEmptyString();
-	});
+    it("Screen height in real pixels", function () {
+        expect(Rho.System.getRealScreenHeight()).isNumberGreaterThenZero();
+    });
 
-	it("Setting screen sleeping", function(){
-		Rho.System.screenSleeping(true);
-		expect(Rho.System.screenSleeping()).toEqual(true);
+    it("Current screen orientation", function () {
+        expect(Rho.System.getScreenOrientation()).isNotEmptyString();
+    });
 
-        Rho.System.screenSleeping(false);
-        expect(Rho.System.screenSleeping()).toEqual(false);
-	});
+    it("Horizontal PPI (Pixels Per Inch)", function () {
+        expect(Rho.System.getPpiX()).isNumberGreaterThenZero();
+    });
+
+    it("Vertical PPI (Pixels Per Inch)", function () {
+        expect(Rho.System.getPpiY()).isNumberGreaterThenZero();
+    });
+
+    it("Device phone number", function () {
+        expect(Rho.System.getPhoneNumber()).isNotEmptyString();
+    });
+
+    it("Push notifications device ID which may be used to receive push messages", function () {
+        expect(Rho.System.getDevicePushId()).isNotEmptyString();
+    });
+
+    it("Hardware based ID", function () {
+        expect(Rho.System.getPhoneId()).isNotEmptyString();
+    });
+
+    it("Name of device", function () {
+        expect(Rho.System.getDeviceName()).isNotEmptyString();
+    });
+
+    it("Version OS of device", function () {
+        expect(Rho.System.getOsVersion()).isNotEmptyString();
+    });
+
+    it("Current device locale", function () {
+        expect(Rho.System.getLocale()).isNotEmptyString();
+    });
+
+    it("Current device country code", function () {
+        expect(Rho.System.getCountry()).isNotEmptyString();
+    });
+
+    it("Is application is running on emulator", function () {
+        expect(Rho.System.getIsEmulator()).isBoolean;
+    });
+
+    it("Is calendar support available", function () {
+        expect(Rho.System.getHasCalendar()).isBoolean();
+    });
+
+    it("Is device supports Motorola device capabilities", function () {
+        expect(Rho.System.getIsMotorolaDevice()).isBoolean();
+    });
+
+    it("The OEM Information string for the terminal", function () {
+        expect(Rho.System.getOemInfo()).isNotEmptyString();
+    });
+
+    it("The Unique Unit IDentifier for the terminal", function () {
+        expect(Rho.System.getUuid()).isNotEmptyString();
+    });
+
+    it("Setting HTTP proxy URI", function () {
+        Rho.System.setHttpProxyURI('http://localhost');
+        expect(Rho.System.getHttpProxyURI()).toEqual('http://localhost');
+    });
+
+    it("Getting keyboard state", function () {
+        expect(Rho.System.getKeyboardState()).toEqual('automatic');
+    });
+
+
+    it("Default port of the local (embedded) HTTP server", function () {
+        expect(Rho.System.getLocalServerPort()).toEqual(8080);
+    });
+
+    it("Setting port of the local (embedded) HTTP server", function () {
+        Rho.System.setLocalServerPort(7777);
+        expect(Rho.System.getLocalServerPort()).toEqual(7777);
+    });
+
+    it("Get free local server port", function () {
+        expect(Rho.System.getFreeServerPort()).isNumberGreaterThenZero();
+    });
+
+    it("Setting screen auto rotate", function () {
+        expect(Rho.System.getScreenAutoRotate()).toEqual(true);
+
+        Rho.System.setScreenAutoRotate(false);
+        expect(Rho.System.getScreenAutoRotate()).toEqual(false);
+    });
+
+    it("Is device has touch screen", function () {
+        expect(Rho.System.getHasTouchscreen()).isBoolean();
+    });
+
+    it("Is the security token check was failed", function () {
+        expect(Rho.System.getSecurityTokenNotPassed()).isBoolean();
+    });
+
+    it("Browser framework identity string", function () {
+        expect(Rho.System.getWebviewFramework()).isNotEmptyString();
+    });
+
+    it("Setting screen sleeping", function () {
+        Rho.System.setScreenSleeping(true);
+        expect(Rho.System.getScreenSleeping()).toEqual(true);
+
+        Rho.System.setScreenSleeping(false);
+        expect(Rho.System.getScreenSleeping()).toEqual(false);
+    });
 
     /* System method specs */
 
-    it("Command line parameters", function(){
+    it("Command line parameters", function () {
         expect(typeof Rho.System.getStartParams()).toEqual('string');
     });
 
-    it("Bring application window to the top of screen", function(){
+    it("Bring application window to the top of screen", function () {
         Rho.System.bringToFront();
     });
 
 
     /* Specs for Android platform */
 
-    if  ('ANDROID' == Rho.System.platform()){
-        it("Primary email of phone owner (works only on Android)", function(){
-            expect( Rho.System.deviceOwnerEmail()).isNotEmptyString();
+    if ('ANDROID' == Rho.System.getPlatform()) {
+        it("Primary email of phone owner (works only on Android)", function () {
+            expect(Rho.System.deviceOwnerEmail()).isNotEmptyString();
         });
 
-        it("Name(account name) of phone owner (works only on Android)", function(){
-            expect( Rho.System.deviceOwnerName()).isNotEmptyString();
+        it("Name(account name) of phone owner (works only on Android)", function () {
+            expect(Rho.System.deviceOwnerName()).isNotEmptyString();
         });
     }
 
     /* Specs for Windows family platforms */
 
-    if  (['WINDOWS_DESKTOP', 'WP8', 'WINODWS'].indexOf(Rho.System.platform()) != -1 ){
-        it("Writing and reading registry value", function(){
-            Rho.System.setRegistrySetting('HKEY_CURRENT_USER', 'REG_SZ', 'phone_spec' ,'phone_spec_registry_key', 'test');
-            expect(Rho.System.getRegistrySetting('HKEY_CURRENT_USER', 'REG_SZ', 'phone_spec' ,'phone_spec_registry_key')).toEqual('test')
+    if (['WINDOWS_DESKTOP', 'WP8', 'WINDOWS'].indexOf(Rho.System.getPlatform()) != -1) {
+        it("Writing and reading registry value", function () {
+            Rho.System.setRegistrySetting('HKEY_CURRENT_USER', 'REG_SZ', 'phone_spec', 'phone_spec_registry_key', 'test');
+            expect(Rho.System.getRegistrySetting('HKEY_CURRENT_USER', 'REG_SZ', 'phone_spec', 'phone_spec_registry_key')).toEqual('test')
+        });
+
+        it("Setting keyboard state", function () {
+            expect(Rho.System.setKeyboardState()).toEqual('shown');
+            expect(Rho.System.getKeyboardState()).toEqual('shown');
+
+            expect(Rho.System.setKeyboardState()).toEqual('hidden');
+            expect(Rho.System.getKeyboardState()).toEqual('hidden');
         });
     }
 
     /* Specs for Windows desktop platfform */
 
-    if  ('WINDOWS_DESKTOP' == Rho.System.platform()){
+    if ('WINDOWS_DESKTOP' == Rho.System.getPlatform()) {
 
-        it("Setting window frame", function(){
+        it("Setting window frame", function () {
             Rho.System.setWindowFrame(1, 1, 200, 200);
         });
 
-        it("Setting window position", function(){
+        it("Setting window position", function () {
             Rho.System.setWindowPosition(1, 1);
         });
 
-        it("Locking the window size", function(){
-            Rho.System.lockWindowSize(true);
-            expect(Rho.System.lockWindowSize()).toEqual(true);
+        it("Locking the window size", function () {
+            Rho.System.setLockWindowSize(true);
+            expect(Rho.System.getLockWindowSize()).toEqual(true);
         });
 
-        it("Unlocking the window size (only for Windows desktop)", function(){
-            Rho.System.lockWindowSize(false);
-            expect(Rho.System.lockWindowSize()).toEqual(false);
+        it("Unlocking the window size (only for Windows desktop)", function () {
+            Rho.System.setLockWindowSize(false);
+            expect(Rho.System.getLockWindowSize()).toEqual(false);
         });
     }
 
     /* Specs for Apple platform */
 
-    if  ('APPLE' == Rho.System.platform()){
-        it("Setting the application icon to have this badge number", function(){
-            Rho.System.set_application_icon_badge(1);
-            expect(Rho.System.applicationIconBadge()).toEqual(1);
+    if ('APPLE' == Rho.System.getPlatform()) {
+        it("Setting the application icon to have this badge number", function () {
+            Rho.System.setApplicationIconBadge(1);
+            expect(Rho.System.getApplicationIconBadge()).toEqual(1);
         });
     }
 
