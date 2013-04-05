3465a098965fad6cd68eeb89bdb6e2e4bf72abf5
genywind <evgeny@rhomobile.com>
2013-04-06 01:52:58 +0400

    win32: fix build and use Millisec in ThreadImpl

--- platform/shared/common/IRhoThreadImpl.h
+++ platform/shared/common/IRhoThreadImpl.h
@@ -42,10 +42,10 @@ struct IRhoThreadImpl
     virtual ~IRhoThreadImpl(void){}
 
     virtual void start(IRhoRunnable* pRunnable, IRhoRunnable::EPriority ePriority) = 0;
-    virtual void stop(unsigned int nTimeoutToKill) = 0;
-    virtual int wait(unsigned int nTimeout) = 0;
+    virtual void stop(unsigned int nTimeoutToKillMs) = 0;
+    virtual int wait(unsigned int nTimeoutMs) = 0;
     virtual void stopWait() = 0;
-    virtual void sleep(unsigned int nTimeout) = 0;
+    virtual void sleep(unsigned int nTimeoutMs) = 0;
 };
 
 }
