a4a55592c00a9618315b327e221c8829b57508f4
Alexey Tikhvinsky <alexey@rhomobile.com>
2013-04-09 18:25:19 +0400

    Android: Get has_camera property from camera extension

---
+++ lib/commonAPI/mediacapture/ext/platform/android/src/com/rho/camera/CameraExtension.java
@@ -0,0 +1,21 @@
+package com.rho.camera;
+
+import com.rhomobile.rhodes.extmanager.AbstractRhoExtension;
+import com.rhomobile.rhodes.extmanager.IRhoExtManager;
+import com.rhomobile.rhodes.extmanager.IRhoExtension;
+
+public class CameraExtension extends AbstractRhoExtension implements IRhoExtension {
+    
+    private static final String TAG = CameraExtension.class.getSimpleName();
+
+    @Override
+    public String onGetProperty(IRhoExtManager extManager, String name) {
+        if (name.equalsIgnoreCase("has_camera")) {
+            if (((CameraFactory)(CameraFactorySingleton.getInstance())).getApiSingletonImplementation().getCameraCount() > 0) {
+                return "1";
+            }
+        }
+        return null;
+    }
+    
+}
