a6deaae543ebb97291e7bbdaa34bc08b180f3a6c
genywind <evgeny@rhomobile.com>
2013-04-13 19:17:27 +0400

    wm: support Application.autoStart rhoconfig property

--- platform/wm/rhodes/Rhodes.cpp
+++ platform/wm/rhodes/Rhodes.cpp
@@ -64,6 +64,11 @@ extern "C" void rho_webview_navigate(const char* url, int index);
 class CEng;
 extern rho::IBrowserEngine* rho_wmimpl_get_webkitBrowserEngine(HWND hwndParent, HINSTANCE rhoAppInstance);
 extern "C" CEng* rho_wmimpl_get_webkitbrowser(HWND hParentWnd, HINSTANCE hInstance);
+
+#if !defined(APP_BUILD_CAPABILITY_MOTOROLA)
+extern "C" LRESULT	rho_wm_appmanager_ProcessOnTopMostWnd(WPARAM wParam, LPARAM lParam){ return 0;}
+#endif
+
 #else
 extern "C" void rho_wm_impl_SetApplicationLicenseObj(void* pAppLicenseObj)
 {
@@ -168,6 +173,7 @@ class CRhodesModule : public CAtlExeModuleT< CRhodesModule >
     CMainWindow m_appWindow;
     rho::String m_strRootPath, m_strRhodesPath, m_logPort, m_strRuntimePath, m_strAppName;//, m_strDebugHost, m_strDebugPort;*/
 	int m_nRestarting;
+    bool m_bMinimized;
 	bool m_isRhoConnectPush;
 #ifndef RHODES_EMULATOR
 	HANDLE m_hMutex;
@@ -197,6 +203,7 @@ public :
     const rho::String& getRhoRootPath();
     const rho::String& getRhoRuntimePath();
     const rho::String& getAppName();
+    void createAutoStartShortcut();
 };
 
 void parseHttpProxyURI(const rho::String &http_proxy);
@@ -220,6 +227,7 @@ rho::IBrowserEngine* rho_wmimpl_createBrowserEngine(HWND hwndParent)
 bool CRhodesModule::ParseCommandLine(LPCTSTR lpCmdLine, HRESULT* pnRetCode ) throw( )
 {
 	m_nRestarting = 1;
+    m_bMinimized = false;
 	LPCTSTR lpszToken = lpCmdLine;
 	LPCTSTR nextToken;
     getRhoRootPath();
@@ -256,6 +264,10 @@ bool CRhodesModule::ParseCommandLine(LPCTSTR lpCmdLine, HRESULT* pnRetCode ) thr
 		if (isCmdLineOpt) {
 			if (WordCmpI(lpszToken, _T("Restarting"))==0) {
 				m_nRestarting = 10;
+			}
+
+            if (WordCmpI(lpszToken, _T("minimized"))==0) {
+				m_bMinimized = true;
 			}
 
 			if (WordCmpI(lpszToken, _T("rhoconnectpush"))==0) {
@@ -535,6 +547,9 @@ HRESULT CRhodesModule::PreMessageLoop(int nShowCmd) throw()
 		return S_FALSE;
     }
 
+    if (RHOCONF().getBool("Application.autoStart"))
+        createAutoStartShortcut();
+
     rho::common::CRhodesApp::Create(m_strRootPath, m_strRootPath, m_strRuntimePath);
     RHODESAPP().setExtManager( &m_oExtManager );
 
@@ -562,6 +577,9 @@ HRESULT CRhodesModule::PreMessageLoop(int nShowCmd) throw()
     {
         return S_FALSE;
     }
+    if (m_bMinimized)
+        nShowCmd = SW_MINIMIZE;
+
     m_appWindow.ShowWindow(nShowCmd);
 
 #ifndef RHODES_EMULATOR
@@ -778,6 +796,24 @@ const rho::String& CRhodesModule::getRhoRuntimePath()
     }
 
     return m_strRuntimePath; 
+}
+
+void CRhodesModule::createAutoStartShortcut()
+{
+#ifdef OS_WINCE
+    StringW strLnk = L"\\Windows\\StartUp\\";
+    strLnk += convertToStringW(getAppName());
+    strLnk += L".lnk";
+
+    StringW strAppPath = L"\"";
+    char rootpath[MAX_PATH];
+    GetModuleFileNameA(NULL,rootpath,MAX_PATH);
+    strAppPath += convertToStringW(rootpath);
+    strAppPath += L" -minimized\"";
+
+    DWORD dwRes = SHCreateShortcut( (LPTSTR)strLnk.c_str(), (LPTSTR)strAppPath.c_str());
+#endif
+
 }
 
 extern "C" int WINAPI _tWinMain(HINSTANCE hInstance, HINSTANCE /*hPrevInstance*/,
