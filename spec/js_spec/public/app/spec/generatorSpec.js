a2b180ffea9524d6c3cb8c857107adc18cf622c8
genywind <evgeny@rhomobile.com>
2013-04-14 01:18:59 +0400

    js-spec: fix

--- spec/js_spec/public/app/spec/generatorSpec.js
+++ spec/js_spec/public/app/spec/generatorSpec.js
@@ -7,11 +7,17 @@ describe("<generator API specs>", function () {
         Rho.GenPropBag.boolProp = true;
         expect(Rho.GenPropBag.boolProp).toEqual(true);
     });
--->
+
     it("Test bool default property", function () {
         expect(Rho.GenPropBag.default().boolProp).toEqual(false);
         Rho.GenPropBag.default().boolProp = true;
         expect(Rho.GenPropBag.default().boolProp).toEqual(true);
     });
+-->
+
+    it("Test enumerate", function () {
+        Rho.GenPropBag.enumerate();
+    });
+    
 <!-- TODO: enumerate -->
 });
