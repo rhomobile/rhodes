233b2cce32915f64c814c4be71bd630253edf689
Alexey Tikhvinsky <alexey@rhomobile.com>
2013-04-16 03:05:45 +0400

    Common API: JS: support objects in result

--- spec/js_spec/extensions/genPropBag/ext/platform/android/src/com/rho/genpropbag/GenPropBag.java
+++ spec/js_spec/extensions/genPropBag/ext/platform/android/src/com/rho/genpropbag/GenPropBag.java
@@ -11,29 +11,5 @@ public class GenPropBag extends GenPropBagBase implements IGenPropBag {
         super(id);
     }
 
-    @Override
-    public void start(IMethodResult result) {
-        result.set("1234567890");
-    }
-
-    @Override
-    public void stop(IMethodResult result) {
-        // TODO Auto-generated method stub
-    }
-
-    @Override
-    public void disable(IMethodResult result) {
-        // TODO Auto-generated method stub
-    }
-
-    @Override
-    public void take(Map<String, String> propertyMap, IMethodResult result) {
-        result.set("1234567890");
-    }
-
-    @Override
-    public void enable(Map<String, String> propertyMap, IMethodResult result) {
-        // TODO Auto-generated method stub
-    }
 
 }
--- spec/js_spec/extensions/genPropBag/ext/platform/android/src/com/rho/genpropbag/GenPropBagSingleton.java
+++ spec/js_spec/extensions/genPropBag/ext/platform/android/src/com/rho/genpropbag/GenPropBagSingleton.java
@@ -9,21 +9,14 @@ class GenPropBagSingleton extends GenPropBagSingletonBase implements IGenPropBag
     public GenPropBagSingleton(GenPropBagFactory factory) {
         super(factory);
     }
-
-    List<String> getIDs() {
-        List<String> ids = new LinkedList<String>();
-        ids.add("SCN1");
-        ids.add("SCN2");
-        return ids;
-    }
-    
     @Override
     protected String getInitialDefaultID() {
-        return getIDs().get(0);
+        return "0";
+    }
+    @Override
+    public void enumerate(IMethodResult result) {
+        // TODO Auto-generated method stub
+        
     }
 
-    @Override
-    public void enumerate(IMethodResult res) {
-        res.set(getIDs());
-    }
 }
