97f4a68d198080977574213c383727f7474b41a2
dmitrys <dmitrys@rhomobile.com>
2013-04-16 03:02:03 +0400

    CommonAPI:iOS:Camera fix XML changes

--- lib/commonAPI/mediacapture/ext/platform/iphone/impl/CameraSingleton.h
+++ lib/commonAPI/mediacapture/ext/platform/iphone/impl/CameraSingleton.h
@@ -12,7 +12,7 @@
 -(void) enumerate:(id<IMethodResult>)methodResult;
 -(void) getCameraByType:(NSString*)cameraType methodResult:(id<IMethodResult>)methodResult;
 
--(void) choosePicture:(id<IMethodResult>)methodResult;
+-(void) choosePicture:(NSDictionary*)propertyMap methodResult:(id<IMethodResult>)methodResult;
 -(void) saveImageToDeviceGallery:(NSString*)pathToImage;
 
 
