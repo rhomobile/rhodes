7b4b77a1afe4b37aedb1aa83584ccb2456cffb93
Stanislav Volodarskiy <stan_volodarsky@mail.ru>
2013-04-16 16:06:08 +0400

    Rho.ORM.js and ormSpec.js intermediate commit

--- spec/js_spec/public/app/spec/ormSpec.js
+++ spec/js_spec/public/app/spec/ormSpec.js
@@ -1,4 +1,4 @@
-describe("<ORM module specs>", function () {
+describe("<ORM module specs>", function() {
 
     beforeEach(function(){
         Rho.ORM.clear();
@@ -20,9 +20,48 @@ describe("<ORM module specs>", function () {
         expect(Rho.ORM.getModel('Model')).toBeUndefined();
     });
 
-    it('creates object, gets properties', function() {
+    it('makes empty object', function() {
+        Rho.ORM.addModel('Model').make();
+    });
+
+    it('creates empty object', function() {
+        Rho.ORM.addModel('Model').create();
+    });
+
+    describe("<model's object>", function() {
+        var Model = undefined;
+        var object = undefined;
+
+        beforeEach(function(){
+            Model = Rho.ORM.addModel('Model');
+            object = Model.make({'key': 'value'});
+        });
+
+        it('gets property', function() {
+            expect(object.get('key')).toBe('value');
+        });
+
+        it('sets property', function() {
+            object.set('key', 'another value');
+            expect(object.get('key')).toBe('another value');
+        });
+
+        it('sets new property', function() {
+            object.set('new key', 'new value');
+            expect(object.get('new key')).toBe('new value');
+            expect(object.get('key')).toBe('value');
+        });
+
+        it('support set chaining', function() {
+            object.set('key', 'another value').set('new key', 'new value');
+            expect(object.get('key')).toBe('another value');
+            expect(object.get('new key')).toBe('new value');
+        });
+    });
+
+    it('creates and destroys objects in database', function() {
         var Model = Rho.ORM.addModel('Model');
         var object = Model.create({'key': 'value'});
-        expect(object.get('key')).toBe('value');
-    });
+        expect(Model.find('all')).toEqual([]);
+    }) ;
 });
