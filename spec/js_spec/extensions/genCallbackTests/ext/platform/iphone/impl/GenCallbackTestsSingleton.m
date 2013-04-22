805b8eae4f85864b0759b750343c60dc708fd76c
Mikhail Malinin <malinin.work@gmail.com>
2013-04-22 13:33:45 +0400

    callback with hash test.

--- spec/js_spec/extensions/genCallbackTests/ext/platform/iphone/impl/GenCallbackTestsSingleton.m
+++ spec/js_spec/extensions/genCallbackTests/ext/platform/iphone/impl/GenCallbackTestsSingleton.m
@@ -41,6 +41,7 @@
     [methodResult setResult:(sorted)];
 }
 
+// Should remove all hashes starting with vowels and reverse value characters
 -(void) testHash:(NSDictionary*)value methodResult:(id<IMethodResult>)methodResult {
     NSCharacterSet *set = [NSCharacterSet characterSetWithCharactersInString:@"aeiouy"];
     NSEnumerator *enumerator = [value keyEnumerator];
@@ -49,7 +50,17 @@
     for(NSString *aKey in enumerator) {
         if ([[aKey lowercaseString] rangeOfCharacterFromSet:set].location != 0)
         {
-            [result setObject:[value valueForKey:aKey] forKey:aKey];
+            NSString *val = [value valueForKey:aKey];
+            
+            NSMutableString *reversedString = [NSMutableString string];
+            NSInteger charIndex = [val length];
+            while (charIndex > 0) {
+                charIndex--;
+                NSRange subStrRange = NSMakeRange(charIndex, 1);
+                [reversedString appendString:[val substringWithRange:subStrRange]];
+            }
+            
+            [result setObject:reversedString forKey:aKey];
         }
     }
 
