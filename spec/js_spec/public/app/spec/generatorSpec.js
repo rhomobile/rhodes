6eeca0ee3b57d0af1c3beb4d36f0f839c4d13602
Dmitry Prokhorov <dipspb@gmail.com>
2013-04-24 21:37:31 +0400

    Common JS API: DEFAULT_INSTANCE support fixed

--- spec/js_spec/public/app/spec/generatorSpec.js
+++ spec/js_spec/public/app/spec/generatorSpec.js
@@ -1,20 +1,22 @@
 
 describe("<generator API specs>", function () {
 
-/*
     it("Test bool property", function () {
+        Rho.GenPropBag.boolProp = false;
         expect(Rho.GenPropBag.boolProp).toEqual(false);
         Rho.GenPropBag.boolProp = true;
         expect(Rho.GenPropBag.boolProp).toEqual(true);
     });
-*/
+
     it("Test bool default property", function () {
+        Rho.GenPropBag.getDefault().boolProp = false;
         expect(Rho.GenPropBag.getDefault().boolProp).toEqual(false);
         Rho.GenPropBag.getDefault().boolProp = true;
         expect(Rho.GenPropBag.getDefault().boolProp).toEqual(true);
     });
 
     it("Test int default property", function () {
+        Rho.GenPropBag.getDefault().intProp = 0;
         expect(Rho.GenPropBag.getDefault().intProp).toEqual(0);
         Rho.GenPropBag.getDefault().intProp = 15;
         expect(Rho.GenPropBag.getDefault().intProp).toEqual(15);
