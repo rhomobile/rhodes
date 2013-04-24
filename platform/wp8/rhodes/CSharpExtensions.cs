55fd4ef0a3da628e23284560d8e0a5ae449119ea
Albert R. Timashev <albert@rhomobile.com>
2013-04-24 21:39:31 +0400

    wp8: initialization of C# extensions (phase 1)

---
+++ platform/wp8/rhodes/CSharpExtensions.cs
@@ -0,0 +1,7 @@
+// WARNING! THIS FILE IS GENERATED AUTOMATICALLY! DO NOT EDIT IT MANUALLY!
+namespace rhodes {
+    public static class CSharpExtensions {
+        public static void InitializeExtenstions() {
+        }
+    }
+}
