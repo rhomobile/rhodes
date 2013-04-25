81effd472689e35bfde58239d108afd21cc17299
nyarlathotep77 <fpc843@motorolasolutions.com>
2013-04-25 16:51:27 +0100

    Add first draft ScreenOrientation API

---
+++ lib/commonAPI/screenorientation/ext/platform/android/src/com/motorolasolutions/rho/screenorientation/ScreenOrientation.java
@@ -0,0 +1,17 @@
+package com.motorolasolutions.rho.screenorientation;
+
+import java.util.Map;
+
+import com.rho.screenorientation.IScreenOrientation;
+import com.rho.screenorientation.ScreenOrientationBase;
+import com.rhomobile.rhodes.api.IMethodResult;
+import com.rhomobile.rhodes.api.MethodResult;
+
+public class ScreenOrientation extends ScreenOrientationBase implements IScreenOrientation {
+    
+    public ScreenOrientation(String id) {
+        super(id);
+    }
+
+    
+}
---
+++ lib/commonAPI/screenorientation/ext/platform/android/src/com/motorolasolutions/rho/screenorientation/ScreenOrientationFactory.java
@@ -0,0 +1,19 @@
+package com.motorolasolutions.rho.screenorientation;
+
+import com.rho.screenorientation.IScreenOrientationFactory;
+import com.rhomobile.rhodes.api.RhoApiFactory;
+
+public class ScreenOrientationFactory
+        extends RhoApiFactory< ScreenOrientation, ScreenOrientationSingleton>
+        implements IScreenOrientationFactory {
+
+    @Override
+    protected ScreenOrientationSingleton createSingleton() {
+        return new ScreenOrientationSingleton(this);
+    }
+
+    @Override
+    protected ScreenOrientation createApiObject(String id) {
+        return new ScreenOrientation(id);
+    }
+}
