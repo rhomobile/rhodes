25f6d3080c40cd4ed8b03084d8fe70feb714064a
Harish <pulimiharish@gmail.com>
2013-04-26 11:22:06 +0100

    Video Capture Extension

--- lib/commonAPI/mediacapture/ext/platform/android/src/com/rho/mediacapture/RhoListener.java
+++ lib/commonAPI/mediacapture/ext/platform/android/src/com/rho/mediacapture/RhoListener.java
@@ -1,6 +1,8 @@
 package com.rho.mediacapture;
 
 import com.rho.camera.CameraFactory;
+import com.motorolasolutions.rho.videocapture.VideocaptureFactory;
+import com.rhomobile.rhodes.Logger;
 import com.rhomobile.rhodes.extmanager.AbstractRhoListener;
 import com.rhomobile.rhodes.extmanager.IRhoExtManager;
 import com.rhomobile.rhodes.extmanager.IRhoListener;
@@ -10,7 +12,15 @@ public class RhoListener extends AbstractRhoListener implements IRhoListener {
     @Override
     public void onCreateApplication(IRhoExtManager extManager) {
 
-        new CameraFactory().onCreateApplication(extManager);
+    	try
+    	{
+    		new CameraFactory().onCreateApplication(extManager);
+    		new VideocaptureFactory().onCreateApplication(extManager);
+    	}
+    	catch(NullPointerException e)
+    	{
+    		Logger.E("RhoListener", "Rho Listener null pointer exception");
+    	}
 
     }
 
