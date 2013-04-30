f38f4cbdf15162cfd459d5f9f2c3e8a91b188bcf
genywind <evgeny@rhomobile.com>
2013-04-30 18:33:28 +0400

    api: TabBar, ToolBar: add ruby files

---
+++ lib/commonAPI/coreapi/RhoNativeToolbarApi.rb
@@ -0,0 +1,38 @@
+if Rho::System.isRhoSimulator || System.get_property('platform') == 'WINDOWS' || System.get_property('platform') == 'WINDOWS_DESKTOP'
+
+module Rho
+class NativeToolbar
+
+  class << self
+ 
+  alias :create_orig create
+  
+  def create(params, options = {})
+  
+    if (params.is_a?(Hash))
+        buttons = params[:buttons]
+    elsif params.is_a?(Array)
+        buttons = params        
+        params = options
+    else
+        raise ArgumentError, 'invalid argument: first parameter should be Hash or Array.'
+    end
+
+    params[:backgroundColor] = params[:background_color] if params[:background_color]
+    params[:maskColor] = params[:mask_color] if params[:mask_color]
+    params[:viewHeight] = params[:view_height] if params[:view_height]
+    
+    if ( buttons && buttons.is_a?(Array))    
+        buttons.each do |btn|
+            btn[:coloredIcon] = btn[:colored_icon] if btn && btn[:colored_icon]
+        end
+    end
+    
+    create_orig( buttons, params )
+    
+  end
+  end #self
+end
+end 
+ 
+end
