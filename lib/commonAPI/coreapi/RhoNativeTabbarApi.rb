f3c4c87fbdbb784de8dec12c55832d80f9f6e1dd
genywind <evgeny@rhomobile.com>
2013-04-30 18:33:28 +0400

    api: TabBar, ToolBar: add ruby files

---
+++ lib/commonAPI/coreapi/RhoNativeTabbarApi.rb
@@ -0,0 +1,46 @@
+if Rho::System.isRhoSimulator || System.get_property('platform') == 'WINDOWS' || System.get_property('platform') == 'WINDOWS_DESKTOP'
+
+module Rho
+class NativeTabbar
+
+  class << self
+ 
+  alias :create_orig create
+  
+  def create(params, options = {})
+  
+    if (params.is_a?(Hash))
+        buttons = params[:tabs]
+    elsif params.is_a?(Array)
+        buttons = params        
+        params = options
+    else
+        raise ArgumentError, 'invalid argument: first parameter should be Hash or Array.'
+    end
+
+    if ( buttons && buttons.is_a?(Array))    
+        buttons.each do |btn|
+            btn[:coloredIcon] = btn[:colored_icon] if btn && btn[:colored_icon]
+            btn[:selectedColor] = btn[:selected_color] if btn && btn[:selected_color]
+            btn[:backgroundColor] = btn[:web_bkg_color] if btn && btn[:web_bkg_color]
+            btn[:useCurrentViewForTab] = btn[:use_current_view_for_tab] if btn && btn[:use_current_view_for_tab]
+            
+            params[:backgroundColor] =btn[:background_color] if btn && btn[:background_color]
+            
+            
+        end
+    end
+
+    params[:backgroundColor] = params[:background_color] if params[:background_color]
+    if params[:on_change_tab_callback] && params[:on_change_tab_callback].length() > 0
+        create_orig( buttons, params, params[:on_change_tab_callback] )
+    else
+        create_orig( buttons, params )
+    end    
+    
+  end
+  end #self
+end
+end 
+ 
+end
