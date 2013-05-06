1e11b3938c4f2e1e6a0437f1a086c866a533eb7e
genywind <evgeny@rhomobile.com>
2013-05-06 18:07:20 +0400

    phone_spec: windows: fix Toolbar spec

--- spec/phone_spec/app/spec/nativebar_spec.rb
+++ spec/phone_spec/app/spec/nativebar_spec.rb
@@ -1,5 +1,6 @@
 describe "NativeBar" do
 
+if !Rho::System.isRhoSimulator && System.get_property('platform') != 'WINDOWS' && System.get_property('platform') != 'WINDOWS_DESKTOP'
   it "should create" do
     NativeBar.create Rho::RhoApplication::NOBAR_TYPE, []
     sleep 20
@@ -14,5 +15,23 @@ describe "NativeBar" do
     NativeBar.started.should ==  false
 
   end
+else
+
+  it "should create" do
+    Rho::NativeToolbar.remove
+    sleep 20
+    Rho::NativeToolbar.isCreated.should ==  false
+
+    Rho::NativeToolbar.create [{:action => :back}]
+    sleep 20
+    Rho::NativeToolbar.isCreated.should ==  true
+
+    Rho::NativeToolbar.remove
+    sleep 20
+    Rho::NativeToolbar.isCreated.should == false
+
+  end
+
+end
 
 end
