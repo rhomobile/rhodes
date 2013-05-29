e54e7674e4ad9e628eb661263d8c45682c640db8
Albert R. Timashev <albert@rhomobile.com>
2013-05-29 21:35:39 +0400

    wp8: Toolbar C# implementation

--- lib/commonAPI/coreapi/ext/platform/wp8/src/NativeToolbar_impl.cs
+++ lib/commonAPI/coreapi/ext/platform/wp8/src/NativeToolbar_impl.cs
@@ -10,6 +10,7 @@ using System.Windows.Media;
 using System.Windows.Media.Animation;
 using System.Windows.Shapes;
 using rhoruntime;
+using rhodes;
 
 namespace rho {
 
@@ -30,19 +31,78 @@ namespace NativeToolbarImpl
             NativeToolbarSingletonComponent _runtime = new NativeToolbarSingletonComponent(this);
         }
 
+        static private MainPage getMainPage()
+        {
+            MainPage mainPage = MainPage.getInstance();
+            return mainPage;
+        }
+
         public void create(IReadOnlyList<string> toolbarElements, IReadOnlyDictionary<string, string> toolBarProperties, IMethodResult oResult)
         {
-            // implement this method in C# here
+            MainPage mp = getMainPage();
+            if (mp != null)
+            {
+                mp.toolbarRemoveAllButtons();
+                for (int i = 0; i < toolbarElements.Count; ++i)
+                {
+                    string tb = toolbarElements[i];
+                    CJSONEntryProxy oEntry = new CJSONEntryProxy(tb);
+                    string label = null;
+                    string action = null;
+                    string icon = null;
+                    if (oEntry.hasName("label"))
+                        label = oEntry.getString("label");
+                    if (oEntry.hasName("action"))
+                        action = oEntry.getString("action");
+                    if (oEntry.hasName("icon"))
+                        icon = oEntry.getString("icon");
+                    CRhoRuntime.getInstance().logEvent("addToolbarButton: Label: '" + (label==null?"":label) + "';Action: '" + (action==null?"":action) + "'");
+                    if ((action != null) && !action.Equals("separator", StringComparison.InvariantCultureIgnoreCase))
+                    {
+                        if ((icon != null) && (icon.Length > 0))
+                        {
+                            icon = CRhoRuntime.getInstance().getRootPath(icon);
+                        }
+                        else
+                        {
+                            if (action.Equals("options", StringComparison.InvariantCultureIgnoreCase))
+                                icon = "res/options_btn.png";
+                            else if (action.Equals("home", StringComparison.InvariantCultureIgnoreCase))
+                                icon = "res/home_btn.png";
+                            else if (action.Equals("refresh", StringComparison.InvariantCultureIgnoreCase))
+                                icon = "res/refresh_btn.png";
+                            else if (action.Equals("back", StringComparison.InvariantCultureIgnoreCase))
+                                icon = "res/back_btn.png";
+                            else if (action.Equals("forward", StringComparison.InvariantCultureIgnoreCase))
+                                icon = "res/forward_btn.png";
+                            if (icon != null)
+                                icon = CRhoRuntime.getInstance().getRERuntimePath("lib/" + icon);
+                        }
+                        //TODO: callback
+                        mp.toolbarAddAction(icon, label, action);
+                    }
+                }
+	            //TODO: setToolbarStyle
+	            //CRhoRuntime::getInstance()->getMainPage()->setToolbarStyle(false, rho::common::convertStringCToWP8(rgbBackColor));
+	            //TODO: toolbarShow
+                //CRhoRuntime::getInstance()->getMainPage()->toolbarShow();
+            }
         }
 
         public void remove(IMethodResult oResult)
         {
-            // implement this method in C# here
+            MainPage mp = getMainPage();
+            if (mp != null)
+            {
+                mp.toolbarRemoveAllButtons();
+                CRhoRuntime.getInstance().logEvent("Toolbar removed");
+            }
         }
 
         public void isCreated(IMethodResult oResult)
         {
-            // implement this method in C# here
+            MainPage mp = getMainPage();
+            oResult.set(mp != null ? (mp.toolbarIsStarted() != 0) : false);
         }
     }
 
