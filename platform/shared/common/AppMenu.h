12c7dba3edfb1f31b505ba18e248a4ec279ae05e
unknown <anton@.(none)>
2013-05-30 11:05:09 -0700

    fix default menu item state

--- platform/shared/common/AppMenu.h
+++ platform/shared/common/AppMenu.h
@@ -45,7 +45,7 @@ struct CAppMenuItem
 	EnumMenuType  m_eType;
 
     CAppMenuItem (const String &label, const String &link);
-    CAppMenuItem () : m_eType(emtNone){}
+    CAppMenuItem () : m_eType(emtNone), m_isEnable(true) {}
     void processCommand();
 };
 
