9658b8d7224272e4581b28f46b048b857cba6d42
genywind <evgeny@rhomobile.com>
2013-06-09 20:39:18 +0400

    JS API: support jscallback in native code

--- platform/shared/common/RhodesApp.cpp
+++ platform/shared/common/RhodesApp.cpp
@@ -1874,6 +1874,16 @@ void CRhodesApp::loadUrl(String url, int nTabIndex/* = -1*/)
     {
         js_callback = true;
         url = url.substr(11);
+    }else if (String_startsWith(url, "jscallback:") )
+    {
+        js_callback = true;
+        url = url.substr(11);
+
+        String strCallback("Rho.callbackHandler( \"");
+        strCallback += url;
+        strCallback += "\", {},\"\")";
+
+        url = strCallback;
     }else if ( strcasecmp(url.c_str(), "exit")==0 || strcasecmp(url.c_str(), "close") == 0 )
     {
         rho_sys_app_exit();
