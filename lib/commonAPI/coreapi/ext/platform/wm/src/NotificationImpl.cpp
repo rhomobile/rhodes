709faf0a42b36c8f9cb1373aeb6a227e83720d9c
Darryn Campbell <darryncampbell@motorolasolutions.com>
2013-06-20 13:23:58 +0100

    Updated Notification API for WM/CE to include Beeper and Vibrate.  Alert / Status is still unimplemented for WM/CE and stubbed.

--- lib/commonAPI/coreapi/ext/platform/wm/src/NotificationImpl.cpp
+++ lib/commonAPI/coreapi/ext/platform/wm/src/NotificationImpl.cpp
@@ -1,12 +1,18 @@
 #include "../../../shared/generated/cpp/NotificationBase.h"
 #include "common/RhodesApp.h"
 #include "common/RhoConf.h"
+#include "NotificationLoader.h"
+#include <algorithm>
 
 extern "C" void alert_show_popup_ex(const rho::Hashtable<rho::String, rho::String>& propertyMap, rho::apiGenerator::CMethodResult& oResult);
 extern "C" void alert_hide_popup();
 extern "C" void alert_show_status_ex(const char* szTitle, const char* szMessage, const char* szHide, rho::apiGenerator::CMethodResult& oResult);
 extern "C" void alert_vibrate(int duration_ms);
 extern "C" void alert_play_file(char* file_name, char* media_type);
+
+#define NOTIFICATIONS_DEFAULT_FREQUENCY 2000
+#define NOTIFICATIONS_DEFAULT_VOLUME 1
+#define NOTIFICATIONS_DEFAULT_DURATION 1000
 
 namespace rho {
 
@@ -16,21 +22,42 @@ using namespace common;
 class CNotificationImpl: public CNotificationSingletonBase
 {
 public:
-	CNotificationImpl(): CNotificationSingletonBase() {}
+	CNotificationImpl(): CNotificationSingletonBase() 
+	{
+		m_bNotificationsLoaded = FALSE;
+		m_pNotifications = new CNotificationLoader();
+		if (m_pNotifications)
+			m_bNotificationsLoaded = m_pNotifications->LoadNotificationDLL();
+		if (!m_pNotifications || !m_bNotificationsLoaded)
+			LOG(INFO) + "Motorola specific notification functionality is not available on this device";
+	
+	}
 
     virtual void showPopup(const Hashtable<String, String>& propertyMap, CMethodResult& oResult)
 	{
+#if defined(OS_WINCE)
+		//  TODO - Alert functionality on WinCE
+#else
 		alert_show_popup_ex(propertyMap, oResult);
+#endif
 	}
 
     virtual void hidePopup(CMethodResult& oResult)
 	{
+#if defined(OS_WINCE)
+		//  TODO - Alert functionality on WinCE
+#else
 		alert_hide_popup();
+#endif
 	}
 
     virtual void showStatus(const String& title, const String& status_text, const String& hide_button_label, CMethodResult& oResult)
 	{
+#if defined(OS_WINCE)
+		//  TODO - Status functionality on WinCE
+#else
 		alert_show_status_ex(title.c_str(), status_text.c_str(), hide_button_label.c_str(), oResult);
+#endif
 	}
 
 	virtual void playFile(const String& path, const String& media_type, CMethodResult& oResult)
@@ -40,13 +67,49 @@ public:
 
     virtual void beep(const Hashtable<String, String>& propertyMap, CMethodResult& oResult)
 	{
-		// TODO: implement
+		int iFrequency = NOTIFICATIONS_DEFAULT_FREQUENCY;
+		int iVolume = NOTIFICATIONS_DEFAULT_VOLUME;
+		int iDuration = NOTIFICATIONS_DEFAULT_DURATION;
+		rho::String comparitor;
+		typedef std::map<rho::String, rho::String>::const_iterator it_type;
+		for (it_type iterator = propertyMap.begin(); iterator != propertyMap.end(); iterator++)
+		{
+			comparitor.assign(iterator->first);
+			std::transform(comparitor.begin(), comparitor.end(), comparitor.begin(), ::tolower);
+			if (comparitor.compare("frequency") == 0)
+				iFrequency = atoi(iterator->second.c_str());
+			else if (comparitor.compare("volume") == 0)
+				iVolume = atoi(iterator->second.c_str());
+			else if (comparitor.compare("duration") == 0)
+				iDuration = atoi(iterator->second.c_str());
+			else
+				LOG(WARNING) + "Unrecognised parameter passed to beep: " + iterator->first;
+		}
+		BOOL bBeepSuccess = FALSE;
+		if (m_pNotifications)
+			bBeepSuccess = m_pNotifications->Beep(iFrequency, iVolume, iDuration);
+		if (!bBeepSuccess)
+			LOG(WARNING) + "Unable to use the device beeper";
 	}
 
     virtual void vibrate(int duration, CMethodResult& oResult)
 	{
+#if defined(OS_WINCE)
+		BOOL bVibrateSuccess = FALSE;
+		if (m_pNotifications)
+			bVibrateSuccess = m_pNotifications->Vibrate(duration);
+		if (!bVibrateSuccess)
+			LOG(WARNING) + "Unable to initiate the device vibrate function";
+#else
+		//  This call is having no effect on Moto Devices, they use Ntfy API (directly above)
 		alert_vibrate(duration);
+#endif
+
 	}
+
+private:
+	CNotificationLoader* m_pNotifications;
+	BOOL m_bNotificationsLoaded;
 };
 
 class CNotificationFactory: public CNotificationFactoryBase
