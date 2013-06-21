e4bc84853c244df7c0f3f67b1acf79570a68ec4d
garethlewis <gareth_lewis2@mac.com>
2013-06-21 10:59:02 +0100

    Changed init file so that we initialize the camera and videocapture extensions.

--- lib/commonAPI/mediacapture/ext/shared/MediacaptureInit.cpp
+++ lib/commonAPI/mediacapture/ext/shared/MediacaptureInit.cpp
@@ -1,8 +1,7 @@
 #include "common/RhodesApp.h"
 
 extern "C" void Init_Camera_API();
-extern "C" void Init_Videocapture_API();
-extern "C" void Init_Videocapture();
+extern "C" void Init_Videocapture_extension();
 
 extern "C" void Init_Mediacapture_extension()
 {
@@ -10,5 +9,6 @@ extern "C" void Init_Mediacapture_extension()
 #ifndef RHO_NO_RUBY_API
     RHODESAPP().getExtManager().requireRubyFile("RhoCameraApi");
 #endif
-	Init_Videocapture();
+	Init_Videocapture_extension();
+	LOG(INFO) + __FUNCTION__ + " Loading Videocapture extension";
 }
