7db0d8b35a52f264d8397f875692db3382976865
Mikhail Malinin <malinin.work@gmail.com>
2013-06-23 14:49:16 +0400

    commonAPI: notification, add project files and generated stubs

---
+++ lib/commonAPI/coreapi/ext/platform/iphone/impl/NotificationSingleton.h
@@ -0,0 +1,20 @@
+
+#import "INotification.h"
+#import "NotificationSingletonBase.h"
+
+@interface NotificationSingleton : NotificationSingletonBase<INotificationSingleton> {
+}
+
+
+
+-(void) showPopup:(NSDictionary*)propertyMap methodResult:(id<IMethodResult>)methodResult;
+-(void) hidePopup;
+-(void) showStatus:(NSString*)title status_text:(NSString*)status_text hide_button_label:(NSString*)hide_button_label;
+-(void) playFile:(NSString*)path media_type:(NSString*)media_type;
+-(void) beep:(NSDictionary*)propertyMap;
+-(void) vibrate:(int)duration;
+
+
+
+
+@end
---
+++ lib/commonAPI/coreapi/ext/platform/iphone/impl/NotificationSingleton.m
@@ -0,0 +1,36 @@
+
+#import "NotificationSingleton.h"
+
+
+@implementation NotificationSingleton
+
+
+
+-(void) showPopup:(NSDictionary*)propertyMap methodResult:(id<IMethodResult>)methodResult {
+
+}
+
+-(void) hidePopup {
+
+}
+
+-(void) showStatus:(NSString*)title status_text:(NSString*)status_text hide_button_label:(NSString*)hide_button_label {
+
+}
+
+-(void) playFile:(NSString*)path media_type:(NSString*)media_type {
+
+}
+
+-(void) beep:(NSDictionary*)propertyMap {
+
+}
+
+-(void) vibrate:(int)duration {
+
+}
+
+
+
+
+@end
