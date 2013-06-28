1f863a9f04172ab338da804360c30456c013a755
anton vishnevskiy <antonv@rhomobile.com>
2013-06-28 19:05:57 +0400

    fix's for pure JS application, WM platform

--- res/generators/templates/api/shared/generated/montana_api_init.cpp
+++ res/generators/templates/api/shared/generated/montana_api_init.cpp
@@ -3,11 +3,14 @@
 extern "C" void Init_RubyAPI_<%= $cur_module.name %>();
 extern "C" void Init_JSAPI_<%= $cur_module.name %>();
 
+extern "C" int  rho_ruby_is_started();
+
 extern "C" void Init_<%= $cur_module.name %>_API()
 {
-#ifndef RHO_NO_RUBY_API
-    Init_RubyAPI_<%= $cur_module.name %>();
-#endif
+    if (rho_ruby_is_started())
+    {
+    	Init_RubyAPI_<%= $cur_module.name %>();
+    }
 
 #ifndef RHO_NO_JS_API
     Init_JSAPI_<%= $cur_module.name %>();
