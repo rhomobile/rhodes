9c1467efb2eb88512f04834e25f20107e5dd5a0f
genywind <evgeny@rhomobile.com>
2013-08-02 17:25:39 +0400

    rakefile: support minify js and css

--- lib/build/jake.rb
+++ lib/build/jake.rb
@@ -747,4 +747,28 @@ class Jake
     
     false
   end
+  
+  def self.getBuildProp2(propName, propName2, config_yml=$app_config)
+    res = nil
+    if config_yml[propName]
+        res = config_yml[propName]
+        res = res[propName2] if res
+    elsif config_yml[$current_platform]
+        res = config_yml[$current_platform][propName]        
+        res = res[propName2] if res
+    end    
+    
+    res
+  end
+
+  def self.getBuildBoolProp2(propName, propName2, config_yml=$app_config, def_value=false)
+    res = getBuildProp2(propName, propName2)
+    
+    return def_value unless res
+     
+    return true if res && (res.to_i() != 0 || res.casecmp("true") == 0 || res.casecmp("yes") == 0 )
+    
+    false
+  end
+  
 end
