a6b8b2b4622c9cad032b1aed88c77dae2f1f4f26
genywind <evgeny@rhomobile.com>
2013-08-06 11:35:27 +0400

    Win32: fix build

--- lib/commonAPI/coreapi/ext/platform/wm/src/SystemImpl.cpp
+++ lib/commonAPI/coreapi/ext/platform/wm/src/SystemImpl.cpp
@@ -272,7 +272,7 @@ void CSystemImpl::getPhoneId(CMethodResult& oResult)
             _toHexString( rgDeviceId[i], strDeviceID, 16);
         }
     }
-#else
+#elif defined( OS_PLATFORM_MOTCE )
     wchar_t buffer[256];    
     BOOL ret = SystemParametersInfoW( SPI_GETOEMINFO, sizeof(buffer), buffer, 0);
 
