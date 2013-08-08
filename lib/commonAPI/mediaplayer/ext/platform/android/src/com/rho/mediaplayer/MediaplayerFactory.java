533fa1a19c58d2eab28f18537803e00356a1348a
Ben Kennedy <ben.kennedy@motorolasolutions.com>
2013-08-08 15:13:43 +0100

    Android: MediaPlayer: Added error checking, resource cleanup etc to fix: 107420, 107436

--- lib/commonAPI/mediaplayer/ext/platform/android/src/com/rho/mediaplayer/MediaplayerFactory.java
+++ lib/commonAPI/mediaplayer/ext/platform/android/src/com/rho/mediaplayer/MediaplayerFactory.java
@@ -1,18 +1,55 @@
 package com.rho.mediaplayer;
 
+import android.view.KeyEvent;
+
 import com.rhomobile.rhodes.api.RhoApiFactory;
 
-public class MediaplayerFactory
-        extends RhoApiFactory< Mediaplayer, MediaplayerSingleton>
-        implements IMediaplayerFactory {
+public class MediaplayerFactory extends RhoApiFactory<Mediaplayer, MediaplayerSingleton> implements IMediaplayerFactory
+{
+	private MediaplayerSingleton singleton;
 
-    @Override
-    protected MediaplayerSingleton createSingleton() {
-        return new MediaplayerSingleton(this);
-    }
+	public MediaplayerFactory()
+	{
+		super();
+		MediaplayerRhoListener.registerListener(this);
+	}
 
-    @Override
-    protected Mediaplayer createApiObject(String id) {
-        return new Mediaplayer(id);
-    }
+	@Override
+	protected MediaplayerSingleton createSingleton()
+	{
+		if(singleton == null) singleton = new MediaplayerSingleton(this); 
+		return singleton;
+	}
+
+	@Override
+	protected Mediaplayer createApiObject(String id)
+	{
+		return new Mediaplayer(id);
+	}
+
+	public void onResume()
+	{
+		if(singleton != null) singleton.onResume();
+	}
+
+	public void onPause()
+	{
+		if(singleton != null) singleton.onPause();
+	}
+
+	public void onDestroy()
+	{
+		if(singleton != null) singleton.onDestroy();
+	}
+
+	public void onStop()
+	{
+		if(singleton != null) singleton.onStop();
+	}
+
+	public boolean onKey(int keyCode, KeyEvent event)
+	{
+		if(singleton != null) return singleton.onKey(keyCode, event);
+		return false;
+	}
 }
