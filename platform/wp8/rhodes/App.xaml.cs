bdd0a73abae9a263aa1592fd3585f1ccea4a41cd
rognar <kostyar2004@bk.ru>
2013-09-17 17:42:18 +0100

    added comment to application_closing handler

--- platform/wp8/rhodes/App.xaml.cs
+++ platform/wp8/rhodes/App.xaml.cs
@@ -112,7 +112,6 @@ namespace rhodes
         // This code will not execute when the application is first launched
         private void Application_Activated(object sender, ActivatedEventArgs e)
         {
-            // TODO: add onActivate call
             rhoruntime.CRhoRuntime.getInstance().onActivate(1);
         }
 
@@ -120,15 +119,15 @@ namespace rhodes
         // This code will not execute when the application is closing
         private void Application_Deactivated(object sender, DeactivatedEventArgs e)
         {
-            // TODO: add onActivate call
             rhoruntime.CRhoRuntime.getInstance().DestroyUi();
         }
 
         // Code to execute when the application is closing (eg, user hit Back)
         // This code will not execute when the application is deactivated
+        // in this case we have to send app to background, we shouldnt close it
+        // confirmed by Evgeniy
         private void Application_Closing(object sender, ClosingEventArgs e)
         {
-            // TODO: add onActivate call ?
             rhoruntime.CRhoRuntime.getInstance().DestroyUi();
         }
 
