8c809dbf87b13a9cc474ada69b48ca6786ddb41f
Albert R. Timashev <albert@rhomobile.com>
2013-10-17 21:20:34 +0400

    rhosimulator/win32: build extensions with QMake (part 1)

--- lib/commonAPI/coreapi/ext/build.bat
+++ lib/commonAPI/coreapi/ext/build.bat
@@ -13,6 +13,14 @@ rake --trace
 
 )
 
+if "%RHO_PLATFORM%" == "win32" (
+
+call "%VS100COMNTOOLS%..\..\VC\vcvarsall.bat" x86
+cd platform\osx
+rake --trace
+
+)
+
 if "%RHO_PLATFORM%" == "bb" (
 
 cd platform\bb
