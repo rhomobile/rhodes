142b7ea5b6cbbab999a61e0db7148cccab3f46b1
Albert R. Timashev <albert@rhomobile.com>
2013-11-05 22:20:57 +0400

    rhosimulator/win32: build with Visual Studio 2012

--- platform/shared/qt/rhosimulator_win32_build.bat
+++ platform/shared/qt/rhosimulator_win32_build.bat
@@ -3,10 +3,10 @@ setlocal
 
 cd /d "%~dp0"
 
-call "%VS100COMNTOOLS%..\..\VC\vcvarsall.bat" x86
+call "%VS110COMNTOOLS%..\..\VC\vcvarsall.bat" x86
 if ERRORLEVEL 1 exit /b %ERRORLEVEL%
 
-"%QTDIR%\bin\qmake" -o Makefile -r -spec win32-msvc2010 "CONFIG-=debug" "CONFIG+=release" "%1" RhoSimulator.pro
+"%QTDIR%\bin\qmake" -o Makefile -r -spec win32-msvc2012 "CONFIG-=debug" "CONFIG+=release" "%1" RhoSimulator.pro
 if ERRORLEVEL 1 exit /b %ERRORLEVEL%
 
 nmake clean
