724ab8ba05ff0df553c19f7af382d2d911a875c8
Alex Epifanoff <alex.epifanoff@gmail.com>
2013-11-27 18:46:03 +0400

    updating WS interface

--- lib/commonAPI/websocket/ext/platform/android/src/com/websocket/HybiParser.java
+++ lib/commonAPI/websocket/ext/platform/android/src/com/websocket/HybiParser.java
@@ -28,7 +28,7 @@
 // OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
 // WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-package com.rho.websocket;
+package com.websocket;
 
 import android.util.Log;
 
