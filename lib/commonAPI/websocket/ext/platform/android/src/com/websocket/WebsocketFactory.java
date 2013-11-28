256a8dfdfc541b14e7a64cf32470fa6944d60632
Alex Epifanoff <alex.epifanoff@gmail.com>
2013-11-28 19:36:56 +0400

    update to new API

--- lib/commonAPI/websocket/ext/platform/android/src/com/websocket/WebsocketFactory.java
+++ lib/commonAPI/websocket/ext/platform/android/src/com/websocket/WebsocketFactory.java
@@ -20,41 +20,10 @@ public class WebsocketFactory
         return new WebsocketSingleton(this);
     }
             
-//    private int mObjId = 1;
-//    Map<String,Websocket> mObjectMap = new HashMap<String,Websocket>();
-/*
-    private String getNextId() {
-        return String.valueOf(mObjId++);
-    }
-*/            
-/*
-    public String create(socketFactory.create: " + id );
-    
-        String id = getNextId();
-
-        Websocket obj = new Websocket(this,id);
-        mObjectMap.put(id,obj);
-        return id;
-    }
-    */
-/*
-    public void destroy(Websocket ws) {
-        Logger.I(TAG,"WebsocketFactory.destroy");
-
-        mObjectMap.remove(ws.getId());
-    }
-*/
     @Override
     protected Websocket createApiObject(String id) {
         Logger.I(TAG,"WebsocketFactory.createApiObject: " + id);
         
         return new Websocket(this,id/*,url*/);
-        /*
-        if ( !mObjectMap.containsKey(id) ) {
-            create(id);
-        }
-            
-        return mObjectMap.get(id);
-        */
     }
 }
