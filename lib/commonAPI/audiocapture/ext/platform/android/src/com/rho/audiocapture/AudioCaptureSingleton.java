04b937e4f3ea782ce5f39cf95044f24dc10b072b
Alexey Tikhvinsky <alexey@rhomobile.com>
2013-12-03 01:50:39 +0400

    AudioCapture properties

--- lib/commonAPI/audiocapture/ext/platform/android/src/com/rho/audiocapture/AudioCaptureSingleton.java
+++ lib/commonAPI/audiocapture/ext/platform/android/src/com/rho/audiocapture/AudioCaptureSingleton.java
@@ -8,12 +8,42 @@ class AudioCaptureSingleton  implements IAudioCaptureSingleton {
 
 
     @Override
-    public void start(IMethodResult res) {
+    public void start(String path, IMethodResult res) {
     }
     @Override
     public void stop(IMethodResult res) {
     }
     @Override
     public void cancel(IMethodResult res) {
+    }
+
+
+    @Override
+    public void getSource(IMethodResult result) {
+    }
+
+
+    @Override
+    public void setSource(String source, IMethodResult result) {
+    }
+
+
+    @Override
+    public void getEncoder(IMethodResult result) {
+    }
+
+
+    @Override
+    public void setEncoder(String encoder, IMethodResult result) {
+    }
+
+
+    @Override
+    public void getMaxDuration(IMethodResult result) {
+    }
+
+
+    @Override
+    public void setMaxDuration(int maxDuration, IMethodResult result) {
     }
 }
