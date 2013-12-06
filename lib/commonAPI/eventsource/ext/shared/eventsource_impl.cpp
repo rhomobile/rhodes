f98dd86dcb61fa87f7b83dfaf3a3dec6df567a50
Alex Epifanoff <alex.epifanoff@gmail.com>
2013-12-06 18:06:59 +0400

    in the middle

--- lib/commonAPI/eventsource/ext/shared/eventsource_impl.cpp
+++ lib/commonAPI/eventsource/ext/shared/eventsource_impl.cpp
@@ -30,18 +30,73 @@ namespace rho {
     {
         rho::common::CAutoPtr<EventSource> m_es;
         
-//        rho::apiGenerator::IMethodResult* m_messageCallback;
+        rho::apiGenerator::CMethodResult* m_onOpenCallback;
+        rho::apiGenerator::CMethodResult* m_onMessageCallback;
+        rho::apiGenerator::CMethodResult* m_onErrorCallback;
+
+        
     public:
+        CEventSourceImpl() :
+            m_onErrorCallback(0), m_onMessageCallback(0), m_onOpenCallback(0)
+        {
+        }
+        
         virtual ~CEventSourceImpl() {}
         
-        virtual void create( const rho::String& url, rho::apiGenerator::CMethodResult& oResult) {
+        virtual void setOnopen(rho::apiGenerator::CMethodResult& oResult) {
+            LOG(INFO) + "CEventSourceImpl::setOnOpen";
+            m_onOpenCallback = &oResult;
+        }
+
+        virtual void setOnmessage(rho::apiGenerator::CMethodResult& oResult) {
+            LOG(INFO) + "CEventSourceImpl::setOnmessage";
+            m_onMessageCallback = &oResult;
+        }
+        
+        virtual void setOnerror(rho::apiGenerator::CMethodResult& oResult) {
+            LOG(INFO) + "CEventSourceImpl::setOnerror";
+            m_onErrorCallback = &oResult;
+        }
+        
+        virtual void getUrl(rho::apiGenerator::CMethodResult& oResult) {
+            LOG(INFO) + "CEventSourceImpl::getUrl";
+            oResult.set("");            
+        }
+        
+        virtual void getReadyState(rho::apiGenerator::CMethodResult& oResult) {
+            LOG(INFO) + "CEventSourceImpl::getReadyState";
+            oResult.set("");
+        }
+        
+        virtual void getWithCredentials(rho::apiGenerator::CMethodResult& oResult) {
+            LOG(INFO) + "CEventSourceImpl::getWithCredentials";
+            oResult.set("");
+        }
+        
+        virtual void create( const rho::String& url,  const rho::Hashtable<rho::String, rho::String>& settings, rho::apiGenerator::CMethodResult& oResult) {
+        
             LOG(INFO) + "CEventSourceImpl::create: + " + url;
             m_es = EventSource::create(url, this);
         }
         
-        virtual void onOpen() {}
-        virtual void onError( const String& error ) {}
-        virtual void onMessage( const String& message) {}
+        virtual void close(rho::apiGenerator::CMethodResult& oResult) {
+            LOG(INFO) + "CEventSourceImpl::close";
+        }
+        
+    private:
+    
+        virtual void onOpen() {
+            LOG(INFO) + "CEventSourceImpl::onOpen";
+        }
+        
+        virtual void onError(const String&) {
+            LOG(INFO) + "CEventSourceImpl::onError";
+        }
+        
+        virtual void onMessage(const String&) {
+            LOG(INFO) + "CEventSourceImpl::onMessage";
+        }
+
     };
     
     ////////////////////////////////////////////////////////////////////////
