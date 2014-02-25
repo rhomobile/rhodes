64747eb5fcaa574c510b005bf8f8dd0e8468602a
Sunil Kumar <FVC683@motorolasolutions.com>
2014-02-25 21:00:55 +0530

    Added fix for SR#EMBPD00123754
    Added fix for SR#EMBPD00123754

--- platform/wm/rhodes/MainWindow.cpp
+++ platform/wm/rhodes/MainWindow.cpp
@@ -71,6 +71,9 @@ extern "C" LRESULT rho_wmimpl_draw_splash_screen(HWND hWnd);
 rho::IBrowserEngine* rho_wmimpl_createBrowserEngine(HWND hwndParent);
 bool Rhodes_WM_ProcessBeforeNavigate(LPCTSTR url);
 
+//New declaration for SR#EMBPD00123754
+bool m_SuspendedThroughPowerButton = false;
+
 using namespace rho::common;
 using namespace rho;
 using namespace stdext;
@@ -666,6 +669,34 @@ LRESULT CMainWindow::OnWindowMinimized (UINT /*uMsg*/, WPARAM /*wParam*/, LPARAM
     return 0;
 }
 
+bool GetPowerButtonPressedValue(HWND hwnd)
+{
+	bool powerButtonPressed=false;
+	wchar_t szBuf[200];
+	if(hwnd!=NULL)
+	{
+		//LOG(INFO) + "Inside if(hwnd!=NULL)";
+		GetWindowText(hwnd,szBuf,199);
+		//LOG(INFO) + "After GetWindowText";
+		LOG(INFO) + szBuf;
+	}
+	if(0==wcscmp(szBuf,L"PowerKey Action"))
+	{
+		//LOG(INFO) + "Before powerButtonPressed=true;";
+		powerButtonPressed=true;
+		m_SuspendedThroughPowerButton=true;
+		//LOG(INFO) + "After m_SuspendedThroughPowerButton=true;";
+	}
+	else
+	{
+		
+		//LOG(INFO) + "Before powerButtonPressed=False;";
+		powerButtonPressed=false;
+	}
+	return powerButtonPressed;
+}
+
+
 LRESULT CMainWindow::OnActivate(UINT /*uMsg*/, WPARAM wParam, LPARAM lParam, BOOL& /*bHandled*/)
 {
     int fActive = LOWORD(wParam);
@@ -693,8 +724,38 @@ LRESULT CMainWindow::OnActivate(UINT /*uMsg*/, WPARAM wParam, LPARAM lParam, BOO
         return 0;
     }
 
-    ProcessActivate( fActive, wParam, lParam );    
-    return 0;
+
+
+
+
+
+	if(m_SuspendedThroughPowerButton==true)//Activation is due to Suspend Through PowerButton and resume
+	{
+	 m_SuspendedThroughPowerButton=false;
+	 //LOG(INFO) + "Activation is due to powerbutton press suspend and resume.Skip it";
+	 return 0;
+	}
+	
+	HWND currentForeGroundWindowHandle;
+	//LOG(INFO) + "Before GetForegroundWindow";
+	currentForeGroundWindowHandle = GetForegroundWindow();
+	//LOG(INFO) + "After GetForegroundWindow";
+	bool powerButtonPressed = GetPowerButtonPressedValue(currentForeGroundWindowHandle);
+	
+
+
+	if(true ==powerButtonPressed)//Deactivation is not due to Suspend Through PowerButton
+	{
+	 //LOG(INFO) + "Deactivation is due to powerbutton press.Skip it";
+	 
+	}
+	else
+	{
+		//LOG(INFO) + "Before ProcessActivate";
+		ProcessActivate( fActive, wParam, lParam );
+	}
+   
+	return 0;
 }
 
 void CMainWindow::ProcessActivate( BOOL fActive, WPARAM wParam, LPARAM lParam )
