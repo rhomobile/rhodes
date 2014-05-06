85e40a32810603a5827fbc6d244efc86f6469979
dmitrys <dmitrys@rhomobile.com>
2014-05-06 22:38:10 +0400

    tune AudioCapture API XML and add iOS implementation

--- lib/commonAPI/audiocapture/ext/platform/iphone/impl/Audiocapture.h
+++ lib/commonAPI/audiocapture/ext/platform/iphone/impl/Audiocapture.h
@@ -1,14 +1,43 @@
 
-#import "IAudiocapture.h"
-#import "AudiocaptureBase.h"
+#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
+#import <AVFoundation/AVFoundation.h>
+#import <AVFoundation/AVAudioSession.h>
 
-@interface Audiocapture : AudiocaptureBase<IAudiocapture> {
+#import "IAudioCapture.h"
+#import "AudioCaptureBase.h"
+
+@interface AudioCapture : AudioCaptureBase<IAudioCapture,AVAudioRecorderDelegate> {
+    
+    id<IMethodResult> callback;
+    NSString* destination;
+    BOOL isSaveFile;
+    int maxDuration;
+    BOOL isStarted;
+    
+    AVAudioRecorder* recorder;
+    NSString* recorderFilePath;
+    NSMutableDictionary* recordSetting;
 }
 
--(void) getPlatformName:(id<IMethodResult>)methodResult;
--(void) calcSumm:(int)a b:(int)b methodResult:(id<IMethodResult>)methodResult;
--(void) joinStrings:(NSString*)a b:(NSString*)b methodResult:(id<IMethodResult>)methodResult;
+@property (readwrite, retain) id<IMethodResult> callback;
+@property (readwrite, retain) NSString *destination;
+@property (readwrite, retain) NSString *recorderFilePath;
 
+
+-(int) getMaxDurationWithHash:(NSDictionary*)options;
+-(NSString*) getPreparedFileNameWithHash:(NSDictionary*)options;
+-(NSString*) getPreparedFileNameWithName:(NSString*)name;
+
+-(void) start:(NSDictionary*)props methodResult:(id<IMethodResult>)methodResult;
+-(void) stop:(id<IMethodResult>)methodResult;
+-(void) cancel:(id<IMethodResult>)methodResult;
+
+- (void)audioRecorderDidFinishRecording:(AVAudioRecorder *) aRecorder successfully:(BOOL)flag;
+
+-(BOOL)exportAssetAsWaveFormat:(NSString*)filePath outPath:(NSString*)outPath;
+
+- (void) fireCallback:(NSString*)status param_name:(NSString*)param_name param_value:(NSString*)param_value;
 
 
 @end
