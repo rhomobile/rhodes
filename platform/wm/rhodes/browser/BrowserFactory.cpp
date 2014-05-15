31d7817ef630535dd883daf92fe70cd038620238
Anton Vishnevski <antonv@rhomobile.com>
2014-05-16 00:31:51 +0400

    new ie: fix project

--- platform/wm/rhodes/browser/BrowserFactory.cpp
+++ platform/wm/rhodes/browser/BrowserFactory.cpp
@@ -24,13 +24,13 @@ IBrowserFactory* BrowserFactory::getInstance()
 
 IBrowserEngine* BrowserFactory::create(HWND hwndParent)
 {
-//#if defined(APP_BUILD_CAPABILITY_WEBKIT_BROWSER)
-//    return rho_wmimpl_get_webkitBrowserEngine(hwndParent, rho_wmimpl_get_appinstance());
-//#elif defined(OS_PLATFORM_MOTCE)
-    //return new CEBrowserEngine(hwndParent, rho_wmimpl_get_appinstance());
-//#else
+#if defined(APP_BUILD_CAPABILITY_WEBKIT_BROWSER)
+    return rho_wmimpl_get_webkitBrowserEngine(hwndParent, rho_wmimpl_get_appinstance());
+#elif defined(OS_PLATFORM_MOTCE)
+    return new CEBrowserEngine(hwndParent, rho_wmimpl_get_appinstance());
+#else
     return CIEBrowserEngine::getInstance(hwndParent, rho_wmimpl_get_appinstance());
-//#endif //APP_BUILD_CAPABILITY_WEBKIT_BROWSER
+#endif //APP_BUILD_CAPABILITY_WEBKIT_BROWSER
 }
 
 }
