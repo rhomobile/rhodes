49eaa8d74522881d6909aac8954fc815d214eb59
Anton Vishnevski <antonv@rhomobile.com>
2014-05-19 21:47:47 +0400

    new ie: add getter for type of browser

--- platform/wm/rhodes/browser/BrowserFactory.cpp
+++ platform/wm/rhodes/browser/BrowserFactory.cpp
@@ -24,13 +24,25 @@ IBrowserFactory* BrowserFactory::getInstance()
 
 IBrowserEngine* BrowserFactory::create(HWND hwndParent)
 {
-#if defined(APP_BUILD_CAPABILITY_WEBKIT_BROWSER)
-    return rho_wmimpl_get_webkitBrowserEngine(hwndParent, rho_wmimpl_get_appinstance());
-#elif defined(OS_PLATFORM_MOTCE)
-    return new CEBrowserEngine(hwndParent, rho_wmimpl_get_appinstance());
-#else
+//#if defined(APP_BUILD_CAPABILITY_WEBKIT_BROWSER)
+//    return rho_wmimpl_get_webkitBrowserEngine(hwndParent, rho_wmimpl_get_appinstance());
+//#elif defined(OS_PLATFORM_MOTCE)
+//    return new CEBrowserEngine(hwndParent, rho_wmimpl_get_appinstance());
+//#else
     return CIEBrowserEngine::getInstance(hwndParent, rho_wmimpl_get_appinstance());
-#endif //APP_BUILD_CAPABILITY_WEBKIT_BROWSER
+//#endif //APP_BUILD_CAPABILITY_WEBKIT_BROWSER
+}
+
+//
+IBrowserFactory::EBrowserEngine BrowserFactory::getCurrentBrowserType()
+{
+    //#if defined(APP_BUILD_CAPABILITY_WEBKIT_BROWSER)
+    //    return IBrowserFactory::eWebkit;
+    //#elif defined(OS_PLATFORM_MOTCE)
+    //    return IBrowserFactory::eIE_CE;
+    //#else
+        return IBrowserFactory::eIE_WM;
+    //#endif 
 }
 
 }
