17a55e05d0391d173a78262412b471a4d8d82836
Malaya <nfx846@motorolasolutions.com>
2014-06-12 11:18:04 +0530

    Update ConnectionCheckingFactory.java
    For SIngleton make changes

--- lib/commonAPI/ConnectionChecking/ext/platform/android/src/com/rho/connectionchecking/ConnectionCheckingFactory.java
+++ lib/commonAPI/ConnectionChecking/ext/platform/android/src/com/rho/connectionchecking/ConnectionCheckingFactory.java
@@ -2,17 +2,21 @@ package com.rho.connectionchecking;
 
 import com.rhomobile.rhodes.api.RhoApiFactory;
 
-public class ConnectionCheckingFactory
-        extends RhoApiFactory< ConnectionChecking, ConnectionCheckingSingleton>
-        implements IConnectionCheckingFactory {
+public class ConnectionCheckingFactory implements IConnectionCheckingFactory {
 
-    @Override
-    protected ConnectionCheckingSingleton createSingleton() {
-        return new ConnectionCheckingSingleton(this);
-    }
+	private ConnectionCheckingSingleton singleton;
 
-    @Override
-    protected ConnectionChecking createApiObject(String id) {
-        return new ConnectionChecking(id);
-    }
+	@Override
+	public
+	IConnectionCheckingSingleton getApiSingleton() {
+		if(singleton == null) singleton = new ConnectionCheckingSingleton(this);
+		return singleton;
+	}
+
+	@Override
+	public
+	IConnectionChecking getApiObject(String id) {
+		
+		return null;
+	}
 }
