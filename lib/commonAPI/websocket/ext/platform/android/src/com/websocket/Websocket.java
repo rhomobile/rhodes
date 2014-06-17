50999ff97799b7acb6553b21bd62ffe93fe90978
Alex Epifanoff <alex.epifanoff@gmail.com>
2014-06-17 21:36:31 +0400

    websocket: fix sync issues, pending open/error/close events will be fired when corresponding callback is set

--- lib/commonAPI/websocket/ext/platform/android/src/com/websocket/Websocket.java
+++ lib/commonAPI/websocket/ext/platform/android/src/com/websocket/Websocket.java
@@ -29,6 +29,13 @@ public class Websocket extends WebsocketBase implements IWebsocket {
     private WebsocketFactory mFactory;
     private String mURL;
     
+    private boolean mOpenEventPending = false;
+    private boolean mErrorEventPending = false;
+    private Exception mErrorPending;
+    private boolean mCloseEventPending = false;
+    private int mCloseCodePending;
+    private String mCloseReasonPending;
+    
     public Websocket(WebsocketFactory factory, String id) {
         super(id);
         
@@ -36,24 +43,41 @@ public class Websocket extends WebsocketBase implements IWebsocket {
         mFactory = factory;
     }
     
-    public void setOnopen(IMethodResult result) {
+    synchronized public void setOnopen(IMethodResult result) {
         Logger.I(TAG,"Websocket setOnopen");
         mOnOpen = result;
+        if ( mOpenEventPending && ( mOnOpen != null ) && ( mOnOpen.hasCallback() ) ) {
+            Logger.I(TAG,"Pending open event, calling callback immediately");
+            mOpenEventPending = false;
+            dispatchOpenEvent();
+        }
     }
     
-    public void setOnmessage(IMethodResult result) {
+    synchronized public void setOnmessage(IMethodResult result) {
         Logger.I(TAG,"Websocket setOnmessage");
         mOnMessage = result;
     }
     
-    public void setOnerror(IMethodResult result) {
+    synchronized public void setOnerror(IMethodResult result) {
         Logger.I(TAG,"Websocket setOnerror");
         mOnError = result;
+        if ( mErrorEventPending && ( mOnError != null ) && ( mOnError.hasCallback() ) ) {
+            Logger.I(TAG,"Pending error event, calling callback immediately");
+
+            mErrorEventPending = false;
+            dispatchErrorEvent(mErrorPending);
+        }
     }
     
-    public void setOnclose(IMethodResult result) {
+    synchronized public void setOnclose(IMethodResult result) {
         Logger.I(TAG,"Websocket setOnclose");
         mOnClose = result;
+        if ( mCloseEventPending && ( mOnClose != null ) && ( mOnClose.hasCallback() ) ) {
+            Logger.I(TAG,"Pending close event, calling callback immediately");
+
+            mCloseEventPending = false;
+            dispatchCloseEvent(mCloseCodePending,mCloseReasonPending);
+        }
     }
     
     public void getUrl(IMethodResult result) {
@@ -86,47 +110,57 @@ public class Websocket extends WebsocketBase implements IWebsocket {
         mClient = new WebSocketClient(URI.create(url),new WebSocketClient.Listener() {
             @Override
             public void onConnect() {
-                mState = STATE_OPEN;
             
-                if ( mOnOpen != null ) {
-                    Map<String,Object> result = new HashMap<String,Object>();
-                    mOnOpen.set(result);
+                synchronized( Websocket.this ) {
+                    mState = STATE_OPEN;
+                    
+                    if ( (mOnOpen != null) && (mOnOpen.hasCallback() ) ) {
+                        dispatchOpenEvent();
+                    } else {
+                        mOpenEventPending = true;
+                    }
                 }
             }
             
             @Override
             public void onMessage(String message) {
-                if ( mOnMessage != null ) {
-                    Map<String,Object> result = new HashMap<String,Object>();
-                    result.put("data",message);
-                    mOnMessage.set(result);
+                synchronized( Websocket.this ) {
+                    dispatchMessageEvent(message);
                 }
             }
             
             @Override
             public void onMessage(byte[] data) {
-                if ( mOnMessage != null ) {
-                    Map<String,Object> result = new HashMap<String,Object>();
-                    result.put("data",new String(data));
-                    mOnMessage.set(result);
+                synchronized( Websocket.this ) {
+                    dispatchMessageEvent(new String(data));
                 }
             }
                       
             @Override
             public void onDisconnect(int code, String reason) {
-                mState = STATE_CLOSED;
-                
-                if ( mOnClose != null ) {
-                    Map<String,Object> result = new HashMap<String,Object>();
-                    result.put("code",code);
-                    result.put("reason",reason);
-                    mOnClose.set(result);
+                synchronized( Websocket.this ) {
+                    mState = STATE_CLOSED;
+                    
+                    if ( ( mOnClose != null ) && ( mOnClose.hasCallback() ) ) {
+                        dispatchCloseEvent(code,reason);
+                    } else {
+                        mCloseEventPending = true;
+                        mCloseCodePending = code;
+                        mCloseReasonPending = reason;
+                    }
                 }
             }
                       
             @Override
             public void onError(Exception error) {
-                reportError(error);
+                synchronized( Websocket.this ) {
+                    if ( ( mOnError != null ) && ( mOnError.hasCallback() ) ) {
+                        dispatchErrorEvent(error);
+                    } else {
+                        mErrorEventPending = true;
+                        mErrorPending = error;
+                    }
+                }
             }
             },
             null
@@ -135,7 +169,7 @@ public class Websocket extends WebsocketBase implements IWebsocket {
         connect(result);
     }
 
-    public void connect( IMethodResult result ) {
+    private void connect( IMethodResult result ) {
         mState = STATE_CONNECTING;
 
         try {
@@ -147,18 +181,18 @@ public class Websocket extends WebsocketBase implements IWebsocket {
     
 
     @Override
-    public void close(IMethodResult result) {
+    synchronized public void close(IMethodResult result) {
         mState = STATE_CLOSING;
         
         try {
-            mClient.disconnect();
+            mClient.disconnect("User request.");
         } catch (Exception e) {
             result.setError( e.getMessage() );
         }
     }
     
     @Override
-    public void send( String message, IMethodResult result) {
+    synchronized public void send( String message, IMethodResult result) {
         try {
             mClient.send(message);
         } catch (Exception e) {
@@ -166,13 +200,37 @@ public class Websocket extends WebsocketBase implements IWebsocket {
         }
     }
     
-    private void reportError(Exception e) {
+    private void dispatchErrorEvent(Exception e) {
         Logger.E(TAG,"Websocket error: " + e.getMessage());
 
         if ( mOnError != null ) {
             Map<String,Object> result = new HashMap<String,Object>();
             result.put("message",e.getMessage());
             mOnError.set(result);
+        }
+    }
+    
+    private void dispatchOpenEvent() {
+        if ( mOnOpen != null ) {
+            Map<String,Object> result = new HashMap<String,Object>();
+            mOnOpen.set(result);
+        }
+    }
+    
+    private void dispatchCloseEvent(int code, String reason) {
+        if ( mOnClose != null ) {
+            Map<String,Object> result = new HashMap<String,Object>();
+            result.put("code",code);
+            result.put("reason",reason);
+            mOnClose.set(result);
+        }
+    }
+    
+    private void dispatchMessageEvent( String message ) {
+        if ( mOnMessage != null ) {
+            Map<String,Object> result = new HashMap<String,Object>();
+            result.put("data",message);
+            mOnMessage.set(result);
         }
     }
 }
