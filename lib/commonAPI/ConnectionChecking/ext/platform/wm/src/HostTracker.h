b49121a3cbd1ae42034e028da569dafee852d1ae
fgqw68 <sabir.vt@motorolasolutions.com>
2014-06-19 13:24:00 +0530

    code after addressing all comments

--- lib/commonAPI/ConnectionChecking/ext/platform/wm/src/HostTracker.h
+++ lib/commonAPI/ConnectionChecking/ext/platform/wm/src/HostTracker.h
@@ -3,7 +3,22 @@
 
 #include "..\..\..\..\..\coreapi\ext\platform\wm\src\NetworkDetect.h"
 
+const UINT WM_ON_CONNECTION_BOX   = WM_USER + 201;
 
+typedef enum 
+{
+	eCancelEventIndex=0,
+	eLicenseScreenPopupEventIndex,
+	eLicenseScreenHidesEventIndex,
+	eNavigateCompleteEventIndex,
+	eMaxEventCount
+}eEventIndex;
+typedef enum 
+{
+	eShowConnectionBox =0,
+	eHideConnectionBox,
+	eNone      
+}eConnectionBoxMode;
 
 class CHostTracker :  public CNetworkDetection
 {
@@ -12,35 +27,39 @@ public:
 	CHostTracker();
 	virtual ~CHostTracker();
 	virtual void run();	
-	virtual bool SetConnectionTimeout (unsigned int iTimeout);
+	virtual bool SetConnectionTimeout (int iTimeout);
 	virtual bool StartNetworkChecking();
 	virtual bool StopNetworkChecking();
 	void Cleanup();
 	virtual bool InitConfig(); 
-	virtual bool SetConnectionDlgTimeout (unsigned int iTimeout);
+	virtual bool SetConnectionDlgTimeout (int iTimeout);
 	virtual bool SetBadLinkUrl(const wchar_t* badLinkUrl);
 	virtual bool SetNavigatedUrl(const wchar_t* navigatedUrl);
-	virtual bool isWaitingForWindowMsg();
 	virtual bool isFeatureEnabled() { return m_bIsFeatureEnabled;}
-	virtual void resumeNetworkChecking(const wchar_t* navigatedUrl);
+	virtual void fireEvent(eEventIndex eventIdx);
+	virtual bool onWndMsg(MSG& oMsg);
 	rho::String getHostURL();
-    unsigned int getDailogTimeout();
-    unsigned int getPollInterval();
+    int getDailogTimeout();
+    int getPollInterval();
 	rho::StringW  getMessage();
 private:
 	static HWND m_hConnectDlg;
 	static rho::StringW m_szConnectionDlgMsg;
-	static DWORD ConnectionDlgCreator(LPVOID lParam);
 	static BOOL CALLBACK ConnectDlgProc(HWND hwnd, UINT Message, WPARAM wParam, LPARAM lParam);
 	rho::StringW CHostTracker::replaceString(rho::StringW inputString,
                       rho::StringW toReplace,
-                      rho::StringW replaceWith);	
+                      rho::StringW replaceWith);
+	eConnectionBoxMode OnTimeoutEvent(int& nPollInterval, int& nBadLinkTimer);
+	eConnectionBoxMode OnNavCompleteEvent(int& nPollInterval, int& nBadLinkTimer);
+	void HandleConnectionBox(eConnectionBoxMode& mode);     
+	void closeAllEvents();
 	rho::StringW m_szBadLinkUrl;
 	rho::StringW m_szNavigatedUrl;	
-	unsigned int m_iConnectionDlgTimeout;
-	unsigned int m_iPingTimeOut;
-	bool m_bIsWaiting;
+	int m_iConnectionDlgTimeout;
+	int m_iPingTimeOut;
 	bool m_bIsFeatureEnabled;
+	bool m_bIsOnBadlink;
+	HANDLE m_hEvents[4];
 	
 };
 
