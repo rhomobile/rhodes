ede99824a786d5ce71597a6fefef7404ace1460d
Malaya <nfx846@motorolasolutions.com>
2014-06-19 11:07:54 +0530

    Update ConnectionCheckingListener.java
    TO include License screen events

--- lib/commonAPI/ConnectionChecking/ext/platform/android/src/com/rho/connectionchecking/ConnectionCheckingListener.java
+++ lib/commonAPI/ConnectionChecking/ext/platform/android/src/com/rho/connectionchecking/ConnectionCheckingListener.java
@@ -8,7 +8,7 @@ import com.rhomobile.rhodes.RhodesActivity;
 import com.rhomobile.rhodes.extmanager.AbstractRhoListener;
 import com.rhomobile.rhodes.extmanager.IRhoExtManager;
 import com.rhomobile.rhodes.extmanager.RhoExtManager;
-
+import com.rhomobile.rhodes.Logger;
 
 public class ConnectionCheckingListener extends AbstractRhoListener {
 	
@@ -24,6 +24,29 @@ public class ConnectionCheckingListener extends AbstractRhoListener {
 	
 	}
 	
+	@Override
+	public void onEBLicenseDestroyed() {
+		
+		Logger.I(TAG, "onEBLicenseDestroyed Recieved");
+		if(!ConnectionPageNavEvent.isCurLinkBadLink() && ConnectionCheckingService.getInstance()!=null)
+			ConnectionCheckingService.getInstance().restartThread();
+		
+		super.onEBLicenseDestroyed();
+	}
+	@Override
+	public void onEBLicenseHidden() {
+		Logger.I(TAG, "onEBLicenseHidden Recieved");
+		if(!ConnectionPageNavEvent.isCurLinkBadLink() && ConnectionCheckingService.getInstance()!=null)
+			ConnectionCheckingService.getInstance().restartThread();
+		super.onEBLicenseHidden();
+	}
+	@Override
+	public void onEBLicenseVisible() {
+		Logger.I(TAG, "onEBLicenseVisible Recieved");
+		if(ConnectionCheckingService.getInstance()!=null)
+			ConnectionCheckingService.getInstance().handleLicenseCreate();
+		super.onEBLicenseVisible();
+	}
 	
 
 }
