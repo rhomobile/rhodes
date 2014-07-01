a74b06b2516e5235a461be0e1b9f7e42172aa348
Alex Epifanoff <alex.epifanoff@gmail.com>
2014-07-01 20:19:24 +0400

    fixed AV: missing null-terminator

--- platform/shared/net/HttpServer.cpp
+++ platform/shared/net/HttpServer.cpp
@@ -638,6 +638,7 @@ bool CHttpServer::receive_request(ByteVector &request)
     
     if (!r.empty()) {
         request.insert(request.end(), r.begin(), r.end());
+        request.push_back('\0');
         if ( !rho_conf_getBool("log_skip_post") )
             RAWTRACE1("Received request:\n%s", &request[0]);
     }
