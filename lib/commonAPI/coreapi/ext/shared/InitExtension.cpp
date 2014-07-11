abf5ef98644ec689109148a21d1a2de5e7d7ce7f
Albert R. Timashev <albert@rhomobile.com>
2014-07-11 15:55:13 +0300

    winrt: rhoruntime DLL build fixed

--- lib/commonAPI/coreapi/ext/shared/InitExtension.cpp
+++ lib/commonAPI/coreapi/ext/shared/InitExtension.cpp
@@ -27,19 +27,20 @@ extern "C" void Init_CoreAPI_Extension()
 	Init_Network();
     Init_SQLite3();
 //#if defined(OS_MACOSX) || defined(OS_ANDROID) || defined(OS_WINCE)
+#if !defined(OS_WINRT)
     Init_NewORM_extension();
-//#endif
+#endif
     Init_Log();
 #if defined(OS_MACOSX) || defined(OS_WINDOWS_DESKTOP) || defined(OS_WINCE) || defined(OS_ANDROID)
     Init_WebView();
-#elif defined(OS_WP8) || defined(OS_WINRT)
+#elif defined(OS_WP8) // || defined(OS_WINRT)
     Init_WebView_extension();
 #endif
 
 #if defined(OS_WINDOWS_DESKTOP) || defined(OS_WINCE) || (defined(OS_MACOSX) && defined(RHODES_EMULATOR)) || defined(OS_ANDROID) || defined(OS_MACOSX)
     Init_NativeToolbar();
     Init_NativeTabbar();
-#elif defined(OS_WP8) || defined(OS_WINRT)
+#elif defined(OS_WP8) // || defined(OS_WINRT)
     Init_NativeToolbar_extension();
     Init_NativeTabbar_extension();
 #endif
