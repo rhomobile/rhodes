499d1018d8cf1b603e1799bca71bff00cc3d48c6
Stanislav Volodarskiy <stan_volodarsky@mail.ru>
2014-09-19 21:26:49 +0400

    lib/build/rho_packages.rb, Rakefile, lib/build/tar_gzip.rb: rho_packages.rb was refactored.

--- lib/build/rho_packages.rb
+++ lib/build/rho_packages.rb
@@ -1,94 +1,178 @@
 require 'yaml'
+require 'set'
 
-module DependencyResolver
+module RhoPackages
 
-  @@config
-  
-  def self.loadConfig( path )
-    @@config = YAML::load_file(path)
-  end
-
-  def self.resolveDependencies( command, platform )
-    deps = getCommandDependencies( command )
-
-    installed = getInstalledPackages
-
-    puts "Command #{command} dependencies: #{deps}, installed packages #{installed}"
-
-    deps.each { |dep|
-      if !installed.include?(dep)
-        installPackage(dep)
-      end
-    }
-  end
-
-  def self.getCommandDependencies( command )
-    packages = []
-
-    @@config['packages'].each { |name,package|
-      commands = package['commands']
-    
-      packages << name if commands and commands.include?( command )
-    }
-
-    deps = packages
-
-    packages.each { |package|
-      deps.concat( getPackageDependencies( package ) )
-    }
-
-    deps.uniq!
-
-    return deps
-  end
-
-  def self.getPackageDependencies( packageName )
-    package = @@config['packages'][packageName]
-    deps = package['dependencies']
-
-    return [] unless deps
-
-    deps.each { |dep|
-      deps.concat(getPackageDependencies( dep ) )
-    }
-
-    deps.uniq!
-
-    return deps
-  end
-
-  def self.getInstalledPackages
-    #TODO: get list of installed packages
-    return [ 'core' ]
-  end
-
-  def self.getAvailablePackages
-    packages = []
-
-    @@config['packages'].each { |name,package|
-      packages << name
-    }
-
-    return packages
-  end
-
-  def self.installPackage(package)
-    available = getAvailablePackages
-
-    if !available.include?(package)
-      raise "Package #{packages} is not available"
+  class NilConfig
+    def command_deps(command_name)
+      []
     end
 
-    puts "Installing package #{package}"
-
-    #TODO: download and install packages
-    raise 'Not Implemented'
+    def package_deps(package_name)
+      []
+    end
   end
 
-  def self.uninstallModule
-    raise 'Not Implemented'
+  class YmlConfig
+    def initialize(config_yml_file)
+      @config = YAML::load_file(config_yml_file)
+    end
+
+    def repository
+      @config['repository']
+    end
+
+    def command_deps(command_name)
+      gather([], Set.new, fetch('commands', command_name)).reverse
+    end
+
+    def package_deps(package_name)
+      gather([], Set.new, [package_name]).reverse
+    end
+
+    private
+
+    def fetch(section, key)
+      (@config['dependencies'][section] || {}).fetch(key, [])
+    end
+
+    def gather(list, set, package_names)
+      package_names.each do |package_name|
+        next if set.include?(package_name)
+
+        list << package_name
+        set << package_name
+
+        gather(list, set, fetch('packages', package_name))
+      end
+      list
+    end
+  end
+
+  class NilRepo
+    def require(package_name)
+    end
+  end
+
+  class Repo
+    def initialize(root_dir)
+      @root_dir = root_dir
+    end
+
+    def require(package_name)
+      install(package_name) unless installed?(package_name)
+    end
+
+    private
+
+    def install(package_name)
+      require_relative 'tar_gzip.rb'
+      require_relative 'jake.rb'
+      fetch(package_name) do |tar_gz_file|
+        TarGzip.unpack(tar_gz_file, @root_dir)
+      end
+      Jake.edit_yml(File.join(@root_dir, 'installed.yml')) do |yml|
+        yml << package_name
+        yml.sort!
+      end
+    end
+
+    def installed?(package_name)
+      YAML::load_file(File.join(@root_dir, 'installed.yml')).include?(package_name)
+    end
+
+    def fetch(package_name)
+      fail 'overload it'
+    end
+  end
+
+  class FileRepo < Repo
+    def initialize(root_dir, repo_path)
+      super(root_dir)
+      @repo_path = repo_path
+    end
+
+    private
+
+    def fetch(package_name)
+      yield File.join(@repo_path, "#{package_name}.tar.gz")
+    end
+  end
+
+  class HttpRepo
+    def initialize(root_dir, repo_url)
+      super(root_dir)
+      @repo_url = repo_url
+    end
+
+    private
+
+    def fetch(package_name)
+      Dir.mktmpdir do |dir|
+        filename = "#{package_name}.tar.gz"
+        file = File.join(dir, filename)
+        http_get("#{@repo_url}/#{filename}", file)
+        yield file
+      end
+    end
+
+    def http_get(url, file, opts={})
+      require 'net/https'
+      require 'uri'
+
+      uri = URI.parse(url)
+
+      if opts[:proxy].nil?
+        http = Net::HTTP.new(uri.host, uri.port)
+      else
+        proxy_uri = URI.parse(proxy)
+        http = Net::HTTP.new(uri.host, uri.port, proxy_uri.host, proxy_uri.port, proxy_uri.user, proxy_uri.password)
+      end
+
+      if uri.scheme == 'https'
+        http.use_ssl = true
+        http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+      end
+
+      header_resp = nil
+      http.start { header_resp = http.head(uri.path) }
+
+      fail unless header_resp.kind_of?(Net::HTTPSuccess)
+
+      File.open(file, 'wb') do |f|
+        http.request_get(uri.path) { |r| f.write(r.body) }
+      end
+    end
+  end
+
+  @@config = NilConfig.new
+  @@repo = NilRepo.new
+
+  def self.set_root(root_dir)
+    @@config = YmlConfig.new(File.join(root_dir, 'config.yml'))
+
+    repo_path = @@config.repository
+    @@repo = NilRepo.new if repo_path.nil?
+    @@repo = HttpRepo.new(root_dir, repo_path) if repo_path.start_with?('http://') or repo_path.start_with?('https://')
+    @@repo = FileRepo.new(root_dir, repo_path)
+  end
+
+  def self.require(*package_names)
+    package_names.each do |package_name|
+      self.require_(@@config.package_deps(package_name))
+    end
+  end
+
+  def self.require_by_command(command_name)
+    self.require_(@@config.command_deps(package_name))
+  end
+
+  private
+
+  def self.require_(package_names)
+    package_names.each do |package_name|
+      @@repo.require(package_name)
+    end
   end
 
 end
-
-DependencyResolver.loadConfig( File.join(File.dirname(__FILE__),'rho_packages.yml'))
---
+++ lib/build/tar_gzip.rb
@@ -0,0 +1,65 @@
+require 'fileutils'
+require 'zlib'
+require 'rubygems/package'
+
+module TarGzip
+
+  def self.pack(tar_gz_file, base_dir, files)
+    self.write_tar_gz(tar_gz_file) do |tar|
+      files.each do |file|
+        path = File.join(base_dir, file)
+        stat = File.stat(path)
+
+        tar.add_file_simple(file, stat.mode, stat.size) do |tar_file|
+          open(path, 'rb') do |file|
+            tar_file.write(file.read(16384)) until file.eof?
+          end
+        end
+      end
+    end
+  end
+
+  def self.unpack(tar_gz_file, base_dir)
+    self.read_tar_gz(tar_gz_file) do |entry|
+      path = File.join(base_dir, entry.full_name)
+
+      FileUtils.rm_rf(path)
+
+      FileUtils.mkdir_p(File.dirname(path))
+
+      open(path, 'wb', entry.header.mode) do |out|
+        out.write(entry.read(16384)) until entry.eof?
+      end
+    end
+  end
+
+  private
+
+  def self.write_tar_gz(tar_gz_file)
+    Zlib::GzipWriter.open(tar_gz_file) do |gz|
+      Gem::Package::TarWriter.new(gz) do |tar|
+        yield tar
+      end
+    end
+  end
+
+  def self.read_tar_gz(tar_gz_file)
+    Zlib::GzipReader.open(tar_gz_file) do |gz|
+      Gem::Package::TarReader.new(gz).each do |entry|
+        yield entry
+      end
+    end
+  end
+end
+=begin
+require 'pathname'
+
+base_dir = 'C:/work'
+files = []
+Dir.glob(File.join(base_dir, 'rho/**/*'), File::FNM_DOTMATCH) do |f|
+  files << Pathname.new(f).relative_path_from(Pathname.new(base_dir)).to_s if File.file?(f)
+end
+TarGz.pack('Z:/shared/tmp/ugu.tar.gz', base_dir, files)
+TarGz.unpack('Z:/shared/tmp/ugu.tar.gz', 'Z:/shared/tmp/ugu')
+=end
+
