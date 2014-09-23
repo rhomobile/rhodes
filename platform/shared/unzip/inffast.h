912e9e52742a3acd26bf52bf19870b9ddae40de4
Alex Epifanoff <alex.epifanoff@gmail.com>
2014-09-23 23:56:07 +0400

    gzip support: ios

---
+++ platform/shared/unzip/inffast.h
@@ -0,0 +1,16 @@
+/* inffast.h -- header to use inffast.c
+ * Copyright (C) 1995-2003, 2010 Mark Adler
+ * For conditions of distribution and use, see copyright notice in zlib.h
+ */
+
+/* WARNING: this file should *not* be used by applications. It is
+   part of the implementation of the compression library and is
+   subject to change. Applications should only use zlib.h.
+ */
+
+namespace zlib
+{
+
+void ZLIB_INTERNAL inflate_fast OF((z_streamp strm, unsigned start));
+
+}
