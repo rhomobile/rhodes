85ce039dea417f394cfecaa2d2aaa57d83d5a333
Alex Epifanoff <alex.epifanoff@gmail.com>
2014-09-24 17:59:05 +0400

    use default cache behavior in webview if not specified to disable cache.

--- platform/android/Rhodes/src/com/rhomobile/rhodes/webview/WebSettingsProviderBase.java
+++ platform/android/Rhodes/src/com/rhomobile/rhodes/webview/WebSettingsProviderBase.java
@@ -27,7 +27,7 @@ public class WebSettingsProviderBase implements IWebSettingsProvider {
         Logger.T(TAG, "Enable Zoom: " + enableZoom);
         
         if (enableCache) {
-            settings.setCacheMode(WebSettings.LOAD_CACHE_ELSE_NETWORK);
+            settings.setCacheMode(WebSettings.LOAD_DEFAULT);
         } else {
             settings.setCacheMode(WebSettings.LOAD_NO_CACHE);
         }
