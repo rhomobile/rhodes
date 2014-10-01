7f53f62f50ef489d3fb4e2487f0e26bfec37c5ab
vmusulainen <vmusulainen@gmail.com>
2014-10-01 09:38:29 +0400

    [server side] Tasks have been refactored

--- lib/build/development/subscriber.rb
+++ lib/build/development/subscriber.rb
@@ -31,32 +31,39 @@ module RhoDevelopment
       self.ip == aString
     end
 
-    def notify_url
+    def notify_url(filename)
       server_ip = Configuration::own_ip_address
       server_port = Configuration::webserver_port
       host = Configuration::webserver_uri
       platform = self.normalized_platform_name
-      filename = Configuration::partial_bundle_name
       query = "package_url=#{host}/download/#{platform}/#{filename}&server_ip=#{server_ip}&server_port=#{server_port}"
       url = "http://#{@uri}/development/update_bundle"
       URI("#{url}?#{query}")
     end
 
-    def notify
+    def notifyUrl(anUrl)
       print "Notifying #{self} ...".primary
-      url = self.notify_url
       begin
-        http = Net::HTTP.new(url.host, url.port)
+        http = Net::HTTP.new(anUrl.host, anUrl.port)
         http.open_timeout = 5
         http.start() { |http|
-          http.get(url.path + '?' + url.query)
+          http.get(anUrl.path + '?' + anUrl.query)
         }
         puts 'done'.success
       rescue Errno::ECONNREFUSED,
           Net::OpenTimeout => e
-        puts "failed".warning
+        puts 'failed'.warning
       end
     end
+
+    def partial_notify
+      self.notify(self.notify_url(Configuration::partial_bundle_name))
+    end
+
+    def full_notify
+      self.notify(self.notify_url(Configuration::full_bundle_name))
+    end
+
 
     def to_s
       "#{self.class}(uri=#{@uri}, name=#{@name}, platform=#{@platform}, app=#{@application})"
