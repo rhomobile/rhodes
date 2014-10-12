a24faaa5777f5d7a4bb875dff8f9d95fe85b3904
Albert R. Timashev <albert@rhomobile.com>
2014-10-12 13:51:59 +0400

    wp8: use shared C++ implementation of properties bag

--- res/generators/templates/api/platform/wp8/lib/MontanaRuntime.cpp
+++ res/generators/templates/api/platform/wp8/lib/MontanaRuntime.cpp
@@ -12,9 +12,18 @@ namespace rhoruntime
 {
 
 <%= $cur_module.name %>RuntimeComponent::<%= $cur_module.name %>RuntimeComponent(I<%= $cur_module.name %>Impl^ impl):
-    _impl(impl)
+    _impl(impl), _cppImpl(0)
 {
     // TODO: implement runtime component constructor
+}
+
+int64 <%= $cur_module.name %>RuntimeComponent::getCppImpl()
+{
+    if (_cppImpl == 0)
+    {
+        _cppImpl = _impl->getNativeImpl();
+    }
+    return _cppImpl;
 }
 
 <%= $cur_module.name %>SingletonComponent::<%= $cur_module.name %>SingletonComponent(I<%= $cur_module.name %>SingletonImpl^ impl):
@@ -23,11 +32,12 @@ namespace rhoruntime
     // TODO: implement singleton component constructor
 }<% $cur_module.methods.each do |module_method|
     component_name = module_method.access == ModuleMethod::ACCESS_STATIC ? 'Singleton' : 'Runtime';
-    next if (!module_method.generateNativeAPI) && (module_method.access != ModuleMethod::ACCESS_STATIC)
+    next if ((!module_method.generateNativeAPI || (/^(getProperty|getProperties|getAllProperties|setProperty|setProperties)$/ !~ module_method.native_name)) && (module_method.access != ModuleMethod::ACCESS_STATIC))
 %>
 
 void <%= $cur_module.name %><%= component_name%>Component::<%= module_method.native_name%>(<%= module_method.cached_data["cli_params"]%>)
 {
+    //TODO: ((*)getCppImpl())->
     _impl-><%= module_method.native_name%>(<%= module_method.cached_data["cli_call_params"]%>);
 }<% end %>
 
