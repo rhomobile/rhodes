b0142f9c3aa4d50ed94379b51e5a48d9c9978da3
Albert R. Timashev <albert@rhomobile.com>
2014-10-12 13:51:59 +0400

    wp8: use shared C++ implementation of properties bag

--- res/generators/templates/api/platform/wp8/lib/MontanaRuntime.h
+++ res/generators/templates/api/platform/wp8/lib/MontanaRuntime.h
@@ -28,7 +28,7 @@
       dynamic_methods += method_def
     end
   end
-  dynamic_methods = "    public:\n" + dynamic_methods if dynamic_methods.length() > 0
+  dynamic_methods = "    public:\n        int64 getNativeImpl();\n        void setNativeImpl(int64 native);\n\n" + dynamic_methods if dynamic_methods.length() > 0
   static_methods = "    public:\n" + static_methods if static_methods.length() > 0
 %>
 namespace rhoruntime
@@ -47,16 +47,19 @@ namespace rhoruntime
         I<%= $cur_module.name %>SingletonImpl^ getSingletonImpl();
     };
 
-    public ref class <%= $cur_module.name %>RuntimeComponent sealed: public I<%= $cur_module.name %>Impl
+    public ref class <%= $cur_module.name %>RuntimeComponent sealed
     {
     public:
         <%= $cur_module.name %>RuntimeComponent(I<%= $cur_module.name %>Impl^ impl);
 <% $cur_module.methods.each do |module_method|
     next if module_method.access == ModuleMethod::ACCESS_STATIC
     next if !module_method.generateNativeAPI
-%>        virtual void <%= module_method.native_name%>(<%= module_method.cached_data["cli_params"]%>);
+    next if /^(getProperty|getProperties|getAllProperties|setProperty|setProperties)$/ !~ module_method.native_name
+%>        void <%= module_method.native_name%>(<%= module_method.cached_data["cli_params"]%>);
 <% end %>    private:
         I<%= $cur_module.name %>Impl^ _impl;
+        int64 _cppImpl;
+        int64 getCppImpl();
     };
 
     public ref class <%= $cur_module.name %>SingletonComponent sealed: public I<%= $cur_module.name %>SingletonImpl
