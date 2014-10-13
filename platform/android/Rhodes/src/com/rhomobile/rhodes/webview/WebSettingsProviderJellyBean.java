1e5e1826c9c175ad3fc81c378c188605ff6ebf5c
Alexey Tikhvinsky <alexey@rhomobile.com>
2014-10-13 18:08:48 +0400

    Android: Fix file URL cookies for honeycomb

--- platform/android/Rhodes/src/com/rhomobile/rhodes/webview/WebSettingsProviderJellyBean.java
+++ platform/android/Rhodes/src/com/rhomobile/rhodes/webview/WebSettingsProviderJellyBean.java
@@ -1,16 +1,14 @@
 package com.rhomobile.rhodes.webview;
 
 import android.webkit.WebSettings;
-import android.webkit.CookieManager;
 
 import com.rhomobile.rhodes.Logger;
 import com.rhomobile.rhodes.extmanager.IRhoConfig;
 
-public class WebSettingsProviderJellyBean extends WebSettingsProviderFroyo implements IWebSettingsProvider {
+public class WebSettingsProviderJellyBean extends WebSettingsProviderHoneycombMR1 implements IWebSettingsProvider {
     public void fillSettings(WebSettings settings, IRhoConfig config) {
         super.fillSettings(settings, config);
         Logger.I(TAG, "Allow universal access from file URL");
         settings.setAllowUniversalAccessFromFileURLs(true);
-        CookieManager.setAcceptFileSchemeCookies(true);
     }
 }
