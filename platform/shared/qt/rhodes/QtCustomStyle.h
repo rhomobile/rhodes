00ac64dd12ae1b216217a3d420b4505fd6c6617a
Albert R. Timashev <albert@rhomobile.com>
2014-12-01 23:37:46 +0300

    rhosimulator + win32: checkbox behavior fixed

---
+++ platform/shared/qt/rhodes/QtCustomStyle.h
@@ -0,0 +1,19 @@
+#ifndef QTCUSTOMSTYLE_H
+#define QTCUSTOMSTYLE_H
+
+#include <QProxyStyle>
+#include <QStyleOption>
+
+class QtCustomStyle : public QProxyStyle
+{
+    void drawControl(ControlElement element, const QStyleOption* option, QPainter* painter, const QWidget* widget = 0) const
+    {
+        if (element == QStyle::CE_CheckBox || 
+            element == QStyle::CE_RadioButton) {
+            option->styleObject->setProperty("_q_no_animation", true);                          
+        }
+        QProxyStyle::drawControl(element, option, painter, widget);
+    }
+};
+
+#endif //QTCUSTOMSTYLE_H
