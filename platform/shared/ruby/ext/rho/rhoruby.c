fb70d7ddeaf80456948ad1c9a4fed11b0158006c
Dmitry Soldatenkov <DSoldatenkoff@mail.ru>
2015-01-22 22:58:37 +0300

    remove Motorola brand from code and images

--- platform/shared/ruby/ext/rho/rhoruby.c
+++ platform/shared/ruby/ext/rho/rhoruby.c
@@ -331,9 +331,6 @@ void RhoRubyStart()
         rb_const_set(rb_cObject, rb_intern("RHOSTUDIO_REMOTE_DEBUG"), Qfalse);
     }
 
-//#if defined(APP_BUILD_CAPABILITY_MOTOROLA)
-//    rb_require("rhomotoapi");
-//#endif //APP_BUILD_CAPABILITY_MOTOROLA
 
 #ifdef ENABLE_RUBY_VM_STAT
     struct timeval  start;
