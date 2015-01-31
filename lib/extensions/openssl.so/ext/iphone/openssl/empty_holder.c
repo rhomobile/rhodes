3fbf5ce4481de9c25d79938a962f2d72064e9ccf
Dmitry Soldatenkov <DSoldatenkoff@mail.ru>
2015-02-01 01:01:33 +0300

    fix openssl.so extension to use prebuilded openssl libraries

---
+++ lib/extensions/openssl.so/ext/iphone/openssl/empty_holder.c
@@ -0,0 +1,20 @@
+//
+//  empty_holder.c
+//  openssl
+//
+//  Created by Dmitry Soldatenkov on 30.01.15.
+//
+//
+
+#include "empty_holder.h"
+
+
+//void SSL_get0_next_proto_negotiated(const void *s, const unsigned char **data, unsigned *len);
+
+
+int empty_holder_for_reserve_place_for_function(int x) {
+    
+    //SSL_get0_next_proto_negotiated(NULL, NULL, NULL);
+    
+    return x*x;
+}
