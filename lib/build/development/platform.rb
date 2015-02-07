2d310da4cb0f95c30b675157ef94b224b3d87a47
vmusulainen <vmusulainen@gmail.com>
2015-02-07 13:14:40 +0300

    dev:update:auto:stop available now

--- lib/build/development/platform.rb
+++ lib/build/development/platform.rb
@@ -7,5 +7,15 @@ module RhoDevelopment
     def self.osx?
       return /darwin/i === RbConfig::CONFIG['host_os']
     end
+
+    def self.terminate_process(pid)
+      if self.windows?
+        system "taskkill /PID #{pid}"
+      elsif self.osx?
+        Process.kill('SIGTERM', pid)
+      else
+        raise "Unknown platform #{RbConfig::CONFIG['host_os']}"
+      end
+    end
   end
 end
