bfe3efbedd175eef440e30244d13bc349dbb85d0
Albert R. Timashev <albert@rhomobile.com>
2015-02-10 04:37:45 +0300

    wp8: mediacapture extension build almost fixed

--- lib/commonAPI/mediacapture/ext/shared/MediacaptureInit.cpp
+++ lib/commonAPI/mediacapture/ext/shared/MediacaptureInit.cpp
@@ -5,7 +5,7 @@ extern "C" void Init_Camera_extension();
 
 extern "C" void Init_Mediacapture_extension()
 {
-#if defined(OS_WINCE)
+#if defined(OS_WINCE) || defined(OS_WP8)
     Init_Camera_extension();
 #endif
 }
