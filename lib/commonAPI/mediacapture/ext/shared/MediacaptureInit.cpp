bf7c6ee7dbf326b9aa51c38bbaaa3d482adb7581
npj473 <npj473@zebra.com>
2015-02-18 13:00:57 +0530

    Sabir: Fixed WP8 build related issue.

--- lib/commonAPI/mediacapture/ext/shared/MediacaptureInit.cpp
+++ lib/commonAPI/mediacapture/ext/shared/MediacaptureInit.cpp
@@ -4,10 +4,9 @@
 extern "C" void Init_Camera_API();
 extern "C" void Init_Videocapture();
 #endif
-#ifdef OS_WINCE || OS_WP8
+#if defined(OS_WINCE) || defined(OS_WP8)
 extern "C" void Init_Camera_extension();
 #endif
-
 extern "C" void Init_Mediacapture_extension()
 {
 #ifndef OS_WP8
@@ -18,7 +17,8 @@ extern "C" void Init_Mediacapture_extension()
     LOG(INFO) + __FUNCTION__ + " Loading Videocapture extension";
     Init_Videocapture(); 
 #endif
-#ifdef OS_WINCE || OS_WP8
+
+#if defined(OS_WINCE) || defined(OS_WP8)
     Init_Camera_extension();
 #endif
 }
