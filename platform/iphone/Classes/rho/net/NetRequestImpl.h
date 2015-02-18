3a1d666f07fea9ce5148d656d8acfb851ec27888
Alex Epifanoff <alex.epifanoff@gmail.com>
2015-02-18 12:43:18 +0300

    in the middle
    Conflicts:
    	platform/iphone/RhoAppBaseLib/RhoAppBaseLib.xcodeproj/project.pbxproj

---
+++ platform/iphone/Classes/rho/net/NetRequestImpl.h
@@ -0,0 +1,41 @@
+//
+//  NetRequestImpl.h
+//  RhoAppBaseLib
+//
+//  Created by Alex Epifanoff on 17.02.15.
+//
+//
+
+#pragma once
+
+#include "net/INetRequest.h"
+
+namespace rho {
+namespace net {
+
+class NetRequestImpl : public INetRequestImpl {
+
+public:
+  virtual ~NetRequestImpl(void){;}
+
+  virtual INetResponse* doRequest( const char* method, const String& strUrl, const String& strBody, IRhoSession* oSession, Hashtable<String,String>* pHeaders );
+  virtual INetResponse* pullFile(const String& strUrl, common::CRhoFile& oFile, IRhoSession* oSession, Hashtable<String,String>* pHeaders);
+  virtual INetResponse* pushMultipartData(const String& strUrl, VectorPtr<CMultipartItem*>& arItems, IRhoSession* oSession, Hashtable<String,String>* pHeaders);
+
+  virtual void cancel();
+
+  virtual boolean getSslVerifyPeer();
+  virtual void setSslVerifyPeer(boolean mode);
+  
+  virtual boolean getFollowRedirects();
+  virtual void setFollowRedirects(boolean follow);
+
+  virtual INetResponse* createEmptyNetResponse();
+    
+  virtual void setCallback(INetRequestCallback*);
+};
+
+
+}
+}
+
