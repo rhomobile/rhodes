d82f1d9552b4e563809c9fae6f7e3d2474c8491b
Alex Epifanoff <alex.epifanoff@gmail.com>
2015-02-19 20:35:21 +0300

    net request: multipart data, ssl peer handling, callbacks, file download

--- platform/iphone/Classes/rho/net/IPhoneNetRequest.mm
+++ platform/iphone/Classes/rho/net/IPhoneNetRequest.mm
@@ -8,16 +8,24 @@
 
 #import "IPhoneNetRequest.h"
 
+#import "common/RhoFile.h"
+#import "common/RhoFilePath.h"
+#import "common/StringConverter.h"
+
 class IURLRequestDelegate
 {
 public:
   virtual ~IURLRequestDelegate() {}
   
   virtual void start() = 0;
-  virtual void done() = 0;
+  virtual void onDone() = 0;
+  virtual bool verifySSLPeers() = 0;
+  virtual void onResponse(NSHTTPURLResponse* r) = 0;
+  virtual void onData(NSData* d) = 0;
+  virtual bool allowRedirects() = 0;
 };
 
-@interface ConnDelegate : NSObject<NSURLConnectionDataDelegate>
+@interface ConnDelegate : NSObject<NSURLConnectionDataDelegate/*,NSURLConnectionDownloadDelegate*/>
 
 {
 @public
@@ -36,6 +44,16 @@ public:
 
 - (void)connection:(NSURLConnection *)connection didReceiveData:(NSData *)data;
 
+- (void)connection:(NSURLConnection *)connection willSendRequestForAuthenticationChallenge:(NSURLAuthenticationChallenge *)challenge;
+
+- (NSURLRequest *)connection:(NSURLConnection *)connection willSendRequest:(NSURLRequest *)request redirectResponse:(NSURLResponse *)response;
+
+/*
+- (void)connectionDidFinishDownloading:(NSURLConnection *)connection destinationURL:(NSURL *) destinationURL;
+
+- (void)connection:(NSURLConnection *)connection didWriteData:(long long)bytesWritten totalBytesWritten:(long long)totalBytesWritten expectedTotalBytes:(long long) expectedTotalBytes;
+*/
+
 @property (retain)NSHTTPURLResponse* response;
 @property (retain)NSError* error;
 @property (retain)NSData* data;
@@ -47,31 +65,31 @@ public:
   - (id) init:(IURLRequestDelegate*)delegate
   {
     self = [super init];
-    
     m_pCppDelegate = delegate;
-    
     return self;
   }
 
   - (void)connection:(NSURLConnection *)connection didFailWithError:(NSError *)err
   {
     self.error = err;
-    m_pCppDelegate->done();
+    m_pCppDelegate->onDone();
   }
 
   - (void)connection:(NSURLConnection *)connection didReceiveResponse:(NSURLResponse *)resp
   {
-    self.response = resp;
+    self.response = (NSHTTPURLResponse*)resp;
+    m_pCppDelegate->onResponse((NSHTTPURLResponse*)resp);
   }
 
   - (void)connection:(NSURLConnection *)connection didReceiveData:(NSData *)d
   {
     self.data = d;
+    m_pCppDelegate->onData(d);
   }
 
   - (void)connectionDidFinishLoading:(NSURLConnection *)connection
   {
-    m_pCppDelegate->done();
+    m_pCppDelegate->onDone();
   }
 
   - (void)startAsyncConnection
@@ -79,11 +97,58 @@ public:
     m_pCppDelegate->start();
   }
 
+  - (void)connection:(NSURLConnection *)connection willSendRequestForAuthenticationChallenge:(NSURLAuthenticationChallenge *)challenge
+  {
+    
+    if ( (!m_pCppDelegate->verifySSLPeers()) && [challenge.protectionSpace.authenticationMethod isEqualToString:NSURLAuthenticationMethodServerTrust] )
+    {
+      [challenge.sender cancelAuthenticationChallenge:challenge];
+    }
+    else
+    {
+      [challenge.sender performDefaultHandlingForAuthenticationChallenge:challenge];
+    }
+  }
+
+  - (NSURLRequest *)connection:(NSURLConnection *)connection willSendRequest:(NSURLRequest *)request redirectResponse:(NSURLResponse *)response
+  {
+    return m_pCppDelegate->allowRedirects()?request:nil;
+  }
+
+/*
+  - (void)connectionDidFinishDownloading:(NSURLConnection *)connection destinationURL:(NSURL *) destinationURL
+  {
+  }
+
+  - (void)connection:(NSURLConnection *)connection didWriteData:(long long)bytesWritten totalBytesWritten:(long long)totalBytesWritten expectedTotalBytes:(long long) expectedTotalBytes
+  {
+  }
+*/
+
   @synthesize error;
   @synthesize response;
   @synthesize data;
 
 
+@end
+
+
+@interface MultipartBodyStream : NSInputStream
+{
+  rho::VectorPtr<rho::net::CMultipartItem*>* arItems;
+}
+-(id)init:(rho::VectorPtr<rho::net::CMultipartItem*>*) items;
+@end
+
+@implementation MultipartBodyStream
+-(id)init:(rho::VectorPtr<rho::net::CMultipartItem*>*) items
+{
+  self = [super init];
+  
+  arItems = items;
+  
+  return self;
+}
 @end
 
 namespace rho {
@@ -102,6 +167,11 @@ namespace net {
       :m_nRespCode(nRespCode)
     {
       m_data.assign(data, size);
+    }
+    
+    CIPhoneNetResponseImpl(int nRespCode)
+      :m_nRespCode(nRespCode)
+    {
     }
     
     virtual const char* getCharData()
@@ -170,6 +240,11 @@ public:
     ,m_pConn( [NSURLConnection alloc] )
     ,m_pPerformCond( [NSCondition alloc] )
     ,m_pConnDelegate( [ConnDelegate alloc] )
+    ,m_verifyPeer(true)
+    ,m_pCallback(0)
+    ,m_allowRedirects(true)
+    ,m_pFile(0)
+    ,m_multipartTempPath(nil)
   {
     [m_pPerformCond init];
     [m_pConnDelegate init:this];
@@ -181,9 +256,17 @@ public:
     [m_pConn release];
     [m_pPerformCond release];
     [m_pConnDelegate release];
+    
+    if ( m_multipartTempPath != nil )
+    {
+      [[NSFileManager defaultManager] removeItemAtPath:m_multipartTempPath error:nil];
+      [m_multipartTempPath release];
+      m_multipartTempPath = nil;
+    }
+
   }
   
-  void init( const char* method, const String& url, const String& body, Hashtable<String,String>* pHeaders )
+  bool init( const char* method, const String& url, const String& body, Hashtable<String,String>* pHeaders, IRhoSession* pSession )
   {
     [m_pReq initWithURL:
       [NSURL URLWithString:
@@ -211,29 +294,286 @@ public:
         ];
       }
     }
+    
+    String session;
+    if (pSession != 0)
+    {
+      session = pSession->getSession();
+    }
+    
+    if (!session.empty())
+    {
+      [m_pReq setValue:[NSString stringWithUTF8String:session.c_str()] forHTTPHeaderField:@"Cookie"];
+    }
+    
+    return true;
   }
   
-  INetResponse* perform()
+  bool init( const char* method, const String& url, VectorPtr<CMultipartItem*>& arItems, Hashtable<String,String>* pHeaders, IRhoSession* pSession )
+  {
+    if ( !init(method,url,"",pHeaders,pSession) )
+    {
+      return false;
+    }
+
+    processMultipartItems(arItems);
+    
+    [m_pReq setValue:[NSString stringWithFormat:@"multipart/form-data; boundary=%s",m_sMultipartBoundary] forKey:@"Content-Type"];
+
+    
+    m_multipartTempPath = getMultipartTempFile();
+    if ( nil == m_multipartTempPath )
+    {
+      return false;
+    }
+    
+    if ( !writeMultipartDataToFile(arItems ,m_multipartTempPath) )
+    {
+      return false;
+    }
+    
+    [m_pReq setHTTPBodyStream:[NSInputStream inputStreamWithFileAtPath:m_multipartTempPath]];
+ 
+    return true;
+  }
+  
+  static bool writeMultipartDataToFile(VectorPtr<CMultipartItem*>& arItems, NSString* path)
+  {
+    NSOutputStream* out = [NSOutputStream outputStreamToFileAtPath:path append:NO];
+    
+    if ( nil == out )
+    {
+      return false;
+    }
+    
+    size_t len = 0;
+
+    //write all items
+    for( int i = 0; i < (int)arItems.size(); i++ )
+    {
+      CMultipartItem& oItem = *arItems.elementAt(i);
+      
+      if ( oItem.m_strFilePath.length() > 0 )
+      {
+        common::CRhoFile oFile;
+        if ( !oFile.open(oItem.m_strFilePath.c_str(),common::CRhoFile::OpenReadOnly) )
+        {
+          [out close];
+          [out release];
+          return false;
+        }
+        common::InputStream* bodyStream = oFile.getInputStream();
+        
+        len = oItem.m_strDataPrefix.length();
+        if ( [out write:(const uint8_t*)oItem.m_strDataPrefix.c_str() maxLength:len] != len )
+        {
+          [out close];
+          [out release];
+          return false;
+        }
+        
+        if ( bodyStream->available() > 0 )
+        {
+          Vector<char> buf;
+          buf.assign(4096, 0);
+          int nReaded = 0;
+          
+          do
+          {
+            nReaded = bodyStream->read(&buf[0],0,(int)buf.size());
+            if ( nReaded > 0 )
+            {
+              if ( [out write:(const uint8_t*)&buf[0] maxLength:nReaded] != nReaded )
+              {
+                [out close];
+                [out release];
+                return false;
+              }
+            }
+          } while(nReaded > 0);
+        }
+        
+      }
+      else
+      {
+        len = oItem.m_strDataPrefix.length();
+        if ( [out write:(const uint8_t*)oItem.m_strDataPrefix.c_str() maxLength:len] != len )
+        {
+          [out close];
+          [out release];
+          return false;
+        }
+        
+        len = oItem.m_strBody.length();
+        if ( [out write:(const uint8_t*)oItem.m_strBody.c_str() maxLength:len] != len )
+        {
+          [out close];
+          [out release];
+          return false;
+        }
+      }
+    }
+    
+    len = strlen(m_sMultipartPostfix);
+    if ( [out write:(const uint8_t*)m_sMultipartPostfix maxLength:len] != len )
+    {
+      [out close];
+      [out release];
+      return false;
+    }
+      
+    [out close];
+    [out release];
+
+    return true;
+  }
+
+  int processMultipartItems( VectorPtr<CMultipartItem*>& arItems )
+  {
+    int nSize = 0;
+    for( int i = 0; i < (int)arItems.size(); i++ )
+    {
+      CMultipartItem& oItem = *arItems.elementAt(i);
+      
+      if ( oItem.m_strName.length() == 0 )
+      oItem.m_strName = "blob";
+      
+      if ( oItem.m_strFileName.length() == 0 )
+      {
+        if ( oItem.m_strFilePath.length() > 0 )
+        {
+          common::CFilePath oPath(oItem.m_strFilePath);
+          oItem.m_strFileName = oPath.getBaseName();
+        }
+      }
+      
+      oItem.m_strDataPrefix = i > 0 ? "\r\n" : "";
+      oItem.m_strDataPrefix += m_sMultipartBoundary;
+      oItem.m_strDataPrefix += "\r\n";
+      oItem.m_strDataPrefix += "Content-Disposition: form-data; name=\"";
+      oItem.m_strDataPrefix += oItem.m_strName + "\"";
+      if (oItem.m_strFileName.length()>0)
+      oItem.m_strDataPrefix += "; filename=\"" + oItem.m_strFileName + "\"";
+      oItem.m_strDataPrefix += "\r\n";
+      if ( oItem.m_strContentType.length() > 0 )
+      oItem.m_strDataPrefix += "Content-Type: " + oItem.m_strContentType + "\r\n";
+      
+      int nContentSize = 0;
+      if ( oItem.m_strFilePath.length() > 0 )
+      {
+        common::CRhoFile oFile;
+        if ( oFile.open(oItem.m_strFilePath.c_str(),common::CRhoFile::OpenReadOnly) )
+        nContentSize = oFile.size();
+      }
+      else
+      {
+        nContentSize = (int)oItem.m_strBody.length();
+      }
+      
+      if ( oItem.m_strContentType.length() > 0 )
+      oItem.m_strDataPrefix += "Content-Length: " + common::convertToStringA(nContentSize) + "\r\n";
+      
+      oItem.m_strDataPrefix += "\r\n";
+      
+      nSize += oItem.m_strDataPrefix.length() + nContentSize;
+      
+    }
+    
+    nSize += strlen(m_sMultipartPostfix);
+    
+    return nSize;
+  }
+
+  static NSString* getMultipartTempFile()
+  {
+    NSString *tempFileTemplate = [NSTemporaryDirectory() stringByAppendingPathComponent:@"rho-multipart-XXXXXX.tmp"];
+    
+    const char *tempFileTemplateCString = [tempFileTemplate fileSystemRepresentation];
+
+    char *tempFileNameCString = (char *)malloc(strlen(tempFileTemplateCString) + 1);
+    strcpy(tempFileNameCString, tempFileTemplateCString);
+    int fileDescriptor = mkstemps(tempFileNameCString, 4);
+
+    // no need to keep it open
+    close(fileDescriptor);
+    
+    if (fileDescriptor == -1) {
+        NSLog(@"Error while creating tmp file");
+        free(tempFileNameCString);
+        return nil;
+    }
+
+    NSString *tempFileName = [[NSFileManager defaultManager]
+        stringWithFileSystemRepresentation:tempFileNameCString
+        length:strlen(tempFileNameCString)];
+
+    free(tempFileNameCString);
+
+    return tempFileName;
+  }
+
+
+  
+  INetResponse* perform( Hashtable<String,String>* pHeaders, IRhoSession* pSession )
   {
     [m_pPerformCond lock];
 
     [m_pConnDelegate performSelectorOnMainThread:@selector(startAsyncConnection) withObject:nil waitUntilDone:NO];
-  
+    
     [m_pPerformCond wait];
     [m_pPerformCond unlock];
+    
+    if ( m_multipartTempPath != nil )
+    {
+      [[NSFileManager defaultManager] removeItemAtPath:m_multipartTempPath error:nil];
+      [m_multipartTempPath release];
+      m_multipartTempPath = nil;
+    }
     
     INetResponse* ret = 0;
     
     if ( m_pConnDelegate.error != nil )
     {
-      ret = new CIPhoneNetResponseImpl("",0,-1);
+      ret = new CIPhoneNetResponseImpl(-1);
+      
+      if ( m_pCallback != 0 )
+      {
+        NetResponse nrw = new CIPhoneNetResponseImpl(-1);
+        m_pCallback->didFail( nrw );
+      }
+      
     } else {
     
       NSData* d = m_pConnDelegate.data;
-
-    
-      ret = new CIPhoneNetResponseImpl( d==nil?"":(const char*)[d bytes], d==nil?0:[d length], (int)[m_pConnDelegate.response statusCode]  );
-    
+      if ( d != nil )
+      {
+        ret = new CIPhoneNetResponseImpl( (const char*)[d bytes], [d length], (int)[m_pConnDelegate.response statusCode]  );
+      } else {
+        ret = new CIPhoneNetResponseImpl( (int)[m_pConnDelegate.response statusCode]  );
+      }
+      
+      if ( pHeaders != 0 )
+      {
+        pHeaders->clear();
+        
+        NSDictionary* respHeadrs = [m_pConnDelegate.response allHeaderFields];
+        
+        for (NSString* key in respHeadrs)
+        {
+          NSString* val = [respHeadrs valueForKey:key];
+          pHeaders->put( [key UTF8String], [val UTF8String] );
+        }
+      }
+      
+      if ( (pSession!=0) && ( [m_pConnDelegate.response statusCode] == 401 ) )
+      {
+        pSession->logout();
+      }
+      
+      if ( m_pCallback != 0 )
+      {
+        m_pCallback->didFinishLoading();
+      }
     }
     
     return ret;
@@ -252,12 +592,74 @@ public:
     [m_pPerformCond unlock];
   }
   
-  virtual void done()
+  virtual void onDone()
   {
     [m_pPerformCond lock];
     [m_pPerformCond signal];
     [m_pPerformCond unlock];
   }
+  
+  virtual bool verifySSLPeers()
+  {
+    return m_verifyPeer;
+  }
+  
+  void setVerifyPeers(bool verify)
+  {
+    m_verifyPeer = verify;
+  }
+  
+  virtual void onResponse(NSHTTPURLResponse* r)
+  {
+    if ( m_pCallback != 0 )
+    {
+      NetResponse resp = new CIPhoneNetResponseImpl(0,0,(int)[r statusCode]);
+      Hashtable<String, String> headers;
+      
+      for ( NSString* key in r.allHeaderFields )
+      {
+        NSString* val = [r.allHeaderFields valueForKey:key];
+        headers.put( [key UTF8String], [val UTF8String] );
+      }
+      
+      m_pCallback->didReceiveResponse(resp, &headers);
+    }
+  }
+  
+  virtual void onData(NSData* d)
+  {
+    if ( m_pFile != 0 )
+    {
+      m_pFile->write([d bytes], (int)[d length]);
+      m_pFile->flush();
+    }
+
+    if ( m_pCallback != 0 )
+    {
+      m_pCallback->didReceiveData((const char*)[d bytes], (int)[d length]);
+    }
+  }
+
+  void setCallback( INetRequestCallback* cb )
+  {
+    m_pCallback = cb;
+  }
+  
+  virtual bool allowRedirects()
+  {
+    return m_allowRedirects;
+  }
+  
+  void setAllowRedirects( bool allow )
+  {
+    m_allowRedirects = allow;
+  }
+  
+  void setFile( common::CRhoFile* file )
+  {
+    m_pFile = file;
+  }
+
 
 private:
 
@@ -265,8 +667,18 @@ private:
     NSURLConnection* m_pConn;
     NSCondition* m_pPerformCond;
     ConnDelegate* m_pConnDelegate;
+    bool m_verifyPeer;
+    INetRequestCallback* m_pCallback;
+    bool m_allowRedirects;
+    common::CRhoFile* m_pFile;
+    NSString* m_multipartTempPath;
+  
+    static const char* m_sMultipartBoundary;
+    static const char* m_sMultipartPostfix;
 };
 
+const char* CIphoneNetRequestHolder::m_sMultipartBoundary = "----------A6174410D6AD474183FDE48F5662FCC5";
+const char* CIphoneNetRequestHolder::m_sMultipartPostfix = "\r\n------------A6174410D6AD474183FDE48F5662FCC5--";
 
 
 CIPhoneNetRequest::CIPhoneNetRequest() :
@@ -281,30 +693,33 @@ CIPhoneNetRequest::~CIPhoneNetRequest()
 
 INetResponse* CIPhoneNetRequest::doRequest( const char* method, const String& strUrl, const String& strBody, IRhoSession* oSession, Hashtable<String,String>* pHeaders )
 {
-
-  m_pHolder->init(method, strUrl, strBody, pHeaders);
+  if ( !m_pHolder->init(method, strUrl, strBody, pHeaders, oSession) )
+  {
+    return createEmptyNetResponse();
+  }
   
-  return m_pHolder->perform();
+  return m_pHolder->perform( pHeaders, oSession );
 }
 
 INetResponse* CIPhoneNetRequest::pullFile(const String& strUrl, common::CRhoFile& oFile, IRhoSession* oSession, Hashtable<String,String>* pHeaders)
 {
-  m_pHolder->init(0, strUrl, "", pHeaders);
+  if ( !m_pHolder->init("GET", strUrl, "", pHeaders, oSession) )
+  {
+    return createEmptyNetResponse();
+  }
   
-  m_pHolder->perform();
-
-
-  return 0;
+  m_pHolder->setFile(&oFile);
+  return m_pHolder->perform( pHeaders, oSession );
 }
 
 INetResponse* CIPhoneNetRequest::pushMultipartData(const String& strUrl, VectorPtr<CMultipartItem*>& arItems, IRhoSession* oSession, Hashtable<String,String>* pHeaders)
 {
-
-  m_pHolder->init(0, strUrl, "", pHeaders);
-
-  m_pHolder->perform();
-
-  return 0;
+  if ( !m_pHolder->init("POST", strUrl, arItems, pHeaders, oSession) )
+  {
+    return createEmptyNetResponse();
+  }
+  
+  return m_pHolder->perform( pHeaders, oSession );
 }
 
 void CIPhoneNetRequest::cancel()
@@ -314,20 +729,22 @@ void CIPhoneNetRequest::cancel()
 
 boolean CIPhoneNetRequest::getSslVerifyPeer()
 {
-  return false;
+  return m_pHolder->verifySSLPeers();
 }
 
 void CIPhoneNetRequest::setSslVerifyPeer(boolean mode)
 {
+  m_pHolder->setVerifyPeers(mode);
 }
   
 boolean CIPhoneNetRequest::getFollowRedirects()
 {
-  return false;
+  return m_pHolder->allowRedirects();
 }
 
 void CIPhoneNetRequest::setFollowRedirects(boolean follow)
 {
+  m_pHolder->setAllowRedirects(follow);
 }
 
 INetResponse* CIPhoneNetRequest::createEmptyNetResponse()
@@ -337,7 +754,7 @@ INetResponse* CIPhoneNetRequest::createEmptyNetResponse()
     
 void CIPhoneNetRequest::setCallback(INetRequestCallback* cb)
 {
-  m_pCallback = cb;
+  m_pHolder->setCallback(cb);
 }
 
 
