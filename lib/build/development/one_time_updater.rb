c6ca5eb13a9e1d5ce6ff372e188ef313ae099c88
vmusulainen <vmusulainen@gmail.com>
2015-03-11 12:54:52 +0300

    Updated docs for some live_update_task classes, minor refactoring in one_time_updater.rb

--- lib/build/development/one_time_updater.rb
+++ lib/build/development/one_time_updater.rb
@@ -7,14 +7,13 @@ module RhoDevelopment
         return
       end
 
-      devDir = File.join(Configuration::application_root, '.development')
-      updatedListFilename = File.join(Configuration::application_root, 'upgrade_package_add_files.txt')
-      removedListFilename = File.join(Configuration::application_root, 'upgrade_package_remove_files.txt')
-      mkdir_p devDir
+      development_directory = File.join(Configuration::application_root, '.development')
+      updated_list_filename = File.join(Configuration::application_root, 'upgrade_package_add_files.txt')
+      removed_list_filename = File.join(Configuration::application_root, 'upgrade_package_remove_files.txt')
+      mkdir_p development_directory
       Configuration::subscriber_platforms.each { |each|
-        RhoDevelopment.setup(devDir, each)
-        #is_require_full_update = RhoDevelopment.is_require_full_update
-        changed = RhoDevelopment.check_changes_from_last_build(updatedListFilename, removedListFilename)
+        RhoDevelopment.setup(development_directory, each)
+        changed = RhoDevelopment.check_changes_from_last_build(updated_list_filename, removed_list_filename)
         if changed
           puts "Source code for platform #{each} was changed".primary
           WebServer.dispatch_task(PlatformPartialUpdateBuildingTask.new(each));
