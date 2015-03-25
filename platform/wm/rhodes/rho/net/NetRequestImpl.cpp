26bc1f9b6a03b2c34ec4d38935cb43a513d00f1d
NHB348 <nhb348@zebra.com>
2015-03-25 13:10:02 +0530

    SPR 26558 fix

--- platform/wm/rhodes/rho/net/NetRequestImpl.cpp
+++ platform/wm/rhodes/rho/net/NetRequestImpl.cpp
@@ -120,7 +120,11 @@ void CNetRequestImpl::init(const char* method, const String& strUrl, IRhoSession
 			m_strErrFunction = L"InternetCrackUrl";
 			break;
 		}
+            int timeout = rho_conf_getInt("net_timeout")*1000;
+            if (timeout == 0 )
+             timeout = 30000;
 
+        InternetSetOption( m_hInternet, INTERNET_OPTION_RECEIVE_TIMEOUT, &timeout, sizeof(timeout) ); 
         m_hConnection = InternetConnect( m_hInternet, m_uri.lpszHostName, m_uri.nPort, _T("anonymous"), 
 										 NULL, INTERNET_SERVICE_HTTP, 0, 0 );
         if ( !m_hConnection ) 
@@ -129,11 +133,11 @@ void CNetRequestImpl::init(const char* method, const String& strUrl, IRhoSession
             break;
         }
 
-        int timeout = rho_conf_getInt("net_timeout")*1000;
-        if (timeout == 0 )
-            timeout = 30000;
+        //int timeout = rho_conf_getInt("net_timeout")*1000;
+        //if (timeout == 0 )
+        //    timeout = 30000;
 
-        InternetSetOption( m_hInternet, INTERNET_OPTION_RECEIVE_TIMEOUT, &timeout, sizeof(timeout) ); 
+        //InternetSetOption( m_hInternet, INTERNET_OPTION_RECEIVE_TIMEOUT, &timeout, sizeof(timeout) ); 
 
         m_strReqUrlW = m_uri.lpszUrlPath;
         m_strReqUrlW += m_uri.lpszExtraInfo;
