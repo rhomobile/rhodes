5b890a6b88aa34c174d8d39e891417697fc0c7e4
Albert R. Timashev <albert@rhomobile.com>
2015-04-16 11:25:18 +0300

    build fixed

--- lib/commonAPI/coreapi/ext/shared/InitExtension.cpp
+++ lib/commonAPI/coreapi/ext/shared/InitExtension.cpp
@@ -74,11 +74,11 @@ extern "C" void Init_CoreAPI_Extension()
     Init_Intent();
 #endif
 
-#if defined(OS_ANDROID) || defined(OS_WINCE) || defined(OS_MACOSX) && !defined(RHODES_EMULATOR)) || defined(OS_WP8)
-	Init_Timer_extension();
+#if defined(OS_ANDROID) || defined(OS_WINCE) || (defined(OS_MACOSX) && !defined(RHODES_EMULATOR)) || defined(OS_WP8)
+    Init_Timer_extension();
 #endif
 
 #if defined(OS_WP8) || (defined(OS_MACOSX) && !defined(RHODES_EMULATOR))
-	Init_Config_extension();
+    Init_Config_extension();
 #endif
 }
