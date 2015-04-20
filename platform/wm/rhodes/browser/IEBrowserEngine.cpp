c2745269efe5636d28616c3bb81fc9f4c7f2c3e9
Abhineet Agarwal <abhineet.agarwal@zebra.com>
2015-04-20 20:00:20 +0530

    Update IEBrowserEngine.cpp
    Handling FS key for refreshing the page if function key capturable is set to 0 and enable function F5 is set to 1.

--- platform/wm/rhodes/browser/IEBrowserEngine.cpp
+++ platform/wm/rhodes/browser/IEBrowserEngine.cpp
@@ -153,7 +153,7 @@ LRESULT CIEBrowserEngine::CreateEngine()
 
     if (!m_hwndTabHTML)
         return S_FALSE;
-
+runme
     CloseHandle (CreateThread(NULL, 0, 
         &CIEBrowserEngine::RegisterWndProcThread, (LPVOID)this, 0, NULL));
 
@@ -280,6 +280,19 @@ void CIEBrowserEngine::RunMessageLoop(CMainWindow& mainWnd)
 	MSG msg;
     while (GetMessage(&msg, NULL, 0, 0))
     {
+    	// If function key capturable is disabled & if VK_F5 key is enabled then we should refresh the page. 
+		// In all other cases, KeyCapture module will take care.
+		if( WM_KEYDOWN == msg.message )
+		{
+			if( msg.wParam == VK_F5 )
+			{
+				if(!rho_wmimpl_get_function_keys_capturable() && rho_wmimpl_get_function_key_enabled(VK_F5-VK_F1))
+				{
+					RHODESAPP().getExtManager().refreshPage(false);
+				}
+			}
+		}	
+		
 		if (RHODESAPP().getExtManager().onWndMsg(msg) )
             continue;
 
