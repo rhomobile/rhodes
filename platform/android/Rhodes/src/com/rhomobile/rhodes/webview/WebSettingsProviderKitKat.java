1c515577e3b8dfe088ac601b062336bee5373aa1
Malaya <nfx846@motorolasolutions.com>
2015-04-22 18:15:42 +0530

    Update WebSettingsProviderKitKat.java
    System.exit was crashing as the changes have to be applied from UI thread

--- platform/android/Rhodes/src/com/rhomobile/rhodes/webview/WebSettingsProviderKitKat.java
+++ platform/android/Rhodes/src/com/rhomobile/rhodes/webview/WebSettingsProviderKitKat.java
@@ -5,6 +5,7 @@ import android.webkit.WebView;
 
 import com.rhomobile.rhodes.Capabilities;
 import com.rhomobile.rhodes.extmanager.IRhoConfig;
+import com.rhomobile.rhodes.util.PerformOnUiThread;
 
 public class WebSettingsProviderKitKat extends
         WebSettingsProviderJellyBean implements IWebSettingsProvider {
@@ -12,9 +13,17 @@ public class WebSettingsProviderKitKat extends
     @Override
     public void fillSettings(WebSettings settings, IRhoConfig config) {
         super.fillSettings(settings, config);
-        if (Capabilities.DEBUG_ENABLED) {
-            WebView.setWebContentsDebuggingEnabled(true);
-        }
+       PerformOnUiThread.exec(new Runnable(){
+
+				@Override
+				public void run() {
+					                if (Capabilities.DEBUG_ENABLED) {
+				                                                                WebView.setWebContentsDebuggingEnabled(true);
+				                                                        }
+					
+				                  }
+				
+			});
     }
 
 }
