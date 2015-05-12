248e8b6bae1fb3cbaf3f20fd6c9b38742cbf0fa1
Dmitry Soldatenkov <DSoldatenkoff@mail.ru>
2015-05-12 15:00:25 +0300

    fix filelist issue in removing files in BundleUpdate

--- platform/shared/common/BundleManager.cpp
+++ platform/shared/common/BundleManager.cpp
@@ -752,7 +752,8 @@ unsigned int CReplaceBundleThread::partialRemoveItemsByList( const String& strLi
             }
             else {
                 LOG(ERROR) + "Cannot remove folder (not found): " + CFilePath::join( strSrcFolder,strPath);
-                // folder can be removed manually etc. It is not real issue - after this cammand files not exist, how it was removed is does not matter
+                filelist->removeFolder(strPath);
+                filelist_apps->removeFolder(strPath);                // folder can be removed manually etc. It is not real issue - after this cammand files not exist, how it was removed is does not matter
                 //nError = 1;
                 //break;
             }
@@ -790,7 +791,8 @@ unsigned int CReplaceBundleThread::partialRemoveItemsByList( const String& strLi
             }
             else {
                 LOG(ERROR) + "Cannot remove file (not found): " + CFilePath::join( strSrcFolder,strPath);
-                // file can be removed manually etc. It is not real issue - after this cammand files not exist, how it was removed is does not matter
+                filelist->removeFile(strPath);
+                filelist_apps->removeFile(strPath);                // file can be removed manually etc. It is not real issue - after this cammand files not exist, how it was removed is does not matter
                 //nError = 1;
                 //break;
             }
