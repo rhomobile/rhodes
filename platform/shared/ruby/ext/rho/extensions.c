0bc2b2a8140568ac9255d24fab20d3722ae78ac2
fgqw68 <sabir.vt@zebra.com>
2015-05-15 21:05:05 +0530

    fix for CE IE engine. Ce device was unable to issue navigate to history inside keycaptureevent. Script invocation logic modified.

--- platform/shared/ruby/ext/rho/extensions.c
+++ platform/shared/ruby/ext/rho/extensions.c
@@ -1,9 +1,60 @@
 // WARNING! THIS FILE IS GENERATED AUTOMATICALLY! DO NOT EDIT IT MANUALLY!
+int rho_ruby_is_started();
+#pragma comment(lib, "CoreAPI.lib")
+#pragma comment(lib, "Mediaplayer.lib")
+#pragma comment(lib, "Printing.lib")
+#pragma comment(lib, "PrintingZebra.lib")
+#pragma comment(lib, "screenorientation.lib")
+#pragma comment(lib, "Sensor.lib")
+#pragma comment(lib, "Barcode.lib")
+#pragma comment(lib, "Cardreader.lib")
+#pragma comment(lib, "AudioCapture.lib")
+#pragma comment(lib, "LogCapture.lib")
+#pragma comment(lib, "Instrumentation.lib")
+#pragma comment(lib, "ConnectionChecking.lib")
+#pragma comment(lib, "Smartcradle.lib")
+#pragma comment(lib, "Symboldevice.lib")
+#pragma comment(lib, "rhowebkit.lib")
+#pragma comment(lib, "Indicators.lib")
+#pragma comment(lib, "Signature.lib")
+#pragma comment(lib, "HardwareKeys.lib")
+#pragma comment(lib, "RubyvmStub.lib")
+#pragma comment(lib, "RhoElementsExt.lib")
 extern void Init_CoreAPI_Extension(void);
-extern void Init_Rhoconnectclient_extension(void);
-extern void Init_Mediacapture_extension(void);
+extern void Init_Mediaplayer_extension(void);
+extern void Init_Printing_extension(void);
+extern void Init_PrintingZebra_extension(void);
+extern void Init_ScreenOrientation_extension(void);
+extern void Init_Sensor_extension(void);
+extern void Init_Barcode_extension(void);
+extern void Init_Cardreader_extension(void);
+extern void Init_AudioCapture_extension(void);
+extern void Init_LogCapture_extension(void);
+extern void Init_Instrumentation_extension(void);
+extern void Init_ConnectionChecking_extension(void);
+extern void Init_Smartcradle_extension(void);
+extern void Init_Symboldevice_extension(void);
+extern void Init_RhoElementsExt_extension(void);
+extern void Init_Indicators_extension(void);
+extern void Init_Signature_extension(void);
+extern void Init_HardwareKeys_extension(void);
 void Init_Extensions(void) {
     Init_CoreAPI_Extension();
-    Init_Rhoconnectclient_extension();
-    Init_Mediacapture_extension();
+    Init_Mediaplayer_extension();
+    Init_Printing_extension();
+    Init_PrintingZebra_extension();
+    Init_ScreenOrientation_extension();
+    Init_Sensor_extension();
+    Init_Barcode_extension();
+    Init_Cardreader_extension();
+    Init_AudioCapture_extension();
+    Init_LogCapture_extension();
+    Init_Instrumentation_extension();
+    Init_ConnectionChecking_extension();
+    Init_Smartcradle_extension();
+    Init_Symboldevice_extension();
+    Init_RhoElementsExt_extension();
+    Init_Indicators_extension();
+    Init_Signature_extension();
+    Init_HardwareKeys_extension();
 }
