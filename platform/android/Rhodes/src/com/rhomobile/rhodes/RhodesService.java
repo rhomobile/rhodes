bfe166098522d8a185614a4e4d257f9ffd95edf0
Joydeep Chakraborty <joydeep.chakraborty83@gmail.com>
2015-05-21 16:28:02 +0530

    SPR27736 fix-fix Has_Camera effecting Camera APIs
    SPR27736 fix-fix Has_Camera effecting Camera APIs
    Chnage the implementation to find out whether camera is there in the device or not.

--- platform/android/Rhodes/src/com/rhomobile/rhodes/RhodesService.java
+++ platform/android/Rhodes/src/com/rhomobile/rhodes/RhodesService.java
@@ -75,8 +75,8 @@ import com.rhomobile.rhodes.util.JSONGenerator;
 import com.rhomobile.rhodes.util.PerformOnUiThread;
 import com.rhomobile.rhodes.util.PhoneId;
 import com.rhomobile.rhodes.util.Utils;
-import com.rhomobile.rhodes.camera.Camera;
-
+//import com.rhomobile.rhodes.camera.Camera;
+import android.hardware.Camera;
 import android.app.AlertDialog;
 import android.app.Notification;
 import android.app.NotificationManager;
@@ -836,7 +836,12 @@ public class RhodesService extends Service {
                 return fetchUUID();
             }
             else if (name.equalsIgnoreCase("has_camera")) {
+            
+            //SPR27736 Fix start
+            
             	boolean hasCamera = false;
+            /*
+            
             	try {
             		if (Camera.getCameraService() != null) {
             			if ((Camera.getCameraService().getMainCamera() != null) || (Camera.getCameraService().getFrontCamera() != null)) {
@@ -848,7 +853,34 @@ public class RhodesService extends Service {
             		e.printStackTrace();
             		Logger.E(TAG, "Exception during detect Camera for has_camera");
             	}
+              */
+              
+              int noofCameras=0;
+				
+            	
+		try 
+		{
+		noofCameras=Camera.getNumberOfCameras();
+		}
+		catch (Throwable e)
+		{
+            		e.printStackTrace();
+            		Logger.E(TAG, "Exception while getting the number of Cameras");
+            	}
+		
+		Logger.I(TAG, "number of of Cameras present" + noofCameras);
+				
+				if(noofCameras > 0)
+				{
+				hasCamera=true;
+				}
+				else
+				{
+				hasCamera=false;
+				}
+              
                 return hasCamera;//Boolean.TRUE;
+                //SPR27736 Fix end
             }
             else {
                 return RhoExtManager.getImplementationInstance().getProperty(name);
