88bcbdf39d4dc094121fe270ecc2392810b041ba
fgqw68 <sabir.vt@zebra.com>
2015-08-13 00:39:29 +0530

    added engine interface

--- platform/wm/rhodes/browser/EngineEventListner.cpp
+++ platform/wm/rhodes/browser/EngineEventListner.cpp
@@ -1,16 +1,15 @@
 #include "stdafx.h"
 #include "EngineEventListner.h"
+#include "../../../shared/common/RhodesApp.h"
+
 namespace rho
 {
 	namespace engineeventlistner
 	{
-		void CEngineEventMngr::notifyEngineCreate(eEngineType eEngType)
+		void CEngineEventMngr::setEngineType(eEngineType eEngType)
 		{
 			m_eEngType = eEngType;
-			/*for ( HashtablePtr<String, IEngineEventListner*>::iterator it = m_hashExtensions.begin(); it != m_hashExtensions.end(); ++it )
-			{
-				(it->second)->notifyEngineCreate( eEngType );
-			}*/
+			
 		}
 		void CEngineEventMngr::registerExtension(const String& strName, IEngineEventListner* pExt)
 		{
@@ -19,6 +18,30 @@ namespace rho
 		eEngineType CEngineEventMngr::getEngineType()
 		{
 			return m_eEngType;
+		}
+		void CEngineEventMngr::notifyDOMInjector()
+		{
+			for ( HashtablePtr<String, IEngineEventListner*>::iterator it = m_hashExtensions.begin(); it != m_hashExtensions.end(); ++it )
+			{
+				(it->second)->notifyDOMInjector();
+			}
+		}
+		void CEngineEventMngr::setEngineInterface(IDOMInjectorEngineInterface* pEngineIf)
+		{
+			m_pEngineIf = pEngineIf;
+		}
+		void CEngineEventMngr::executAnonymousJs(wchar_t* szFunctionText)
+		{
+			int nTabID = RHODESAPP().getExtManager().getCurrentTabID();
+			if(m_pEngineIf)
+			{
+				m_pEngineIf->executAnonymousJs(szFunctionText, nTabID);
+			}
+
+		}
+		void CEngineEventListner::notifyDOMInjector()
+		{
+			//default implementation
 		}
 
 	}
