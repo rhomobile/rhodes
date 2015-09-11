08ab93812ef164abdbf86e04b8acd848e76d1265
Rakesh Kumar B Badiger <nrpj67@users.noreply.github.com>
2015-09-11 17:27:09 +0530

    Added IntentListner class
    Gets IntentReceiver values from Config.xml and acts accordingly for registering the receiver

---
+++ lib/commonAPI/coreapi/ext/platform/android/src/com/rho/intent/IntentListener.java
@@ -0,0 +1,79 @@
+package com.rho.intent;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Environment;
+
+import com.rhomobile.rhodes.RhodesActivity;
+import com.rhomobile.rhodes.extmanager.AbstractRhoExtension;
+import com.rhomobile.rhodes.extmanager.IRhoConfig;
+import com.rhomobile.rhodes.extmanager.IRhoExtManager;
+
+public class IntentListener extends AbstractRhoExtension{
+
+	public static HashMap mapAction = new HashMap();
+	public static HashMap mapCategory = new HashMap();
+	@Override
+	public boolean onNewConfig(IRhoExtManager extManager, IRhoConfig config,
+			String name, boolean res) {
+		if (config.isExist("enablereceiver")) {
+			String EnableReceiver = config.getString("enablereceiver");
+			int reg_receiver = 0;
+			IntentFilter intentfilter = new IntentFilter();
+			IntentReceiver mReceiver = new IntentReceiver();
+			if (EnableReceiver != null) {
+				reg_receiver = Integer.parseInt(EnableReceiver);
+				if(reg_receiver == 1){
+					String externalSharedPath = Environment.getExternalStorageDirectory().toString();
+                    String configPath = new File(externalSharedPath, "/Android/data/com.symbol.enterprisebrowser/Config.xml").getAbsolutePath();
+                    File fXmlFile = new File(configPath);
+                    DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+                    DocumentBuilder dBuilder = null;
+                    Document doc = null;
+					try {
+						dBuilder = dbFactory.newDocumentBuilder();
+						doc = dBuilder.parse(fXmlFile);
+					} catch (ParserConfigurationException e1) {
+						e1.printStackTrace();
+					} catch (SAXException e) {
+						e.printStackTrace();
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+                    doc.getDocumentElement().normalize();
+                    NodeList nList = doc.getElementsByTagName("IntentAction");
+                    mapAction.clear();
+                    mapCategory.clear();
+                    for (int temp = 0; temp < nList.getLength(); temp++) {
+                    	mapAction.put(temp, nList.item(temp).getAttributes().getNamedItem("value").getTextContent());
+                    	intentfilter.addAction(nList.item(temp).getAttributes().getNamedItem("value").getTextContent());
+                    	RhodesActivity.getContext().registerReceiver(mReceiver, intentfilter);
+                    	RhodesActivity.getContext().sendBroadcast(new Intent(nList.item(temp).getAttributes().getNamedItem("value").getTextContent()));
+                    }
+                    NodeList nList_cat = doc.getElementsByTagName("IntentCategory");
+                    for (int i = 0; i < nList_cat.getLength(); i++) {
+                    	mapCategory.put(i, nList_cat.item(i).getAttributes().getNamedItem("value").getTextContent());
+                    	intentfilter.addCategory(nList_cat.item(i).getAttributes().getNamedItem("value").getTextContent());
+                    	RhodesActivity.getContext().registerReceiver(mReceiver, intentfilter);
+                    	RhodesActivity.getContext().sendBroadcast(new Intent(nList_cat.item(i).getAttributes().getNamedItem("value").getTextContent()));
+					}
+				}
+			}
+			
+		}
+		return super.onNewConfig(extManager, config, name, res);
+	}
+}
