26500cb75b1c446bac38cfbf3531a353a32dc949
Dmitry Soldatenkov <DSoldatenkoff@mail.ru>
2015-10-12 03:13:31 +0300

    change version to 1.0.2

--- lib/framework/version.rb
+++ lib/framework/version.rb
@@ -1,6 +1,6 @@
 module RhodesFramework
   unless defined? RhodesFramework::VERSION
-    VERSION = '1.0.1'
+    VERSION = '1.0.2'
   end
   unless defined? RhodesFramework::DBVERSION
     DBVERSION = '2.2.2'
