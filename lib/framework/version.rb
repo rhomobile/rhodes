d9bdefba709b671f6fa611ddec6e7bcd70d5a46a
Thani <dvh638@zebra.com>
2015-10-27 19:26:27 +0530

    Rhode VERSION updated to 5.3.0

--- lib/framework/version.rb
+++ lib/framework/version.rb
@@ -1,6 +1,6 @@
 module RhodesFramework
   unless defined? RhodesFramework::VERSION
-    VERSION = '5.2.0'
+    VERSION = '5.3.0'
   end
   unless defined? RhodesFramework::DBVERSION
     DBVERSION = '2.2.2'
