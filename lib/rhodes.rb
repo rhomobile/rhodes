d8bef202efef7d34a968135bc5214b9df3f1719e
Abhineet Agarwal <abhineet.agarwal@zebra.com>
2015-11-21 15:14:00 +0530

    Update rhodes.rb

--- lib/rhodes.rb
+++ lib/rhodes.rb
@@ -1,6 +1,6 @@
 module Rhodes
   unless defined? Rhodes::VERSION
-    VERSION = File.read(File.expand_path('../../version', __FILE__)).strip rescue '5.3.0'
+    VERSION = '5.4.0'
   end
   unless defined? Rhodes::DBVERSION
     DBVERSION = '2.2.0'
