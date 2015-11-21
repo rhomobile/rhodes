d96273ebc375d43ba76b3eb510d8d5e04d6a3fac
Abhineet Agarwal <abhineet.agarwal@zebra.com>
2015-11-21 15:23:40 +0530

    Update version.rb
    Reverting the change as it is resulting in crash on WM device

--- lib/framework/version.rb
+++ lib/framework/version.rb
@@ -1,6 +1,6 @@
 module RhodesFramework
   unless defined? RhodesFramework::VERSION
-    VERSION = File.read(File.expand_path('../../../version', __FILE__)).strip rescue '5.3.0'
+    VERSION = '5.4.0'
   end
   unless defined? RhodesFramework::DBVERSION
     DBVERSION = '2.2.2'
