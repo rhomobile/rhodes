5a32a03ecfddb2ac1a94d4d689f34c145f887b5b
NHB348 <nhb348@zebra.com>
2015-12-02 14:42:21 +0530

    SPR fix

--- platform/wm/rhodes/browser/CEBrowserEngine.cpp
+++ platform/wm/rhodes/browser/CEBrowserEngine.cpp
@@ -527,7 +527,7 @@ HRESULT CEBrowserEngine::TranslateAccelerator(
 		}
 	}
 */
-	if (lpMsg && (lpMsg->message == WM_KEYDOWN))
+/*	if (lpMsg && (lpMsg->message == WM_KEYDOWN))
 	{
 		if (lpMsg->wParam == VK_LEFT ||	lpMsg->wParam == VK_RIGHT || lpMsg->wParam == VK_UP || lpMsg->wParam == VK_DOWN || lpMsg->wParam == VK_RETURN)
 		{
@@ -535,6 +535,7 @@ HRESULT CEBrowserEngine::TranslateAccelerator(
 			return S_OK;
 		}
 	}
+*/
 	return S_FALSE;
 }
 
