02ee4fe69f62aafa53e3bbe1a6be89e820ec1d78
darryncampbell <jrq768@zebra.com>
2016-01-12 09:56:36 +0000

    Examples of updating copyright information

--- platform/iphone/RhodesBaseDelegate.m
+++ platform/iphone/RhodesBaseDelegate.m
@@ -4,6 +4,7 @@
 //
 //  Created by Dmitry Soldatenkov on 20.11.13.
 //  Copyright (c) 2013 Rhomobile. All rights reserved.
+//  Copyright (c) 2014-2016 Symbol Technologies LLC
 //
 
 #import "RhodesBaseDelegate.h"
