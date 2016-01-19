528b1c0bc8cdb8da43de1a20cef6e3d0755cba9d
Joydeep Chakraborty <joydeep.chakraborty83@gmail.com>
2016-01-19 19:54:49 +0530

    Added m_dwDocumentTimeout in header file
    Added m_dwDocumentTimeout in header file

--- platform/wm/rhodes/browser/CEBrowserEngine.h
+++ platform/wm/rhodes/browser/CEBrowserEngine.h
@@ -155,6 +155,7 @@ private:
     RECT              m_rcViewSize;
 
     unsigned int      m_dwNavigationTimeout;
+    unsigned int      m_dwDocumentTimeout;
     BOOL              m_bLoadingComplete;
     BOOL              m_bNavigationError;
 	bool              m_bInitialised;
