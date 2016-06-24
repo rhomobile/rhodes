1a5085f1a761892e35a93d5e36221b3b4989dae1
Alex Epifanov <aepifanov@tau-technologies.com>
2016-06-24 19:53:21 +0300

    working on maven dependencies extractor

---
+++ platform/android/build/maven_deps_extractor.rb
@@ -0,0 +1,194 @@
+require 'zip'
+require 'singleton'
+
+module AndroidTools
+
+class MavenDepsExtractor
+  include Singleton
+
+  def initialize
+    $logger.debug "MavenDepsExtractor.initialize"
+
+    @dependencies = {}
+
+    rhoroot = File.join( File.dirname(__FILE__),'..','..','..')
+    @m2home = File.join( rhoroot, 'res', 'build-tools', 'apache-maven-3.3.9' )
+    @mvnbin = File.join( @m2home, 'bin', 'mvn' )
+
+    if RUBY_PLATFORM =~ /(win|w)32$/
+      @mvnbin += '.cmd'
+    end
+
+
+  end
+
+  def add_dependency( mod=nil, dep )
+    if !mod
+      mod = '.rho'
+    end
+
+    $logger.info "Adding maven dependency for #{mod}: #{dep}"
+
+    if !@dependencies[mod]
+      @dependencies[mod] = [ dep ]
+    else
+      @dependencies[mod] << dep
+    end
+  end
+
+  def get_dependencies
+    @dependencies
+  end
+
+  def set_temp_dir( d )
+    raise "Maven extractor directory was already set to #{@temp_dir}" if @temp_dir
+
+    @temp_dir = File.join(d,'.m2')
+
+    rm_r @temp_dir if File.directory?(@temp_dir)
+    mkdir_p(@temp_dir)
+  end
+
+  def set_java_home ( d )
+    @java_home = File.dirname(d) #no /bin part
+  end
+
+  def extract_all
+    $logger.info 'Extracting maven dependencies'
+
+    @dependencies.each do |name,deps|
+      $logger.debug "Extracting dependencies for extension #{name}"
+
+      path = File.join( @temp_dir, name )
+      mkdir_p( path )
+      deps.each { |dep| extract( dep, path ) }
+    end
+  end
+
+  def extract( dep, path )
+    $logger.debug "Extracting dependency #{dep} to #{path}"
+
+    copy_dependency( dep, path )
+
+    $logger.debug "Processing dependencies"
+
+    Dir[File.join(path,'*')].each do |f|
+      if File.extname(f) == '.aar'
+        target = File.join(path,File.basename(f,'.aar'))
+
+        $logger.debug "Dependency artefact #{f} is AAR so will unzip it to #{target}"
+
+        mkdir_p target
+        Jake.unzip(f,target)
+        rm f
+      end
+    end
+  end
+
+  def maven_env
+    { 'JAVA_HOME' => @java_home, 'M2_HOME' => @m2home }
+  end
+
+  def dep_copy_cmd
+    argv = []
+    argv << @mvnbin
+    argv << 'dependency:copy-dependencies'
+#    argv << '-o' #work offline
+    argv << "-Dmaven.repo.local=#{File.join(@m2home,'m2')}"
+    argv << '-e' if USE_TRACES
+    argv << '-X' if USE_TRACES
+  
+    argv.join(' ')
+  end
+
+  def copy_dependency( dep, path )
+    pom = File.join( path, 'pom.xml')
+    generate_pom( dep, pom )
+
+    cmd = dep_copy_cmd
+    env = maven_env
+    pwd = path
+
+    $logger.info "Running Maven dependency copy plugin"
+    Jake.run3( cmd, pwd, env )
+    rm pom
+
+    $logger.debug "Moving extracted dependencies to #{path}"
+    Dir[File.join(path,'target','dependency','*')].each { |f|mv(f,path) }
+    rm_r File.join(path,'target')
+  end
+
+  def split_dependency( dep )
+    dep.split(':')
+  end
+
+  def generate_pom(dep, path)
+
+    $logger.debug "Generating POM file for #{dep} in #{path}"
+
+    dep_grp_id, dep_art_id, dep_ver = split_dependency( dep )
+
+    r1 = File.join($androidsdkpath,'extras','google','m2repository')
+    r2 = File.join($androidsdkpath,'extras','android','m2repository')
+
+    pom =
+      "<project xmlns='http://maven.apache.org/POM/4.0.0'\n"\
+      "  xmlns:xsi='http://www.w3.org/2001/XMLSchema-instance'\n"\
+      "  xsi:schemaLocation='http://maven.apache.org/POM/4.0.0 http://maven.apache.org/xsd/maven-4.0.0.xsd'>\n"\
+      "  <modelVersion>4.0.0</modelVersion>\n"\
+      "  <groupId>com.rhomobile.rhodes</groupId>\n"\
+      "  <artifactId>deps-extractor</artifactId>\n"\
+      "  <version>1.0-SNAPSHOT</version>\n"\
+      "  <name>Maven Quick Start Archetype</name>\n"\
+      "  <url>http://maven.apache.org</url>\n"\
+      "  <dependencies>\n"\
+      "    <dependency>\n"\
+      "      <groupId>#{dep_grp_id}</groupId>\n"\
+      "      <artifactId>#{dep_art_id}</artifactId>\n"\
+      "      <version>#{dep_ver}</version>\n"\
+      "      <type>aar</type>\n"\
+      "    </dependency>\n"\
+      "  </dependencies>\n"\
+      "  <repositories>\n"\
+      "    <repository>\n"\
+      "      <id>repo1</id>\n"\
+      "      <name>android-maven-repo-1</name>\n"\
+      "      <url>file://#{r1}</url>\n"\
+      "    </repository>\n"\
+      "    <repository>\n"\
+      "      <id>repo2</id>\n"\
+      "      <name>android-maven-repo-2</name>\n"\
+      "      <url>file://#{r2}</url>\n"\
+      "    </repository>\n"\
+      "  </repositories>\n"\
+      "  <build>\n"\
+      "    <plugins>\n"\
+      "      <plugin>\n"\
+      "        <groupId>org.apache.maven.plugins</groupId>\n"\
+      "        <artifactId>maven-dependency-plugin</artifactId>\n"\
+      "        <version>2.10</version>\n"\
+      "        <executions>\n"\
+      "          <execution>\n"\
+      "            <id>unpack-dependencies</id>\n"\
+      "            <phase>package</phase>\n"\
+      "            <goals>\n"\
+      "              <goal>unpack-dependencies</goal>\n"\
+      "            </goals>\n"\
+      "            <configuration>\n"\
+      "              <!-- configure the plugin here -->\n"\
+      "              <outputDirectory>.</outputDirectory>\n"\
+      "            </configuration>\n"\
+      "          </execution>\n"\
+      "        </executions>\n"\
+      "      </plugin>\n"\
+      "    </plugins>\n"\
+      "  </build>\n"\
+      "</project>"
+
+    f = File.open( path,'w')
+    f.write( pom )
+    f.close
+  end
+end
+
+end
