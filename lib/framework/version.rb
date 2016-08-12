e4db86fed31ce3b26692509b8a6b472b36a87697
Dmitry Soldatenkov <dsoldatenkov@tau-technologies.com>
2016-08-12 04:21:52 +0300

    change gem version

--- lib/framework/version.rb
+++ lib/framework/version.rb
@@ -1,6 +1,6 @@
 module RhodesFramework
   unless defined? RhodesFramework::VERSION
-    VERSION = '5.4.0'
+    VERSION = '5.4.8'
   end
   unless defined? RhodesFramework::DBVERSION
     DBVERSION = '2.2.2'
--- lib/rhodes.rb
+++ lib/rhodes.rb
@@ -1,6 +1,6 @@
 module Rhodes
   unless defined? Rhodes::VERSION
-    VERSION = '5.4.0'
+    VERSION = '5.4.8'
   end
   unless defined? Rhodes::DBVERSION
     DBVERSION = '2.2.0'
