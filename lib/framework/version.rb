03d047d9f62443353f92deada740649ac25bd170
alex-epifanoff <alex.epifanoff@gmail.com>
2016-09-16 16:06:27 +0300

    version 5.4.10

--- lib/framework/version.rb
+++ lib/framework/version.rb
@@ -1,6 +1,6 @@
 module RhodesFramework
   unless defined? RhodesFramework::VERSION
-    VERSION = '5.4.9'
+    VERSION = '5.4.10'
   end
   unless defined? RhodesFramework::DBVERSION
     DBVERSION = '2.2.2'
