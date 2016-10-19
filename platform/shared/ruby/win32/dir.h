693e899065599a8c45c2a753b9c46d3a61c6ab1d
alex-epifanoff <alex.epifanoff@gmail.com>
2016-10-19 03:48:36 +0300

    update win32

--- platform/shared/ruby/win32/dir.h
+++ platform/shared/ruby/win32/dir.h
@@ -1,20 +1,19 @@
 #ifndef RUBY_WIN32_DIR_H
 #define RUBY_WIN32_DIR_H
 
-#ifdef __BORLANDC__
-#  ifndef WIN32_DIR_H_
-#    define WIN32_DIR_H_
-#    include <sys/types.h>
-#  endif
-#endif
+#define DT_UNKNOWN 0
+#define DT_DIR (S_IFDIR>>12)
+#define DT_REG (S_IFREG>>12)
+#define DT_LNK 10
 
 struct direct
 {
     long d_namlen;
     ino_t d_ino;
     char *d_name;
-    char d_isdir; /* directory */
-    char d_isrep; /* reparse point */
+    char *d_altname; /* short name */
+    short d_altlen;
+    uint8_t d_type;
 };
 typedef struct {
     WCHAR *start;
@@ -28,20 +27,18 @@ typedef struct {
 
 
 DIR*           rb_w32_opendir(const char*);
-struct direct* rb_w32_readdir(DIR *);
-#ifdef RUBY_ENCODING_H
-struct direct* rb_w32_readdir_with_enc(DIR *, rb_encoding *);
-#endif
+DIR*           rb_w32_uopendir(const char*);
+struct direct* rb_w32_readdir(DIR *, rb_encoding *);
 long           rb_w32_telldir(DIR *);
 void           rb_w32_seekdir(DIR *, long);
 void           rb_w32_rewinddir(DIR *);
 void           rb_w32_closedir(DIR *);
 
-#define opendir   rb_w32_opendir
-#define readdir   rb_w32_readdir
-#define telldir   rb_w32_telldir
-#define seekdir   rb_w32_seekdir
-#define rewinddir rb_w32_rewinddir
-#define closedir  rb_w32_closedir
+#define opendir(s)   rb_w32_opendir((s))
+#define readdir(d)   rb_w32_readdir((d), 0)
+#define telldir(d)   rb_w32_telldir((d))
+#define seekdir(d, l)   rb_w32_seekdir((d), (l))
+#define rewinddir(d) rb_w32_rewinddir((d))
+#define closedir(d)  rb_w32_closedir((d))
 
 #endif /* RUBY_WIN32_DIR_H */
