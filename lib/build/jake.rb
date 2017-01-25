ffe0058b482d9e9435e714527f06af1dae714b0f
Alex Epifanov <aepifanov@tau-technologies.com>
2017-01-25 13:08:41 +0300

    support 'rho_app_id' in build.yml

--- lib/build/jake.rb
+++ lib/build/jake.rb
@@ -876,6 +876,7 @@ class Jake
     app_version += "\r\napp_name='#{$app_config["name"]}'"
     app_version += "\r\ntitle_text='#{$app_config["name"]}'"  if $current_platform == "win32"
     app_version += "\r\norg_name='#{$app_config["vendor"]}'"  if $current_platform == "win32"
+    app_version += "\r\nrho_app_id='#{$app_config["rho_app_id"]}'" if $app_config['rho_app_id']
 
     File.open(File.join($srcdir,'apps/rhoconfig.txt'), "a"){ |f| f.write(app_version) }
     File.open(File.join($srcdir,'apps/rhoconfig.txt.timestamp'), "w"){ |f| f.write(Time.now.to_f().to_s()) }
