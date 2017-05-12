4580b23782cec1992f0dd72a18d2b52b5d3dc8b1
Alex Epifanov <aepifanov@tau-technologies.com>
2017-05-12 15:36:06 +0300

    config reference for debug build

--- platform/shared/qt/rhosimulator_win32_build.bat
+++ platform/shared/qt/rhosimulator_win32_build.bat
@@ -6,6 +6,7 @@ cd /d "%~dp0"
 call "%RHO_VSCMNTOOLS%..\..\VC\vcvarsall.bat" x86
 if ERRORLEVEL 1 exit /b %ERRORLEVEL%
 
+rem "%QTDIR%\bin\qmake" -o Makefile -r -spec %RHO_QMAKE_SPEC% "CONFIG+=debug" "CONFIG-=release" "%1" RhoSimulator.pro
 "%QTDIR%\bin\qmake" -o Makefile -r -spec %RHO_QMAKE_SPEC% "CONFIG-=debug" "CONFIG+=release" "%1" RhoSimulator.pro
 if ERRORLEVEL 1 exit /b %ERRORLEVEL%
 
