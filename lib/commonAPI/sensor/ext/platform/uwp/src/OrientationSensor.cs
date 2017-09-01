73b1adbb9b911d01ba31b62394d524f973cf15c0
Georgy Shabalev <gshabalev@tau-technologies.com>
2017-09-01 20:38:00 +0300

    Updating orientation sensor

--- lib/commonAPI/sensor/ext/platform/uwp/src/OrientationSensor.cs
+++ lib/commonAPI/sensor/ext/platform/uwp/src/OrientationSensor.cs
@@ -11,6 +11,43 @@ namespace rho
     {
         public class OrientationSensor : Sensor
         {
+            protected string type = SensorBase.SENSOR_TYPE_ORIENTATION;
+
+            public override void getMinimumGap(IMethodResult oResult)
+            {
+                // implement this method in C# here
+            }
+
+            public override void setMinimumGap(int minimumGap, IMethodResult oResult)
+            {
+                // implement this method in C# here
+            }
+
+            public override void getType(IMethodResult oResult)
+            {
+                // implement this method in C# here
+            }
+
+            public override void getStatus(IMethodResult oResult)
+            {
+                // implement this method in C# here
+            }
+
+            public override void start(IMethodResult oResult)
+            {
+                // implement this method in C# here
+            }
+
+            public override void readData(IMethodResult oResult)
+            {
+                // implement this method in C# here
+            }
+
+            public override void stop(IMethodResult oResult)
+            {
+                // implement this method in C# here
+            }
+
 
         }
     }
--- lib/commonAPI/sensor/ext/platform/uwp/src/Sensor_impl.cs
+++ lib/commonAPI/sensor/ext/platform/uwp/src/Sensor_impl.cs
@@ -7,72 +7,110 @@ using rhoruntime;
 
 namespace rho {
 
-namespace SensorImpl
-{
-    public class Sensor : SensorBase
+    namespace SensorImpl
     {
-        public Sensor()
+        public class Sensor : SensorBase
         {
-            // initialize class instance in C# here
+            public Sensor()
+            {
+                // initialize class instance in C# here
+            }
+
+            public override void getMinimumGap(IMethodResult oResult)
+            {
+                // implement this method in C# here
+            }
+
+            public override void setMinimumGap(int minimumGap, IMethodResult oResult)
+            {
+                // implement this method in C# here
+            }
+
+            public override void getType(IMethodResult oResult)
+            {
+                // implement this method in C# here
+            }
+
+            public override void getStatus(IMethodResult oResult)
+            {
+                // implement this method in C# here
+            }
+
+            public override void start(IMethodResult oResult)
+            {
+                // implement this method in C# here
+            }
+
+            public override void readData(IMethodResult oResult)
+            {
+                // implement this method in C# here
+            }
+
+            public override void stop(IMethodResult oResult)
+            {
+                // implement this method in C# here
+            }
         }
 
-        public override void getMinimumGap(IMethodResult oResult)
+        public class SensorSingleton : SensorSingletonBase
         {
-            // implement this method in C# here
+
+            SortedDictionary<string, Sensor> keeper = new SortedDictionary<string, Sensor>();
+
+            public SensorSingleton()
+            {
+               
+            }
+
+            public override void makeSensorByType(string type, IMethodResult oResult)
+            {
+                if (!keeper.ContainsKey(type))
+                {
+                    switch (type){
+                        case SensorBase.SENSOR_TYPE_ORIENTATION:
+                            keeper.Add(type, new OrientationSensor());
+                            break;
+                        default:
+                            oResult.set("nil");
+                            break;
+                    }
+                    
+                }
+
+                if (keeper.ContainsKey(type))
+                {
+                    oResult.set(type);
+                }
+                
+            }
+
+            public Sensor getModuleByID(string id)
+            {
+                if (keeper.ContainsKey(id))
+                {
+                    return keeper[id];
+                }
+                else
+                {
+                    return new Sensor();
+                }
+            }
+
+
+            public override void enumerate(IMethodResult oResult)
+            {
+                LinkedList<string> list = new LinkedList<string>();
+                
+                list.AddLast(SensorBase.SENSOR_TYPE_ORIENTATION);
+                oResult.set((IReadOnlyList<string>)list);
+            }
         }
 
-        public override void setMinimumGap(int minimumGap, IMethodResult oResult)
-        {
-            // implement this method in C# here
-        }
+        public class SensorFactory : SensorFactoryBase{
 
-        public override void getType(IMethodResult oResult)
-        {
-            // implement this method in C# here
-        }
+            
 
-        public override void getStatus(IMethodResult oResult)
-        {
-            // implement this method in C# here
-        }
-
-        public override void start(IMethodResult oResult)
-        {
-            // implement this method in C# here
-        }
-
-        public override void readData(IMethodResult oResult)
-        {
-            // implement this method in C# here
-        }
-
-        public override void stop(IMethodResult oResult)
-        {
-            // implement this method in C# here
         }
     }
-
-    public class SensorSingleton : SensorSingletonBase
-    {
-        public SensorSingleton()
-        {
-            // initialize singleton instance in C# here
-        }
-
-        public override void makeSensorByType(string type, IMethodResult oResult)
-        {
-            // implement this method in C# here
-        }
-
-        public override void enumerate(IMethodResult oResult)
-        {
-            // implement this method in C# here
-        }
-    }
-
-    public class SensorFactory : SensorFactoryBase
-    {
-    }
-}
 
 }
