3a5f0c3a4a10bf38f5cff8e98218431275892e7c
Georgy Shabalev <gshabalev@tau-technologies.com>
2017-09-01 13:43:08 +0300

    Updating sensor extention

---
+++ lib/commonAPI/sensor/ext/platform/uwp/src/OrientationSensor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using Windows.UI.Core;
+using System.Threading.Tasks;
+using rhoruntime;
+
+namespace rho
+{
+    namespace SensorImpl
+    {
+        public class OrientationSensor : Sensor
+        {
+
+        }
+    }
+}
