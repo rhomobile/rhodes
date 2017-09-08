e8c44ffb7c56ef8865a0852ab535ea4f172e934b
Alex Epifanov <aepifanov@tau-technologies.com>
2017-09-08 13:43:34 +0300

    rollback to original viewfinder size

--- lib/commonAPI/barcode/ext/platform/android/src/com/google/zxing/client/android/camera/CameraManager.java
+++ lib/commonAPI/barcode/ext/platform/android/src/com/google/zxing/client/android/camera/CameraManager.java
@@ -48,8 +48,8 @@ public final class CameraManager {
 
   private static final int MIN_FRAME_WIDTH = 240;
   private static final int MIN_FRAME_HEIGHT = 240;
-  private static final int MAX_FRAME_WIDTH = 1920 / 8 * 5;
-  private static final int MAX_FRAME_HEIGHT = 1080 / 8 * 5;
+  private static final int MAX_FRAME_WIDTH = 480;//1920 / 8 * 5;//480;
+  private static final int MAX_FRAME_HEIGHT = 360;//1080 / 8 * 5;//360;
 
   private static CameraManager cameraManager;
 
