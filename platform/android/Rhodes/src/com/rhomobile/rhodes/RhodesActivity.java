b5030034c7e10935f08dd66a5a58d092dd374a78
Georgy Shabalev <gshabalev@tau-technologies.com>
2017-09-12 17:48:46 +0300

    android: fixing Camera onCancel result

--- platform/android/Rhodes/src/com/rhomobile/rhodes/RhodesActivity.java
+++ platform/android/Rhodes/src/com/rhomobile/rhodes/RhodesActivity.java
@@ -68,6 +68,7 @@ import android.view.ViewGroup;
 import android.view.ViewTreeObserver;
 import android.view.Window;
 import android.widget.FrameLayout;
+import android.util.Log;
 
 import android.support.v4.content.PermissionChecker;
 import android.support.v4.app.ActivityCompat;
@@ -412,8 +413,20 @@ public class RhodesActivity extends BaseActivity implements SplashScreen.SplashS
 
         RhoBluetoothManager.onActivityResult(requestCode, resultCode, data);
        // com.rhomobile.rhodes.camera.Camera.onActivityResult(requestCode, resultCode, data);
-        
-        RhoExtManager.getImplementationInstance().onActivityResult(this, requestCode, resultCode, data);
+        try{
+            RhoExtManager.getImplementationInstance().onActivityResult(this, requestCode, resultCode, data);
+        }catch(Exception e){
+            String stackTrace = Log.getStackTraceString(e);
+            Logger.W(TAG, e);
+
+            StringBuilder sb = new StringBuilder();
+            for (StackTraceElement element : e.getStackTrace()) {
+                sb.append(element.toString());
+                sb.append("\n");
+            }
+
+            Logger.W(TAG, sb.toString());
+        };
     }
 
     @Override
