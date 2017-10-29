6e6e60d0763fbef137735df62d4fa603f72dd7fc
Alex Epifanov <aepifanov@tau-technologies.com>
2017-10-30 01:52:16 +0300

    update matrix and fix

--- .ci/app_repo.rb
+++ .ci/app_repo.rb
@@ -2,5 +2,5 @@
 
 require 'yaml'
 
-cfg = YAML.load_file( File.join( File.dirname( __FILE__ ) 'app_cfg.yml' ) )
+cfg = YAML.load_file( File.join( File.dirname( __FILE__ ), 'app_cfg.yml' ) )
 puts cfg[ENV['RHO_APP']['repo']]
