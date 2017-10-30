b327a4e3ae085b859769cd3329b18fb03d239012
Alex Epifanov <aepifanov@tau-technologies.com>
2017-10-30 16:02:42 +0300

    fix

--- .ci/install_rhoconnect-client.sh
+++ .ci/install_rhoconnect-client.sh
@@ -1,7 +1,16 @@
 #!/usr/bin/env bash
 echo "Installing Rhoconnect client"
 
-git clone -b $TRAVIS_BRANCH https://github.com/rhomobile/rhoconnect-client.git $HOME/rhoconnect-client
+set -e
+
+if [ $TRAVIS_BRANCH == "travis" ]
+then
+	BRANCH = "master"
+else
+	BRANCH = $TRAVIS_BRANCH
+fi
+
+git clone -b $BRANCH https://github.com/rhomobile/rhoconnect-client.git $HOME/rhoconnect-client
 cd $HOME/rhoconnect-client
 rake gem:make_gem
 gem install ./rhoconnect-client-$(cat ./version).gem --no-document
