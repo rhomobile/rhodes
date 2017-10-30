e745c06355077f0d7bddf9d00cff386a65970870
Alex Epifanov <aepifanov@tau-technologies.com>
2017-10-30 15:10:05 +0300

    rhosim error code

--- .ci/targets/rhosimulator/pre_gem.sh
+++ .ci/targets/rhosimulator/pre_gem.sh
@@ -1,4 +1,14 @@
 #!/usr/bin/env bash
 
 rake build:rhosimulator
+OUT=$?
+
 git clean -fdx
+
+if [ $OUT -eq 0 ];then
+   echo "RhoSimulator built successfully"
+else
+   echo "Error building RhoSimulator"
+fi
+
+exit $OUT
