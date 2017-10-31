56d66bf51045d2de02e8bea6e6e4bcfd90b79dc2
Alex Epifanov <aepifanov@tau-technologies.com>
2017-10-31 18:36:53 +0300

    running/building with API level 25

--- .ci/targets/android/install.sh
+++ .ci/targets/android/install.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+		#!/usr/bin/env bash
 
 #get Android NDK
 wget https://dl.google.com/android/repository/android-ndk-r12b-linux-x86_64.zip -O $HOME/ndk.zip
@@ -8,6 +8,6 @@ wget https://s3.amazonaws.com/files.tau-technologies.com/buildenv/addon-symbol-e
 unzip -q $HOME/emdk.zip -d $ANDROID_HOME/add-ons
 #genearate keystore
 cd $HOME/.android && printf "android\nandroid\nTRAVIS-CI\nCI-SYSTEM\nTAU\nUNIVERSE\nUNIVERSE\nUN\nyes\n\n" | keytool -genkey -v -keystore debug.keystore -alias androiddebugkey -keyalg RSA -keysize 2048 -validity 10000
-#download required images
-echo yes | $ANDROID_HOME/tools/bin/sdkmanager "system-images;android-19;default;armeabi-v7a"
-echo yes | $ANDROID_HOME/tools/bin/sdkmanager "system-images;android-19;google_apis;armeabi-v7a"
+#download 
+echo yes | $ANDROID_HOME/tools/bin/sdkmanager "system-images;android-${RHO_ANDROID_SYS_IMG_LEVEL:-19};default;armeabi-v7a"
+echo yes | $ANDROID_HOME/tools/bin/sdkmanager "system-images;android-${RHO_ANDROID_SYS_IMG_LEVEL:-19};google_apis;armeabi-v7a"
