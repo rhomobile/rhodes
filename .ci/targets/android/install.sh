297067af45cdbd04935ec657a67b20b83f99ac14
Alex Epifanov <aepifanov@tau-technologies.com>
2017-10-31 18:44:15 +0300

    update var name

--- .ci/targets/android/install.sh
+++ .ci/targets/android/install.sh
@@ -9,5 +9,5 @@ unzip -q $HOME/emdk.zip -d $ANDROID_HOME/add-ons
 #genearate keystore
 cd $HOME/.android && printf "android\nandroid\nTRAVIS-CI\nCI-SYSTEM\nTAU\nUNIVERSE\nUNIVERSE\nUN\nyes\n\n" | keytool -genkey -v -keystore debug.keystore -alias androiddebugkey -keyalg RSA -keysize 2048 -validity 10000
 #download 
-echo yes | $ANDROID_HOME/tools/bin/sdkmanager "system-images;android-${RHO_ANDROID_SYS_IMG_LEVEL:-19};default;armeabi-v7a"
-echo yes | $ANDROID_HOME/tools/bin/sdkmanager "system-images;android-${RHO_ANDROID_SYS_IMG_LEVEL:-19};google_apis;armeabi-v7a"
+echo yes | $ANDROID_HOME/tools/bin/sdkmanager "system-images;android-${RHO_ANDROID_LEVEL:-19};default;armeabi-v7a"
+echo yes | $ANDROID_HOME/tools/bin/sdkmanager "system-images;android-${RHO_ANDROID_LEVEL:-19};google_apis;armeabi-v7a"
