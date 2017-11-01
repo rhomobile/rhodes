785fb4d6505752604c75286f696366689ccb08c5
Alex Epifanov <aepifanov@tau-technologies.com>
2017-11-02 00:47:48 +0300

    setting up travis

---
+++ .ci/install_rhoconnect-client.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+echo "Installing Rhoconnect client"
+
+set -e
+
+if [ $TRAVIS_BRANCH == "CI" ]
+then
+	BRANCH="master"
+else
+	BRANCH=$TRAVIS_BRANCH
+fi
+
+git clone -b $BRANCH https://github.com/rhomobile/rhoconnect-client.git $HOME/rhoconnect-client
+cd $HOME/rhoconnect-client
+rake gem:make_gem
+gem install ./rhoconnect-client-$(cat ./version).gem --no-document
+cd $TRAVIS_BUILD_DIR
---
+++ .ci/targets/android/rho_setup.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+set -x
+printf "$JAVA_HOME\n$ANDROID_HOME\n$ANDROID_NDK_HOME\n\n\n\n\n\n\n" | rhodes-setup
