1c43b1e31a465309a6ac042099d22553bf62cad2
ashishkin <ashishkin@dataarmor.ru>
2018-02-03 20:58:30 +0300

    add timeout for sailfish

--- platform/shared/net/HttpServer.cpp
+++ platform/shared/net/HttpServer.cpp
@@ -44,7 +44,7 @@
 #include <arpa/inet.h>
 #endif
 
-#if !defined(OS_WINCE)
+#if !defined(OS_WINCE) && !defined(OS_SAILFISH)
 #include <common/stat.h>
 #define HTTP_EAGAIN_TIMEOUT 10
 #define HTTP_EAGAIN_TIMEOUT_STR "10"
