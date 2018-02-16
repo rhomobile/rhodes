700c43ddf1c14ea1633611a36b3c923e8274b8ce
George Shabalev <hramovnik@mail.ru>
2018-02-16 18:14:56 +0300

    win32: fixing some last commits

--- platform/shared/net/HttpServer.cpp
+++ platform/shared/net/HttpServer.cpp
@@ -575,7 +575,7 @@ int CHttpServer::select_internal( SOCKET listener, fd_set& readfds )
 #ifndef RHO_NO_RUBY_API
     }        
 #endif //RHO_NO_RUBY_API
-
+    return ret;
 }
 
 bool CHttpServer::run()
