75e50729799b357399ddd17ebf6fe8addb3f7c08
George Shabalev <hramovnik@mail.ru>
2018-06-13 15:42:31 +0300

    osx: travis downloading Qt5.9.5 from S3 for building rhosimulator

--- .ci/targets/rhosimulator/pre_gem.sh
+++ .ci/targets/rhosimulator/pre_gem.sh
@@ -9,7 +9,10 @@ set -e
 echo Downloading Qt
 #DOWNLOAD_URL=https://download.qt.io/archive/qt/5.9/5.9.5/qt-opensource-mac-x64-5.9.5.dmg
 #./dmginstall.sh https://download.qt.io/official_releases/qt/5.9/5.9.5/qt-opensource-mac-x64-5.9.5.dmg
-brew update && brew install qt5 --with-qtwebengine
+#brew update && brew install qt5 --with-qtwebengine
+https://s3.amazonaws.com/files.tau-technologies.com/buildenv/Qt5.9.5.tar.gz
+wget -q https://s3.amazonaws.com/files.tau-technologies.com/buildenv/Qt5.9.5.tar.gz -O $HOME/Qt5.9.5.tar.gz
+unzip -q $HOME/Qt5.9.5.tar.gz -d $HOME/
 echo Qt installed
 
 if [ $TRAVIS_BRANCH == "CI" ]
