443980cda96017d5a0f1f13d6d3dcb21c8ef8c8f
George Shabalev <hramovnik@mail.ru>
2018-08-29 23:01:01 +0300

    android: fixing travis

--- .ci/targets/android/install.sh
+++ .ci/targets/android/install.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 set -x
+ANDROID_NDK_HOME="${HOME}/android-ndk-r${NDK_NUMBER}"
 #get Android NDK
 #wget -q https://dl.google.com/android/repository/android-ndk-r12b-linux-x86_64.zip -O $HOME/ndk.zip
 wget -q https://dl.google.com/android/repository/android-ndk-r$NDK_NUMBER-linux-x86_64.zip -O $HOME/ndk.zip
