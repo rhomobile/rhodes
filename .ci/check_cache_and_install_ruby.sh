3f5fcd3274c854f69aaeb6b513e0d77579cd25a2
George Shabalev <hramovnik@mail.ru>
2018-08-31 11:59:06 +0300

    travis: fix

--- .ci/check_cache_and_install_ruby.sh
+++ .ci/check_cache_and_install_ruby.sh
@@ -3,7 +3,7 @@ set -x
 
 RUBY_INSTALLED=$(rvm list | grep ruby-2.3.4)
 echo $RUBY_INSTALLED
-if [[ -z RUBY_INSTALLED ]]; then
+if [[ -z $RUBY_INSTALLED ]]; then
 rvm install 2.3.4
 rvm use 2.3.4
 rvm gemset create rhodes
