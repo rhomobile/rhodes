0fb2b0ff1940a6f6fff497011e7474ad46891d3c
George Shabalev <hramovnik@mail.ru>
2018-09-05 00:40:32 +0300

    ruby: updating ffi to 1.9.24

--- .ci/check_cache_and_install_gems.sh
+++ .ci/check_cache_and_install_gems.sh
@@ -20,7 +20,7 @@ gem install naturally -v 1.3.2 --no-document
 gem install simctl -v 1.5.6 --no-document
 gem install listen -v 3.0.6 --no-document
 gem install rubyzip -v 1.2.1 --no-document
-gem install ffi -v 1.9.14 --no-document
+gem install ffi -v 1.9.24 --no-document
 gem install rdoc -v 4.2.2 --no-document
 gem install deep_merge -v 1.1.1 --no-document
 gem install nokogiri -v 1.8.2 --no-document
