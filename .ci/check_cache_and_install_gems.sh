f7115d8ced3f6de14b2bd46e29ae3e0d26b2c0c8
George Shabalev <hramovnik@mail.ru>
2018-09-11 11:18:50 +0300

    gems: updating rubyzip version

--- .ci/check_cache_and_install_gems.sh
+++ .ci/check_cache_and_install_gems.sh
@@ -19,7 +19,7 @@ gem install CFPropertyList -v 2.2.8 --no-document
 gem install naturally -v 1.3.2 --no-document
 gem install simctl -v 1.5.6 --no-document
 gem install listen -v 3.0.6 --no-document
-gem install rubyzip -v 1.2.1 --no-document
+gem install rubyzip -v 1.2.2 --no-document
 gem install ffi -v 1.9.24 --no-document
 gem install rdoc -v 4.2.2 --no-document
 gem install deep_merge -v 1.1.1 --no-document
