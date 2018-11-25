e3d8b8f7c875699461a3d983c88fe0e979ac20eb
Alex Epifanov <aepifanov@tau-technologies.com>
2018-11-25 04:36:31 +0300

    travis: never use API 25

--- .ci/targets/android/install.sh
+++ .ci/targets/android/install.sh
@@ -8,6 +8,10 @@ unzip -q $HOME/ndk.zip -d $HOME
 rm $HOME/ndk.zip
 wget -q https://s3.amazonaws.com/files.tau-technologies.com/buildenv/addon-symbol-emdk_v4.2-API-22.zip -O $HOME/emdk.zip
 unzip -q $HOME/emdk.zip -d $ANDROID_HOME/add-ons
+
+#hack - remove android platform 25 since it has no corresponding ndk api.
+rm -r $ANDROID_HOME/platforms/android-25
+
 #genearate keystore
 cd $HOME/.android && printf "android\nandroid\nTRAVIS-CI\nCI-SYSTEM\nTAU\nUNIVERSE\nUNIVERSE\nUN\nyes\n\n" | keytool -genkey -v -keystore debug.keystore -alias androiddebugkey -keyalg RSA -keysize 2048 -validity 10000
 #download 
