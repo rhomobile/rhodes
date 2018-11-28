67a5e917f7746a1820954d238971bb8d5e7f484e
ashishkin <ashishkin@dataarmor.ru>
2018-11-28 17:07:37 +0300

    support RTC_WAKEUP mode

--- lib/commonAPI/coreapi/ext/platform/android/src/com/rho/notification/NotificationScheduler.java
+++ lib/commonAPI/coreapi/ext/platform/android/src/com/rho/notification/NotificationScheduler.java
@@ -42,6 +42,17 @@ public class NotificationScheduler {
     private static int interval = 0;
     private static Boolean repeats = false;
     private static final String storage_name = "rho_notification_storage";
+    private static int hour = -1, minute = -1, seconds = -1;
+
+    private static Boolean checkTime()
+    {
+        return (hour >= 0 && hour <= 23) && (minute >= 0 && minute <= 59) && (seconds >= 0 && seconds <= 59);
+    }
+
+    private static Boolean isCurrentTime()
+    {
+        return hour == -1 || minute == -1 || seconds == -1;
+    }
 
     public static void setReminderEx(Map<String, Object> propertyMap, IMethodResult result) {
         Object titleObj = propertyMap.get(NotificationSingleton.HK_TITLE);
@@ -60,12 +71,35 @@ public class NotificationScheduler {
         if (repeatObj != null && (repeatObj instanceof Boolean))
             repeats = (Boolean) repeatObj;
 
+        Object startObj = propertyMap.get(NotificationSingleton.HK_START);
+        if (startObj != null && (startObj instanceof Map<?, ?>))
+        {
+            Map<String, Object> timeMap = (Map<String, Object>)startObj;
+            Object hourObj = timeMap.get(NotificationSingleton.HK_HOUR);        
+            if (hourObj != null && (hourObj instanceof Integer))            
+                hour = (Integer) hourObj;
+            else hour = -1;
+
+            Object minuteObj = timeMap.get(NotificationSingleton.HK_MINUTE);        
+            if (minuteObj != null && (minuteObj instanceof Integer))            
+                minute = (Integer) minuteObj;
+            else minute = -1;
+
+            Object secondObj = timeMap.get(NotificationSingleton.HK_SECONDS);        
+            if (secondObj != null && (secondObj instanceof Integer))            
+                seconds = (Integer) secondObj;
+            else seconds = -1;
+        }
+
         SharedPreferences settings = activity.getSharedPreferences(storage_name, Context.MODE_PRIVATE);
         SharedPreferences.Editor editor = settings.edit();
 
         editor.putString(NotificationSingleton.HK_TITLE, title);
         editor.putString(NotificationSingleton.HK_MESSAGE, message);
-        if (interval != 0)
+        editor.putInt(NotificationSingleton.HK_HOUR, hour);
+        editor.putInt(NotificationSingleton.HK_MINUTE, minute);
+        editor.putInt(NotificationSingleton.HK_SECONDS, seconds);
+        if (interval > 0)
             editor.putInt(NotificationSingleton.HK_INTERVAL, interval);
         if (repeats)
             editor.putBoolean(NotificationSingleton.HK_REPEATS, repeats);
@@ -107,6 +141,9 @@ public class NotificationScheduler {
             }
 
             repeats = settings.getBoolean(NotificationSingleton.HK_REPEATS, false);
+            hour = settings.getInt(NotificationSingleton.HK_HOUR, deadInt);
+            seconds = settings.getInt(NotificationSingleton.HK_SECONDS, deadInt);
+            minute = settings.getInt(NotificationSingleton.HK_MINUTE, deadInt);
         } 
         catch (ClassCastException e) 
         {
@@ -117,18 +154,24 @@ public class NotificationScheduler {
 
     }
 
-    public static void setReminder(Context context, Class<?> cls) {
-        // Calendar calendar = Calendar.getInstance();
+    public static void setReminder(Context context, Class<?> cls) 
+    {
+        Calendar now = Calendar.getInstance();
+        Calendar target = Calendar.getInstance();
 
-        // Calendar setcalendar = Calendar.getInstance();
-        // setcalendar.set(Calendar.HOUR_OF_DAY, hour);
-        // setcalendar.set(Calendar.MINUTE, min);
-        // setcalendar.set(Calendar.SECOND, 0);
+        if (!isCurrentTime()) 
+        {
+            target.set(Calendar.HOUR_OF_DAY, hour);
+            target.set(Calendar.MINUTE, minute);
+            target.set(Calendar.SECOND, seconds);
+
+            if(target.before(now))    
+                target.add(Calendar.DATE,1);
+
+        }
+
 
         cancelReminder(context, cls);
-
-        // if(setcalendar.before(calendar))
-        // setcalendar.add(Calendar.DATE,1);
 
         ComponentName receiver = new ComponentName(context, cls);
         PackageManager pm = context.getPackageManager();
@@ -140,10 +183,30 @@ public class NotificationScheduler {
         PendingIntent pendingIntent = PendingIntent.getBroadcast(context, RHO_NOTIFICATION_CODE, new_intent,
                 PendingIntent.FLAG_UPDATE_CURRENT);
         AlarmManager am = (AlarmManager) context.getSystemService(ALARM_SERVICE);
-        // am.setInexactRepeating(AlarmManager.RTC_WAKEUP,
-        // setcalendar.getTimeInMillis(), AlarmManager.INTERVAL_DAY, pendingIntent);
-        am.setRepeating(AlarmManager.ELAPSED_REALTIME_WAKEUP, SystemClock.elapsedRealtime() + (interval * 1000),
-                repeats ? (interval * 1000) : 0, pendingIntent);
+        
+        if(interval < 0)
+        {
+            Logger.E(TAG, "Interval is negative!!! Ignore...");
+            interval = 0;
+        }
+
+        if (isCurrentTime()) {
+
+            Logger.I(TAG, "Scheduling: " + "mode - ELAPSED_REALTIME_WAKEUP, interval - " + Integer.toString(interval));
+
+            am.setRepeating(AlarmManager.ELAPSED_REALTIME_WAKEUP, SystemClock.elapsedRealtime() + (interval * 1000),
+                    repeats ? (interval * 1000) : 0, pendingIntent);
+        }
+        else
+        {
+            Logger.I(TAG, "Scheduling: " + "mode - RTC_WAKEUP, time - " +
+                   Integer.toString(target.get(Calendar.HOUR_OF_DAY)) + ":" + 
+                   Integer.toString(target.get(Calendar.MINUTE)) + ":" + 
+                   Integer.toString(target.get(Calendar.SECOND)));
+
+            am.setRepeating(AlarmManager.RTC_WAKEUP, target.getTimeInMillis(),
+                    repeats ? (interval * 1000) : 0, pendingIntent);
+        }
 
     }
 
