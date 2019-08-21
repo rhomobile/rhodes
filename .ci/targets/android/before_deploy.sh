35300d3b907108affece55adffb91518509c28b2
Alex Epifanov <aepifanov@tau-technologies.com>
2019-08-22 02:56:08 +0300

    upload spec logs to s3

--- .ci/targets/android/before_deploy.sh
+++ .ci/targets/android/before_deploy.sh
@@ -3,3 +3,7 @@
 set -x
 
 cp $HOME/$RHO_APP/$($TRAVIS_BUILD_DIR/.ci/app_build_root.rb)/bin/target/android/*.apk $BUILD_ARTEFACTS_DIR
+
+if [[ -z $SKIP_TESTS ]]; then
+    cp $HOME/$RHO_APP/$($TRAVIS_BUILD_DIR/.ci/app_build_root.rb)/RhoLogSpec.txt $BUILD_ARTEFACTS_DIR
+fi
