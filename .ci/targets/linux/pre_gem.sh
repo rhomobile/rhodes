8a0a428dbf71d404332ae4caf1e2f4dda15fd65f
Hramovnik <hramovnik@mail.ru>
2019-09-17 19:17:42 +0300

    linux: travis - setting g++ to v9

--- .ci/targets/linux/pre_gem.sh
+++ .ci/targets/linux/pre_gem.sh
@@ -6,10 +6,10 @@ cp ./rhobuild.yml.example ./rhobuild.yml
 #sudo add-apt-repository --yes ppa:ubuntu-sdk-team/ppa
 sudo apt-get update -qq
 
-sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
-sudo apt-get update -qq
-if [ "$CXX" = "g++" ]; then sudo apt-get install -qq g++-4.9; fi
-if [ "$CXX" = "g++" ]; then export CXX="g++-4.9" CC="gcc-4.9"; fi
+sudo add-apt-repository ppa:ubuntu-toolchain-r/test
+sudo apt-get update
+sudo apt-get install gcc-snapshot
+sudo apt-get install gcc-9 g++-9
 
 sudo apt-get install qt5-default qtbase5-dev qtdeclarative5-dev qttools5-dev-tools
 sudo apt-get install qtmultimedia5-dev libqt5multimediawidgets5 
