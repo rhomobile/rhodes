880dfeaaffc741d7eb98c01eb5ab12a5f486e1ea
Hramovnik <hramovnik@mail.ru>
2019-09-17 19:30:23 +0300

    linux: travis - updating

--- .ci/targets/linux/pre_gem.sh
+++ .ci/targets/linux/pre_gem.sh
@@ -4,12 +4,12 @@ set -x
 cp ./rhobuild.yml.example ./rhobuild.yml
 
 #sudo add-apt-repository --yes ppa:ubuntu-sdk-team/ppa
-sudo apt-get update -qq
 
-sudo add-apt-repository ppa:ubuntu-toolchain-r/test
-sudo apt-get update
-sudo apt-get install gcc-snapshot
-sudo apt-get install gcc-9 g++-9
+
+#sudo add-apt-repository ppa:ubuntu-toolchain-r/test
+sudo apt-get update -qq
+#sudo apt-get install gcc-snapshot
+#sudo apt-get install gcc-9 g++-9
 
 sudo apt-get install qt5-default qtbase5-dev qtdeclarative5-dev qttools5-dev-tools
 sudo apt-get install qtmultimedia5-dev libqt5multimediawidgets5 
