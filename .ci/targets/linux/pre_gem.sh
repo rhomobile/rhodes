706f0979bfe0de7920faff01b131e3b27032133c
Hramovnik <hramovnik@mail.ru>
2019-09-17 17:59:28 +0300

    linux: travis - updating gcc

--- .ci/targets/linux/pre_gem.sh
+++ .ci/targets/linux/pre_gem.sh
@@ -5,6 +5,12 @@ cp ./rhobuild.yml.example ./rhobuild.yml
 
 #sudo add-apt-repository --yes ppa:ubuntu-sdk-team/ppa
 sudo apt-get update -qq
+
+sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
+sudo apt-get update -qq
+if [ "$CXX" = "g++" ]; then sudo apt-get install -qq g++-4.9; fi
+if [ "$CXX" = "g++" ]; then export CXX="g++-4.9" CC="gcc-4.9"; fi
+
 sudo apt-get install qt5-default qtbase5-dev qtdeclarative5-dev qttools5-dev-tools
 sudo apt-get install qtmultimedia5-dev libqt5multimediawidgets5 
 sudo apt-get install libqt5multimedia5-plugins qtwebengine5-dev 
