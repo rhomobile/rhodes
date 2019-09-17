07480b6b40e180156943d24308cd2e81e19e16aa
Hramovnik <hramovnik@mail.ru>
2019-09-17 13:39:05 +0300

    linux: using ruby 2.5.3

--- .ci/check_cache_and_install_ruby.sh
+++ .ci/check_cache_and_install_ruby.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 set -x
 
-RUBY_INSTALLED=$(rvm list | grep ruby-2.3.4)
+RUBY_INSTALLED=$(rvm list | grep ruby-2.5.3)
 echo $RUBY_INSTALLED
 if [[ -z $RUBY_INSTALLED ]]; then
-rvm install 2.3.4
+rvm install 2.5.3
 fi
 
 
--- .ci/targets/linux/pre_gem.sh
+++ .ci/targets/linux/pre_gem.sh
@@ -3,13 +3,11 @@ set -x
 
 cp ./rhobuild.yml.example ./rhobuild.yml
 
-sudo add-apt-repository --yes ppa:ubuntu-sdk-team/ppa
+#sudo add-apt-repository --yes ppa:ubuntu-sdk-team/ppa
 sudo apt-get update -qq
-sudo apt-get install qtbase5-dev qtdeclarative5-dev libqt5webkit5-dev libsqlite3-dev
-sudo apt-get install qt5-default qttools5-dev-tools
+sudo apt-get install qt5-default qtbase5-dev qtdeclarative5-dev qttools5-dev-tools
 sudo apt-get install qtmultimedia5-dev libqt5multimediawidgets5 
-sudo apt-get install libqt5multimedia5-plugins libqt5multimedia5 qtwebengine5-dev 
-sudo apt-get install libx11-xcb-dev libglu1-mesa-dev libxrender-dev libfontconfig1-dev
+sudo apt-get install libqt5multimedia5-plugins qtwebengine5-dev 
 
 sudo find / -name qmake
 
