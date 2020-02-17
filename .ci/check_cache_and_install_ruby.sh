6deb5c338a97b608c4f13b7bea8dcdd8abe767b9
ashishkin <ashishkin@dataarmor.ru>
2020-02-17 20:25:19 +0300

    try 2 to fix "cannot load such file -- openssl"

--- .ci/check_cache_and_install_ruby.sh
+++ .ci/check_cache_and_install_ruby.sh
@@ -5,7 +5,7 @@ RUBY_INSTALLED=$(rvm list | grep ruby-2.3.4)
 rvm pkg install openssl
 echo $RUBY_INSTALLED
 if [[ -z $RUBY_INSTALLED ]]; then
-rvm install 2.3.4
+rvm install 2.3.4 --with-openssl-dir=$rvm_path/usr
 fi
 
 
