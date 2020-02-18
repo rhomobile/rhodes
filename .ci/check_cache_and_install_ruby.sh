3aeb2170c94311996774d29dc2f19315e45e92a6
akaquazzar <31536445+akaquazzar@users.noreply.github.com>
2020-02-18 15:00:11 +0300

    Fix ci openssl error (#1028)
    * try to fix c"annot load such file -- openssl"
    
    * try 2 to fix "cannot load such file -- openssl"

--- .ci/check_cache_and_install_ruby.sh
+++ .ci/check_cache_and_install_ruby.sh
@@ -2,9 +2,10 @@
 set -x
 
 RUBY_INSTALLED=$(rvm list | grep ruby-2.3.4)
+rvm pkg install openssl
 echo $RUBY_INSTALLED
 if [[ -z $RUBY_INSTALLED ]]; then
-rvm install 2.3.4
+rvm install 2.3.4 --with-openssl-dir=$rvm_path/usr
 fi
 
 
