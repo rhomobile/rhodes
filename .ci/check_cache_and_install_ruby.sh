de52e48f30f972c90b215b3c0d6f2f46391d4d17
ashishkin <ashishkin@dataarmor.ru>
2020-02-18 20:56:40 +0300

    try 6

--- .ci/check_cache_and_install_ruby.sh
+++ .ci/check_cache_and_install_ruby.sh
@@ -6,9 +6,9 @@ rvm pkg install openssl
 echo $RUBY_INSTALLED
 if [[ -z $RUBY_INSTALLED ]]; then
 rvm install 2.3.4 --with-openssl-dir=$rvm_path/usr
-ls /Users/travis/.rvm/log/
-#DEBUG_LOG=$(ls /Users/travis/.rvm/log/*_ruby-2.3.4/rubygems.install.log | sort -V | tail -n 1)
-cat $DEBUG_LOG
+#ls /Users/travis/.rvm/log/
+#DEBUG_LOG=$(ls /Users/travis/.rvm/log/*/rubygems.install.log | sort -V | tail -n 1)
+#cat $DEBUG_LOG
 fi
 
 
