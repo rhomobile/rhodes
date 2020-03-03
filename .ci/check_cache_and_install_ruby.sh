86446ff6705d9f5573f573f758351363fd13141a
Dmitry Soldatenkov <dsoldatenkov@tau-technologies.com>
2020-03-04 00:20:05 +0300

    test Travis fix 07

--- .ci/check_cache_and_install_ruby.sh
+++ .ci/check_cache_and_install_ruby.sh
@@ -5,6 +5,6 @@ RUBY_INSTALLED=$(rvm list | grep ruby-2.3.4)
 echo $RUBY_INSTALLED
 if [[ -z $RUBY_INSTALLED ]]; then
 export HOMEBREW_NO_INSTALL_CLEANUP=1
-brew install openssl
-rvm install --autolibs=disable 2.3.4
+#brew install openssl
+rvm install --autolibs=disable 2.5.1
 fi
