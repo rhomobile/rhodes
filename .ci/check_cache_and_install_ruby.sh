6f1183cb8b5b42e25cc05031fab3a388c56a5dbf
Alex Epifanov <aepifanov@tau-technologies.com>
2020-05-15 02:00:00 +0300

    travis

--- .ci/check_cache_and_install_ruby.sh
+++ .ci/check_cache_and_install_ruby.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 set -x
 
-RUBY_INSTALLED=$(rvm list | grep ruby-$RUBY_VERSION)
+RUBY_INSTALLED=$(rvm list | grep ruby-$RHO_RUBY)
 echo $RUBY_INSTALLED
 if [[ -z $RUBY_INSTALLED ]]; then
 export HOMEBREW_NO_INSTALL_CLEANUP=1
 #brew install openssl
 rvm get stable
-rvm install --autolibs=disable $RUBY_VERSION
+rvm install --autolibs=disable $RHO_RUBY
 fi
