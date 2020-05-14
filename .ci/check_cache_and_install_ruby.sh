06a1445adb54c36b80d038b33738ca33bb22c9d9
Alex Epifanov <aepifanov@tau-technologies.com>
2020-05-15 02:16:35 +0300

    travis: remove rvm get stable

--- .ci/check_cache_and_install_ruby.sh
+++ .ci/check_cache_and_install_ruby.sh
@@ -6,7 +6,6 @@ echo $RUBY_INSTALLED
 if [[ -z $RUBY_INSTALLED ]]; then
 export HOMEBREW_NO_INSTALL_CLEANUP=1
 #brew install openssl
-rvm get stable
 rvm install --autolibs=disable $RHO_RUBY
 rvm reload
 fi
