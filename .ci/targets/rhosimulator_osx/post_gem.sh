c486b5e75e15bcc660f3ff110cae6e5bbd0665bc
Alex Epifanov <aepifanov@tau-technologies.com>
2020-05-17 19:44:20 +0300

    comment-out post_gem for rhosim

--- .ci/targets/rhosimulator_osx/post_gem.sh
+++ .ci/targets/rhosimulator_osx/post_gem.sh
@@ -1,24 +1,24 @@
 #!/usr/bin/env bash
 set -x
 
-echo "Building rhosim"
-cd $TRAVIS_BUILD_DIR
+#echo "Building rhosim"
+#cd $TRAVIS_BUILD_DIR
 #rm $TRAVIS_BUILD_DIR/platform/osx/bin/RhoSimulator/RhoSimulator.app.zip
-rm -rf $TRAVIS_BUILD_DIR/platform/osx/bin/RhoSimulator/*
-rake build:osx:rhosimulator
-cd $TRAVIS_BUILD_DIR/platform/osx/bin/RhoSimulator/
-zip -r -y RhoSimulator.app.zip RhoSimulator.app
-rm -rf $TRAVIS_BUILD_DIR/platform/osx/bin/RhoSimulator/RhoSimulator.app
-cd $TRAVIS_BUILD_DIR
+#rm -rf $TRAVIS_BUILD_DIR/platform/osx/bin/RhoSimulator/*
+#rake build:osx:rhosimulator
+#cd $TRAVIS_BUILD_DIR/platform/osx/bin/RhoSimulator/
+#zip -r -y RhoSimulator.app.zip RhoSimulator.app
+#rm -rf $TRAVIS_BUILD_DIR/platform/osx/bin/RhoSimulator/RhoSimulator.app
+#cd $TRAVIS_BUILD_DIR
 # > build.log
 
-OUT=$?
+#OUT=$?
 
-if [ $OUT -eq 0 ];then
-   echo "RhoSimulator built successfully"
-else
-   echo "Error building RhoSimulator"
-   cat build.log
-fi
+#if [ $OUT -eq 0 ];then
+#   echo "RhoSimulator built successfully"
+#else
+#   echo "Error building RhoSimulator"
+#   cat build.log
+#fi
 
-exit $OUT
+#exit $OUT
