23d319a9756261cfca3afe53a30a670dcd8ba440
Hramovnik <hramovnik@mail.ru>
2020-06-30 04:47:12 +0300

    linux: some optimisation for last script

--- platform/linux/tasks/install_dev_depes_rosa_x4.sh
+++ platform/linux/tasks/install_dev_depes_rosa_x4.sh
@@ -1,10 +1,9 @@
-sudo urpmi git
-git clone https://github.com/ruby/ruby 
+#!/bin/bash
 sudo urpmi git gcc gcc-c++ ruby ruby-devel libgmp-devel lib64gl-devel lib64glu-devel rpm-build lib64gmp-devel
 sudo urpmi qt5-devel lib64qt5webenginecore-devel lib64qt5webenginewidgets-devel lib64qt5multimedia-devel libglfw-devel
+sudo urpmi lib64z-devel libopenssl-devel bison bison++
 
-sudo urpmi lib64z-devel libopenssl-devel
-sudo urpmi bison bison++
+git clone https://github.com/ruby/ruby 
 cd ruby
 git checkout ruby_2_3
 autoconf
@@ -14,6 +13,9 @@ sudo make install
 
 sudo mv /usr/bin/ruby /usr/bin/ruby_backup
 sudo ln -s /usr/local/bin/ruby /usr/bin/ruby
+
+sudo mv /usr/bin/gem /usr/bin/gem_backup
+sudo ln -s /usr/local/bin/gem /usr/bin/gem
 
 grep -qxF 'export QTDIR=/usr/lib64/qt5' ~/.bashrc || echo 'export QTDIR=/usr/lib64/qt5' >> ~/.bashrc
 sudo ln -s /bin/tar /usr/bin/tar
