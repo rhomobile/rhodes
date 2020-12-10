3f798bf42857a25a53b21e851a19846bc3854f3e
Dmitry Soldatenkov <dsoldatenkov@tau-technologies.com>
2020-12-10 23:55:55 +0300

    Travis: check first stage of test

--- .ci/targets/iphone_native_lib/run_app_on_simulator.sh
+++ .ci/targets/iphone_native_lib/run_app_on_simulator.sh
@@ -3,4 +3,11 @@ set -x
 
 rake build:iphone:rhodeslib_framework["$HOME/$RHO_APP/Native_iOS"]
 
+if [ $? -eq 0 ]; then
+    echo RHo: rhodes lib builded OK !
+else
+    echo RHO: rhodes lib failed !
+    exit -1
+fi
+
 rake build:iphone:rhodeslib_bundle["$HOME/$RHO_APP/Native_iOS"]
