940a395ec509dfadb0fc846c95e90f855e3c81b1
Georgy Shabalev <hramovnik@mail.ru>
2021-03-02 21:49:04 +0300

    travis: try to set bundle version for android deploy

--- .ci/targets/android/before_deploy.sh
+++ .ci/targets/android/before_deploy.sh
@@ -2,4 +2,10 @@
 
 set -x
 
+bundle info bundler
+gem list bundler
+
+gem uninstall bundler
+gem install bundler -v 2.1.4
+
 cp $HOME/$RHO_APP/$($TRAVIS_BUILD_DIR/.ci/app_build_root.rb)/bin/target/android/*.apk $BUILD_ARTEFACTS_DIR
