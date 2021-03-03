744d9df8f1a02a7a3cbf56f5876dcb71f259f5b1
Georgy Shabalev <hramovnik@mail.ru>
2021-03-03 11:56:51 +0300

    travis: what if we disable cache

--- .ci/targets/android/before_deploy.sh
+++ .ci/targets/android/before_deploy.sh
@@ -2,10 +2,4 @@
 
 set -x
 
-bundle info bundler
-gem list bundler
-
-gem uninstall bundler
-gem install bundler -v 2.1.4
-
 cp $HOME/$RHO_APP/$($TRAVIS_BUILD_DIR/.ci/app_build_root.rb)/bin/target/android/*.apk $BUILD_ARTEFACTS_DIR
