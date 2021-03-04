7140f81f9967bf85c38fea8b04d3134c6d48b05c
Georgy Shabalev <hramovnik@mail.ru>
2021-03-04 19:16:11 +0300

    travis: another try

--- .ci/targets/android_native_lib/build_artefact_for_deploy.sh
+++ .ci/targets/android_native_lib/build_artefact_for_deploy.sh
@@ -5,10 +5,10 @@ set -x
 rake build:android:rhodeslib_lib["$HOME/$RHO_APP/Native_Android/MyApplication/app/libs/"]
 
 if [ $? -eq 0 ]; then
-    echo RHO: rhodes lib built !
+    echo RHO: rhodes lib built ok !
 else
-    echo RHO: rhodes lib failed !
-#    exit -1
+    echo RHO: rhodes lib built failed !
+    exit -1
 fi
 
 rake build:android:rhodeslib_bundle["$HOME/$RHO_APP/Native_Android/MyApplication/app/src/main/assets/"]
