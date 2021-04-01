ef157e7c5a59a945552e414c44dba7bfc012d3e5
Alex Epifanov <aepifanov@tau-platform.com>
2021-04-01 19:08:52 +0300

    net: clear input request headers map before saving response headers

--- platform/android/Rhodes/jni/src/JNINetRequest.cpp
+++ platform/android/Rhodes/jni/src/JNINetRequest.cpp
@@ -540,6 +540,8 @@ void rho::net::JNINetRequest::getResponseHeader(rho::Hashtable<rho::String, rho:
     if(keys.get()) JArraytoVectorString(keys, v_keys);
     if(values.get()) JArraytoVectorString(values, v_values);
 
+    headers.clear();
+
     if(v_keys.size() == v_values.size()) {
         for(size_t i = 0; i < v_keys.size(); ++i) {
             headers.emplace(v_keys[i], v_values[i]);
