5a7d65808bb4389bef0e0dc31365b32a25b8c8ff
hramovnik <hramovnik@mail.ru>
2021-04-01 18:15:46 +0300

    travis: another try to fix

--- .ci/targets/android/run_app_on_simulator.sh
+++ .ci/targets/android/run_app_on_simulator.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 set -x
 
-#$TRAVIS_BUILD_DIR/.ci/modify_target_abi.rb build.yml $ANDROID_ABIS
+$TRAVIS_BUILD_DIR/.ci/modify_target_abi.rb build.yml $ANDROID_ABIS
 rake run:android
