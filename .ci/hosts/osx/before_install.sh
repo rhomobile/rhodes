d62bb328354a370d44f62f331f5c6bec7b13b6b6
Alex Epifanov <aepifanov@tau-platform.com>
2021-11-13 18:42:27 +0300

    update

--- .ci/hosts/osx/before_install.sh
+++ .ci/hosts/osx/before_install.sh
@@ -3,7 +3,7 @@
 set -e
 set -x
 
-brew install gpg2
+brew install gnupg gnupg2
 gpg2 --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
 \curl -sSL https://get.rvm.io | bash -s stable
 source ~/.rvm/scripts/rvm
