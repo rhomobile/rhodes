21e2008660a48e623e95442166e98f256ada564c
Alex Epifanov <aepifanov@tau-platform.com>
2021-11-22 16:59:20 +0000

    trying some hacks to run on updated runtimes

--- platform/shared/ruby/win32/win32.c
+++ platform/shared/ruby/win32/win32.c
@@ -2437,13 +2437,13 @@ static size_t pioinfo_extra = 0;	/* workaround for VC++8 SP1 */
 static void
 set_pioinfo_extra(void)
 {
-#if RUBY_MSVCRT_VERSION >= 140
+#if RUBY_MSVCRT_VERSION >= 140 
 # define FUNCTION_RET 0xc3 /* ret */
 
 //RHO start : trying to get _asatty address from both debug and release crt dlls
 //   vvv
 # ifdef _DEBUG
-#  define UCRTBASE "ucrtbased.dll"
+#  define UCRTBASE "ucrtbase.dll"
 # else
 #  define UCRTBASE "ucrtbase.dll"
 # endif
@@ -2476,14 +2476,14 @@ set_pioinfo_extra(void)
 //RHO start
 //   vvv
 
-#define FUNCTION_BEFORE_RET_MARK "\x5d"
+#define FUNCTION_BEFORE_RET_MARK "\xc9"
 
 #  define FUNCTION_SKIP_BYTES 0
     /* mov eax,dword ptr [eax*4+100EB430h] */
 #  define PIOINFO_MARK "\x8B\x04\x85"
 # endif
     if (p) {
-        for (pend += 10; pend < p + 300; pend++) {
+        for (pend += 10; pend < p + 500; pend++) {
 
 			int beforeRetFound = (memcmp(pend, FUNCTION_BEFORE_RET_MARK, sizeof(FUNCTION_BEFORE_RET_MARK) - 1) == 0);
 			char* pret = (pend + (sizeof(FUNCTION_BEFORE_RET_MARK) - 1) + FUNCTION_SKIP_BYTES);
