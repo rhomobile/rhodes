4475737755829bb4da434ac7a578f0de6ba306c6
Dmitry Soldatenkov <dsoldatenkov@tau-platform.com>
2023-04-15 09:11:37 +0300

    add new swift files to iphone project template

---
+++ res/generators/templates/iphone_project/Classes/SimpleSwiftClass.swift
@@ -0,0 +1,20 @@
+//
+//  SimpleSwiftClass.swift
+//  rhorunner
+//
+//  Created by Dmitry Soldatenkov on 13.04.2023.
+//
+
+import Foundation
+import UIKit
+import os
+import WebKit
+
+
+public class SimpleSwiftClass {
+    
+    
+    
+    
+}
+    
