4805e185d69fa0cb5877e567153a9ac071146123
Dmitry Soldatenkov <dsoldatenkov@tau-platform.com>
2024-07-16 17:04:31 +0300

    azure: update rhosimulator script

--- .ci/targets/rhosimulator_osx/install.sh
+++ .ci/targets/rhosimulator_osx/install.sh
@@ -5,3 +5,23 @@
 #gem install templater --no-document
 #gem install uuid --no-document
 #gem install listen --no-document
+
+gem install highline -v 2.1.0
+gem install cocoapods -v 1.10.2
+gem install templater -v 1.0.0
+gem install rake -v 12.3.3
+gem install uuid -v 2.3.7
+gem install systemu -v 2.6.5
+gem install json -v 1.8.6
+gem install rest-client -v 2.0.2
+gem install net-scp -v 1.2.1
+gem install net-ssh -v 6.0.2
+gem install CFPropertyList -v 2.3.3
+gem install naturally -v 1.3.2
+gem install simctl -v 1.6.8
+gem install listen -v 3.0.6
+gem install rubyzip -v 1.3.0
+gem install rdoc -v 4.2.2
+gem install deep_merge -v 1.1.1
+gem install nokogiri -v 1.11.7
+gem install webrick -v 1.7.0
